0c9c686b92e901f8546bb72f2e77e4208fefa711
Bosco Ho <apposite@gmail.com>
2015-10-16 17:04:35 +1100

    separated model from plotting, new files

--- model.py
+++ model.py
@@ -9,9 +9,10 @@ time: years
 
 """
 
-import math
+
 from scipy.integrate import odeint
-import matplotlib.pylab as pylab
+
+
 
 
 class PopulationSystem():
@@ -54,74 +55,9 @@ class PopulationSystem():
         init_y = self.get_init_list()
         self.soln = odeint(derivative_fn, init_y, times)
 
-    def make_time_plots(self, plot_labels, png=None):
-        n_row = int(math.ceil(len(plot_labels) / 2.))
-        n_col=2
-
-        for i_plot, plot_label in enumerate(plot_labels):
-            i_label = self.labels.index(plot_label)
-            vals = self.soln[:, i_label]
-            pylab.subplot(n_row, n_col, i_plot+1)
-            pylab.plot(self.times, vals, linewidth=2)
-            pylab.ylabel(self.labels[i_label])
-            pylab.xlabel('time')
-            pylab.tight_layout()
-        
-        if png is None:
-            pylab.show()
-        else:
-            pylab.savefig(png)
-            
-    def make_time_plots_color(self, plot_labels, png=None):
-        n_row = int(math.ceil(len(plot_labels) / 2.))
-        n_col=2
-
-        for i_plot, plot_label in enumerate(plot_labels):
-            i_label = self.labels.index(plot_label)
-            vals = self.soln[:, i_label]
-            colors = ('r', 'b', 'm', 'g', 'k') 
-            for row in range(n_row):
-                for col in range(n_col):
-                    pylab.subplot(n_row, n_col, i_plot+1)
-                    color = colors[i_plot % len(colors)]
-                    pylab.plot(self.times, vals, linewidth=2, color=color)
-        
-        pylab.xlabel('time')
-        pylab.tight_layout()
-
-        if png is None:
-            pylab.show()
-        else:
-            pylab.savefig(png)
-            
-#Plot all compartments in one panel     
-    def make_time_plots_one_panel(self, plot_labels, png=None):
-        for i_plot, plot_label in enumerate(plot_labels):
-            i_label = self.labels.index(plot_label)
-            print (i_label)
-        pylab.subplot(211)
-        pylab.plot(self.times,self.soln[:,0], '-r', label = 'Susceptible', linewidth=2)
-        pylab.plot(self.times,self.soln[:,1], '-b', label = 'Latent_early', linewidth=2) 
-        pylab.plot(self.times,self.soln[:,2], '-m', label = 'Latent_late', linewidth=2) 
-        pylab.plot(self.times,self.soln[:,3], '-g', label = 'Active', linewidth=2) 
-        pylab.plot(self.times,self.soln[:,4], '-k', label = 'Under treatment', linewidth=2) 
-        pylab.xlabel('Time')
-        pylab.ylabel('Number of patients')
-        pylab.legend(loc=0)
-        pylab.show()
-        
-        pylab.subplot(212)
-        pylab.plot(self.times,self.soln[:,1], '-b', label = 'Latent_early', linewidth=2) 
-        pylab.plot(self.times,self.soln[:,2], '-m', label = 'Latent_late', linewidth=2) 
-        pylab.plot(self.times,self.soln[:,3], '-g', label = 'Active', linewidth=2) 
-        pylab.plot(self.times,self.soln[:,4], '-k', label = 'Under treatment', linewidth=2) 
-        pylab.xlabel('Time')
-        pylab.ylabel('Number of patients')
-        pylab.legend(loc=0)
-        pylab.show()
-        
-    def get_optima_compatible_soln(self):
-        return self.soln[0, :]
+    def get_soln(self, label):
+        i_label = self.labels.index(label)
+        return self.soln[:, i_label]
 
     def calculate_tracked_vars(self):
         self.tracked_vars = {}
@@ -208,51 +144,11 @@ class PopulationSystem():
     def checks(self):
         for label in self.labels:  # Check all compartments are positive
             assert self.compartments[label] >= 0.0
-            #assert sum(self.flows.values())==0.0 
         # Check total flows sum (approximately) to zero
         assert abs((sum(self.flows.values()) - 2 * self.flows["births"])) < 0.1
         
         
-def make_times(start, end, step):
-    times = []
-    time = start
-    while time < end:
-        times.append(time)
-        time += step
-    return times
+    
 
 
-population = PopulationSystem()
 
-population.set_compartment("susceptible", 1e6)
-population.set_compartment("latent_early", 0.)
-population.set_compartment("latent_late", 0.)
-population.set_compartment("active", 1.)
-population.set_compartment("undertreatment", 0.)
-
-population.set_param("rate_pop_birth", 20. / 1e3)
-population.set_param("rate_pop_death", 1. / 65)
-
-population.set_param("n_tbfixed_contact", 10.)
-population.set_param("rate_tbfixed_earlyprog", .1 / .5)
-population.set_param("rate_tbfixed_lateprog", .1 / 100.)
-population.set_param("rate_tbfixed_stabilise", .9 / .5)
-population.set_param("rate_tbfixed_recover", .6 / 3.)
-population.set_param("rate_tbfixed_death", .4 / 3.)
-
-time_treatment = .5
-population.set_param("rate_tbprog_detect", 1.)
-population.set_param("time_treatment", time_treatment)
-population.set_param("rate_tbprog_completion", .9 / time_treatment)
-population.set_param("rate_tbprog_default", .05 / time_treatment)
-population.set_param("rate_tbprog_death", .05 / time_treatment)
-
-times = make_times(0, 50, 1)
-population.integrate(times)
-
-labels = population.labels
-population.make_time_plots(labels)
-population.make_time_plots_color(labels)
-population.make_time_plots_one_panel(labels)
-
-print (population.get_optima_compatible_soln())
---
+++ test.py
@@ -0,0 +1,108 @@
+
+
+from model import PopulationSystem
+import math
+import pylab
+
+
+
+def make_times(start, end, step):
+    times = []
+    time = start
+    while time < end:
+        times.append(time)
+        time += step
+    return times
+
+
+def make_time_plots(population, plot_labels, png=None):
+    n_row = int(math.ceil(len(plot_labels) / 2.))
+    n_col=2
+
+    for i_plot, plot_label in enumerate(plot_labels):
+        pylab.subplot(n_row, n_col, i_plot+1)
+        pylab.plot(
+            population.times, population.get_soln(plot_label), linewidth=2)
+        pylab.ylabel(plot_label)
+        pylab.xlabel('time')
+        pylab.tight_layout()
+    
+    if png is None:
+        pylab.show()
+    else:
+        pylab.savefig(png)
+        
+
+def make_time_plots_color(population, plot_labels, png=None):
+    n_row = int(math.ceil(len(plot_labels) / 2.))
+    n_col=2
+    colors = ('r', 'b', 'm', 'g', 'k') 
+    for i_plot, plot_label in enumerate(plot_labels):
+        pylab.subplot(n_row, n_col, i_plot+1)
+        pylab.plot(
+            population.times, 
+            population.get_soln(plot_label), 
+            linewidth=2,
+            color=colors[i_plot % len(colors)])
+        pylab.ylabel(plot_label)
+        pylab.xlabel('time')
+        pylab.tight_layout()
+    
+    if png is None:
+        pylab.show()
+    else:
+        pylab.savefig(png)
+
+
+def make_time_plots_one_panel(population, plot_labels, png=None):
+    line_styles = ['-r', '-b', '-m', '-g', '-k']
+    pylab.subplot(211)
+    for plot_label, line_style in zip(plot_labels, line_styles):
+        pylab.plot(
+            population.times, 
+            population.get_soln(plot_label), 
+            line_style,
+            label=plot_label.title(), linewidth=2)
+    pylab.xlabel('Time')
+    pylab.ylabel('Number of patients')
+    pylab.legend(loc=0)
+    pylab.show()
+
+
+population = PopulationSystem()
+
+population.set_compartment("susceptible", 1e6)
+population.set_compartment("latent_early", 0.)
+population.set_compartment("latent_late", 0.)
+population.set_compartment("active", 1.)
+population.set_compartment("undertreatment", 0.)
+
+population.set_param("rate_pop_birth", 20. / 1e3)
+population.set_param("rate_pop_death", 1. / 65)
+
+population.set_param("n_tbfixed_contact", 10.)
+population.set_param("rate_tbfixed_earlyprog", .1 / .5)
+population.set_param("rate_tbfixed_lateprog", .1 / 100.)
+population.set_param("rate_tbfixed_stabilise", .9 / .5)
+population.set_param("rate_tbfixed_recover", .6 / 3.)
+population.set_param("rate_tbfixed_death", .4 / 3.)
+
+time_treatment = .5
+population.set_param("rate_tbprog_detect", 1.)
+population.set_param("time_treatment", time_treatment)
+population.set_param("rate_tbprog_completion", .9 / time_treatment)
+population.set_param("rate_tbprog_default", .05 / time_treatment)
+population.set_param("rate_tbprog_death", .05 / time_treatment)
+
+times = make_times(0, 50, 1)
+population.integrate(times)
+
+labels = population.labels
+
+make_time_plots(population, labels)
+make_time_plots_color(population, labels)
+make_time_plots_one_panel(population, labels)
+make_time_plots_one_panel(population, labels[1:])
+
+
+

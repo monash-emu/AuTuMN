e097bf338dcc16b79867ed5fa801268b150af2b0
Bosco Ho <apposite@gmail.com>
2015-10-26 14:55:57 +1100

    find_init_equilbrium works now

--- find_init_equilibrium.py
+++ find_init_equilibrium.py
@@ -1,15 +1,22 @@
 
 from autumn.model import SimplePopluationSystem, make_steps
-from autumn.plotting import make_time_plots_color
-
+from autumn.plotting import plot_fractions
+import pylab
 """
 find equilibrium
 """
 
+# look back 10 years
+equil_cutoff_time = 10
+
+# 1% difference allowed for equilibrium
+max_equil_frac_diff = 0.01 
+
+label = "latent"
+
 population = SimplePopluationSystem()
 labels = population.labels
-
-for time in make_steps(50, 1000, 10):
+for time in make_steps(100, 1000, 10):
 
     times = make_steps(0, time, 1)
     population.integrate_explicit(times)
@@ -19,23 +26,21 @@ for time in make_steps(50, 1000, 10):
     labels = population.labels
     population.calculate_fractions()
 
-    n = len(population.times)
-    label = "active"
     fraction = population.fractions[label]
-    n_step_test = 10
-    i = 0
-    max_diff = 0
-    for i in range(n_step_test):
-        if i == 0:
-            continue
-        j = -i
-        diff = (fraction[j + 1] - fraction[j])
-        if abs(diff) > max_diff:
-            max_diff = diff
-        if abs(diff) > 0.5:
+
+    i = -2
+    max_fraction_diff = 0
+    time_diff = 0
+    while time_diff < equil_cutoff_time:
+        i -= 1
+        time_diff = abs(times[-1] - times[i])
+        frac_diff = (fraction[-1] - fraction[i])
+        if abs(frac_diff) > max_fraction_diff:
+            max_fraction_diff = frac_diff
+        if abs(frac_diff) > max_equil_frac_diff:
             is_converged = False
 
-    print time, max_diff, is_converged
+    print time, max_fraction_diff, is_converged
 
 
 

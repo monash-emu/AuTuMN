b8374bc3560c3c1a1396f186f103912d2b431b95
jtrauer <jtrauer@burnet.edu.au>
2015-10-27 12:14:44 +1100

    Changed class name, changed effective contact rate
    Changed class name for Cliff & Romesh's model. Increased effective
    contact rate so that TB doesn't die out (presumably R0 < 1).

--- test_simple_model.py
+++ test_simple_model.py
@@ -1,10 +1,10 @@
 
-from autumn.model import SimplePopluationSystem, make_steps
+from autumn.model import SingleLatentCompartmentWithNoBypassPopluation, make_steps
 from autumn.plotting import plot_fractions
 
 import pylab
 
-population = SimplePopluationSystem()
+population = SingleLatentCompartmentWithNoBypassPopluation()
 population.integrate_explicit(make_steps(1700, 2050, 0.05))
 
 plot_fractions(population, population.labels)

ff5de0c7e54d1d14417082b5b6819c309b5cbcb5
Tan Doan <ntdoan@5260L-121690-W.workspace.unimelb.edu.au>
2015-10-28 18:52:47 +1100

    It links all modules together. Run this module to see model outputs

---
+++ autumn/execution.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 27 15:44:37 2015
+
+@author: ntdoan
+"""
+
+from read_spreadsheet import get_input 
+from model import SingleComponentPopluationSystem, make_steps
+from plotting import make_time_plots_color, make_time_plots_one_panel, plot_fractions
+import pylab
+from pprint import pprint
+
+import_params = get_input('input.xlsx')
+pprint(import_params)
+
+population = SingleComponentPopluationSystem()
+
+initials = import_params['const']['initials_for_compartments']
+for key, value in initials.items():
+    population.set_compartment(key, value[0])
+    
+parameters = import_params['const']['model_parameters']
+for key, value in parameters.items():
+    population.set_param(key, value[0])
+    print(key,value,population.params[key])
+      
+population.integrate_scipy(make_steps(0, 50, 1))
+labels = population.labels
+plot_fractions(population, population.labels)
+pylab.show()
+make_time_plots_color(population, labels)
+make_time_plots_one_panel(population, labels, labels[1:])

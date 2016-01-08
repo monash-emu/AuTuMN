cd59a10b02e230d9501c3d6004a4986149a2eb39
jtrauer <jtrauer@burnet.edu.au>
2016-01-08 13:30:56 +1100

    Comment out reference to module I can't load to run

--- test_stage4.py
+++ test_stage4.py
@@ -178,7 +178,7 @@ class Stage4PopulationSystem(BasePopulationSystem):
 
 population = Stage4PopulationSystem()
 population.set_flows()
-population.make_graph('stage4.graph.png')
+#population.make_graph('stage4.graph.png')
 population.integrate_scipy(make_steps(0, 50, 1))
 plot_fractions(population, population.labels[:])
 pylab.savefig('stage4.fraction.png', dpi=300)

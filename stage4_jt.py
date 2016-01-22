9bd3ee87c3cf9bacad1870b24e2acd2dfc41fc39
jtrauer <jtrauer@burnet.edu.au>
2016-01-22 16:46:25 +1100

    Debugged stage 4 model, fixed issue with pulmonary manifestation

--- stage4_jt.py
+++ stage4_jt.py
@@ -11,7 +11,7 @@
 """
 
 
-from modules.model import BasePopulationSystem, make_steps
+from modules.model_jt import BasePopulationSystem, make_steps
 from modules.plotting import plot_fractions
 import pylab
 import os
@@ -28,50 +28,59 @@ class Stage4PopulationSystem(BasePopulationSystem):
 
         BasePopulationSystem.__init__(self)
 
-        self.set_compartment("susceptible_unvac", 1e6)
+        self.set_compartment("susceptible_fully", 1e6)
         self.set_compartment("susceptible_vac", 0.)
         self.set_compartment("susceptible_treated", 0.)
         self.set_compartment("latent_early", 0.)
         self.set_compartment("latent_late", 0.)
 
         self.pulmonary_status = [
-            '_smearpospulm', 
-            '_smearnegpulm', 
-            '_extrapulm']
+            "_smearpos",
+            "_smearneg",
+            "_extrapul"]
 
-        self.set_param("ratio_force_smearpospulm", 1.)
-        self.set_param("ratio_force_smearnegpulm", .25)
-        self.set_param("ratio_force_extrapulm", 0.0)
+        self.set_param("proportion_cases_smearpos", 0.6)
+        self.set_param("proportion_cases_smearneg", 0.2)
+        self.set_param("proportion_cases_extrapul", 0.2)
+        ''' Previously all flows that represented developing new TB were replicated
+        three times for each status, but should be shared between statuses'''
+
+        self.set_param("tb_multiplier_force_smearpos", 1.)
+        self.set_param("tb_multiplier_force_smearneg", .25)
+        self.set_param("tb_multiplier_force_extrapul", 0.0)
         for status in self.pulmonary_status:
             self.set_compartment("active" + status, 1.)
             self.set_compartment("detect" + status, 0.)
-            self.set_compartment("faildetect" + status, 0.)
+            self.set_compartment("missed" + status, 0.)
             self.set_compartment("treatment_infect" + status, 0.)
             self.set_compartment("treatment_noninfect" + status, 0.)
+            self.set_param("tb_rate_earlyprogress" + status,
+                            .2 * self.params["proportion_cases" + status])
+            self.set_param("tb_rate_lateprogress" + status,
+                            .0005 * self.params["proportion_cases" + status])
 
         self.set_param("rate_birth", 20. / 1e3)
         self.set_param("rate_death", 1. / 65)
 
         self.set_param("tb_n_contact", 15.)
 
-        self.set_param("tb_rate_early_active", .2)
-        self.set_param("tb_rate_late_active", .0005 )
+        self.set_param("tb_rate_lateprogress", .0005)
         self.set_param("tb_rate_stabilise", .8)
         self.set_param("tb_rate_recover", .5 * .3)
-        self.set_param("tb_rate_death", .5 * .3 )
+        self.set_param("tb_rate_death", .5 * .3)
 
         self.set_param("program_prop_vac", .9)
         self.set_param("program_prop_unvac", .1)
 
         self.set_param("program_rate_detect", 0.8)
-        self.set_param("program_rate_faildetect", 0.2)
+        self.set_param("program_rate_missed", 0.2)
 
         self.set_param("program_rate_start_treatment", 26.)
-        self.set_param("program_rate_sick_of_waiting", 4.)
+        self.set_param("program_rate_giveup_waiting", 4.)
 
-        self.set_param("program_rate_completion_infect", 26 * 0.9 )
-        self.set_param("program_rate_default_infect", 26 * 0.05 )
-        self.set_param("program_rate_death_infect", 26 * 0.05 )
+        self.set_param("program_rate_completion_infect", 26 * 0.9)
+        self.set_param("program_rate_default_infect", 26 * 0.05)
+        self.set_param("program_rate_death_infect", 26 * 0.05)
 
         self.set_param("program_rate_completion_noninfect", 2 * 0.7)
         self.set_param("program_rate_default_noninfect", 2 * 0.1)
@@ -90,9 +99,9 @@ class Stage4PopulationSystem(BasePopulationSystem):
         self.vars["infected_populaton"] = 0.0
         for status in self.pulmonary_status:
             for label in self.labels:
-                if status in label:
+                if status in label and "_noninfect" not in label:
                     self.vars["infected_populaton"] += \
-                        self.params["ratio_force" + status] \
+                        self.params["tb_multiplier_force" + status] \
                            * self.compartments[label]
 
         self.vars["rate_force"] = \
@@ -104,12 +113,12 @@ class Stage4PopulationSystem(BasePopulationSystem):
 
     def set_flows(self):
         self.set_var_flow(
-            "susceptible_unvac", "births_unvac" )
+            "susceptible_fully", "births_unvac")
         self.set_var_flow(
-            "susceptible_vac", "births_vac" )
+            "susceptible_vac", "births_vac")
 
         self.set_var_transfer_rate_flow(
-            "susceptible_unvac", "latent_early", "rate_force")
+            "susceptible_fully", "latent_early", "rate_force")
         self.set_var_transfer_rate_flow(
             "susceptible_vac", "latent_early", "rate_force_weak")
         self.set_var_transfer_rate_flow(
@@ -118,19 +127,17 @@ class Stage4PopulationSystem(BasePopulationSystem):
             "latent_late", "latent_early", "rate_force_weak")
 
         self.set_fixed_transfer_rate_flow(
-            "latent_early", 
-            "latent_late", 
-            "tb_rate_stabilise")
+            "latent_early", "latent_late", "tb_rate_stabilise")
 
         for status in self.pulmonary_status:
             self.set_fixed_transfer_rate_flow(
-                "latent_early", 
-                "active" + status, 
-                "tb_rate_early_active")
+                "latent_early",
+                "active" + status,
+                "tb_rate_earlyprogress" + status)
             self.set_fixed_transfer_rate_flow(
                 "latent_late", 
-                "active" + status, 
-                "tb_rate_late_active")
+                "active" + status,
+                "tb_rate_lateprogress" + status)
             self.set_fixed_transfer_rate_flow(
                 "active" + status, 
                 "latent_late", 
@@ -141,16 +148,16 @@ class Stage4PopulationSystem(BasePopulationSystem):
                 "program_rate_detect")
             self.set_fixed_transfer_rate_flow(
                 "active" + status, 
-                "faildetect" + status, 
-                "program_rate_faildetect")
+                "missed" + status,
+                "program_rate_missed")
             self.set_fixed_transfer_rate_flow(
-                "detect" + status, 
-                "treatment_infect" + status, 
+                "detect" + status,
+                "treatment_infect" + status,
                 "program_rate_start_treatment")
             self.set_fixed_transfer_rate_flow(
-                "faildetect" + status, 
+                "missed" + status,
                 "active" + status, 
-                "program_rate_sick_of_waiting")
+                "program_rate_giveup_waiting")
             self.set_fixed_transfer_rate_flow(
                 "treatment_infect" + status, 
                 "treatment_noninfect" + status, 

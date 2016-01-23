8b133065140d7096135f8f0bc564f9810e0688b0
jtrauer <jtrauer@burnet.edu.au>
2016-01-23 21:07:55 +1100

    Setting parameters outside of object definition

--- stage5_jt.py
+++ stage5_jt.py
@@ -11,14 +11,14 @@
 """
 
 
-from modules.model_jt import BasePopulationSystem, make_steps
+from modules.model_jt import BasePopulationSystem
 from modules.plotting import plot_fractions
 import pylab
 import os
 from scipy.stats import uniform
 
-import modules.parameter_estimation
 import modules.parameter_setting as parameter_setting
+
 
 class Stage5PopulationSystem(BasePopulationSystem):
 
@@ -26,7 +26,7 @@ class Stage5PopulationSystem(BasePopulationSystem):
     This model based on James' thesis
     """
 
-    def __init__(self):
+    def __init__(self, input_parameters):
 
         BasePopulationSystem.__init__(self)
 
@@ -41,60 +41,36 @@ class Stage5PopulationSystem(BasePopulationSystem):
             "_smearneg",
             "_extrapul"]
 
-        self.set_param("proportion_cases_smearpos", 0.6)
-        self.set_param("proportion_cases_smearneg", 0.2)
-        self.set_param("proportion_cases_extrapul", 0.2)
-        ''' Previously all flows that represented developing new TB were replicated
-        three times for each status, but should be shared between statuses'''
+        for parameter in input_parameters:
+            self.set_param(
+                    parameter, input_parameters[parameter])
 
-        self.set_param("tb_multiplier_force_smearpos", 1.)
-        self.set_param("tb_multiplier_force_smearneg",
-                       parameter_setting.multiplier_force_smearneg.prior_estimate)
-        self.set_param("tb_multiplier_force_extrapul", 0.0)
         for status in self.pulmonary_status:
             self.set_compartment("active" + status, 1.)
             self.set_compartment("detect" + status, 0.)
             self.set_compartment("missed" + status, 0.)
             self.set_compartment("treatment_infect" + status, 0.)
             self.set_compartment("treatment_noninfect" + status, 0.)
+
             self.set_param("tb_rate_earlyprogress" + status,
-                           parameter_setting.proportion_early_progression.prior_estimate
-                           / parameter_setting.timeperiod_early_latent.prior_estimate
+                           self.params["proportion_early_progression"]
+                           / self.params["timeperiod_early_latent"]
                            * self.params["proportion_cases" + status])
             self.set_param("tb_rate_lateprogress" + status,
-                           parameter_setting.rate_late_progression.prior_estimate
+                           self.params["rate_late_progression"]
                            * self.params["proportion_cases" + status])
 
-        self.set_param("rate_birth", 20. / 1e3)
-        self.set_param("rate_death", 1. / 65)
-
-        self.set_param("tb_n_contact", 25.)
-
         self.set_param("tb_rate_stabilise",
-                       (1 - parameter_setting.proportion_early_progression.ppf(uniform.rvs()))
-                       / parameter_setting.timeperiod_early_latent.prior_estimate)
+                       (1 - self.params["proportion_early_progression"])
+                       / self.params["timeperiod_early_latent"])
         self.set_param("tb_rate_recover",
-                       (1 - parameter_setting.proportion_casefatality_active_untreated_smearpos.prior_estimate)
-                       / parameter_setting.timeperiod_activeuntreated.prior_estimate)
+                       (1 - self.params["proportion_casefatality_active_untreated_smearpos"])
+                       / self.params["timeperiod_activeuntreated"])
         self.set_param("tb_rate_death",
-                       parameter_setting.proportion_casefatality_active_untreated_smearpos.prior_estimate
-                       / parameter_setting.timeperiod_activeuntreated.prior_estimate)
-        self.set_param("program_prop_vac", .9)
-        self.set_param("program_prop_unvac", .1)
+                       self.params["proportion_casefatality_active_untreated_smearpos"]
+                       / self.params["timeperiod_activeuntreated"])
 
-        self.set_param("program_rate_detect", 0.8)
-        self.set_param("program_rate_missed", 0.2)
-
-        self.set_param("program_rate_start_treatment", 26.)
-        self.set_param("program_rate_giveup_waiting", 4.)
-
-        self.set_param("program_rate_completion_infect", 26 * 0.9)
-        self.set_param("program_rate_default_infect", 26 * 0.05)
-        self.set_param("program_rate_death_infect", 26 * 0.05)
-
-        self.set_param("program_rate_completion_noninfect", 2 * 0.7)
-        self.set_param("program_rate_default_noninfect", 2 * 0.1)
-        self.set_param("program_rate_death_noninfect", 2 * 0.1)
+        print(self.params)
 
     def calculate_vars(self):
         self.vars["population"] = sum(self.compartments.values())
@@ -147,19 +123,19 @@ class Stage5PopulationSystem(BasePopulationSystem):
                 "active" + status,
                 "tb_rate_earlyprogress" + status)
             self.set_fixed_transfer_rate_flow(
-                "latent_late", 
+                "latent_late",
                 "active" + status,
                 "tb_rate_lateprogress" + status)
             self.set_fixed_transfer_rate_flow(
-                "active" + status, 
-                "latent_late", 
+                "active" + status,
+                "latent_late",
                 "tb_rate_recover")
             self.set_fixed_transfer_rate_flow(
-                "active" + status, 
-                "detect" + status, 
+                "active" + status,
+                "detect" + status,
                 "program_rate_detect")
             self.set_fixed_transfer_rate_flow(
-                "active" + status, 
+                "active" + status,
                 "missed" + status,
                 "program_rate_missed")
             self.set_fixed_transfer_rate_flow(
@@ -168,23 +144,23 @@ class Stage5PopulationSystem(BasePopulationSystem):
                 "program_rate_start_treatment")
             self.set_fixed_transfer_rate_flow(
                 "missed" + status,
-                "active" + status, 
+                "active" + status,
                 "program_rate_giveup_waiting")
             self.set_fixed_transfer_rate_flow(
-                "treatment_infect" + status, 
-                "treatment_noninfect" + status, 
+                "treatment_infect" + status,
+                "treatment_noninfect" + status,
                 "program_rate_completion_infect")
             self.set_fixed_transfer_rate_flow(
-                "treatment_infect" + status, 
-                "active" + status, 
+                "treatment_infect" + status,
+                "active" + status,
                 "program_rate_default_infect")
             self.set_fixed_transfer_rate_flow(
-                "treatment_noninfect" + status, 
-                "active" + status, 
+                "treatment_noninfect" + status,
+                "active" + status,
                 "program_rate_default_noninfect")
             self.set_fixed_transfer_rate_flow(
-                "treatment_noninfect" + status, 
-                "susceptible_treated", 
+                "treatment_noninfect" + status,
+                "susceptible_treated",
                 "program_rate_completion_noninfect")
 
         # death flows
@@ -192,25 +168,65 @@ class Stage5PopulationSystem(BasePopulationSystem):
 
         for status in self.pulmonary_status:
             self.set_death_rate_flow(
-                "active" + status, 
+                "active" + status,
                 "tb_rate_death")
             self.set_death_rate_flow(
-                "detect" + status, 
+                "detect" + status,
                 "tb_rate_death")
             self.set_death_rate_flow(
-                "treatment_infect" + status, 
+                "treatment_infect" + status,
                 "program_rate_death_infect")
             self.set_death_rate_flow(
-                "treatment_noninfect" + status, 
+                "treatment_noninfect" + status,
                 "program_rate_death_noninfect")
 
 
 if __name__ == "__main__":
+    input_parameters = \
+        {"proportion_cases_smearpos": 0.6,
+         "proportion_cases_smearneg": 0.2,
+         "proportion_cases_extrapul": 0.2,
+         "tb_multiplier_force_smearpos": 1.,
+         "tb_multiplier_force_smearneg":
+            parameter_setting.multiplier_force_smearneg.prior_estimate,
+         "tb_multiplier_force_extrapul": 0.0,
+         "rate_birth": 20. / 1e3,
+         "rate_death": 1. / 65,
+         "tb_n_contact": 25.,
+         "proportion_early_progression":
+            parameter_setting.proportion_early_progression.prior_estimate,
+         "timeperiod_early_latent":
+            parameter_setting.timeperiod_early_latent.prior_estimate,
+         "rate_late_progression":
+            parameter_setting.rate_late_progression.prior_estimate,
+         "proportion_casefatality_active_untreated_smearpos":
+            parameter_setting.proportion_casefatality_active_untreated_smearpos.prior_estimate,
+         "timeperiod_activeuntreated":
+            parameter_setting.timeperiod_activeuntreated.prior_estimate,
+         "proportion_casefatality_active_untreated_smearpos":
+            parameter_setting.proportion_casefatality_active_untreated_smearpos.prior_estimate,
+         "timeperiod_activeuntreated":
+            parameter_setting.timeperiod_activeuntreated.prior_estimate,
+         "program_prop_vac": .9,
+         "program_prop_unvac": .1,
+         "program_rate_detect": 0.8,
+         "program_rate_missed": 0.2,
+         "program_rate_start_treatment": 26.,
+         "program_rate_giveup_waiting": 4.,
+         "program_rate_completion_infect": 26 * 0.9,
+         "program_rate_default_infect": 26 * 0.05,
+         "program_rate_death_infect": 26 * 0.05,
+         "program_rate_completion_noninfect": 2 * 0.7,
+         "program_rate_default_noninfect": 2 * 0.1,
+         "program_rate_death_noninfect": 2 * 0.1}
 
-    population = Stage5PopulationSystem()
+
+
+    population = Stage5PopulationSystem(input_parameters)
     population.set_flows()
     population.make_graph('stage5.graph.png')
-    population.integrate_scipy(make_steps(0, 50, 1))
+    population.make_steps(1950., 2000., 20)
+    population.integrate_scipy()  # Need to fix for integrate explicit as well
 
     plot_fractions(population, population.labels[:])
     pylab.savefig('stage5.fraction.png', dpi=300)

e5e7551272bab501e7f6210735855722de0ebbb7
jtrauer <jtrauer@burnet.edu.au>
2016-02-15 11:18:30 +1100

    Revert "Finished visualisation for proportions"
    This reverts commit 86b4f277eaad561498ba86f410b19dd5dd8247ef.

--- autumn/plotting.py
+++ autumn/plotting.py
@@ -18,67 +18,6 @@ def make_default_line_styles():
             line_styles.append(line + color)
     return line_styles
 
-def make_related_line_styles(model, labels):
-    colours = {}
-    patterns = {}
-    compartment_full_names = {}
-    for label in labels:
-        if "susceptible_fully" in label:
-            colours[label] = (0, 0, 0)
-        elif "susceptible_vac" in label:
-            colours[label] = (0.3, 0.3, 0.3)
-        elif "susceptible_treated" in label:
-            colours[label] = (0.6, 0.6, 0.6)
-        elif "latent_early" in label:
-            colours[label] = (0, 0.4, 0.8)
-        elif "latent_late" in label:
-            colours[label] = (0, 0.2, 0.4)
-        elif "active" in label:
-            colours[label] = (0.9, 0, 0)
-        elif "detect" in label:
-            colours[label] = (0, 0.5, 0)
-        elif "missed" in label:
-            colours[label] = (0.5, 0, 0.5)
-        elif "treatment_infect" in label:
-            colours[label] = (1, 1, 0)
-        elif "treatment_noninfect" in label:
-            colours[label] = (1, 0.5, 0)
-        if "smearneg" in label:
-            patterns[label] = "-."
-        elif "extrapul" in label:
-            patterns[label] = ":"
-        else:
-            patterns[label] = "-"
-        if "susceptible_fully" in label:
-            compartment_full_names[label] = "Fully susceptible"
-        elif "susceptible_vac" in label:
-            compartment_full_names[label] = "BCG vaccinated, susceptible"
-        elif "susceptible_treated" in label:
-            compartment_full_names[label] = "Previously treated, susceptible"
-        elif "latent_early" in label:
-            compartment_full_names[label] = "Early latency"
-        elif "latent_late" in label:
-            compartment_full_names[label] = "Late latency"
-        elif "active" in label:
-            compartment_full_names[label] = "Active TB, "
-        elif "detect" in label:
-            compartment_full_names[label] = "Detected, "
-        elif "missed" in label:
-            compartment_full_names[label] = "Missed, "
-        elif "treatment_infect" in label:
-            compartment_full_names[label] = "Infectious under treatment, "
-        elif "treatment_noninfect" in label:
-            compartment_full_names[label] = "Non-infectious under treatment, "
-        else:
-            compartment_full_names[label] = compartment_full_names[label]
-        if "smearpos" in label:
-            compartment_full_names[label] = compartment_full_names[label] + "\n" + "smear-positive"
-        elif "smearneg" in label:
-            compartment_full_names[label] = compartment_full_names[label] + "\n" + "smear-negative"
-        elif "extrapul" in label:
-            compartment_full_names[label] = compartment_full_names[label] + "\n" + "extrapulmonary"
-    return colours, patterns, compartment_full_names
-
 
 def make_axes_with_room_for_legend():
     fig = pyplot.figure()
@@ -116,27 +55,6 @@ def plot_fractions(model, labels, png=None):
             line_styles[i_plot % n_style],
             label=plot_label, linewidth=2)
     set_axes_props(ax, 'year', 'fraction')
-    save_png(png)
-
-
-def plot_fractions_jt(model, labels, png=None):
-    colours, patterns, compartment_full_names\
-        = make_related_line_styles(model, labels)
-    ax = make_axes_with_room_for_legend()
-    axis_labels = []
-    for i_plot, plot_label in enumerate(labels):
-        ax.plot(
-            model.times,
-            model.fraction_soln[plot_label],
-            label=plot_label, linewidth=1,
-            color=colours[plot_label],
-            linestyle=patterns[plot_label])
-        axis_labels.append(compartment_full_names[plot_label])
-    handles, labels = ax.get_legend_handles_labels()
-    set_axes_props(ax, 'Year', 'Proportion of population',
-        'Total population by subgroups', False)
-    ax.legend(handles, axis_labels, bbox_to_anchor=(1.05, 1),
-        loc=2, borderaxespad=0., prop={'size': 7})
     save_png(png)
 
 

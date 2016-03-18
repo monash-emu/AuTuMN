5f8a127698ee295f888ec529067a370b6da5830f
jtrauer <jtrauer@burnet.edu.au>
2016-03-18 11:28:51 +1100

    Reorganise and reorder code
    Plus include option for making line pattern refer to either strain or
    organ involvement

--- autumn/plotting.py
+++ autumn/plotting.py
@@ -11,6 +11,86 @@ Module for plotting population systems
 
 """
 
+def make_axes_with_room_for_legend():
+    fig = pyplot.figure()
+    ax = fig.add_axes([0.1, 0.1, 0.6, 0.75])
+    return ax
+
+
+def set_axes_props(
+        ax, xlabel=None, ylabel=None, title=None, is_legend=True,
+        axis_labels=None):
+    frame_color = "grey"
+
+    # hide top and right border of plot
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.yaxis.set_ticks_position('left')
+    ax.xaxis.set_ticks_position('bottom')
+
+    if xlabel is not None:
+        ax.set_xlabel(xlabel)
+
+    if ylabel is not None:
+        ax.set_ylabel(ylabel)
+
+    if is_legend:
+        if axis_labels:
+            handles, labels = ax.get_legend_handles_labels()
+            leg = ax.legend(
+                handles,
+                axis_labels,
+                bbox_to_anchor=(1.05, 1),
+                loc=2,
+                borderaxespad=0.,
+                frameon=False,
+                prop={'size':7})
+        else:
+            leg = ax.legend(
+                bbox_to_anchor=(1.05, 1),
+                loc=2,
+                borderaxespad=0.,
+                frameon=False,
+                prop={'size':7})
+        for text in leg.get_texts():
+            text.set_color(frame_color)
+
+    if title is not None:
+        t = ax.set_title(title)
+        t.set_color(frame_color)
+
+    for label in (ax.get_xticklabels() + ax.get_yticklabels()):
+        label.set_fontname('Arial')
+        label.set_fontsize(8)
+
+    ax.tick_params(color=frame_color, labelcolor=frame_color)
+    for spine in ax.spines.values():
+        spine.set_edgecolor(frame_color)
+    ax.xaxis.label.set_color(frame_color)
+    ax.yaxis.label.set_color(frame_color)
+
+    humanise_y_ticks(ax)
+
+
+def humanise_y_ticks(ax):
+    vals = list(ax.get_yticks())
+    max_val = max([abs(v) for v in vals])
+    if max_val < 1e3:
+        return map(str, vals)
+    if max_val >= 1e3 and max_val < 1e6:
+        labels = ["%.1fK" % (v/1e3) for v in vals]
+    elif max_val >= 1e6 and max_val < 1e9:
+        labels = ["%.1fM" % (v/1e6) for v in vals]
+    elif max_val >= 1e9:
+        labels = ["%.1fB" % (v/1e9) for v in vals]
+    is_fraction = False
+    for label in labels:
+        if label[-3:-1] != ".0":
+            is_fraction = True
+    if not is_fraction:
+        labels = [l[:-3] + l[-1] for l in labels]
+    ax.set_yticklabels(labels)
+
 
 def make_default_line_styles():
     """
@@ -23,7 +103,22 @@ def make_default_line_styles():
     return line_styles
 
 
-def get_line_style(label):
+def make_related_line_styles(labels, strain_or_organ):
+    colours = {}
+    patterns = {}
+    compartment_full_names = {}
+    markers = {}
+    for label in labels:
+        colour, pattern, compartment_full_name, marker =\
+            get_line_style(label, strain_or_organ)
+        colours[label] = colour
+        patterns[label] = pattern
+        compartment_full_names[label] = compartment_full_name
+        markers[label] = marker
+    return colours, patterns, compartment_full_names, markers
+
+
+def get_line_style(label, strain_or_organ):
     # Unassigned groups remain black
     colour = (0, 0, 0)
     if "susceptible_vac" in label:  # susceptible_unvac remains black
@@ -45,7 +140,7 @@ def get_line_style(label):
     if "treatment_noninfect" in label:
         colour = (1, 1, 0)
 
-    pattern = make_line_pattern(label)
+    pattern = get_line_pattern(label, strain_or_organ)
 
     if "susceptible" in label:
         category_full_name = "Susceptible"
@@ -93,130 +188,56 @@ def get_line_style(label):
     return colour, pattern, category_full_name, marker
 
 
-def make_line_pattern(label):
-    pattern = "-"  # Default filled line
-    # if "smearneg" in label:
-    #     pattern = "-."
-    # elif "extrapul" in label:
-    #     pattern = ":"
+def get_line_pattern(label, strain_or_organ):
 
-    if "_mdr" in label:
-        pattern = '-.'
-    elif "_xdr" in label:
-        pattern = '.'
+    pattern = "-"  # Default solid line
+    if strain_or_organ == "organ":
+        if "smearneg" in label:
+            pattern = "-."
+        elif "extrapul" in label:
+            pattern = ":"
+    elif strain_or_organ == "strain":
+        if "_mdr" in label:
+            pattern = '-.'
+        elif "_xdr" in label:
+            pattern = '.'
 
     return pattern
 
 
-def make_related_line_styles(labels):
-    colours = {}
-    patterns = {}
-    compartment_full_names = {}
-    markers = {}
-    for label in labels:
-        colour, pattern, compartment_full_name, marker = get_line_style(label)
-        colours[label] = colour
-        patterns[label] = pattern
-        compartment_full_names[label] = compartment_full_name
-        markers[label] = marker
-    return colours, patterns, compartment_full_names, markers
+def make_plot_title(model, labels):
+    if labels is model.labels:
+        title = "by each individual compartment"
+    elif labels is model.compartment_types:
+        title = "by types of compartments"
+    elif labels is model.broad_compartment_types:
+        title = "by broad types of compartments"
+    elif labels is model.groups["ever_infected"]:
+        title = "within ever infected compartments"
+    elif labels is model.groups["infected"]:
+        title = "within infected compartments"
+    elif labels is model.groups["active"]:
+        title = "within active disease compartments"
+    elif labels is model.groups["infectious"]:
+        title = "within infectious compartments"
+    elif labels is model.groups["identified"]:
+        title = "within identified compartments"
+    elif labels is model.groups["treatment"]:
+        title = "within treatment compartments"
+    else:
+        title = "not sure"
+    return title
 
 
-def make_axes_with_room_for_legend():
-    fig = pyplot.figure()
-    ax = fig.add_axes([0.1, 0.1, 0.6, 0.75])
-    return ax
-
-
-def humanise_y_ticks(ax):
-    vals = list(ax.get_yticks())
-    max_val = max([abs(v) for v in vals])
-    if max_val < 1e3:
-        return map(str, vals)
-    if max_val >= 1e3 and max_val < 1e6:
-        labels = ["%.1fK" % (v/1e3) for v in vals]
-    elif max_val >= 1e6 and max_val < 1e9:
-        labels = ["%.1fM" % (v/1e6) for v in vals]
-    elif max_val >= 1e9:
-        labels = ["%.1fB" % (v/1e9) for v in vals]
-    is_fraction = False
-    for label in labels:
-        if label[-3:-1] != ".0":
-            is_fraction = True
-    if not is_fraction:
-        labels = [l[:-3] + l[-1] for l in labels]
-    ax.set_yticklabels(labels)
-
-
-def set_axes_props(
-        ax, xlabel=None, ylabel=None, title=None, is_legend=True,
-        axis_labels=None):
-    frame_color = "grey"
-
-    # hide top and right border of plot
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.yaxis.set_ticks_position('left')
-    ax.xaxis.set_ticks_position('bottom')
-
-    if xlabel is not None:
-        ax.set_xlabel(xlabel)
-
-    if ylabel is not None:
-        ax.set_ylabel(ylabel)
-
-    if is_legend:
-        if axis_labels:
-            handles, labels = ax.get_legend_handles_labels()
-            leg = ax.legend(
-                handles, 
-                axis_labels,
-                bbox_to_anchor=(1.05, 1),
-                loc=2, 
-                borderaxespad=0., 
-                frameon=False,
-                prop={'size':7})
-        else:
-            leg = ax.legend(
-                bbox_to_anchor=(1.05, 1),
-                loc=2, 
-                borderaxespad=0., 
-                frameon=False,
-                prop={'size':7})
-        for text in leg.get_texts():
-            text.set_color(frame_color)
-
-    if title is not None:
-        t = ax.set_title(title)
-        t.set_color(frame_color)
-
-    for label in (ax.get_xticklabels() + ax.get_yticklabels()):
-        label.set_fontname('Arial')
-        label.set_fontsize(8)
-
-    ax.tick_params(color=frame_color, labelcolor=frame_color)
-    for spine in ax.spines.values():
-        spine.set_edgecolor(frame_color)
-    ax.xaxis.label.set_color(frame_color)
-    ax.yaxis.label.set_color(frame_color)
-
-    humanise_y_ticks(ax)
-
-
-def save_png(png):
-    if png is not None:
-        pylab.savefig(png, dpi=300)
-
-
-def plot_populations(model, labels, values, right_xlimit, png=None):
-    # Never going to argue that the following code is the most elegant way to to this
+def plot_populations(model, labels, values, right_xlimit, strain_or_organ, png=None):
+    # Not going to argue that the following code is the most elegant approach
     right_xlimit_index = len(model.times) - 1
     for i in range(len(model.times)):
         if model.times[i] > right_xlimit:
             left_xlimit_index = i
             break
     colours, patterns, compartment_full_names, markers\
-        = make_related_line_styles(labels)
+        = make_related_line_styles(labels, strain_or_organ)
     ax = make_axes_with_room_for_legend()
     axis_labels = []
     ax.plot(
@@ -244,14 +265,14 @@ def plot_populations(model, labels, values, right_xlimit, png=None):
     save_png(png)
 
 
-def plot_fractions(model, labels, values, right_xlimit, png=None):
+def plot_fractions(model, labels, values, right_xlimit, strain_or_organ, png=None):
     right_xlimit_index = len(model.times) - 1
     for i in range(len(model.times)):
         if model.times[i] > right_xlimit:
             left_xlimit_index = i
             break
     colours, patterns, compartment_full_names, markers\
-        = make_related_line_styles(labels)
+        = make_related_line_styles(labels, strain_or_organ)
     ax = make_axes_with_room_for_legend()
     axis_labels = []
     for i_plot, plot_label in enumerate(labels):
@@ -267,30 +288,6 @@ def plot_fractions(model, labels, values, right_xlimit, png=None):
     set_axes_props(ax, 'Year', 'Proportion of population',
         'Population, ' + title, True, axis_labels)
     save_png(png)
-
-
-def make_plot_title(model, labels):
-    if labels is model.labels:
-        title = "by each individual compartment"
-    elif labels is model.compartment_types:
-        title = "by types of compartments"
-    elif labels is model.broad_compartment_types:
-        title = "by broad types of compartments"
-    elif labels is model.groups["ever_infected"]:
-        title = "within ever infected compartments"
-    elif labels is model.groups["infected"]:
-        title = "within infected compartments"
-    elif labels is model.groups["active"]:
-        title = "within active disease compartments"
-    elif labels is model.groups["infectious"]:
-        title = "within infectious compartments"
-    elif labels is model.groups["identified"]:
-        title = "within identified compartments"
-    elif labels is model.groups["treatment"]:
-        title = "within treatment compartments"
-    else:
-        title = "not sure"
-    return title
 
 
 def plot_outputs(model, labels, right_xlimit, png=None):
@@ -348,6 +345,11 @@ def plot_flows(model, labels, png=None):
                    'Aggregate flows in/out of compartment',
                    True, axis_labels)
     save_png(png)
+
+
+def save_png(png):
+    if png is not None:
+        pylab.savefig(png, dpi=300)
 
 
 def open_pngs(pngs):

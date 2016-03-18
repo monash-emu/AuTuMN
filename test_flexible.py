083e43ca41df0f8a6fe6c116d6f0ce39ed8b8e9e
jtrauer <jtrauer@burnet.edu.au>
2016-03-18 12:20:22 +1100

    Code should have gone with earlier commit
    For consistency

--- test_flexible.py
+++ test_flexible.py
@@ -10,7 +10,7 @@ out_dir = 'flexible_graphs'
 if not os.path.isdir(out_dir):
     os.makedirs(out_dir)
 
-models_to_run = [[3, 1, 1]]
+models_to_run = [[3, 2, 1]]
 
 for running_model in range(len(models_to_run)):
     name = 'model%d' % running_model
@@ -26,12 +26,14 @@ for running_model in range(len(models_to_run)):
     model.make_times(start_time, 2015., 0.05)
     model.integrate_explicit()
     model.make_graph(base + '.workflow')
-    # autumn.plotting.plot_fractions(
-    #     model, model.labels, model.fraction_soln, base + '.fraction.png')
-    # autumn.plotting.plot_fractions(
-    #     model, model.compartment_types, model.compartment_type_fraction_soln, base + '.type_fraction.png')
     autumn.plotting.plot_fractions(
-        model, model.broad_compartment_types, model.broad_fraction_soln, recent_time, base + '.broad_fraction.png')
+        model, model.labels, model.fraction_soln, recent_time, "strain", base + '.fraction.png')
+    autumn.plotting.plot_fractions(
+        model, model.compartment_types, model.compartment_type_fraction_soln, recent_time, "strain", base + '.type_fraction.png')
+    autumn.plotting.plot_fractions(
+        model, model.broad_compartment_types, model.broad_fraction_soln, recent_time, "strain", base + '.broad_fraction.png')
+    autumn.plotting.plot_fractions(
+        model, model.compartment_types_bystrain, model.compartment_type_bystrain_fraction_soln, recent_time, "strain", base + '.bystrain_fraction.png')
     # autumn.plotting.plot_fractions(
     #     model, model.groups["treatment"], model.treatment_fraction_soln, base + '.treatment.png')
     # autumn.plotting.plot_fractions(
@@ -40,9 +42,11 @@ for running_model in range(len(models_to_run)):
     # autumn.plotting.plot_populations(
     #     model, model.labels, model.compartment_soln, recent_time, base + '.population.png')
     autumn.plotting.plot_populations(
-        model, model.compartment_types, model.compartment_type_soln, recent_time, base + '.summed_population.png')
+        model, model.compartment_types, model.compartment_type_soln, recent_time, "strain", base + '.summed_population.png')
     autumn.plotting.plot_populations(
-        model, model.broad_compartment_types, model.broad_compartment_soln, recent_time, base + '.broad_population.png')
+        model, model.compartment_types_bystrain, model.compartment_type_bystrain_soln, recent_time, "strain", base + '.summed_population.png')
+    autumn.plotting.plot_populations(
+        model, model.broad_compartment_types, model.broad_compartment_soln, recent_time, "strain", base + '.broad_population.png')
 
     autumn.plotting.plot_outputs(
         model, ["incidence", "mortality", "prevalence"], recent_time, base + '.recent_rate_outputs.png')

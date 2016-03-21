8208a701e519a69cf5e56c5deaf6108dbdfc34b6
Bosco Ho <apposite@gmail.com>
2016-03-22 10:08:42 +1100

    a few plotting fixes; split off simplifiedmodel from basetbmodel

--- autumn/model.py
+++ autumn/model.py
@@ -65,6 +65,7 @@ class BaseModel():
 
         self.scaleup_fns = {}
         self.scaleup_transfer_rate_flows = []
+        self.scaleup_transfer_rate_flows = []
 
     def make_times(self, start, end, delta):
         "Return steps with n or delta"
@@ -293,7 +294,7 @@ class BaseModel():
 
         self.additional_diagnostics()
 
-    def addtional_diagnostics(self):
+    def additional_diagnostics(self):
         pass
 
     def get_compartment_soln(self, label):
@@ -412,6 +413,126 @@ class BaseModel():
         self.graph = apply_styles(self.graph, styles)
 
         self.graph.render(base)
+
+
+class SimplifiedModel(BaseModel):
+
+    """
+    Initial Autumn model designed by James
+    """
+
+    def __init__(self):
+
+        BaseModel.__init__(self)
+
+        self.set_compartment("susceptible", 1e6)
+        self.set_compartment("latent_early", 0.)
+        self.set_compartment("latent_late", 0.)
+        self.set_compartment("active", 1.)
+        self.set_compartment("treatment_infect", 0.)
+        self.set_compartment("treatment_noninfect", 0.)
+
+        self.set_param("demo_rate_birth", 20. / 1e3)
+        self.set_param("demo_rate_death", 1. / 65)
+
+        self.set_param("tb_n_contact", 40.)
+        self.set_param("tb_rate_earlyprogress", .1 / .5)
+        self.set_param("tb_rate_lateprogress", .1 / 100.)
+        self.set_param("tb_rate_stabilise", .9 / .5)
+        self.set_param("tb_rate_recover", .6 / 3.)
+        self.set_param("tb_rate_death", .4 / 3.)
+
+        self.set_param("tb_bcg_multiplier", .5)
+
+        self.set_param("program_rate_detect", 1.)
+        time_treatment = .5
+        self.set_param("program_time_treatment", time_treatment)
+        self.set_param("program_rate_completion_infect", .9 / time_treatment)
+        self.set_param("program_rate_default_infect", .05 / time_treatment)
+        self.set_param("program_rate_death_infect", .05 / time_treatment)
+        self.set_param("program_rate_completion_noninfect", .9 / time_treatment)
+        self.set_param("program_rate_default_noninfect", .05 / time_treatment)
+        self.set_param("program_rate_death_noninfect", .05 / time_treatment)
+
+    def calculate_variable_rates(self):
+
+        self.vars["population"] = sum(self.compartments.values())
+        self.vars["rate_birth"] = \
+            self.params["demo_rate_birth"] * self.vars["population"]
+
+        self.vars["infectious_population"] = 0.0
+        for label in self.labels:
+            if 'active' in label or '_infect' in label:
+                self.vars["infectious_population"] += \
+                    self.compartments[label]
+
+        self.vars["rate_force"] = \
+                self.params["tb_n_contact"] \
+              * self.vars["infectious_population"] \
+              / self.vars["population"]
+
+        self.vars["rate_force_weak"] = (
+            self.params["tb_bcg_multiplier"]
+              * self.vars["rate_force"])
+
+    def set_flows(self):
+        self.set_var_entry_rate_flow("susceptible", "rate_birth")
+
+        self.set_var_transfer_rate_flow(
+            "susceptible", "latent_early", "rate_force")
+
+        self.set_fixed_transfer_rate_flow(
+            "latent_early", "active", "tb_rate_earlyprogress")
+        self.set_fixed_transfer_rate_flow(
+            "latent_early", "latent_late", "tb_rate_stabilise")
+
+        self.set_fixed_transfer_rate_flow(
+            "latent_late", "active", "tb_rate_lateprogress")
+        # self.set_var_transfer_rate_flow(
+        #     "latent_late", "latent_early", "rate_force_weak")
+
+        self.set_fixed_transfer_rate_flow(
+            "active", "latent_late", "tb_rate_recover")
+
+        y = self.params["program_rate_detect"]
+        self.scaleup_fns["program_rate_detect"] = make_two_step_curve(
+            0, 0.5*y, y, 1950, 1995, 2015)
+        self.set_scaleup_transfer_rate_flow(
+            "active", "treatment_infect", "program_rate_detect")
+
+        self.set_fixed_transfer_rate_flow(
+            "treatment_infect", "treatment_noninfect", "program_rate_completion_infect")
+        self.set_fixed_transfer_rate_flow(
+            "treatment_infect", "active", "program_rate_default_infect")
+
+        self.set_fixed_transfer_rate_flow(
+            "treatment_noninfect", "susceptible", "program_rate_completion_noninfect")
+        self.set_fixed_transfer_rate_flow(
+            "treatment_noninfect", "active", "program_rate_default_noninfect")
+
+        self.set_population_death_rate("demo_rate_death")
+        self.set_infection_death_rate_flow(
+            "active", "tb_rate_death")
+        self.set_infection_death_rate_flow(
+            "treatment_infect", "program_rate_death_infect")
+        self.set_infection_death_rate_flow(
+            "treatment_noninfect", "program_rate_death_noninfect")
+
+    def calculate_outputs(self):
+
+        BaseModel.calculate_outputs(self)
+
+        self.vars["latent"] = 0.0
+        self.vars["prevalence"] = 0.0
+        for label in self.labels:
+            if "latent" in label:
+                self.vars["latent"] += (
+                    self.compartments[label] 
+                     / self.vars["population"] * 1E5)
+            if "susceptible" not in label and "latent" not in label:
+                self.vars["prevalence"] += (
+                    self.compartments[label]
+                     / self.vars["population"] * 1E5)
 
 
 class BaseTbModel(BaseModel):
@@ -628,126 +749,6 @@ class BaseTbModel(BaseModel):
                             summed_denominator += self.compartment_soln[label]
 
         return summed_soln, summed_denominator, compartment_types_bystrain
-
-
-class SimplifiedModel(BaseTbModel):
-
-    """
-    Initial Autumn model designed by James
-    """
-
-    def __init__(self):
-
-        BaseModel.__init__(self)
-
-        self.set_compartment("susceptible", 1e6)
-        self.set_compartment("latent_early", 0.)
-        self.set_compartment("latent_late", 0.)
-        self.set_compartment("active", 1.)
-        self.set_compartment("treatment_infect", 0.)
-        self.set_compartment("treatment_noninfect", 0.)
-
-        self.set_param("demo_rate_birth", 20. / 1e3)
-        self.set_param("demo_rate_death", 1. / 65)
-
-        self.set_param("tb_n_contact", 40.)
-        self.set_param("tb_rate_earlyprogress", .1 / .5)
-        self.set_param("tb_rate_lateprogress", .1 / 100.)
-        self.set_param("tb_rate_stabilise", .9 / .5)
-        self.set_param("tb_rate_recover", .6 / 3.)
-        self.set_param("tb_rate_death", .4 / 3.)
-
-        self.set_param("tb_bcg_multiplier", .5)
-
-        self.set_param("program_rate_detect", 1.)
-        time_treatment = .5
-        self.set_param("program_time_treatment", time_treatment)
-        self.set_param("program_rate_completion_infect", .9 / time_treatment)
-        self.set_param("program_rate_default_infect", .05 / time_treatment)
-        self.set_param("program_rate_death_infect", .05 / time_treatment)
-        self.set_param("program_rate_completion_noninfect", .9 / time_treatment)
-        self.set_param("program_rate_default_noninfect", .05 / time_treatment)
-        self.set_param("program_rate_death_noninfect", .05 / time_treatment)
-
-    def calculate_variable_rates(self):
-
-        self.vars["population"] = sum(self.compartments.values())
-        self.vars["rate_birth"] = \
-            self.params["demo_rate_birth"] * self.vars["population"]
-
-        self.vars["infectious_population"] = 0.0
-        for label in self.labels:
-            if 'active' in label or '_infect' in label:
-                self.vars["infectious_population"] += \
-                    self.compartments[label]
-
-        self.vars["rate_force"] = \
-                self.params["tb_n_contact"] \
-              * self.vars["infectious_population"] \
-              / self.vars["population"]
-
-        self.vars["rate_force_weak"] = (
-            self.params["tb_bcg_multiplier"]
-              * self.vars["rate_force"])
-
-    def set_flows(self):
-        self.set_var_entry_rate_flow("susceptible", "rate_birth")
-
-        self.set_var_transfer_rate_flow(
-            "susceptible", "latent_early", "rate_force")
-
-        self.set_fixed_transfer_rate_flow(
-            "latent_early", "active", "tb_rate_earlyprogress")
-        self.set_fixed_transfer_rate_flow(
-            "latent_early", "latent_late", "tb_rate_stabilise")
-
-        self.set_fixed_transfer_rate_flow(
-            "latent_late", "active", "tb_rate_lateprogress")
-        # self.set_var_transfer_rate_flow(
-        #     "latent_late", "latent_early", "rate_force_weak")
-
-        self.set_fixed_transfer_rate_flow(
-            "active", "latent_late", "tb_rate_recover")
-
-        y = self.params["program_rate_detect"]
-        self.scaleup_fns["program_rate_detect"] = make_two_step_curve(
-            0, 0.5*y, y, 1950, 1995, 2015)
-        self.set_scaleup_transfer_rate_flow(
-            "active", "treatment_infect", "program_rate_detect")
-
-        self.set_fixed_transfer_rate_flow(
-            "treatment_infect", "treatment_noninfect", "program_rate_completion_infect")
-        self.set_fixed_transfer_rate_flow(
-            "treatment_infect", "active", "program_rate_default_infect")
-
-        self.set_fixed_transfer_rate_flow(
-            "treatment_noninfect", "susceptible", "program_rate_completion_noninfect")
-        self.set_fixed_transfer_rate_flow(
-            "treatment_noninfect", "active", "program_rate_default_noninfect")
-
-        self.set_population_death_rate("demo_rate_death")
-        self.set_infection_death_rate_flow(
-            "active", "tb_rate_death")
-        self.set_infection_death_rate_flow(
-            "treatment_infect", "program_rate_death_infect")
-        self.set_infection_death_rate_flow(
-            "treatment_noninfect", "program_rate_death_noninfect")
-
-    def calculate_outputs(self):
-
-        BaseTbModel.calculate_outputs(self)
-
-        self.vars["latent"] = 0.0
-        self.vars["prevalence"] = 0.0
-        for label in self.labels:
-            if "latent" in label:
-                self.vars["latent"] += (
-                    self.compartments[label] 
-                     / self.vars["population"] * 1E5)
-            if "susceptible" not in label and "latent" not in label:
-                self.vars["prevalence"] += (
-                    self.compartments[label]
-                     / self.vars["population"] * 1E5)
 
 
 class FlexibleModel(BaseTbModel):
--- flexible_graphs/model0.workflow
+++ flexible_graphs/model0.workflow
@@ -8,20 +8,10 @@ digraph {
 		latent_early_ds_nocomorbs
 		latent_late_ds_nocomorbs
 		active_smearpos_ds_nocomorbs
-		active_smearneg_ds_nocomorbs
-		active_extrapul_ds_nocomorbs
 		detect_smearpos_ds_nocomorbs
-		detect_smearneg_ds_nocomorbs
-		detect_extrapul_ds_nocomorbs
 		missed_smearpos_ds_nocomorbs
-		missed_smearneg_ds_nocomorbs
-		missed_extrapul_ds_nocomorbs
 		treatment_infect_smearpos_ds_nocomorbs
-		treatment_infect_smearneg_ds_nocomorbs
-		treatment_infect_extrapul_ds_nocomorbs
 		treatment_noninfect_smearpos_ds_nocomorbs
-		treatment_noninfect_smearneg_ds_nocomorbs
-		treatment_noninfect_extrapul_ds_nocomorbs
 		tb_death
 			susceptible_fully_nocomorbs -> latent_early_ds_nocomorbs [label=rate_force_ds]
 			susceptible_vac_nocomorbs -> latent_early_ds_nocomorbs [label=rate_force_weak_ds]
@@ -31,46 +21,16 @@ digraph {
 			latent_early_ds_nocomorbs -> active_smearpos_ds_nocomorbs [label=0.20]
 			latent_late_ds_nocomorbs -> active_smearpos_ds_nocomorbs [label=0.0042]
 			active_smearpos_ds_nocomorbs -> latent_late_ds_nocomorbs [label=0.10]
-			latent_early_ds_nocomorbs -> active_smearneg_ds_nocomorbs [label=0.07]
-			latent_late_ds_nocomorbs -> active_smearneg_ds_nocomorbs [label=0.0014]
-			active_smearneg_ds_nocomorbs -> latent_late_ds_nocomorbs [label=0.30]
-			latent_early_ds_nocomorbs -> active_extrapul_ds_nocomorbs [label=0.07]
-			latent_late_ds_nocomorbs -> active_extrapul_ds_nocomorbs [label=0.0014]
-			active_extrapul_ds_nocomorbs -> latent_late_ds_nocomorbs [label=0.30]
 			active_smearpos_ds_nocomorbs -> detect_smearpos_ds_nocomorbs [label=2.9]
 			active_smearpos_ds_nocomorbs -> missed_smearpos_ds_nocomorbs [label=0.47]
 			detect_smearpos_ds_nocomorbs -> treatment_infect_smearpos_ds_nocomorbs [label=25.6]
 			missed_smearpos_ds_nocomorbs -> active_smearpos_ds_nocomorbs [label=0.27]
-			active_smearneg_ds_nocomorbs -> detect_smearneg_ds_nocomorbs [label=2.9]
-			active_smearneg_ds_nocomorbs -> missed_smearneg_ds_nocomorbs [label=0.47]
-			detect_smearneg_ds_nocomorbs -> treatment_infect_smearneg_ds_nocomorbs [label=25.6]
-			missed_smearneg_ds_nocomorbs -> active_smearneg_ds_nocomorbs [label=0.27]
-			active_extrapul_ds_nocomorbs -> detect_extrapul_ds_nocomorbs [label=2.9]
-			active_extrapul_ds_nocomorbs -> missed_extrapul_ds_nocomorbs [label=0.47]
-			detect_extrapul_ds_nocomorbs -> treatment_infect_extrapul_ds_nocomorbs [label=25.6]
-			missed_extrapul_ds_nocomorbs -> active_extrapul_ds_nocomorbs [label=0.27]
 			treatment_infect_smearpos_ds_nocomorbs -> treatment_noninfect_smearpos_ds_nocomorbs [label=28.0]
 			treatment_infect_smearpos_ds_nocomorbs -> active_smearpos_ds_nocomorbs [label=0.18]
 			treatment_noninfect_smearpos_ds_nocomorbs -> active_smearpos_ds_nocomorbs [label=0.17]
 			treatment_noninfect_smearpos_ds_nocomorbs -> susceptible_treated_nocomorbs [label=1.7]
-			treatment_infect_smearneg_ds_nocomorbs -> treatment_noninfect_smearneg_ds_nocomorbs [label=28.0]
-			treatment_infect_smearneg_ds_nocomorbs -> active_smearneg_ds_nocomorbs [label=0.18]
-			treatment_noninfect_smearneg_ds_nocomorbs -> active_smearneg_ds_nocomorbs [label=0.17]
-			treatment_noninfect_smearneg_ds_nocomorbs -> susceptible_treated_nocomorbs [label=1.7]
-			treatment_infect_extrapul_ds_nocomorbs -> treatment_noninfect_extrapul_ds_nocomorbs [label=28.0]
-			treatment_infect_extrapul_ds_nocomorbs -> active_extrapul_ds_nocomorbs [label=0.18]
-			treatment_noninfect_extrapul_ds_nocomorbs -> active_extrapul_ds_nocomorbs [label=0.17]
-			treatment_noninfect_extrapul_ds_nocomorbs -> susceptible_treated_nocomorbs [label=1.7]
 			active_smearpos_ds_nocomorbs -> tb_death [label=0.26]
 			detect_smearpos_ds_nocomorbs -> tb_death [label=0.26]
-			active_smearneg_ds_nocomorbs -> tb_death [label=0.07]
-			detect_smearneg_ds_nocomorbs -> tb_death [label=0.07]
-			active_extrapul_ds_nocomorbs -> tb_death [label=0.07]
-			detect_extrapul_ds_nocomorbs -> tb_death [label=0.07]
 			treatment_infect_smearpos_ds_nocomorbs -> tb_death [label=0.18]
 			treatment_noninfect_smearpos_ds_nocomorbs -> tb_death [label=0.17]
-			treatment_infect_smearneg_ds_nocomorbs -> tb_death [label=0.18]
-			treatment_noninfect_smearneg_ds_nocomorbs -> tb_death [label=0.17]
-			treatment_infect_extrapul_ds_nocomorbs -> tb_death [label=0.18]
-			treatment_noninfect_extrapul_ds_nocomorbs -> tb_death [label=0.17]
 }

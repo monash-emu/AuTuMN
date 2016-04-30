91ba12689a927d7f9caee95227c9eea761ffe7bd
jtrauer <jtrauer@burnet.edu.au>
2016-04-30 20:35:27 +1000

    Attempt to get GitHub desktop to work
    Try closing Excel workbook and comment out the file-writing line

--- autumn/spreadsheet.py
+++ autumn/spreadsheet.py
@@ -548,6 +548,7 @@ def read_xls_with_sheet_readers(sheet_readers=[]):
             for i_col in range(reader.start_column, sheet.ncols):
                 reader.parse_col(sheet.col_values(i_col))
         result[reader.key] = reader.get_data()
+        workbook.release_resources()
     return result
 
 
@@ -774,7 +775,7 @@ def read_input_data_xls(from_test, sheets_to_read):
 if __name__ == "__main__":
     import json
     data = read_input_data_xls(False, ['bcg', 'tb'])
-    open('spreadsheet.out.txt', 'w').write(json.dumps(data, indent=2))
+    # open('spreadsheet.out.txt', 'w').write(json.dumps(data, indent=2))
     #print(data)
 
 

ff0af926de2528f4b545c2ce740bf07188da7c10
jtrauer <jtrauer@burnet.edu.au>
2016-05-23 14:59:01 +1000

    File change should have been part of last commit

--- test_full_models.py
+++ test_full_models.py
@@ -3,7 +3,6 @@
 import os
 import glob
 import datetime
-from pprint import pprint
 import autumn.model
 import numpy
 import autumn.plotting
@@ -17,9 +16,6 @@ start_realtime = datetime.datetime.now()
 out_dir = 'fullmodel_graphs'
 if not os.path.isdir(out_dir):
     os.makedirs(out_dir)
-
-start_time = 1850.
-recent_time = 1990.
 
 country = u'Fiji'
 
@@ -35,23 +31,15 @@ keys_of_sheets_to_read = [
     'notifications',
     'outcomes']
 
-data = read_and_process_data(True, keys_of_sheets_to_read, country, start_time)
-
-# To run all possible models
-strata_to_run = [0, 2, 3]
-true_false = [True, False]
-
-# To run a single simple example
-strata_to_run = [0]
-true_false = [False]
+data = read_and_process_data(True, keys_of_sheets_to_read, country)
 
 # Note that it takes about one hour to run all of the possible model structures
-for n_organs in strata_to_run:
-    for n_strains in strata_to_run:
-        for n_comorbidities in strata_to_run:
-            for is_quality in true_false:
-                for is_amplification in true_false:
-                    for is_misassignment in true_false:
+for n_organs in data['attributes']['n_organs']:
+    for n_strains in data['attributes']['n_strains']:
+        for n_comorbidities in data['attributes']['n_comorbidities']:
+            for is_quality in data['attributes']['is_lowquality']:
+                for is_amplification in data['attributes']['is_amplification']:
+                    for is_misassignment in data['attributes']['is_misassignment']:
                         if (is_misassignment and not is_amplification) \
                                 or (n_strains <= 1 and (is_amplification or is_misassignment)):
                             pass
@@ -60,15 +48,13 @@ for n_organs in strata_to_run:
                             base = os.path.join(out_dir, name)
 
                             model = autumn.model.ConsolidatedModel(
-                                [],  # List of breakpoints for age stratification (or empty list for no stratification)
                                 n_organs,
                                 n_strains,
                                 n_comorbidities,
                                 is_quality,  # Low quality care
                                 is_amplification,  # Amplification
                                 is_misassignment,  # Misassignment by strain
-                                data,
-                                start_time)
+                                data)
                             print(str(n_organs) + " organ(s),   " +
                                   str(n_strains) + " strain(s),   " +
                                   str(n_comorbidities) + " comorbidity(ies),   " +
@@ -81,82 +67,82 @@ for n_organs in strata_to_run:
                             for key, value in data['miscellaneous'].items():
                                 model.set_parameter(key, value)
 
-                            model.make_times(start_time, 2015.1, 0.1)
+                            model.make_times(data['attributes']['start_time'], 2015.1, 0.1)
                             model.integrate_explicit()
                             if n_organs + n_strains + n_comorbidities <= 5:
                                 model.make_graph(base + '.workflow')
 
                             # INDIVIDUAL COMPARTMENTS
                             # autumn.plotting.plot_fractions(
-                            #     model, model.labels, model.compartment_soln, recent_time,
+                            #     model, model.labels, model.compartment_soln, data['attributes']['recent_time'],
                             #     "strain", base + '.individ_total_bystrain.png')
                             # autumn.plotting.plot_fractions(
-                            #     model, model.labels, model.compartment_soln, recent_time,
+                            #     model, model.labels, model.compartment_soln, data['attributes']['recent_time'],
                             #     "organ", base + '.individ_total_byorgan.png')
                             # autumn.plotting.plot_fractions(
-                            #     model, model.labels, model.fraction_soln, recent_time,
+                            #     model, model.labels, model.fraction_soln, data['attributes']['recent_time'],
                             #     "strain", base + '.individ_fraction_bystrain.png')
                             # autumn.plotting.plot_fractions(
-                            #     model, model.labels, model.fraction_soln, recent_time,
+                            #     model, model.labels, model.fraction_soln, data['attributes']['recent_time'],
                             #     "organ", base + '.individ_fraction_byorgan.png')
                             # COMPARTMENT TYPES
                             # autumn.plotting.plot_fractions(
-                            #     model, model.compartment_types, model.compartment_type_fraction_soln, recent_time,
+                            #     model, model.compartment_types, model.compartment_type_fraction_soln, data['attributes']['recent_time'],
                             #     "", base + '.types_fraction.png')
                             # autumn.plotting.plot_fractions(
-                            #     model, model.compartment_types, model.compartment_type_soln, recent_time,
+                            #     model, model.compartment_types, model.compartment_type_soln, data['attributes']['recent_time'],
                             #     "", base + '.types_total.png')
                             # autumn.plotting.plot_fractions(
-                            #     model, model.compartment_types_bystrain, model.compartment_type_bystrain_fraction_soln, recent_time,
+                            #     model, model.compartment_types_bystrain, model.compartment_type_bystrain_fraction_soln, data['attributes']['recent_time'],
                             #     "strain", base + '.types_fraction_bystrain.png')
                             # BROAD COMPARTMENT TYPES
                             # autumn.plotting.plot_fractions(
-                            #     model, model.broad_compartment_types, model.broad_fraction_soln, recent_time,
+                            #     model, model.broad_compartment_types, model.broad_fraction_soln, data['attributes']['recent_time'],
                             #     "", base + '.broad_fraction.png')
                             # autumn.plotting.plot_fractions(
-                            #     model, model.broad_compartment_types_bystrain, model.broad_compartment_type_bystrain_fraction_soln, recent_time,
+                            #     model, model.broad_compartment_types_bystrain, model.broad_compartment_type_bystrain_fraction_soln, data['attributes']['recent_time'],
                             #     "strain", base + '.broadtypes_fraction_bystrain.png')
                             # autumn.plotting.plot_fractions(
-                            #     model, model.broad_compartment_types_byorgan, model.broad_compartment_type_byorgan_fraction_soln, recent_time,
+                            #     model, model.broad_compartment_types_byorgan, model.broad_compartment_type_byorgan_fraction_soln, data['attributes']['recent_time'],
                             #     "organ", base + '.broadtypes_fraction_byorgan.png')
                             # SUBGROUPS
                             # autumn.plotting.plot_fractions(
-                            #     model, model.groups["treatment"], model.treatment_fraction_soln, recent_time,
+                            #     model, model.groups["treatment"], model.treatment_fraction_soln, data['attributes']['recent_time'],
                             #     "", base + '.treatment_fraction.png')
                             # autumn.plotting.plot_fractions(
-                            #     model, model.groups["identified"], model.identified_fraction_soln, recent_time,
+                            #     model, model.groups["identified"], model.identified_fraction_soln, data['attributes']['recent_time'],
                             #     "", base + '.identified.png')
                             # OUTPUT RATES
                             # if n_strains >= 2:
                             #     autumn.plotting.plot_outputs(
                             #         model, ["incidence_ds", "incidence_mdr", "mortality_ds", "mortality_mdr", "prevalence_ds", "prevalence_mdr",
                             #                 "notifications_ds", "notifications_mdr"],
-                            #         start_time, base + '.rate_bystrain_outputs.png')
+                            #         data['attributes']['start_time'], base + '.rate_bystrain_outputs.png')
                             autumn.plotting.plot_outputs(
                                 model, ["incidence", "mortality", "prevalence", "notifications"],
-                                start_time, base + '.rate_outputs.png')
-                            # autumn.plotting.plot_outputs_against_gtb(
-                            #     model, "incidence",
-                            #     recent_time, base + '.rate_outputs_gtb.png',
-                            #     data)
+                                data['attributes']['start_time'], base + '.rate_outputs.png')
+                            autumn.plotting.plot_outputs_against_gtb(
+                                model, "incidence",
+                                data['attributes']['recent_time'], base + '.rate_outputs_gtb.png',
+                                data)
                             # if n_strains >= 2:
                             #     autumn.plotting.plot_outputs(
                             #         model, ["incidence_ds", "incidence_mdr", "mortality_ds", "mortality_mdr", "prevalence_ds", "prevalence_mdr",
                             #                 "notifications_ds", "notifications_mdr"],
-                            #         recent_time, base + '.rate_bystrain_outputs_recent.png')
+                            #         data['attributes']['recent_time'], base + '.rate_bystrain_outputs_recent.png')
                             #     autumn.plotting.plot_outputs(
                             #         model, ["incidence_ds", "incidence_mdr", "mortality_ds", "mortality_mdr", "prevalence_ds", "prevalence_mdr",
                             #                 "notifications_ds", "notifications_mdr"],
                             #         start_time, base + '.rate_outputs.png')
                             #     autumn.plotting.plot_outputs(
                             #         model, ["incidence_mdr", "prevalence_mdr", "mortality_mdr"],
-                            #         start_time, base + '.mdr_outputs.png')
+                            #         data['attributes']['start_time'], base + '.mdr_outputs.png')
                             #     autumn.plotting.plot_outputs(
                             #         model, ["incidence_mdr", "prevalence_mdr", "mortality_mdr"],
-                            #         recent_time, base + '.mdr_outputs_recent.png')
+                            #         data['attributes']['recent_time'], base + '.mdr_outputs_recent.png')
                             #     autumn.plotting.plot_outputs(
                             #         model, ["proportion_mdr"],
-                            #         start_time, base + '.mdr_proportion_recent.png')
+                            #         data['attributes']['start_time'], base + '.mdr_proportion_recent.png')
                             #
                             autumn.plotting.plot_scaleup_fns(model,
                                                              ["program_prop_algorithm_sensitivity",
@@ -168,7 +154,7 @@ for n_organs in strata_to_run:
                                                               "program_proportion_success",
                                                               "program_proportion_death",
                                                               "tb_proportion_amplification"],
-                                                             base + '.scaleups.png', start_time)
+                                                             base + '.scaleups.png', data['attributes']['start_time'])
                             #     year = indices(model.times, lambda x: x >= 2015.)[0]
                             #     print("2015 incidence is: ")
                             #     print(model.get_var_soln("incidence")[year])

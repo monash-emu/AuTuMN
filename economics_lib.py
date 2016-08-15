d9f141effceb96c3570e50431afc49e1c6899e8c
Romain Ragonnet <romain.ragonnet@burnet.edu.au>
2016-08-15 16:41:42 +1000

    Upgrade of 'economics_lib.py'
    Now runs all interventions but IPT. The main function
    'economics_diagnostic' has to be run after modle integration. It returns
    a dictionnary containing all the costing results. To be able to handle
    IPT, the spreadsheet has to be improved.

--- economics_lib.py
+++ economics_lib.py
@@ -65,7 +65,8 @@ def discount_cost(cost_uninflated, discount_rate, t_into_future):
     assert t_into_future >= 0, 't_into_future must be >= 0'
     return cost_uninflated / ((1 + discount_rate)**t_into_future)
 
-def economics_diagnostic(model, period_end, interventions = ['bcg'] ):
+def economics_diagnostic(model, period_end,
+                         interventions=['vaccination', 'xpert', 'treatment_support', 'smearacf', 'xpertacf']):
     """
     Run the economics diagnostics associated with a model run. Integration is supposed to have been run at this point
     Args:
@@ -74,7 +75,7 @@ def economics_diagnostic(model, period_end, interventions = ['bcg'] ):
         interventions: list of interventions considered for costing
 
     Returns:
-        the spending
+        the costs associated to each intervention and at each time step. These are yearly costs.
     """
     start_time = model.inputs.model_constants['recent_time'] # start time for cost calculations
     start_index = indices(model.times, lambda x: x >= start_time)[0]
@@ -83,42 +84,62 @@ def economics_diagnostic(model, period_end, interventions = ['bcg'] ):
     assert period_end <= end_time_integration, 'period_end must be <= end_time_integration'
     end_index = indices(model.times, lambda x: x >= period_end)[0]
 
-    param_keys = {'bcg': 'program_prop_vaccination', 'ipt': 'econ_program_prop_ipt'}  # to be completed
-    c_reflection_costs = {'bcg': 'econ_program_reflectioncost_vaccination'}  # to be completed
-    unitcosts = {'bcg': 'econ_program_unitcost_vaccination'}  # to be completed
+    # prepare the references to fetch the data and model outputs
+    param_key_base = 'econ_program_prop_'
+    c_reflection_cost_base ='econ_program_reflectioncost_'
+    unitcost_base = 'econ_program_unitcost_'
+    popsize_label_base = 'popsize_'
 
-    discount_rate = 0.03
+    discount_rate = 0.03 # not ideal... perhaps best to get it from a spreadsheet
     cpi_function = model.scaleup_fns['econ_cpi']
     year_current = model.inputs.model_constants['current_time']
     current_cpi = cpi_function(year_current)
 
-    for intervention in interventions: # for each intervention
-        print '#######################################'
-        print intervention
-        param_key = param_keys[intervention]
-        coverage_function = coverage_over_time(model, param_key)
-        c_reflection_cost_function = model.scaleup_fns[c_reflection_costs[intervention]]
-        unit_cost_function = model.scaleup_fns[unitcosts[intervention]]
+    # prepare the storage. 'costs' will store all the costs and will be returned
+    costs = {'cost_times': []}
 
-        for i in range(start_index, end_index + 1): # for each step time
+    for intervention in interventions: # for each intervention
+        costs[intervention] = {'uninflated_cost': [], 'inflated_cost': [], 'discounted_cost': []}
+
+        param_key = param_key_base + intervention # name of the corresponding parameter
+        coverage_function = coverage_over_time(model, param_key) # create a function to calculate coverage over time
+
+        c_reflection_cost_label = c_reflection_cost_base + intervention # key of the scale up function for reflection cost
+        c_reflection_cost_function = model.scaleup_fns[c_reflection_cost_label]
+
+        unitcost_label = unitcost_base + intervention # key of the scale up function for unit cost
+        unit_cost_function = model.scaleup_fns[unitcost_label]
+
+        popsize_label= popsize_label_base + intervention
+        pop_size_index = model.var_labels.index(popsize_label) # column index in model.var_array that corresponds to the intervention
+
+        saturation = 1.001  # provisional
+
+        for i in range(start_index, end_index + 1): # for each step time. We may want to change this bit. No need for all time steps
             t = model.times[i]
-            print t
+            costs['cost_times'].append(t) # storage of the time
+
+            # calculate the time variants that feed into the logistic function
             coverage = coverage_function(t)
             c_reflection_cost = c_reflection_cost_function(t)
-            saturation = 1.001  # provisional
             unit_cost = unit_cost_function(t)
-            pop_size = 15000. # To be linked with James' work
-            cost = get_cost_from_coverage(coverage, c_reflection_cost, saturation, unit_cost, pop_size)
-            print cost
+            pop_size = model.var_array[i, pop_size_index]
 
+            # calculate uninflated cost
+            cost = get_cost_from_coverage(coverage, c_reflection_cost, saturation, unit_cost, pop_size)
+            costs[intervention]['uninflated_cost'].append(cost) # storage
+
+            # calculate inflated cost
             cpi_time_variant = cpi_function(t)
             inflated_cost = inflate_cost(cost, current_cpi, cpi_time_variant)
-            print inflated_cost
+            costs[intervention]['inflated_cost'].append(inflated_cost) # storage
 
+            # calculate discounted cost
             t_into_future = max(0, (t - year_current))
             discounted_cost = discount_cost(cost, discount_rate, t_into_future)
-            print discounted_cost
+            costs[intervention]['discounted_cost'].append(discounted_cost) # storage
 
+    return costs
 
 # Functions related to the more complicated direction, where there is a feed back loop
 def get_coverage_from_cost(cost, c_reflection_cost, saturation, unit_cost, pop_size, alpha = 1.0):
@@ -140,6 +161,24 @@ def get_coverage_from_cost(cost, c_reflection_cost, saturation, unit_cost, pop_s
     b = ((2.0 ** (alpha + 1.0)) / (alpha * (saturation - a) * unit_cost * pop_size))
     coverage_estimated = a + (saturation - a) /((1 + math.exp((-b) * (cost - c_reflection_cost)))**alpha)
     return coverage_estimated
+
+
+if __name__ == "__main__":
+    country = read_input_data_xls(True, ['control_panel'])['control_panel']['country']
+    print(country)
+
+    inputs = autumn.data_processing.Inputs(True)
+    inputs.read_and_load_data()
+
+    scenario = None
+
+    model = autumn.model.ConsolidatedModel(scenario, inputs)
+    model.integrate()
+
+    costs = economics_diagnostic(model, period_end = 2020.)
+
+    print costs
+
 
 """
 Needed for integration in the epi model:
@@ -164,16 +203,3 @@ Needed for integration in the epi model:
 
 
 """
-
-country = read_input_data_xls(True, ['control_panel'])['control_panel']['country']
-print(country)
-
-inputs = autumn.data_processing.Inputs(True)
-inputs.read_and_load_data()
-
-scenario = None
-
-model = autumn.model.ConsolidatedModel(scenario, inputs)
-model.integrate()
-
-economics_diagnostic(model, period_end = 2020., interventions=['bcg'])

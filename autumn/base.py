d2b974f5a37dde6bc1648daeab15983c93512f02
jtrauer <jtrauer@burnet.edu.au>
2016-08-22 11:58:26 +1000

    Add economics start time to spreadsheets

--- autumn/base.py
+++ autumn/base.py
@@ -7,6 +7,65 @@ from tool_kit import indices
 
 from autumn.curve import make_two_step_curve
 
+
+def get_cost_from_coverage(coverage, c_inflection_cost, saturation, unit_cost, pop_size, alpha=1.):
+
+    """
+    Estimate the global uninflated cost associated with a given coverage
+    Args:
+        coverage: the coverage (as a proportion, then lives in 0-1)
+        c_inflection_cost: cost at which inflection occurs on the curve. It's also the configuration leading to the
+                            best efficiency.
+        saturation: maximal acceptable coverage, ie upper asymptote
+        unit_cost: unit cost of the intervention
+        pop_size: size of the population targeted by the intervention
+        alpha: steepness parameter
+
+    Returns:
+        uninflated cost
+
+    """
+
+    if pop_size * unit_cost == 0.:  # if unit cost or pop_size is null, return 0
+        return 0.
+    assert 0. <= coverage < saturation, 'coverage must verify 0 <= coverage < saturation'
+
+    a = saturation / (1. - 2. ** alpha)
+    b = ((2. ** (alpha + 1.)) / (alpha * (saturation - a) * unit_cost * pop_size))
+    cost_uninflated = c_inflection_cost - 1.0 / b * scipy.log(
+        (((saturation - a) / (coverage - a)) ** (1.0 / alpha)) - 1.0)
+
+    return cost_uninflated
+
+
+def inflate_cost(cost_uninflated, current_cpi, cpi_time_variant):
+    """
+    Calculate the inflated cost associated with cost_uninflated and considering the current cpi and the cpi correponding
+    to the date considered (cpi_time_variant)
+
+    Returns:
+        the inflated cost
+    """
+    return cost_uninflated * current_cpi / cpi_time_variant
+
+
+def discount_cost(cost_uninflated, discount_rate, t_into_future):
+
+    """
+    Calculate the discounted cost associated with cost_uninflated at time (t + t_into_future)
+    Args:
+        cost_uninflated: cost without accounting for discounting
+        discount_rate: discount rate (/year)
+        t_into_future: number of years into future at which we want to calculate the discounted cost
+
+    Returns:
+        the discounted cost
+    """
+
+    assert t_into_future >= 0, 't_into_future must be >= 0'
+    return (cost_uninflated / ((1 + discount_rate) ** t_into_future))
+
+
 class BaseModel:
 
     def __init__(self):
@@ -250,6 +309,7 @@ class BaseModel:
         assert not self.times is None, 'Times have not been set yet'
 
     def integrate_scipy(self, dt_max=0.05):
+
         """ Uses Adams method coded in the LSODA Fortran package. This method is programmed to "slow down" when a tricky
         point is encountered. Then we need to allow for a high maximal number of iterations (mxstep)so that the
         algorithm does not get stuck.
@@ -472,70 +532,20 @@ class BaseModel:
         return coverage_function
 
     def calculate_economics_diagnostics(self):
+
         """
-        Run the economics diagnostics associated with a model run. Integration is supposed to have been run at this point
-        Args:
-            self the model object
-        Returns:
-            nothing
+        Run the economics diagnostics associated with a model run.
+        Integration is supposed to have been completed by this point.
         """
 
-        def get_cost_from_coverage(coverage, c_inflection_cost, saturation, unit_cost, pop_size, alpha=1.0):
-            """
-            Estimate the global uninflated cost associated with a given coverage
-            Args:
-                coverage: the coverage (as a proportion, then lives in 0-1)
-                c_inflection_cost: cost at which inflection occurs on the curve. It's also the configuration leading to the
-                                    best efficiency.
-                saturation: maximal acceptable coverage, ie upper asymptote
-                unit_cost: unit cost of the intervention
-                pop_size: size of the population targeted by the intervention
-                alpha: steepness parameter
-
-            Returns:
-                uninflated cost
-
-            """
-            if pop_size * unit_cost == 0:  # if unit cost or pop_size is null, return 0
-                return 0
-            assert 0 <= coverage < saturation, 'coverage must verify 0 <= coverage < saturation'
-
-            a = saturation / (1.0 - 2 ** alpha)
-            b = ((2.0 ** (alpha + 1.0)) / (alpha * (saturation - a) * unit_cost * pop_size))
-            cost_uninflated = c_inflection_cost - 1.0 / b * scipy.log(
-                (((saturation - a) / (coverage - a)) ** (1.0 / alpha)) - 1.0)
-            return cost_uninflated
-
-        def inflate_cost(cost_uninflated, current_cpi, cpi_time_variant):
-            """
-            Calculate the inflated cost associated with cost_uninflated and considering the current cpi and the cpi correponding
-            to the date considered (cpi_time_variant)
-
-            Returns:
-                the inflated cost
-            """
-            return cost_uninflated * current_cpi / cpi_time_variant
-
-        def discount_cost(cost_uninflated, discount_rate, t_into_future):
-            """
-            Calculate the discounted cost associated with cost_uninflated at time (t + t_into_future)
-            Args:
-                cost_uninflated: cost without accounting for discounting
-                discount_rate: discount rate (/year)
-                t_into_future: number of years into future at which we want to calculate the discounted cost
-
-            Returns:
-                the discounted cost
-            """
-            assert t_into_future >= 0, 't_into_future must be >= 0'
-            return (cost_uninflated / ((1 + discount_rate) ** t_into_future))
-
-        start_time = self.inputs.model_constants['recent_time']  # start time for cost calculations
+        # Starting time for cost calculations, doesn't have to be (and probably shouldn't be) the start of the model run
+        start_time = self.inputs.model_constants['recent_time']
         start_index = indices(self.times, lambda x: x >= start_time)[0]
 
-        end_time_integration = self.inputs.model_constants['scenario_end_time']
-        assert self.end_period_costing <= end_time_integration, 'period_end must be <= end_time_integration'
-        end_index = indices(self.times, lambda x: x >= self.end_period_costing)[0]
+        # Assertion check - probably move to data processing
+        assert self.inputs.model_constants['economics_start_time'] <= self.inputs.model_constants['scenario_end_time'], \
+            'period_end must be before the end of the model integration time'
+        end_index = indices(self.times, lambda x: x >= self.inputs.model_constants['scenario_end_time'])[0]
 
         # prepare the references to fetch the data and model outputs
         param_key_base = 'econ_program_prop_'

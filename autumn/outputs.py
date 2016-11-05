5fc8778d7d07e1f577e059ee390ca78a258f9aad
jtrauer <jtrauer@burnet.edu.au>
2016-11-05 21:46:00 +1100

    Get compartment fractions running again

--- autumn/outputs.py
+++ autumn/outputs.py
@@ -1783,7 +1783,8 @@ class Project:
         """
 
         # Get values to be plotted
-        subgroup_solns, subgroup_fractions = autumn.tool_kit.find_fractions(self.model_runner.model_dict['baseline'])
+        subgroup_solns, subgroup_fractions \
+            = autumn.tool_kit.find_fractions(self.model_runner.model_dict['manual_baseline'])
         for i, category in enumerate(subgroup_fractions):
             values = subgroup_fractions[category]
 
@@ -1801,7 +1802,7 @@ class Project:
             # Plot population fractions
             for plot_label in values.keys():
                 ax.plot(
-                    self.model_runner.model_dict['baseline'].times,
+                    self.model_runner.model_dict['manual_baseline'].times,
                     values[plot_label],
                     label=plot_label, linewidth=1,
                     color=colours[plot_label],
@@ -1812,7 +1813,7 @@ class Project:
             # Finishing touches
             ax.set_xlim(self.inputs.model_constants['plot_start_time'],
                         self.inputs.model_constants['plot_end_time'])
-            title = make_plot_title(self.model_runner.model_dict['baseline'], values.keys())
+            title = make_plot_title(self.model_runner.model_dict['manual_baseline'], values.keys())
             set_axes_props(ax, 'Year', 'Proportion of population', 'Population, ' + title, True, axis_labels)
 
             # Saving

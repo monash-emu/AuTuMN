98ae757a170c2e1b7323a879b86063d05ecaf8be
jtrauer <jtrauer@burnet.edu.au>
2016-11-26 15:02:21 +1100

    Some simplification to spreadsheets

--- autumn/model.py
+++ autumn/model.py
@@ -301,9 +301,7 @@ class ConsolidatedModel(StratifiedModel):
         """
 
         # The parameter values are calculated from the costs, but only in the future
-        if self.eco_drives_epi:
-            if self.time > self.inputs.model_constants['current_time']:
-                self.update_vars_from_cost()
+        if self.eco_drives_epi and self.time > self.inputs.model_constants['current_time']: self.update_vars_from_cost()
         self.vars['population'] = sum(self.compartments.values())
         self.calculate_birth_rates_vars()
         self.calculate_force_infection_vars()

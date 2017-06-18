9ec298aaa6bc036162805a7f245b1abf28474705
jtrauer <james.trauer@monash.edu>
2017-06-18 15:56:54 +1000

    Allow no demographic user inputs
    Allow the demographic time-variant parameters (i.e. birth rate and life expectancy) to be taken from the loaded spreadsheets if no user-defined values are provided.

--- autumn/data_processing.py
+++ autumn/data_processing.py
@@ -685,12 +685,19 @@ class Inputs:
         Similarly to previous methods, only performed if requested and only populated where absent.
         """
 
+        # for the two types of demographic parameters
         for demo_parameter in ['life_expectancy', 'rate_birth']:
-            if self.time_variants['demo_' + demo_parameter]['load_data'] == u'yes':
+
+            # if there are data available from the user-derived sheets and loading external data is requested
+            if 'demo_' + demo_parameter in self.time_variants \
+                    and self.time_variants['demo_' + demo_parameter]['load_data'] == u'yes':
                 for year in self.original_data[demo_parameter]:
                     if year not in self.time_variants['demo_' + demo_parameter]:
-                        self.time_variants['demo_' + demo_parameter][year] = self.original_data[demo_parameter][
-                            year]
+                        self.time_variants['demo_' + demo_parameter][year] = self.original_data[demo_parameter][year]
+
+            # if there are no data available from the user sheets
+            else:
+                self.time_variants['demo_' + demo_parameter] = self.original_data[demo_parameter]
 
     def add_organ_status_to_timevariants(self):
         """

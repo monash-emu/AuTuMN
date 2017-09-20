4abdbb2f9d523b87200cb6ffb931980e28c2cdb0
jtrauer <james.trauer@monash.edu>
2017-09-21 02:00:37 +1000

    Little debug to make scale-up plotting work

--- autumn/outputs.py
+++ autumn/outputs.py
@@ -1519,7 +1519,8 @@ class Project:
 
         # different figure for each type of function
         for classification in self.classified_scaleups:
-            if len(self.classified_scaleups[classification]) > 0:
+            if len(self.classified_scaleups[classification]) > 0 \
+                    and classification in self.model_runner.model_dict['manual_baseline'].scaleup_fns[function]:
 
                 # find the list of the scale-up functions to work with and some x-values
                 function_list = self.classified_scaleups[classification]

5e9938758817c739c51918d3a2edc87cf13e9793
Romain Ragonnet <romain.ragonnet@burnet.edu.au>
2017-11-24 10:17:09 +1100

    Fix Food vouchers intervention

--- autumn/model.py
+++ autumn/model.py
@@ -725,16 +725,17 @@ class ConsolidatedModel(StratifiedModel):
             #             * self.params['int_prop_treatment_support_improvement'] \
             #             * (1. - self.vars['int_prop_ngo_activities'])
 
-            # add some extra treatment success if food vouchers are provided to treated cases
-            # if 'int_prop_food_voucher' + strain in self.relevant_interventions:
-            #     self.vars['program_prop_treatment_success' + strain] \
-            #        += (1. - self.vars['program_prop_treatment_success' + strain]) \
-            #           * self.params['int_prop_food_voucher_improvement'] \
-            #           * self.vars['int_prop_food_voucher' + strain]
-            #     self.vars['program_prop_treatment_death' + strain] \
-            #         -= self.vars['program_prop_treatment_death' + strain] \
-            #           * self.params['int_prop_food_voucher_improvement'] \
-            #           * self.vars['int_prop_food_voucher' + strain]
+            #  add some extra treatment success if food vouchers are provided to treated cases
+            if 'int_prop_food_voucher' + strain in self.relevant_interventions:
+                for history in self.histories:
+                    self.vars['program_prop_treatment' + strain + history + '_success'] \
+                       += (1. - self.vars['program_prop_treatment' + strain + history + '_success']) \
+                          * self.params['int_prop_food_voucher_improvement'] \
+                          * self.vars['int_prop_food_voucher' + strain]
+                    self.vars['program_prop_treatment' + strain + history + '_death'] \
+                        -= self.vars['program_prop_treatment' + strain + history + '_death'] \
+                          * self.params['int_prop_food_voucher_improvement'] \
+                          * self.vars['int_prop_food_voucher' + strain]
 
             # calculate the default proportion as the remainder from success and death
             for history in self.histories:

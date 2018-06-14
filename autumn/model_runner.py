73821a27851cda8f8a1c33b59ee16065afdd4f89
jtrauer <james.trauer@monash.edu>
2018-06-14 12:08:26 +1000

    Debug uncertainty storage
    For some reason appears that storing a new scenario under uncertainty was wiping out all the previous scenario runs by setting the whole 'epi' dictionary within 'epi_uncertainty' to empty. Perhaps not noticed because we aren't usually running scenarios with epi uncertainty these days.

--- autumn/model_runner.py
+++ autumn/model_runner.py
@@ -636,7 +636,8 @@ class ModelRunner:
                 outputs_for_comparison \
                     = [self.outputs['epi_uncertainty']['epi'][0]['incidence'][
                            last_run_output_index, t_k.find_first_list_element_at_least(
-                               self.outputs['epi_uncertainty']['epi'][0]['times'], float(year))] for year in years_to_compare]
+                               self.outputs['epi_uncertainty']['epi'][0]['times'], float(year))]
+                       for year in years_to_compare]
 
                 # calculate likelihood
                 prior_log_likelihood, posterior_log_likelihood = 0., 0.
@@ -836,7 +837,7 @@ class ModelRunner:
 
             # create third tier if outputs haven't been recorded yet for this scenario
             if scenario not in self.outputs[uncertainty_type][output_type]:
-                self.outputs[uncertainty_type][output_type] = {scenario: {}}
+                self.outputs[uncertainty_type][output_type][scenario] = {}
 
             # for each epi or cost output available
             for output in new_outputs[output_type]:
@@ -990,6 +991,7 @@ class ModelRunner:
         calibrate a single parameter.
         Return the value of the calibrated parameter.
         """
+
         self.add_comment_to_gui_window('Rapid calibration commenced')
         params_to_calibrate = 'tb_n_contact'   # hard-coded
         param_dict = {}

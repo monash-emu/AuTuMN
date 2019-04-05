ba66d23e8996ead8ae5ce0899f32a64033e27eba
swas0001 <swasnik@gmail.com>
2019-04-05 14:32:19 +1100

    bgui localhost port 3000 in api.py and handler logging fix

--- bgui/server/server/api.py
+++ bgui/server/server/api.py
@@ -266,7 +266,7 @@ def index():
 # Allow Cross-Origin-Resource-Sharing, mainly for working with hot reloading webclient
 @app.after_request
 def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:8080')
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     response.headers.add('Access-Control-Allow-Credentials', 'true')

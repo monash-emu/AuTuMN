1ee5ef64fda284e9404e319f88c8922f962b8b86
Romain Ragonnet <romain.ragonnet@monash.edu>
2019-08-28 13:30:01 +1000

    Create new building function for mongolia model

--- autumn_from_summer/mongolia_tb_model.py
+++ autumn_from_summer/mongolia_tb_model.py
@@ -115,9 +115,86 @@ def build_model_for_calibration(start_time=1800., stratify_by=['age'], time_vari
     return _tb_model
 
 
+def new_build_model_for_calibration(stratify_by):
+    input_database = InputDB()
+
+    external_params = {'start_time': 1800.,
+                       'case_fatality_rate': 0.4,
+                       'untreated_disease_duration': 3.0
+                       }
+
+    integration_times = numpy.linspace(external_params['start_time'], 2020.0, 50).tolist()
+    # set basic parameters, flows and times, then functionally add latency
+
+    parameters = \
+        {"contact_rate": 100.,
+         "recovery": external_params['case_fatality_rate'] / external_params['untreated_disease_duration'],
+         "infect_death": (1.0 - external_params['case_fatality_rate']) / external_params['untreated_disease_duration'],
+         "universal_death_rate": 1.0 / 50.0,
+         "case_detection": 0.,
+         "dr_amplification": 0.001,  # high value for testing
+         "crude_birth_rate": 20.0 / 1e3}
+    parameters.update(change_parameter_unit(provide_aggregated_latency_parameters(), 365.251))
+
+    # sequentially add groups of flows
+    flows = add_standard_infection_flows([])
+    flows = add_standard_latency_flows(flows)
+    flows = add_standard_natural_history_flows(flows)
+
+    # compartments
+    compartments = ["susceptible", "early_latent", "late_latent", "infectious", "recovered"]
+
+    # define model     #replace_deaths
+    if len(stratify_by) > 0:
+        _tb_model = StratifiedModel(
+            integration_times, compartments, {"infectious": 1e-3}, parameters, flows, birth_approach="replace_deaths",
+            starting_population=3000000)
+    else:
+        _tb_model = EpiModel(
+            integration_times, compartments, {"infectious": 1e-3}, parameters, flows, birth_approach="replace_deaths",
+            starting_population=3000000)
+
+    # add crude birth rate from un estimates
+    _tb_model = get_birth_rate_functions(_tb_model, input_database, 'MNG')
+
+    # add case detection process to basic model
+    _tb_model.add_transition_flow(
+        {"type": "standard_flows", "parameter": "case_detection", "origin": "infectious", "to": "recovered"})
+
+    # loading time-variant case detection rate
+    input_database = InputDB()
+    res = input_database.db_query("gtb_2015", column="c_cdr", is_filter="country", value="Mongolia")
+
+    # add scaling case detection rate
+    cdr_adjustment_factor = .5
+    cdr_mongolia = res["c_cdr"].values / 1e2 * cdr_adjustment_factor
+    cdr_mongolia = numpy.concatenate(([0.0], cdr_mongolia))
+    res = input_database.db_query("gtb_2015", column="year", is_filter="country", value="Mongolia")
+    cdr_mongolia_year = res["year"].values
+    cdr_mongolia_year = numpy.concatenate(([1950.], cdr_mongolia_year))
+    cdr_scaleup = scale_up_function(cdr_mongolia_year, cdr_mongolia, smoothness=0.2, method=5)
+    prop_to_rate = convert_competing_proportion_to_rate(1.0 / external_params['untreated_disease_duration'])
+    detect_rate = return_function_of_function(cdr_scaleup, prop_to_rate)
+    _tb_model.time_variants["case_detection"] = detect_rate
+
+    if "strain" in stratify_by:
+        _tb_model.stratify("strain", ["ds", "mdr"], ["early_latent", "late_latent", "infectious"], verbose=False,
+                           requested_proportions={"mdr": 0.})
+        _tb_model.add_transition_flow(
+            {"type": "standard_flows", "parameter": "dr_amplification",
+             "origin": "infectiousXstrain_ds", "to": "infectiousXstrain_mdr",
+             "implement": len(_tb_model.all_stratifications)})
+
+    _tb_model.transition_flows.to_csv("transitions.csv")
+    _tb_model.death_flows.to_csv("deaths.csv")
+
+
+    return _tb_model
+
+
 if __name__ == "__main__":
-    stratify_by = ['age']
-    mongolia_model = build_model_for_calibration(stratify_by=stratify_by)
+    stratify_by = ['strain']
+    mongolia_model = new_build_model_for_calibration(stratify_by=stratify_by)
     mongolia_model.run_model()
 
     req_outputs = ['distribution_of_strataXage', 'distribution_of_strataXlocation', 'distribution_of_strataXhousing',

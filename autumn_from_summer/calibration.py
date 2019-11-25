Fix indentation error in loglikelihood db storage

autumn_from_summer/calibration.py
@@ -105,18 +105,17 @@ class Calibration:
             data = np.array(target['values'])
             model_output = np.array(self.post_processing.generated_outputs[key])
 
-
             if self.run_mode == 'lsm':
                 ll += np.sum((data - model_output)**2)
             else:
                 ll += -(0.5/target['sd']**2)*np.sum((data - model_output)**2)
-            mcmc_run_dict = {k: v for k, v in zip(self.param_list, params)}
-            mcmc_run_dict['loglikelihood'] = ll
-            mcmc_run_colnames = self.param_list.copy()
-            mcmc_run_colnames = mcmc_run_colnames.append('loglikelihood')
-            mcmc_run_df = pd.DataFrame(mcmc_run_dict, columns=mcmc_run_colnames, index=[self.iter_num])
-            store_tb_database(mcmc_run_df, table_name='mcmc_run', run_idx=self.iter_num,
-                              database_name=output_db_path, append=True)
+        mcmc_run_dict = {k: v for k, v in zip(self.param_list, params)}
+        mcmc_run_dict['loglikelihood'] = ll
+        mcmc_run_colnames = self.param_list.copy()
+        mcmc_run_colnames = mcmc_run_colnames.append('loglikelihood')
+        mcmc_run_df = pd.DataFrame(mcmc_run_dict, columns=mcmc_run_colnames, index=[self.iter_num])
+        store_tb_database(mcmc_run_df, table_name='mcmc_run', run_idx=self.iter_num,
+                          database_name=output_db_path, append=True)
 
 
         return ll
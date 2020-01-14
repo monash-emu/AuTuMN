a5a3c2743d5ec0cd4cafec25b744bd826bde2a4a
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-01-14 15:34:43 +1100

    Use correct scenario index in PBI outputs.

--- autumn_from_summer/tb_model.py
+++ autumn_from_summer/tb_model.py
@@ -150,7 +150,8 @@ def store_run_models(models, scenarios, database_name="../databases/outputs.db")
         output_df = pd.DataFrame(model.outputs, columns=model.compartment_names)
         derived_output_df = pd.DataFrame.from_dict(model.derived_outputs)
         pbi_outputs = unpivot_outputs(model)
-        store_tb_database(pbi_outputs, table_name='pbi_scenario_' + str(scenarios[i]),  database_name=database_name)
+        store_tb_database(pbi_outputs, table_name='pbi_scenario_' + str(scenarios[i]),  database_name=database_name,
+                          scenario=scenarios[i])
         store_tb_database(derived_output_df, scenario=scenarios[i], table_name='derived_outputs', database_name=database_name)
         store_tb_database(output_df, scenario=scenarios[i], times=model.times, database_name=database_name, append=True)
 

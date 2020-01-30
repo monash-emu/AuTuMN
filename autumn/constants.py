194339f8b4ebba7ed53e88b5a11bc3f7d418a5b8
Matthew Segal <mattdsegal@gmail.com>
2020-01-30 12:51:02 +1100

    Add more tests

--- autumn/constants.py
+++ autumn/constants.py
@@ -2,10 +2,12 @@
 Constants used in building the AuTuMN / SUMMER models.
 """
 
+
 class Compartment:
     """
     A model compartment.
     """
+
     SUSCEPTIBLE = "susceptible"
     INFECTIOUS = "infectious"
     RECOVERED = "recovered"
@@ -15,5 +17,26 @@ class Flow:
     """
     A type of flow between model compartments
     """
+
     INFECTION_FREQUENCY = "infection_frequency"
     INFECTION_DENSITY = "infection_density"
+    DEATH = "compartment_death"
+
+
+class BirthApproach:
+    """
+    Options for birth rate settings in model
+    """
+
+    NO_BIRTH = "no_birth"
+    ADD_CRUDE = "add_crude_birth_rate"
+    REPLACE_DEATHS = "replace_deaths"
+
+
+class IntegrationType:
+    """
+    Options for ODE solver used by model
+    """
+
+    ODE_INT = "odeint"
+    SOLVE_IVP = "solve_ivp"
--- tests/test_epi_model.py
+++ tests/test_epi_model.py
@@ -4,7 +4,7 @@ End-to-end tests for the EpiModel - a disease agnostic compartmental model from 
 import numpy as np
 
 from summer_py.summer_model import EpiModel
-from autumn.constants import Compartment, Flow
+from autumn.constants import Compartment, Flow, BirthApproach
 from autumn.tool_kit import get_integration_times
 
 
@@ -25,8 +25,77 @@ def test_epi_model__with_static_dynamics__expect_no_change():
     # Run the model for 5 years.
     model.run_model()
     # Expect that no one has moved from sucsceptible to infections at any point in time
-    assert len(model.outputs) == 6
-    assert (model.outputs == [100.0, 0.0]).all()
+    expected_output = [
+        [100.0, 0.0],
+        [100.0, 0.0],
+        [100.0, 0.0],
+        [100.0, 0.0],
+        [100.0, 0.0],
+        [100.0, 0.0],
+    ]
+    actual_output = np.round(model.outputs)
+    assert (actual_output == np.array(expected_output)).all()
+
+
+def test_epi_model__with_birth_rate__expect_pop_increase():
+    """
+    Ensure that a model with two compartments and only birth rate dynamics results in more people.
+    """
+    # Set up a model with 100 people, all susceptible, no transmission possible.
+    # Add some babies at ~2 babies / 100 / year.
+    pop = 100
+    model = EpiModel(
+        times=get_integration_times(2000, 2005, 1),
+        compartment_types=[Compartment.SUSCEPTIBLE, Compartment.INFECTIOUS],
+        initial_conditions={Compartment.SUSCEPTIBLE: pop},
+        parameters={"crude_birth_rate": 2e-2},
+        requested_flows=[],
+        birth_approach=BirthApproach.ADD_CRUDE,
+        starting_population=pop,
+    )
+    # Run the model for 5 years.
+    model.run_model()
+    # Expect that we have more people in the population
+    expected_output = [
+        [[100.0, 0.0], [102.0, 0.0], [104.0, 0.0], [106.0, 0.0], [108.0, 0.0], [111.0, 0.0]]
+    ]
+    actual_output = np.round(model.outputs)
+    assert (actual_output == np.array(expected_output)).all()
+
+
+def test_epi_model__with_death_rate__expect_pop_decrease():
+    """
+    Ensure that a model with two compartments and only death rate dynamics results in fewer people.
+    """
+    # Set up a model with 100 people, all susceptible, no transmission possible.
+    # Add some dying at ~2 people / 100 / year.
+    pop = 100
+    model = EpiModel(
+        times=get_integration_times(2000, 2005, 1),
+        compartment_types=[Compartment.SUSCEPTIBLE, Compartment.INFECTIOUS],
+        initial_conditions={Compartment.SUSCEPTIBLE: pop},
+        parameters={"universal_death_rate": 2e-2},
+        requested_flows=[],
+        birth_approach=BirthApproach.NO_BIRTH,
+        starting_population=pop,
+    )
+    # Run the model for 5 years.
+    model.run_model()
+    # Expect that we have more people in the population
+    expected_output = [
+        [100.0, 0.0],
+        [98.0, 0.0],
+        [96.0, 0.0],
+        [94.0, 0.0],
+        [92.0, 0.0],
+        [91.0, 0.0],
+    ]
+    actual_output = np.round(model.outputs)
+    assert (actual_output == np.array(expected_output)).all()
+
+
+def test_epi_model__with_infect_death_rate__expect_infected_pop_decrease():
+    pass
 
 
 def test_epi_model__with_no_infected__expect_no_change():
@@ -53,21 +122,29 @@ def test_epi_model__with_no_infected__expect_no_change():
     # Run the model for 5 years.
     model.run_model()
     # Expect that no one has moved from sucsceptible to infections at any point in time
-    assert len(model.outputs) == 6
-    assert (model.outputs == [100.0, 0.0]).all()
+    expected_output = [
+        [100.0, 0.0],
+        [100.0, 0.0],
+        [100.0, 0.0],
+        [100.0, 0.0],
+        [100.0, 0.0],
+        [100.0, 0.0],
+    ]
+    actual_output = np.round(model.outputs)
+    assert (actual_output == np.array(expected_output)).all()
 
 
 def test_epi_model__with_infection_frequency__expect_all_infected():
     """
     Ensure that a model with two compartments and one-way internal dynamics results in all infected.
     """
-    # Set up a model with 100 people, all susceptible execept 1 infected, transmission highly likely.
+    # Set up a model with 100 people, all susceptible execept 1 infected.
     pop = 100
     model = EpiModel(
         times=get_integration_times(2000, 2005, 1),
         compartment_types=[Compartment.SUSCEPTIBLE, Compartment.INFECTIOUS],
         initial_conditions={Compartment.INFECTIOUS: 1},
-        parameters={"contact_rate": 10},
+        parameters={"contact_rate": 3},
         requested_flows=[
             {
                 "type": Flow.INFECTION_FREQUENCY,
@@ -80,29 +157,40 @@ def test_epi_model__with_infection_frequency__expect_all_infected():
     )
     # Run the model for 5 years.
     model.run_model()
-    # Expect that everyone gets infected very quickly.
+    # Expect that everyone gets infected eventually.
     expected_output = [
-        [99.0, 1.0],
-        [0.0, 100.0],
-        [0.0, 100.0],
-        [0.0, 100.0],
-        [0.0, 100.0],
-        [0.0, 100.0],
+        [[99.0, 1.0], [83.0, 17.0], [20.0, 80.0], [1.0, 99.0], [0.0, 100.0], [0.0, 100.0]]
     ]
-    assert (np.round(model.outputs) == np.array(expected_output)).all()
+    actual_output = np.round(model.outputs)
+    assert (actual_output == np.array(expected_output)).all()
 
 
 def test_epi_model__with_infection_density__expect_all_infected():
-    pass
-
-
-def test_epi_model__with_birth_rate__expect_pop_increase():
-    pass
-
-
-def test_epi_model__with_death_rate__expect_pop_decrease():
-    pass
-
-
-def test_epi_model__with_birth_and_death_rate__expect_NOT_SURE_YET():
-    pass
+    """
+    Ensure that a model with two compartments and one-way internal dynamics results in all infected.
+    """
+    # Set up a model with 100 people, all susceptible execept 1 infected.
+    pop = 100
+    model = EpiModel(
+        times=get_integration_times(2000, 2005, 1),
+        compartment_types=[Compartment.SUSCEPTIBLE, Compartment.INFECTIOUS],
+        initial_conditions={Compartment.INFECTIOUS: 1},
+        parameters={"contact_rate": 0.03},
+        requested_flows=[
+            {
+                "type": Flow.INFECTION_DENSITY,
+                "parameter": "contact_rate",
+                "origin": Compartment.SUSCEPTIBLE,
+                "to": Compartment.INFECTIOUS,
+            }
+        ],
+        starting_population=pop,
+    )
+    # Run the model for 5 years.
+    model.run_model()
+    # Expect that everyone gets infected eventually.
+    expected_output = [
+        [[99.0, 1.0], [83.0, 17.0], [20.0, 80.0], [1.0, 99.0], [0.0, 100.0], [0.0, 100.0]]
+    ]
+    actual_output = np.round(model.outputs)
+    assert (actual_output == np.array(expected_output)).all()

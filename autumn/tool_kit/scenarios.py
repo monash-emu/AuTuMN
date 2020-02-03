cee67d0a4d63a40a7c42b90a515f6872ec275e22
Matthew Segal <mattdsegal@gmail.com>
2020-02-03 12:18:27 +1100

    Add scenario tests

--- autumn/tool_kit/scenarios.py
+++ autumn/tool_kit/scenarios.py
@@ -1,62 +1,75 @@
 """
 Utilities for running multiple model scenarios
 """
-import copy
 
-
-def run_multi_scenario(scenario_params, scenario_start_time, model_builder):
+def run_multi_scenario(param_lookup, scenario_start_time, model_builder):
     """
     Run a baseline model and scenarios
-    :param scenario_params: a dictionary keyed with scenario numbers (0 for baseline). values are dictionaries
-    containing parameter updates
+    FIXME: [Matt] Wouldn't it make more sense if param_lookup was just a list?
+
+    :param param_lookup: 
+        A dictionary keyed with scenario numbers (0 for baseline). 
+        Values are dictionaries containing parameter updates.
     :return: a list of model objects
     """
-    param_updates_for_baseline = scenario_params[0] if 0 in scenario_params.keys() else {}
-    baseline_model = model_builder(param_updates_for_baseline)
-    print("____________________  Now running Baseline Scenario ")
+    # Run baseline model as scenario '0'
+    baseline_params =  param_lookup[0] if 0 in param_lookup else {}
+    baseline_model = model_builder(baseline_params)
+    print("____________________  Running baseline scenario ")
     baseline_model.run_model()
-
     models = [baseline_model]
 
-    for scenario_index in scenario_params.keys():
-        if scenario_index == 0:
+    for scenario_idx, scenario_params in param_lookup.items():
+        # Ignore scenario '0' because we've already run it.
+        if scenario_idx == 0:
             continue
-        print("____________________  Now running Scenario " + str(scenario_index))
-        scenario_params[scenario_index]["start_time"] = scenario_start_time
+
+        print(f"____________________  Running scenario #{scenario_idx}")
+        scenario_params["start_time"] = scenario_start_time
         scenario_model = initialise_scenario_run(
-            baseline_model, scenario_params[scenario_index], model_builder
+            baseline_model, scenario_params, model_builder
         )
         scenario_model.run_model()
-        models.append(copy.deepcopy(scenario_model))
+        models.append(scenario_model)
 
     return models
 
 
-def initialise_scenario_run(baseline_model, update_params, model_builder):
+def initialise_scenario_run(baseline_model, scenario_params, model_builder):
     """
-    function to run a scenario. Running time starts at start_time.the initial conditions will be loaded form the
-    run baseline_model
+    Build a model to run a scenario. Running time starts at start_time, which must
+    be specified in `scenario_params`. The initial conditions will be loaded from the
+    run baseline_model.
+
     :return: the run scenario model
     """
+    baseline_times = baseline_model.times
+    baseline_outputs = baseline_model.outputs
 
-    if isinstance(baseline_model, dict):
-        baseline_model_times = baseline_model["times"]
-        baseline_model_outputs = baseline_model["outputs"]
-    else:
-        baseline_model_times = baseline_model.times
-        baseline_model_outputs = baseline_model.outputs
+    # Find the timestep from which we will start the scenario.
+    scenario_start_index = get_scenario_start_index(baseline_times, scenario_params["start_time"])
+    scenario_start_time = baseline_times[scenario_start_index]
+    scenario_init_compartments = baseline_outputs[scenario_start_index, :]
 
-    # find last integrated time and its index before start_time in baseline_model
-    first_index_over = min(
-        [x[0] for x in enumerate(baseline_model_times) if x[1] > update_params["start_time"]]
-    )
-    index_of_interest = max([0, first_index_over - 1])
-    integration_start_time = baseline_model_times[index_of_interest]
-    init_compartments = baseline_model_outputs[index_of_interest, :]
+    # Create the new scenario model using the scenario-specific params,
+    # ensuring the initial conditions are the same for the given start time.
+    scenario_params["start_time"] = scenario_start_time
+    model = model_builder(scenario_params)
+    model.compartment_values = scenario_init_compartments
+    return model
 
-    update_params["start_time"] = integration_start_time
 
-    sc_model = model_builder(update_params)
-    sc_model.compartment_values = init_compartments
+def get_scenario_start_index(baseline_times, scenario_start_time):
+    """
+    Returns the index of the closest timestep that is at, or before the scenario start time.
+    """
+    indexs_after_start_index = [
+        idx for idx, time in enumerate(baseline_times) if time > scenario_start_time
+    ]
+    if not indexs_after_start_index:
+        msg = f"Scenario start time {scenario_start_time} is set after the baseline time range."
+        raise ValueError(msg)
 
-    return sc_model
+    index_after_start_index = min(indexs_after_start_index)
+    start_index = max([0, index_after_start_index - 1])
+    return start_index

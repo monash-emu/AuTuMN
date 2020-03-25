2cd572bf323c53a5a9803ae4667b18090346203e
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-03-25 16:13:30 +1100

    Format John Hopkins data for calibration

--- applications/covid_19/covid_calibration.py
+++ applications/covid_19/covid_calibration.py
@@ -2,6 +2,10 @@ from autumn.calibration import Calibration
 
 from applications.covid_19.covid_model import build_covid_model
 
+import pandas as pd
+import os
+from numpy import diff, linspace
+
 
 def run_calibration_chain(max_seconds: int, run_id: int):
     """
@@ -29,12 +33,39 @@ PAR_PRIORS = [
     {"param_name": "contact_rate", "distribution": "uniform", "distri_params": [0.5, 0.7]},
 ]
 
+
+def read_john_hopkins_data_from_csv(variable="confirmed", country="Australia"):
+    """
+    Read John Hopkins data from previously generated csv files
+    :param variable: one of "confirmed", "deaths", "recovered"
+    :param country: country
+    """
+    filename = "covid_" + variable + ".csv"
+    path = os.path.join('applications', 'covid_19', 'JH_data', filename)
+    data = pd.read_csv(path)
+    data = data[data['Country/Region'] == country]
+    data_series = []
+    for (columnName, columnData) in data.iteritems():
+        if columnName.count("/") > 1:
+            cumul_this_day = sum(columnData.values)
+            data_series.append(cumul_this_day)
+
+    # for confirmed and deaths, we want the daily counts and not the cumulative number
+    if variable != 'recovered':
+        data_series = diff(data_series)
+    return data_series
+
+# for JH data, day_1 is '1/22/20', that is 22 Jan 2020
+
+
+n_daily_cases = read_john_hopkins_data_from_csv('confirmed')
+
+
 TARGET_OUTPUTS = [
     {
-        "output_key": "prevXinfectiousXamong",
-        "years": [100.0],
-        "values": [.15],
-        "cis": [(.10, .20)],
+        "output_key": "incidence",
+        "years": linspace(22, 21 + len(n_daily_cases), num=len(n_daily_cases)),
+        "values": n_daily_cases
     }
 ]
 

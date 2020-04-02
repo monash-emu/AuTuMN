60612c80124d13e6f85232e1a99260afe0b32ecc
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-04-03 10:33:46 +1100

    Use relative path to access JH data csv file

--- applications/covid_19/JH_data/process_JH_data.py
+++ applications/covid_19/JH_data/process_JH_data.py
@@ -1,14 +1,14 @@
 import pandas as pd
-
+import os
 
 def get_all_jh_countries():
     """
     Determine the list of available countries from the John Hopkins database
     :return: a list of country names
     """
-
-    filename = "covid_confirmed.csv"
-    data = pd.read_csv(filename)
+    filepath=os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), "covid_confirmed.csv")
+    data = pd.read_csv(filepath)
     countries = data['Country/Region'].to_list()
 
     # remove duplicates

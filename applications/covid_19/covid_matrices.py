e92809884f4a36c4059724799c5ed3731e0420a7
James Trauer <james.trauer@monash.edu>
2020-04-04 22:40:31 +1100

    use smoother function and more data points for changes in mixing patterns with time

--- applications/covid_19/covid_matrices.py
+++ applications/covid_19/covid_matrices.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from autumn.curve import scale_up_function
 from autumn.demography.social_mixing import load_all_prem_types
 
 
@@ -25,20 +26,14 @@ def build_covid_matrices(country, mixing_params):
             mixing_matrix = mixing_matrix_components['all_locations']
             for location in \
                     [loc for loc in ['home', 'other_locations', 'school', 'work']
-                     if loc in mixing_params[i_scenario]]:
-                school_closure_change = \
-                    np.piecewise(
-                        time,
-                        [time <
-                         mixing_params[i_scenario]['school_closure'],
-                         mixing_params[i_scenario]['school_closure'] <=
-                         time <
-                         mixing_params[i_scenario]['school_reopening'],
-                         mixing_params[i_scenario]['school_reopening'] <=
-                         time],
-                        [0., mixing_params[i_scenario][location], 0.]
+                     if loc + '_times' in mixing_params[i_scenario]]:
+                location_change = \
+                    scale_up_function(
+                        mixing_params[i_scenario][location + '_times'],
+                        mixing_params[i_scenario][location + '_values']
                     )
-                mixing_matrix = np.add(mixing_matrix, school_closure_change * mixing_matrix_components[location])
+                mixing_matrix = np.add(mixing_matrix, location_change(time) * mixing_matrix_components[location])
             return mixing_matrix
+
         mixing_functions[i_scenario] = mixing_matrix_function
     return mixing_functions

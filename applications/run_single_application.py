d291bd3e0457747d1479ac669374fff320bfbe2b
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-04-06 14:50:07 +1000

    Use old code to produce some important graphs

--- applications/run_single_application.py
+++ applications/run_single_application.py
@@ -150,6 +150,11 @@ def run_model(application):
         outputs_plotter.save_flows_sheets()
         outputs_plotter.run_input_plots()
 
+        # Old code to plot requested_outputs and derived_outputs
+        # old_outputs_plotter = Outputs(models, pps, output_options, targets_to_plot=output_options['targets_to_plot'],
+        #                               out_dir=output_directory, plot_start_time=1940)
+        # old_outputs_plotter.plot_requested_outputs()
+
 
 if __name__ == '__main__':
     run_model('covid_19')

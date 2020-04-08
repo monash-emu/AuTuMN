9dda43beaa429145a88ab7d8f96644eb83798f42
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-04-08 11:35:27 +1000

    COVID: Use 14 days following 100th case for calibration

--- applications/covid_19/covid_calibration.py
+++ applications/covid_19/covid_calibration.py
@@ -1,4 +1,5 @@
 from autumn.calibration import Calibration
+from autumn.tool_kit.utils import find_first_index_reaching_cumulative_sum
 
 from applications.covid_19.covid_model import build_covid_model, PARAMS_PATH
 from applications.covid_19.JH_data.process_JH_data import read_john_hopkins_data_from_csv
@@ -34,22 +35,32 @@ def run_calibration_chain(max_seconds: int, run_id: int):
     )
     print(f"Finished calibration for run {run_id}.")
 
+# for JH data, day_1 is '1/22/20', that is 22 Jan 2020
+n_daily_cases = read_john_hopkins_data_from_csv('confirmed', country=params['default']['country'])
+
+# get the subset of data points starting after 100th case detected and recording next 14 days
+index_100 = find_first_index_reaching_cumulative_sum(n_daily_cases, 100)
+data_of_interest = n_daily_cases[index_100: index_100 + 14]
+
+start_day = index_100 + 22  # because JH data starts 22/1
+
 
 PAR_PRIORS = [
     {"param_name": "contact_rate", "distribution": "uniform", "distri_params": [.3, .6]},
-    # {"param_name": "start_time", "distribution": "uniform", "distri_params": [0., 65.]}
+    {"param_name": "start_time", "distribution": "uniform", "distri_params": [-30, start_day - 1]}
 ]
-# for JH data, day_1 is '1/22/20', that is 22 Jan 2020
-n_daily_cases = read_john_hopkins_data_from_csv('confirmed', country=params['default']['country'])
 
 TARGET_OUTPUTS = [
     {
         "output_key": "notifications",
-        "years": linspace(22, 21 + len(n_daily_cases), num=len(n_daily_cases))[-15:],
-        "values": n_daily_cases[-15:]
+        "years": linspace(start_day, start_day + len(data_of_interest) - 1, num=len(data_of_interest)),
+        "values": data_of_interest,
+        "loglikelihood_distri": 'poisson'
     }
 ]
 
 MULTIPLIERS = {
 
 }
+
+run_calibration_chain(120, 1)

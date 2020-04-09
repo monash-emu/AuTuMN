9efa4609e11c77d95a228ed4c486bbc9b893469d
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-04-09 15:52:16 +1000

    COVID: draft method to plot fitted model curve vs data (needs some clean up)

--- applications/covid_19/JH_data/process_JH_data.py
+++ applications/covid_19/JH_data/process_JH_data.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import os
-from numpy import diff
+from numpy import diff, linspace
 
 import matplotlib.pyplot as plt
+from autumn.tool_kit.utils import find_first_index_reaching_cumulative_sum
+from applications.covid_19.covid_model import *
+from autumn.db import get_iso3_from_country_name
 
+import yaml
 
 def get_all_jh_countries():
     """
@@ -67,3 +71,54 @@ def plot_jh_data(data):
         filename = "daily_cases_" + country + ".png"
         path = os.path.join(dir_path, filename)
         plt.savefig(path)
+
+
+def plot_fitted_model(country):
+    dir_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data_graphs')
+
+    # load the calibrated parameters
+    file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'mle_estimates',
+                                             'covid_' + country, 'mle_params.yml'
+                             )
+    with open(file_path, 'r') as yaml_file:
+        calibrated_params = yaml.safe_load(yaml_file)
+    for param_name in calibrated_params:
+        calibrated_params[param_name] = float(calibrated_params[param_name])
+    calibrated_params['country'] = country
+    calibrated_params['iso3'] = get_iso3_from_country_name(input_database, country)
+    calibrated_params['end_time'] = 100
+    # build the model
+    this_model = build_covid_model(calibrated_params)
+    this_model.run_model()
+
+    model_times = this_model.derived_outputs['times']
+    plot_times = [t - 22 for t in model_times]
+    notifications = this_model.derived_outputs['notifications']
+
+    # get the data
+    n_daily_cases = read_john_hopkins_data_from_csv('confirmed', country=country)
+    # get the subset of data points starting after 100th case detected and recording next 14 days
+    index_100 = find_first_index_reaching_cumulative_sum(n_daily_cases, 100)
+    data_of_interest = n_daily_cases[index_100: index_100 + 14]
+    start_day = index_100 + 22  # because JH data starts 22/1
+
+    plt.figure()
+    x = list(range(len(n_daily_cases)))
+    plt.bar(x, list(n_daily_cases), color='purple')
+    x_2 = linspace(index_100, index_100+13, num=14)
+    plt.bar(x_2, list(data_of_interest), color='magenta')
+
+    plt.plot(plot_times, notifications, color='crimson', linewidth=2)
+    plt.xlim((0, max(x)))
+    plt.xlabel('days since 22/1/2020')
+    plt.ylabel('daily notifications')
+    plt.title(country)
+    # plt.ylim((0, 1000))
+
+    filename = "fitted_model_" + country + ".png"
+    path = os.path.join(dir_path, filename)
+    plt.savefig(path)
+
+#
+# for _country in ['Canada', 'Germany', 'France', 'Australia', 'Italy', 'China', 'Pakistan', 'Spain']:
+#     plot_fitted_model(_country)

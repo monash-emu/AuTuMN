5c4ba8566c84f71f09370c748d65f7a22517d232
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-04-14 15:21:19 +1000

    COVID: implement first optimisation with test for herd immunity

--- applications/covid_19/mixing_optimisation/mixing_opti.py
+++ applications/covid_19/mixing_optimisation/mixing_opti.py
@@ -1,5 +1,6 @@
 from applications.run_single_application import *
 from autumn.db import Database, get_iso3_from_country_name
+import copy
 
 INPUT_DB_PATH = os.path.join(constants.DATA_PATH, 'inputs.db')
 input_database = Database(database_name=INPUT_DB_PATH)
@@ -25,6 +26,15 @@ main_params['default']['country'] = COUNTRY
 main_params['default']['iso3'] = ISO3
 
 
+def has_immunity_been_reached(_model):
+    """
+    Determine whether herd immunity has been reached after running a model
+    :param _model: a model run with no-intervention setting for testing herd-immunity
+    :return: a boolean
+    """
+    return max(_model.derived_outputs['incidence']) == _model.derived_outputs['incidence'][0]
+
+
 def objective_function(mixing_multipliers):
     # build the model
     model_function = build_covid_model
@@ -41,26 +51,55 @@ def objective_function(mixing_multipliers):
     mixing_multipliers_matrix[13:, 3:13] = mixing_multipliers['f'] * np.ones((3, 10))
     mixing_multipliers_matrix[3:13, 13:] = mixing_multipliers['f'] * np.ones((10, 3))
 
+    # save params without mixing multipliers for scenario 1
+    main_params_sc1 = copy.copy(main_params['default'])
+    main_params_sc1['end_time'] = 300
+
     # Prepare scenario data
     main_params['default'].update({'mixing_matrix_multipliers': mixing_multipliers_matrix})
-    scenario_params = {0: main_params['default']}
+    scenario_params = {0: main_params['default'],
+                       1: main_params_sc1}
 
     # run the model
-    model = run_multi_scenario(
+    models = run_multi_scenario(
         scenario_params,
-        main_params['scenario_start'],
+        main_params['default']['end_time'] - 1,
         model_function,
         mixing_progression,
         run_kwargs=SOLVER_KWARGS
-    )[0]
+    )
 
     # Has herd immunity been reached?
-    herd_immunity = True
+    herd_immunity = has_immunity_been_reached(models[1])
 
     # How many deaths
-    total_nb_deaths = sum(model.derived_outputs['infection_deathsXall'])
+    total_nb_deaths = sum(models[0].derived_outputs['infection_deathsXall'])
 
-    return herd_immunity, total_nb_deaths
+    return herd_immunity, total_nb_deaths, models
 
 
-h_i, obj = objective_function(mixing_multipliers=main_params['default']['mixing_matrix_multipliers'])
+def visualise_simulation(_models):
+
+    pps = []
+    for scenario_index in range(len(_models)):
+
+        pps.append(
+                    post_proc.PostProcessing(
+                        _models[scenario_index],
+                        requested_outputs=['prevXinfectiousXamong', 'prevXrecoveredXamong'],
+                        scenario_number=scenario_index,
+                        requested_times={}
+                    )
+                )
+
+    old_outputs_plotter = Outputs(_models, pps, {}, plot_start_time=0)
+    old_outputs_plotter.plot_requested_outputs()
+
+
+# parameters to optimise (all bounded in [0., 2.])
+mixing_mult = {'a': 1., 'b': 1.5, 'c': 0., 'd': .5, 'e': .7, 'f': 1.3}
+
+h_immu, obj, models = objective_function(mixing_multipliers=mixing_mult)
+
+# visualise_simulation(models)
+

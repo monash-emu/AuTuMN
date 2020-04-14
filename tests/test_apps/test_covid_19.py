4ce5bbc77cc785e1562750283a6a35f1f5e1e19e
Matthew Segal <mattdsegal@gmail.com>
2020-04-14 11:10:50 +1000

    Update COVID smoke test names

--- tests/test_apps/test_covid_19.py
+++ tests/test_apps/test_covid_19.py
@@ -12,16 +12,16 @@ IS_GITHUB_CI = os.environ.get("GITHUB_ACTION", False)
 # FIXME: the order of these tests matter, STRATIFY_BY patch isn't working as expected.
 @mock.patch("builtins.input", return_value="")
 @mock.patch("autumn.tb_model.outputs.Outputs")
-def test_run_marshall_model(mock_output_cls, mock_input):
+def test_run_covid_model(mock_output_cls, mock_input):
     """
-    Ensure Marshall Islands model runs.
+    Ensure COVID model runs.
     """
-    run_model('covid_19')
+    run_model("covid_19")
 
 
-def test_build_marshall_model():
+def test_build_covid_model():
     """
-    Ensure we can build the Marshall Islands model with nothing crashing
+    Ensure we can build the COVID model with nothing crashing
     """
     model = build_covid_model({})
     assert type(model) is StratifiedModel

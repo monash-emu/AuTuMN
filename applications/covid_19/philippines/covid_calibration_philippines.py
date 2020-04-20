fbd9a2fba77c9b47b518c920afd5729d9a6de8ae
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-04-21 09:47:48 +1000

    Prepare some code for PHL and MYS COVID models

--- applications/covid_19/philippines/covid_calibration_philippines.py
+++ applications/covid_19/philippines/covid_calibration_philippines.py
@@ -1,27 +1,17 @@
 from applications.covid_19.covid_calibration import *
 from numpy import linspace
 
-country = 'Philippines'
+country = 'philippines'
+PAR_PRIORS, TARGET_OUTPUTS = get_priors_and_targets(country, 'deaths', 2)
+
+
+
+# get rid of time in the params to calibrate
+del PAR_PRIORS[1]
 
 # start_date = 11/3/2020 (day 70) for first item of the death_counts list
-death_counts = [3, 3, 4, 4, 5, 2, 11, 4, 10, 11, 8, 10, 15, 7, 7, 12, 8, 10, 12, 9, 8, 6, 3, 2, 5, 1]
-data_times = linspace(70, 70 + 25, num=26)
-nb_time_points = 10
-death_counts = death_counts[:nb_time_points]
-data_times = data_times[:nb_time_points].tolist()
 
-target_to_plots = {'infection_deathXall': {'times': data_times, 'values': [[d] for d in death_counts]}}
+target_to_plots = {'infection_deathXall': {'times': TARGET_OUTPUTS[0]['years'], 'values': [[d] for d in TARGET_OUTPUTS[0]['values']]}}
 print(target_to_plots)
-PAR_PRIORS = [
-    {"param_name": "contact_rate", "distribution": "uniform", "distri_params": [.1, 4.]},
-    {"param_name": "start_time", "distribution": "uniform", "distri_params": [-30, 69]}
-]
-
-TARGET_OUTPUTS = [
-    {"output_key": "infection_deathsXall",
-     "years": data_times,
-     "values": death_counts,
-     "loglikelihood_distri": 'poisson'}
-]
 
 run_calibration_chain(120, 1, country, PAR_PRIORS, TARGET_OUTPUTS)

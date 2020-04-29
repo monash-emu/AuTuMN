96f15b0d863115d4a61df4ab5619bf6d1c5dfac2
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-04-29 16:09:09 +1000

    Add time-variant age-specific adjustments to mixing matrices

--- apps/run_pycharm.py
+++ apps/run_pycharm.py
@@ -12,8 +12,8 @@ RUN_DESCRIPTION = "trying to x and y the z"
 # covid_19.phl.run_model(RUN_NAME)
 # covid_19.mys.run_model(RUN_NAME)
 # covid_19.aus.run_model(RUN_NAME, RUN_DESCRIPTION)
-# covid_19.vic.run_model(RUN_NAME, RUN_DESCRIPTION)
+covid_19.vic.run_model(RUN_NAME, RUN_DESCRIPTION)
 
 
-MODEL_RUN_PATH = "data/covid_victoria/model-run-27-04-2020--17-06-42/"
-plot_from_database(MODEL_RUN_PATH)
+# MODEL_RUN_PATH = "data/covid_victoria/model-run-27-04-2020--17-06-42/"
+# plot_from_database(MODEL_RUN_PATH)

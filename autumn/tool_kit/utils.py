360a14c87acc0ac196b8f4dfe8f0357726c54b37
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-04-30 16:47:06 +1000

    Implement toolkit to work out a distribution params based on specified mean and CI (beta only for now)

--- autumn/tool_kit/utils.py
+++ autumn/tool_kit/utils.py
@@ -8,6 +8,8 @@ import itertools
 import hashlib
 import json
 from datetime import date
+from scipy.stats import beta, gamma
+from scipy.optimize import minimize
 
 from summer.model import find_name_components
 
@@ -284,3 +286,44 @@ def element_wise_list_division(numerator, denominator):
     Simple function to find the quotients of two lists.
     """
     return [num / den for num, den in zip(numerator, denominator)]
+
+
+def find_distribution_params_from_mean_and_ci(distribution, mean, ci, ci_width=.95):
+    """
+    Work out the parameters of a given distribution based on a desired mean and CI
+    :param distribution: string
+        either 'gamma' or 'beta' for now
+    :param mean: float
+        the desired mean value
+    :param ci: list or tuple of length 2
+        the lower and upper bounds of the CI
+    :param ci_width:
+        the width of the desired CI
+    :return:
+        a dictionary with the parameters
+    """
+    assert len(ci) == 2 and ci[1] > ci[0] and 0. < ci_width < 1.
+    percentile_low = (1. - ci_width) / 2.
+    percentile_up = 1. - percentile_low
+
+    if distribution == 'beta':
+        assert 0. < ci[0] < 1. and 0. < ci[1] < 1. and 0. < mean < 1.
+
+        def distance_to_minimise(a):
+            b = a * (1. - mean) / mean
+            vals = beta.ppf([percentile_low, percentile_up], a, b)
+            dist = sum([(ci[i] - vals[i])**2 for i in range(2)])
+            return dist
+
+        sol = minimize(distance_to_minimise, [1.], bounds=[(0., None)])
+        best_a = sol.x
+        best_b = best_a * (1. - mean) / mean
+        params = {'a': best_a, 'b': best_b}
+
+    elif distribution == 'gamma':
+        assert ci[0] > 0 and ci[1] > 0 and mean > 0.
+        params = {'shape': None, 'scale': None}
+    else:
+        raise ValueError(distribution + " distribution is not supported for the moment")
+
+    return params

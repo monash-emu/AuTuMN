6a46745aeefdc90c85b8d31ce5cdecf61dd2b7d3
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-05-01 14:51:40 +1000

    Add distribution details on prior plots

--- autumn/outputs/calibration_plots.py
+++ autumn/outputs/calibration_plots.py
@@ -18,7 +18,7 @@ def plot_all_priors(priors, directory):
     path = os.path.join(directory, 'prior_plots')
     os.makedirs(path)
     for i, prior_dict in enumerate(priors):
-        plt.figure(i)
+        fig, ax = plt.subplots()
         x_range = workout_plot_x_range(prior_dict)
         x_values = linspace(x_range[0], x_range[1], num=1000)
         y_values = [calculate_prior(prior_dict, x, log=False) for x in x_values]
@@ -26,6 +26,16 @@ def plot_all_priors(priors, directory):
         plt.fill_between(x_values, y_values, zeros, color='cornflowerblue')
         plt.xlabel(prior_dict['param_name'])
         plt.ylabel('prior PDF')
+
+        # place a text box in upper left corner to indicate the prior details
+        props = dict(boxstyle='round', facecolor='dimgray', alpha=0.5)
+        textstr = prior_dict['distribution'] + "\n(" + str(round(float(prior_dict['distri_params'][0]), 3)) + \
+                  ", " + str(round(float(prior_dict['distri_params'][1]), 3))\
+                  + ")"
+        ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14,
+                verticalalignment='top', bbox=props)
+
+        plt.tight_layout()
         filename = os.path.join(path, prior_dict['param_name'] + ".png")
         plt.savefig(filename)
 

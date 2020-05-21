aeae2e84e939b16a4e55ada9079fae2945e17e07
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-05-21 15:13:46 +1000

    Update targets for Malaysia

--- apps/covid_19/calibration/malaysia.py
+++ apps/covid_19/calibration/malaysia.py
@@ -56,25 +56,26 @@ PAR_PRIORS = [
 ]
 
 # notification data, provided by the country
-notification_times = [63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134]
-notification_counts = [7, 14, 5, 28, 10, 6, 18, 12, 20, 9, 45, 35, 190, 125, 120, 117, 110, 130, 153, 123, 212, 106, 172, 235, 130, 159, 150, 156, 140, 142, 208, 217, 150, 179, 131, 170, 156, 109, 118, 184, 153, 134, 170, 85, 110, 69, 54, 84, 36, 57, 50, 71, 88, 51, 38, 40, 31, 94, 57, 69, 105, 122, 55, 30, 45, 39, 68, 54, 67, 70, 16, 37]
+notification_times = [63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141]
+notification_counts = [7, 14, 5, 28, 10, 6, 18, 12, 20, 9, 45, 35, 190, 125, 120, 117, 110, 130, 153, 123, 212, 106, 172, 235, 130, 159, 150, 156, 140, 142, 208, 217, 150, 179, 131, 170, 156, 109, 118, 184, 153, 134, 170, 85, 110, 69, 54, 84, 36, 57, 50, 71, 88, 51, 38, 40, 31, 94, 57, 69, 105, 122, 55, 30, 45, 39, 68, 54, 67, 70, 16, 37, 40, 36, 17, 22, 47, 37, 31]
 
 # ICU data (prev / million pop), provided by the country
-icu_times = [71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134]
-icu_counts = [2.0, 3.0, 4.0, 5.0, 9.0, 12.0, 12.0, 15.0, 20.0, 26.0, 37.0, 46.0, 57.0, 64.0, 45.0, 45.0, 54.0, 73.0, 73.0, 94.0, 94.0, 102.0, 105.0, 108.0, 99.0, 99.0, 102.0, 92.0, 76.0, 72.0, 69.0, 72.0, 66.0, 66.0, 60.0, 56.0, 56.0, 51.0, 49.0, 46.0, 45.0, 43.0, 43.0, 42.0, 41.0, 36.0, 36.0, 37.0, 36.0, 40.0, 36.0, 37.0, 31.0, 27.0, 28.0, 24.0, 22.0, 19.0, 18.0, 18.0, 18.0, 20.0, 16., 16.]
+icu_times = [71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141]
+icu_counts = [2.0, 3.0, 4.0, 5.0, 9.0, 12.0, 12.0, 15.0, 20.0, 26.0, 37.0, 46.0, 57.0, 64.0, 45.0, 45.0, 54.0, 73.0, 73.0, 94.0, 94.0, 102.0, 105.0, 108.0, 99.0, 99.0, 102.0, 92.0, 76.0, 72.0, 69.0, 72.0, 66.0, 66.0, 60.0, 56.0, 56.0, 51.0, 49.0, 46.0, 45.0, 43.0, 43.0, 42.0, 41.0, 36.0, 36.0, 37.0, 36.0, 40.0, 36.0, 37.0, 31.0, 27.0, 28.0, 24.0, 22.0, 19.0, 18.0, 18.0, 18.0, 20.0, 16.0, 16.0, 16.0, 14.0, 13.0, 13.0, 13.0, 11.0, 11.0]
+
 
 TARGET_OUTPUTS = [
     {
         "output_key": "notifications",
         "years": notification_times,
         "values": notification_counts,
-        "loglikelihood_distri": "negative_binomial",
+        "loglikelihood_distri": "poisson",  # "negative_binomial",
     },
     {
         "output_key": "prevXlateXclinical_icuXamong",
         "years": icu_times,
         "values": icu_counts,
-        "loglikelihood_distri": "negative_binomial",
+        "loglikelihood_distri": "poisson",  # "negative_binomial",
     }
 ]
 
@@ -83,16 +84,16 @@ MULTIPLIERS = {'prevXlateXclinical_icuXamong': 32364904.}  # to get absolute pop
 # __________  For the grid-based calibration approach
 # define a grid of parameter values. The posterior probability will be evaluated at each node
 par_grid = [
-    {"param_name": "contact_rate", 'lower': .018, 'upper': .023, 'n': 6},
-    {"param_name": "start_time", 'lower': 25., 'upper': 35., 'n': 11}
+    {"param_name": "contact_rate", 'lower': .01, 'upper': .02, 'n': 6},
+    {"param_name": "start_time", 'lower': 0., 'upper': 50., 'n': 11}
 ]
 
 
 def run_mys_calibration_chain(max_seconds: int, run_id: int):
-    # run_calibration_chain(max_seconds, run_id, country, PAR_PRIORS, TARGET_OUTPUTS, mode='grid_based',
-    #                       _grid_info=par_grid, _run_extra_scenarios=False, _multipliers=MULTIPLIERS)
-    run_calibration_chain(max_seconds, run_id, country, PAR_PRIORS, TARGET_OUTPUTS, mode='autumn_mcmc',
-                          _run_extra_scenarios=False, _multipliers=MULTIPLIERS)
+    run_calibration_chain(max_seconds, run_id, country, PAR_PRIORS, TARGET_OUTPUTS, mode='grid_based',
+                          _grid_info=par_grid, _run_extra_scenarios=False, _multipliers=MULTIPLIERS)
+    # run_calibration_chain(max_seconds, run_id, country, PAR_PRIORS, TARGET_OUTPUTS, mode='autumn_mcmc',
+    #                       _run_extra_scenarios=False, _multipliers=MULTIPLIERS)
 
 
 if __name__ == "__main__":

85da453a5a7a40a430629a6381b96970cf6d3921
Matthew Segal <mattdsegal@gmail.com>
2020-05-21 10:36:58 +1000

    Add parameter updating function for calibration

--- autumn/tool_kit/params.py
+++ autumn/tool_kit/params.py
@@ -2,7 +2,9 @@
 Used to load model parameters from file
 """
 import os
+import re
 import yaml
+from copy import deepcopy
 from os import path
 
 from autumn.constants import APPS_PATH
@@ -64,3 +66,61 @@ def load_params(app_name: str, region_name: str):
         "scenarios": scenarios,
         "scenario_start_time": scenario_start_time,
     }
+
+
+def update_params(params: dict, updates: dict) -> dict:
+    """
+    Update parameter dict according to string based update requests in update dict.
+    Update requests made as follows:
+
+        - dict entries updated as "key": val
+        - nested dict entries updated as "key1.key2": val
+        - array entries updated as "arr[0]": 1
+
+    Example
+
+        params = {"foo": 1, "bar" {"baz": 2}, "bing": [1, 2]}
+        updates = {"foo": 2, "bar.baz": 3, "bing[1]": 4}
+        returns {"foo": 2, "bar" {"baz": 3}, "bing": [1, 4]}
+
+    See tests for more details.
+    """
+    ps = deepcopy(params)
+    for key, val in updates.items():
+        ps = _update_params(ps, key, val)
+
+    return ps
+
+
+# Regex to match an array update request eg. "foo[1]"
+ARRAY_REQUEST_REGEX = r"^\w+\[\d\]$"
+
+
+def _update_params(params: dict, update_key: str, update_val) -> dict:
+    ps = deepcopy(params)
+    keys = update_key.split(".")
+    current_key, nested_keys = keys[0], keys[1:]
+    is_arr_update = re.match(ARRAY_REQUEST_REGEX, current_key)
+    is_nested_update = bool(nested_keys)
+    if is_arr_update and is_nested_update:
+        # Array item replacement followed by nested dictionary replacement.
+        key, idx_str = current_key.replace("]", "").split("[")
+        idx = int(idx_str)
+        child_key = ".".join(nested_keys)
+        ps[key][idx] = _update_params(ps[key][idx], child_key, update_val)
+    elif is_arr_update:
+        # Array item replacement.
+        key, idx_str = update_key.replace("]", "").split("[")
+        idx = int(idx_str)
+        ps[key][idx] = update_val
+    elif is_nested_update:
+        # Nested dictionary replacement.
+        child_key = ".".join(nested_keys)
+        key = current_key
+        ps[key] = _update_params(ps[key], child_key, update_val)
+    else:
+        # Simple key lookup, just replace key with val.
+        key = current_key
+        ps[key] = update_val
+
+    return ps
---
+++ tests/test_toolkit/test_params.py
@@ -0,0 +1,95 @@
+from autumn.tool_kit.params import update_params
+
+
+def test_update_params__no_request__expect_no_change():
+    old_params = {
+        "foo": 1,
+        "bar": {"baz": 2, "boop": [7, 8, 9], "bing": {"bonk": 3,}},
+        "boop": [4, 5, 6],
+    }
+    update_request = {}
+    expected_new_params = {
+        "foo": 1,
+        "bar": {"baz": 2, "boop": [7, 8, 9], "bing": {"bonk": 3,}},
+        "boop": [4, 5, 6],
+    }
+    actual_new_params = update_params(old_params, update_request)
+    assert actual_new_params == expected_new_params
+
+
+def test_update_params__multiple_shallow_requests__expect_updated():
+    old_params = {
+        "foo": 1,
+        "bank": 10,
+        "bar": {"baz": 2, "boop": [7, 8, 9], "bing": {"bonk": 3,}},
+        "boop": [4, 5, 6],
+    }
+    update_request = {"foo": 2, "bank": 3}
+    expected_new_params = {
+        "foo": 2,
+        "bank": 3,
+        "bar": {"baz": 2, "boop": [7, 8, 9], "bing": {"bonk": 3,}},
+        "boop": [4, 5, 6],
+    }
+    actual_new_params = update_params(old_params, update_request)
+    assert actual_new_params == expected_new_params
+
+
+def test_update_params__deep_request__expect_updated():
+    old_params = {
+        "foo": 1,
+        "bar": {"baz": 2, "boop": [7, 8, 9], "bing": {"bonk": 3,}},
+        "boop": [4, 5, 6],
+    }
+    update_request = {"bar.baz": 3}
+    expected_new_params = {
+        "foo": 1,
+        "bar": {"baz": 3, "boop": [7, 8, 9], "bing": {"bonk": 3,}},
+        "boop": [4, 5, 6],
+    }
+    actual_new_params = update_params(old_params, update_request)
+    assert actual_new_params == expected_new_params
+
+
+def test_update_params__array_request__expect_updated():
+    old_params = {
+        "foo": 1,
+        "bar": {"baz": 2, "boop": [7, 8, 9], "bing": {"bonk": 3,}},
+        "boop": [4, 5, 6],
+    }
+    update_request = {"boop[1]": 1}
+    expected_new_params = {
+        "foo": 1,
+        "bar": {"baz": 2, "boop": [7, 8, 9], "bing": {"bonk": 3,}},
+        "boop": [4, 1, 6],
+    }
+    actual_new_params = update_params(old_params, update_request)
+    assert actual_new_params == expected_new_params
+
+
+def test_update_params__deep_array_request__expect_updated():
+    old_params = {
+        "foo": 1,
+        "bar": {"baz": 2, "boop": [7, 8, 9], "bing": {"bonk": 3,}},
+        "boop": [4, 5, 6],
+    }
+    update_request = {"bar.boop[0]": 1}
+    expected_new_params = {
+        "foo": 1,
+        "bar": {"baz": 2, "boop": [1, 8, 9], "bing": {"bonk": 3,}},
+        "boop": [4, 5, 6],
+    }
+    actual_new_params = update_params(old_params, update_request)
+    assert actual_new_params == expected_new_params
+
+
+def test_update_params__dict_in_array_request__expect_updated():
+    old_params = {
+        "foo": [{"a": 1}, {"a": 2}, {"a": 3}],
+    }
+    update_request = {"foo[1].a": 4}
+    expected_new_params = {
+        "foo": [{"a": 1}, {"a": 4}, {"a": 3}],
+    }
+    actual_new_params = update_params(old_params, update_request)
+    assert actual_new_params == expected_new_params

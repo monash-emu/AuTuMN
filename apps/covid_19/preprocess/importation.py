375fc62ac509d17575bea79cdf8039f1165d653d
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-05-21 14:50:48 +1000

    Add lower_boung for scale-up curve creation (importation)

--- apps/covid_19/preprocess/importation.py
+++ apps/covid_19/preprocess/importation.py
@@ -59,7 +59,8 @@ def get_importation_rate_func_as_birth_rates(
         importation_n_cases[i] /= detect_prop_func(time)
     # scale-up curve for importation numbers
     importation_numbers_scale_up = scale_up_function(
-        importation_times, importation_n_cases, method=5, smoothness=5.0
+        importation_times, importation_n_cases, method=5, smoothness=5.0,
+        bound_low=0.
     )
 
     def recruitment_rate(t):

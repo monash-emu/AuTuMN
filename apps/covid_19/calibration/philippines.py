c650db7316d78daa94ccb10d5e849afba7fb21d2
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-05-24 17:10:42 +1000

    Prepare a calibration file for the PHL

--- apps/covid_19/calibration/philippines.py
+++ apps/covid_19/calibration/philippines.py
@@ -1,19 +1,47 @@
 from apps.covid_19.calibration.base import run_calibration_chain, get_priors_and_targets
 
 country = "philippines"
-PAR_PRIORS, TARGET_OUTPUTS = get_priors_and_targets(country, "deaths", 2)
 
-# Get rid of time in the params to calibrate
-del PAR_PRIORS[1]
 
-# target_to_plots = {
-#     "infection_deathsXall": {
-#         "times": TARGET_OUTPUTS[0]["years"],
-#         "values": [[d] for d in TARGET_OUTPUTS[0]["values"]],
-#     }
-# }
-# print(target_to_plots)
+PAR_PRIORS = [
+    {
+        'param_name': 'contact_rate',
+        'distribution': 'uniform',
+        'distri_params': [0.010, 0.030]
+    },
+    {
+        'param_name': 'start_time',
+        'distribution': 'uniform',
+        'distri_params': [0., 40.]
+    },
+    # Add extra params for negative binomial likelihood
+    {
+        'param_name': 'infection_deathsXall_dispersion_param',
+        'distribution': 'uniform',
+        'distri_params': [.1, 5.]
+    },
+]
+
+# Death counts:
+death_times = [31, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 128, 129, 131, 133]
+death_values = [1, 3, 3, 4, 5, 6, 4, 12, 8, 14, 15, 12, 19, 24, 19, 19, 25, 28, 22, 24, 24, 20, 19, 18, 18, 18, 16, 17, 13, 16, 11, 17, 14, 11, 8, 5, 13, 2, 6, 10, 7, 8, 6, 4, 13, 4, 3, 5, 12, 2, 2, 3, 1, 4, 6, 2, 4, 4, 1, 6, 2, 1]
+
+TARGET_OUTPUTS = [
+    {
+        "output_key": "infection_deathsXall",
+        "years": death_times,
+        "values": death_values,
+        "loglikelihood_distri": "negative_binomial",
+    }
+]
 
 
 def run_phl_calibration_chain(max_seconds: int, run_id: int):
-    run_calibration_chain(max_seconds, run_id, country, PAR_PRIORS, TARGET_OUTPUTS)
+    # run_calibration_chain(max_seconds, run_id, country, PAR_PRIORS, TARGET_OUTPUTS, mode='grid_based',
+    #                       _grid_info=par_grid, _run_extra_scenarios=False, _multipliers=MULTIPLIERS)
+    run_calibration_chain(max_seconds, run_id, country, PAR_PRIORS, TARGET_OUTPUTS, mode='autumn_mcmc',
+                          _run_extra_scenarios=False)
+
+
+if __name__ == "__main__":
+    run_phl_calibration_chain(15 * 60 * 60, 0)  # first argument only relevant for autumn_mcmc mode (time limit in seconds)

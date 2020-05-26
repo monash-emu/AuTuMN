61ff6311c183d6a8495de1ec1765db566114aa16
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-05-26 17:04:38 +1000

    Fix function collect_map_estimate

--- autumn/calibration/utils.py
+++ autumn/calibration/utils.py
@@ -80,7 +80,8 @@ def collect_map_estimate(calib_dirpath: str):
     """
     mcmc_tables = []
     db_paths = [
-        os.path.join(calib_dirpath, f) for f in os.listdir(calib_dirpath) if f.endswith(".db")
+        os.path.join(calib_dirpath, f) for f in os.listdir(calib_dirpath) if f.endswith(".db") and
+                                                                             not f.startswith("mcmc_percentiles")
     ]
     for db_path in db_paths:
         db = Database(db_path)

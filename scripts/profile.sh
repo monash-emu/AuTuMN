990eb9388c32262f0cdd69456a6c521461c174b9
Matthew Segal <mattdsegal@gmail.com>
2020-06-15 14:19:47 +1000

    Add profiling script

---
+++ scripts/profile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Runs a script called profile.secret.py in the project root and profiles performance using cProfile
+# See this blog for more details https://julien.danjou.info/guide-to-python-profiling-cprofile-concrete-case-carbonara/
+# Requires KCachegrind https://kcachegrind.github.io/html/Home.html
+# python -m cProfile -o data/profile-longer.cprof profile.secret.py
+pyprof2calltree -k -i data/profile.cprof

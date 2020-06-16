66e8ed795a1941a55b932d79cadd9b9c516e3d8a
Emma McBryde <jc343421@ad.jcu.edu.au>
2020-06-16 13:24:49 +1000

    added self help

--- apps/run_pycharm.py
+++ apps/run_pycharm.py
@@ -6,7 +6,7 @@ from apps import covid_19, marshall_islands, mongolia, sir_example
 
 # from autumn.plots.database_plots import plot_from_database
 
-RUN_NAME = "manual-calibration"
+RUN_NAME = "manual-calibration-victoria"
 RUN_DESCRIPTION = "trying to x and y the z"
 
 # marshall_islands.run_model()
@@ -14,8 +14,8 @@ RUN_DESCRIPTION = "trying to x and y the z"
 # covid_19.mys.run_model(RUN_NAME)
 # covid_19.lbr.run_model(RUN_NAME)
 # covid_19.aus.run_model(RUN_NAME, RUN_DESCRIPTION)
-# covid_19.vic.run_model(RUN_NAME, RUN_DESCRIPTION)
-covid_19.man.run_model(RUN_NAME)
+covid_19.vic.run_model(RUN_NAME, RUN_DESCRIPTION)
+#covid_19.man.run_model(RUN_NAME)
 
 # Simple SIR model for demonstration
 # sir_example.aus.run_model(RUN_NAME, RUN_DESCRIPTION)

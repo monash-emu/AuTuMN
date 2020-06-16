1dbce7dfba8624f54cb8f884158d39c1171d8e3f
milinda-abay <43531318+milinda-abay@users.noreply.github.com>
2020-06-16 11:51:37 +1000

    Updated inputs.db and some of the scenario files

--- autumn/demography/social_mixing.py
+++ autumn/demography/social_mixing.py
@@ -20,7 +20,10 @@ def load_specific_prem_sheet(mixing_location, country):
     if country == "victoria":
         country = "australia"
     elif country == "manila":
-        country == "philippines"
+        country = "philippines"
+    elif country == "calabarzon":
+        country = "philippines"
+        
 
     # Files with name ending with _1 have a header, but not those ending with _2 - plus need to determine file to read
     sheet_number, header_argument = (

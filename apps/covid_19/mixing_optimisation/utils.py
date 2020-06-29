187ddc088102a8dddacd25fbb29fea681d592a08
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-06-29 15:56:45 +1000

    Create function to compute percentiles of time-variant input that's MCMC estimated

--- apps/covid_19/mixing_optimisation/utils.py
+++ apps/covid_19/mixing_optimisation/utils.py
@@ -4,7 +4,7 @@ from autumn.plots.plots import _overwrite_non_accepted_mcmc_runs
 import pandas as pd
 import os
 from matplotlib import pyplot as plt
-from autumn.curve import scale_up_function
+from autumn.curve import scale_up_function, tanh_based_scaleup
 import numpy as np
 import copy
 
@@ -140,7 +140,7 @@ def get_target_outputs_for_opti(country, data_start_time=22, update_jh_data=Fals
     return target_outputs
 
 
-def extract_n_mcmc_samples(calibration_output_path, n_samples=100, burn_in=500):
+def combine_and_burn_samples(calibration_output_path, burn_in=500):
     mcmc_tables = load_mcmc_tables(calibration_output_path)
     col_names = mcmc_tables[0].columns
 
@@ -150,10 +150,12 @@ def extract_n_mcmc_samples(calibration_output_path, n_samples=100, burn_in=500):
     for i, mcmc_table in enumerate(mcmc_tables):
         mcmc_tables[i] = mcmc_table.iloc[burn_in:]
 
-    combined_burned_samples = pd.concat(mcmc_tables)
+    return pd.concat(mcmc_tables)
+
+def extract_n_mcmc_samples(calibration_output_path, n_samples=100, burn_in=500):
+    combined_burned_samples = combine_and_burn_samples(calibration_output_path, burn_in)
     nb_rows = combined_burned_samples.shape[0]
     thining_jump = int(nb_rows / n_samples)
-
     selected_indices = [i * thining_jump for i in range(n_samples)]
 
     thined_samples = combined_burned_samples.iloc[selected_indices, ]
@@ -178,7 +180,7 @@ def plot_mixing_params_over_time(mixing_params, npi_effectiveness_range):
 
     titles = {'home': 'Household', 'work': 'Workplace', 'school': 'School', 'other_locations': 'Other locations'}
     y_labs = {'home': 'h', 'work': 'w', 'school': 's', 'other_locations': 'l'}
-    date_ticks = {61: '1/3', 76: '16/3', 92: '1/4', 107: '16/4', 122: '1/5', 137: '16/5', 152: '1/6'}
+    date_ticks = {32: '1/2', 47: '16/2', 61: '1/3', 76: '16/3', 92: '1/4', 107: '16/4', 122: '1/5', 137: '16/5', 152: '1/6'}
     # use italics for y_labs
     for key in y_labs:
         y_labs[key] = '$\it{' + y_labs[key] + '}$(t)'
@@ -206,7 +208,7 @@ def plot_mixing_params_over_time(mixing_params, npi_effectiveness_range):
             plt.plot(x, _y, color='navy')
 
         plt.fill_between(x, y[0], y[1], color='cornflowerblue')
-        plt.xlim((60., 152.))
+        plt.xlim((30., 152.))
         plt.ylim((0, 1.1))
 
         plt.xticks(list(date_ticks.keys()), list(date_ticks.values()))
@@ -239,7 +241,31 @@ def apply_npi_effectiveness(mixing_params, npi_effectiveness):
     return mixing_params
 
 
+def get_posterior_percentiles_time_variant_profile(calibration_path, function='detection', burn_in=0):
+    """
+    :param calibration_path: string
+    :param function: only 'detection' for now
+    :param burn_in: integer
+    """
+    combined_burned_samples = combine_and_burn_samples(calibration_path, burn_in)
+    calculated_times = range(200)
+    store_matrix = np.zeros((len(calculated_times), combined_burned_samples.shape[0]))
+    if function == 'detection':
+        i = 0
+        for index, row in combined_burned_samples.iterrows():
+            my_func = tanh_based_scaleup(row['tv_detection_b'], row['tv_detection_c'], 0.)
+            detect_vals = [row['prop_detected_among_symptomatic'] * my_func(t) for t in calculated_times]
+            store_matrix[:, i] = detect_vals
+            i += 1
+    perc = np.percentile(store_matrix, [2.5, 25, 50, 75, 97.5], axis=1)
+    calculated_times = np.array([calculated_times])
+    perc = np.concatenate((calculated_times, perc))
+    np.savetxt(function + ".csv", perc, delimiter=',')
+
+
 # prepare_table_of_param_sets("../../../data/covid_united-kingdom/calibration-covid_united-kingdom-c4c45836-20-06-2020")
+
+# get_posterior_percentiles_time_variant_profile("../../../data/covid_united-kingdom/calibration-covid_united-kingdom-c4c45836-20-06-2020", 'detection', 1000)
 
 # mixing_pars = {'other_locations_times': [46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171], 'other_locations_values': [0.896666666666667, 0.933333333333333, 1.03, 1.01, 1, 0.993333333333334, 1.00333333333333, 0.973333333333333, 1, 0.98, 1.01333333333333, 1, 1.01, 1.00666666666667, 1, 1.04666666666667, 1.02666666666667, 1.01333333333333, 1.01, 0.993333333333333, 1.04, 1.00333333333333, 1.02666666666667, 1.00333333333333, 0.996666666666667, 0.996666666666667, 1.00666666666667, 1, 0.94, 0.933333333333333, 1.00333333333333, 0.93, 0.89, 0.886666666666667, 0.806666666666667, 0.683333333333333, 0.613333333333333, 0.646666666666667, 0.47, 0.443333333333333, 0.426666666666667, 0.413333333333333, 0.346666666666667, 0.313333333333333, 0.403333333333333, 0.413333333333333, 0.4, 0.4, 0.406666666666667, 0.37, 0.356666666666667, 0.413333333333333, 0.43, 0.433333333333333, 0.463333333333333, 0.38, 0.39, 0.256666666666667, 0.343333333333333, 0.446666666666667, 0.44, 0.433333333333333, 0.413333333333333, 0.37, 0.38, 0.436666666666667, 0.443333333333333, 0.443333333333333, 0.446666666666667, 0.45, 0.416666666666667, 0.403333333333333, 0.453333333333333, 0.406666666666667, 0.426666666666667, 0.43, 0.44, 0.43, 0.4, 0.466666666666667, 0.47, 0.49, 0.52, 0.43, 0.436666666666667, 0.383333333333333, 0.48, 0.49, 0.49, 0.503333333333333, 0.5, 0.476666666666667, 0.473333333333333, 0.526666666666667, 0.536666666666667, 0.546666666666667, 0.54, 0.523333333333333, 0.486666666666667, 0.506666666666667, 0.51, 0.57, 0.563333333333333, 0.576666666666667, 0.576666666666667, 0.553333333333333, 0.563333333333333, 0.6, 0.583333333333333, 0.54, 0.553333333333333, 0.546666666666667, 0.503333333333333, 0.53, 0.576666666666667, 0.573333333333333, 0.54, 0.553333333333333, 0.55, 0.563333333333333, 0.57, 0.633333333333333, 0.62, 0.606666666666667, 0.583333333333333, 0.613333333333333], 'work_times': [46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164], 'work_values': [0.96, 0.97, 0.86, 0.86, 0.86, 0.86, 0.85, 0.98, 0.99, 0.98, 1.01, 1.01, 1, 0.99, 1, 1.01, 1.01, 1.01, 1.01, 1, 1.01, 1.01, 1.01, 1, 1, 1, 0.99, 0.96, 0.98, 0.96, 0.91, 0.84, 0.77, 0.73, 0.71, 0.75, 0.7, 0.55, 0.42, 0.36, 0.34, 0.34, 0.43, 0.45, 0.31, 0.3, 0.31, 0.3, 0.31, 0.43, 0.46, 0.3, 0.3, 0.3, 0.3, 0.2, 0.43, 0.42, 0.17, 0.3, 0.31, 0.31, 0.32, 0.46, 0.51, 0.33, 0.33, 0.34, 0.34, 0.35, 0.49, 0.52, 0.35, 0.34, 0.34, 0.34, 0.36, 0.51, 0.54, 0.36, 0.36, 0.37, 0.37, 0.23, 0.52, 0.55, 0.38, 0.38, 0.39, 0.39, 0.4, 0.57, 0.61, 0.41, 0.41, 0.41, 0.41, 0.42, 0.59, 0.64, 0.23, 0.41, 0.42, 0.42, 0.44, 0.69, 0.74, 0.47, 0.46, 0.46, 0.46, 0.47, 0.67, 0.72, 0.48, 0.48, 0.48, 0.48, 0.49], 'school_times': [78, 80], 'school_values': [1.0, 0.1]}
 # npi_effectiveness_range = {

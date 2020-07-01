f51b213717df9f48e814142b2c74a0d0b3779172
Matthew Segal <mattdsegal@gmail.com>
2020-07-01 12:47:53 +1000

    Remove Liberia from calibration list

--- apps/covid_19/calibration/__init__.py
+++ apps/covid_19/calibration/__init__.py
@@ -16,7 +16,8 @@ CALIBRATIONS = {
     Region.BICOL: bicol.run_calibration_chain,
     Region.CALABARZON: calabarzon.run_calibration_chain,
     Region.CENTRAL_VISAYAS: central_visayas.run_calibration_chain,
-    Region.LIBERIA: liberia.run_calibration_chain,
+    # No Google Mobility data for Liberia
+    # Region.LIBERIA: liberia.run_calibration_chain,
     Region.MALAYSIA: malaysia.run_calibration_chain,
     Region.MANILA: manila.run_calibration_chain,
     Region.PHILIPPINES: philippines.run_calibration_chain,

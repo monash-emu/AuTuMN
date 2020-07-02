a98189c8f730a386497e8b0c8c4287a102f6280c
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-07-02 17:10:51 +1000

    Refactor optimisation code

--- apps/covid_19/mixing_optimisation/mixing_opti.py
+++ apps/covid_19/mixing_optimisation/mixing_opti.py
@@ -8,7 +8,7 @@ from autumn.constants import Region
 import autumn.post_processing as post_proc
 from autumn.tool_kit.scenarios import Scenario
 from autumn.tool_kit.params import update_params
-from datetime import date
+from datetime import date, timedelta
 
 from apps.covid_19 import RegionApp
 
@@ -22,7 +22,7 @@ with open(OPTI_PARAMS_PATH, "r") as yaml_file:
 available_countries = [Region.UNITED_KINGDOM]
 
 
-def objective_function(decision_variables, mode="by_age", country=Region.UNITED_KINGDOM, config=0,
+def objective_function(decision_variables, root_model, mode="by_age", country=Region.UNITED_KINGDOM, config=0,
                        calibrated_params={}):
     """
     :param decision_variables: dictionary containing
@@ -41,25 +41,28 @@ def objective_function(decision_variables, mode="by_age", country=Region.UNITED_
     params["default"].update(opti_params["default"])
 
     # update params with calibrated parameters
-    for par in calibrated_params:
-        params["default"] = update_params(params['default'], calibrated_params)
+    params["default"] = update_params(params['default'], calibrated_params)
 
     # update params with specific config (Sensitivity analyses)
     params["default"].update(opti_params["configurations"][config])
 
-    # Define the two scenarios:
-    #   baseline: using the decision variables
-    #   scenario 1: after intervention to test immunity
+    # create parameters for scenario 1 which includes Phases 2 and 3
+    ref_date = date(2019, 12, 31)
+    phase_2_end = [366, 274, 366 + 181]  # depending on the config (0: 6 months, 1: 3 months, 2: 1 year)
+    phase_2_end_date = ref_date + timedelta(days=phase_2_end[config])
+    phase_3_first_day = phase_2_end_date + timedelta(days=1)
+    sc_1_params = {}
     if mode == "by_age":
         age_mixing_update = {}
         for age_group in range(16):
             age_mixing_update["age_" + str(age_group)] = {
-                'times': [date(2020, 6, 29), date(2020, 7, 1)],
-                'values': [1.0, decision_variables[age_group]]
+                'times': [date(2020, 6, 30), date(2020, 7, 1), phase_2_end_date, phase_3_first_day],
+                'values': [1.0, decision_variables[age_group], decision_variables[age_group], 1.0]
             }
-        params["default"]["mixing_age_adjust"].update(age_mixing_update)
+        sc_1_params["mixing_age_adjust"] = age_mixing_update
 
     # set location-specific mixing back to pre-COVID rates on 1st of July or use the opti decision variable
+    sc_1_params['mixing'] = {}
     for loc in ["other_locations", "school", "work"]:
         if mode == "by_age":  # just return mixing to pre-COVID
             new_mixing_adjustment = 1.0
@@ -68,44 +71,70 @@ def objective_function(decision_variables, mode="by_age", country=Region.UNITED_
         else:
             raise ValueError("The requested mode is not supported")
 
-        if loc == 'school':
-            last_value = params["default"]["mixing"][loc]['values'][-1]
-            params["default"]["mixing"][loc]['times'] += [date(2020, 6, 29), date(2020, 7, 1)]
-            params["default"]["mixing"][loc]['values'] += [last_value, 1.]
-        else:
-            XXX = .6  # FIXME last value of mixing adjustment
-            params["default"]["mixing"][loc] = {
-                'times': [date(2020, 6, 29), date(2020, 7, 1)],
-                'values': [XXX, new_mixing_adjustment],
-                'append': True
-            }
+        sc_1_params['mixing'][loc] = {
+                'times': [date(2020, 6, 30), phase_2_end_date, phase_3_first_day],
+                'values': [new_mixing_adjustment, new_mixing_adjustment, 1.],
+                'append': False
+        }
 
-    # Add a scenario without any mixing multipliers
-    end_time = params["default"]["end_time"]
-    params["scenario_start_time"] = end_time - 1
-    params["scenarios"][1] = {"end_time": end_time + 50, "mixing": {}, "mixing_age_adjust": {}}
-    scenario_0 = Scenario(build_model, idx=0, params=params)
+    sc_1_params['data'] = {
+        'times_imported_cases': [phase_2_end[config], phase_2_end[config] + 1, phase_2_end[config] + 2,
+                                 phase_2_end[config] + 3],
+        'n_imported_cases': [0, 80, 80, 0]
+    }
+    sc_1_params['end_time'] = phase_2_end[config] + 365
+    params["scenarios"][1] = sc_1_params
+
     scenario_1 = Scenario(build_model, idx=1, params=params)
-    scenario_0.run()
-    scenario_1.run(base_model=scenario_0.model)
-    models = [scenario_0.model, scenario_1.model]
 
-    # Has herd immunity been reached?
-    herd_immunity = has_immunity_been_reached(models[1])
+    scenario_1.run(base_model=root_model)
+    models = [root_model, scenario_1.model]
 
-    # How many deaths after 1 July 2020
-    first_july_index = models[0].derived_outputs["times"].index(183)
-    total_nb_deaths = sum(models[0].derived_outputs["infection_deathsXall"][first_july_index:])
+    # How many deaths during Phase 2
+    first_july_index = models[1].derived_outputs["times"].index(183)
+    end_phase2_index = models[1].derived_outputs["times"].index(phase_2_end[config])
+    total_nb_deaths = sum(models[1].derived_outputs["infection_deathsXall"][first_july_index: end_phase2_index + 1])
+
+    # What proportion immune at end of Phase 2
     recovered_indices = [
         i
-        for i in range(len(models[0].compartment_names))
-        if "recovered" in models[0].compartment_names[i]
+        for i in range(len(models[1].compartment_names))
+        if "recovered" in models[1].compartment_names[i]
     ]
-    nb_reco = sum([models[0].outputs[-1, i] for i in recovered_indices])
-    total_pop = sum(models[0].compartment_values)
+    nb_reco = sum([models[1].outputs[end_phase2_index, i] for i in recovered_indices])
+    total_pop = sum([models[1].outputs[end_phase2_index, i] for i in range(len(models[1].compartment_names))])
     prop_immune = nb_reco / total_pop
 
+    # Has herd immunity been reached?
+    herd_immunity = has_immunity_been_reached(models[1], end_phase2_index)   # FIXME change this
+
     return herd_immunity, total_nb_deaths, prop_immune, models
+
+
+def run_root_model(country=Region.UNITED_KINGDOM, calibrated_params={}):
+    """
+    This function runs a model to simulate the past epidemic (up until 1/7/2020) using a given calibrated parameter set.
+    Returns an integrated model for the past epidemic.
+    """
+    running_model = RegionApp(country)
+    build_model = running_model.build_model
+
+    params = copy.deepcopy(running_model.params)
+    # update params with optimisation default config
+    params["default"].update(opti_params["default"])
+    # update params with calibrated parameters
+    params["default"] = update_params(params['default'], calibrated_params)
+
+    # prepare importation rates for herd immunity testing
+    params["default"]["data"] = {
+        'times_imported_cases': [0],
+        'n_imported_cases': [0]
+    }
+
+    scenario_0 = Scenario(build_model, idx=0, params=params)
+    scenario_0.run()
+
+    return scenario_0.model
 
 
 def visualise_simulation(_models):
@@ -127,13 +156,16 @@ def visualise_simulation(_models):
     # old_outputs_plotter.plot_requested_outputs()
 
 
-def has_immunity_been_reached(_model):
+def has_immunity_been_reached(_model, phase_2_end_index):
     """
     Determine whether herd immunity has been reached after running a model
-    :param _model: a model run with no-intervention setting for testing herd-immunity
+    :param _model: a model run with Phase 2 and
     :return: a boolean
     """
-    return max(_model.derived_outputs["incidence"]) == _model.derived_outputs["incidence"][0]
+    # validate herd immunity if disease incidence always decreases after 1 week in phase 3
+    future_incidence_list = _model.derived_outputs["incidence"][phase_2_end_index + 7:]
+    print(future_incidence_list)
+    return max(future_incidence_list) == future_incidence_list[0]
 
 
 def read_list_of_param_sets_from_csv(country, config):
@@ -168,7 +200,7 @@ if __name__ == "__main__":
     # looping through all countries and optimisation modes for testing purpose
     # optimisation will have to be performed separately for the different countries and modes.
     decision_vars = {
-        "by_age": [1.] * 16,
+        "by_age": [.0] * 16, # [0.30210397, 0.455783819,	0.250627758,	0.903096598,	0.075936739,	0.24088156,	0.002722042,	0.129826402,	0.131136458,	0.119729594,	0.000211481,	0.003760947,	0.103899082,	0.137976494,	0.057792135,	0.072422987]
         "by_location": {"other_locations": 1.0, "school": 1.0, "work": 1.0},
     }
 
@@ -177,9 +209,12 @@ if __name__ == "__main__":
             for config in [0]:  # opti_params["configurations"]:
                 param_set_list = read_list_of_param_sets_from_csv(country, config)
                 for param_set in param_set_list:
-                    h, d, p_immune, m = objective_function(decision_vars[mode], mode, country, config, param_set)
+                    # Run this line of code every time we use a new param_set and before performing optimisation
+                    # This is an initialisation step
+                    root_model = run_root_model(country, param_set)
+
+                    # The following line is the one to be run again and again during optimisation
+                    h, d, p_immune, m = objective_function(decision_vars[mode], root_model, mode, country, config,
+                                                           param_set)
                     print("Immunity: " + str(h) + "\n" + "Deaths: " + str(round(d)) + "\n" + "Prop immune: " +
                           str(round(p_immune, 3)))
-
-
-

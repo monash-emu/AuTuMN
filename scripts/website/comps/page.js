2893c6121cb297aeca9baab5a082a61f8bc2c587
Matthew Segal <mattdsegal@gmail.com>
2020-07-15 10:35:08 +1000

    Tweak autumn website

--- scripts/website/comps/page.js
+++ scripts/website/comps/page.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { Menu, Container } from 'semantic-ui-react'
+import styled from 'styled-components'
 
 export const Page = ({ title, children }) => (
   <>
@@ -10,12 +11,20 @@ export const Page = ({ title, children }) => (
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
       />
+      <link rel="shortcut icon" href="/favicon.ico" />
     </Head>
     <Menu inverted>
       <Link href="/">
-        <Menu.Item>Autumn Calibrations</Menu.Item>
+        <Menu.Item>
+          <TitleImage src="/maple-leaf.png" />
+          Autumn Data
+        </Menu.Item>
       </Link>
     </Menu>
     <Container>{children}</Container>
   </>
 )
+
+const TitleImage = styled.img`
+  transform: translate(-3px, -3px);
+`

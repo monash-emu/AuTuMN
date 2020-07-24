22b549942ab3bc1adc5808fbe9809f506fe78885
James Trauer <james.trauer@monash.edu>
2020-07-24 14:05:27 +1000

    Simplify importation adjustment for case detection code

--- apps/covid_19/preprocess/importation.py
+++ apps/covid_19/preprocess/importation.py
@@ -1,4 +1,4 @@
-from typing import List, Callable
+from typing import List
 from autumn.curve import scale_up_function
 
 
@@ -10,16 +10,20 @@ def get_importation_rate_func_as_birth_rates(
 ):
     """
     When imported cases are explicitly simulated as part of the modelled population. They enter the late_infectious
-    compartment through a birth process
+    compartment through a birth process.
     """
-    # inflate importation numbers to account for undetected cases (assumed to be asymptomatic or sympt non hospital)
-    for i, time in enumerate(importation_times):
-        importation_n_cases[i] /= detect_prop_func(time)
-    # scale-up curve for importation numbers
+
+    # Inflate importation numbers to account for undetected cases (assumed to be asymptomatic or sympt non hospital)
+    actual_imported_cases = \
+        [importation_n_cases[i_time] / detect_prop_func(time) for
+         i_time, time in enumerate(importation_times)]
+
+    # Scale-up curve for importation numbers
     importation_numbers_scale_up = scale_up_function(
-        importation_times, importation_n_cases, method=4, smoothness=5.0, bound_low=0.0
+        importation_times, actual_imported_cases, method=4, smoothness=5.0, bound_low=0.0
     )
 
+    # Divide through by total population to create per capita rate and return
     def recruitment_rate(t):
         return importation_numbers_scale_up(t) / sum(starting_pops)
 

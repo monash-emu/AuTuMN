9a4ef9e716db70f21042ffc73c9dc55ae4b07694
James Trauer <james.trauer@monash.edu>
2020-07-26 13:48:33 +1000

    Whoops! Correction to last commit

--- apps/covid_19/validate.py
+++ apps/covid_19/validate.py
@@ -73,7 +73,6 @@ validate_params = sb.build_validator(
     symptomatic_props_imported=float,
     hospital_props_imported=float,
     icu_prop_imported=float,
-    enforced_isolation_effect=float,
     data=sb.Dict(times_imported_cases=sb.List(float), n_imported_cases=sb.List(float),),
     microdistancing=sb.Nullable(sb.Dict(function_type=str, parameters=dict)),
     # Other stuff

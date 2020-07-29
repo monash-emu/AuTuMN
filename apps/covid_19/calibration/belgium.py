a83adb257aac3754c8c1c565e4d731d2d8d9f894
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-07-29 13:34:25 +1000

    Push hospital targets France

--- apps/covid_19/calibration/belgium.py
+++ apps/covid_19/calibration/belgium.py
@@ -18,13 +18,7 @@ for i, par in enumerate(PAR_PRIORS):
         PAR_PRIORS[i]["distri_params"] = [0.02, 0.04]
         PAR_PRIORS[i]["start_time"] = [0., 20.]
 
-
-
 TARGET_OUTPUTS = get_target_outputs_for_opti(country, source='who', data_start_time=61, data_end_time=197)  # notifications and deaths
-
-hospital_outputs = get_hospital_targets_for_opti(country, data_start_time=61, data_end_time=197)
-
-TARGET_OUTPUTS += hospital_outputs
 
 # Use weekly counts
 for target in TARGET_OUTPUTS:
--- apps/covid_19/mixing_optimisation/utils.py
+++ apps/covid_19/mixing_optimisation/utils.py
@@ -162,14 +162,20 @@ def get_target_outputs_for_opti(country, data_start_time=22, data_end_time=152, 
             }
         )
 
+    hospital_targets = get_hospital_targets_for_opti(country, data_start_time, data_end_time)
+    target_outputs += hospital_targets
     return target_outputs
 
 
 def get_hospital_targets_for_opti(country, data_start_time=22, data_end_time=152):
 
     output = {
-        'belgium': 'hospital_occupancy'
+        'belgium': 'hospital_occupancy',
+        'france': 'hospital_occupancy',
     }
+    if country not in output:
+        print("hospital output not specifoed for " + country)
+        return []
     output_mapping = {"hospital_occupancy": "hospital_occupancy", "icu_occupancy": "icu_occupancy"}
     times, data = read_hospital_data_from_csv(output[country], country, data_start_time, data_end_time)
 

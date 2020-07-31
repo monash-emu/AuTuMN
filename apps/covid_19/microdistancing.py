ace128a9e48910a2d851c126b06d974b3d133be2
milinda-abay <43531318+milinda-abay@users.noreply.github.com>
2020-07-31 11:55:17 +1000

    Micro-distancing data and pandas data frame for Australia.

---
+++ apps/covid_19/microdistancing.py
@@ -0,0 +1,19 @@
+"""
+Extracts Nick Golding's survey CSV files from data\inputs\micro-distancing and
+transforms it into a pandas data frame in preparation for loading
+"""
+
+import os
+import pandas as pd
+from autumn.constants import DATA_PATH
+
+DATA_DIR = os.path.join(DATA_PATH, "inputs","micro-distancing")
+
+file_list = os.listdir(DATA_DIR)
+file_list = [os.path.join(DATA_DIR, each) for each in file_list ]
+
+list_of_df = [pd.read_csv(each) for each in file_list]
+df = pd.concat(list_of_df, axis=0, ignore_index =True)
+
+df = df[((df.state=='VIC') | (df.state=='Victoria')) & (df.question=='1.5m compliance') & (df.response=='Always')]
+df['micro-distancing'] = 1-(df['count'] / df['respondents'])
---
+++ data/inputs/micro-distancing/Readme.txt
@@ -0,0 +1,6 @@
+Nick Golding survey data taken from 
+raw data files: https://www.dropbox.com/sh/na4dh79hec4q9j8/AACIP6QLEVFdfmruQZtDSCCWa?dl=0
+
+also for reference
+ - fitted trends: https://www.dropbox.com/s/rrtqzc5rmmnjftf/microdistancing_trends.RDS?dl=0
+ - summary table with just the peaks and latest values: https://www.dropbox.com/s/68a7lijxvq5iu75/microdistancing_trend_summary.RDS?dl=0

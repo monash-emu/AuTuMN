f35b7b5d055349ded1c896a02d7137abe020f3a8
James Trauer <james.trauer@monash.edu>
2020-08-07 10:32:03 +1000

    More tweaks to Victoria calibration

--- apps/covid_19/calibration/victoria.py
+++ apps/covid_19/calibration/victoria.py
@@ -1,7 +1,7 @@
 from autumn.constants import Region
 from apps.covid_19.calibration import base
 from apps.covid_19.calibration.base import provide_default_calibration_params, add_standard_dispersion_parameter
-
+from apps.covid_19.mixing_optimisation.utils import add_dispersion_param_prior_for_gaussian
 
 def run_calibration_chain(max_seconds: int, run_id: int, num_chains: int):
     base.run_calibration_chain(
@@ -658,21 +658,21 @@ TARGET_OUTPUTS = [
         "output_key": "notifications",
         "years": case_times,
         "values": case_counts,
-        "loglikelihood_distri": "negative_binomial",
+        "loglikelihood_distri": "normal",
         "time_weights": list(range(1, len(case_times) - 6)) + [250.] * 7,
     },
     {
         "output_key": "icu_occupancy",
         "years": icu_times,
         "values": icu_counts,
-        "loglikelihood_distri": "negative_binomial",
+        "loglikelihood_distri": "normal",
         "time_weights": list(range(1, len(icu_times) - 6)) + [250.] * 7,
     },
 ]
 
 PAR_PRIORS = provide_default_calibration_params(["start_time"])
-PAR_PRIORS = add_standard_dispersion_parameter(PAR_PRIORS, TARGET_OUTPUTS, "notifications")
-PAR_PRIORS = add_standard_dispersion_parameter(PAR_PRIORS, TARGET_OUTPUTS, "icu_occupancy")
+# PAR_PRIORS = add_standard_dispersion_parameter(PAR_PRIORS, TARGET_OUTPUTS, "notifications")
+# PAR_PRIORS = add_standard_dispersion_parameter(PAR_PRIORS, TARGET_OUTPUTS, "icu_occupancy")
 
 PAR_PRIORS += [
     # Programmatic parameters
@@ -681,12 +681,12 @@ PAR_PRIORS += [
         "distribution": "uniform",
         "distri_params": [0., 0.4],
     },
-    {
-        "param_name": "time_variant_detection.end_value",
-        "distribution": "beta",
-        "distri_mean": 0.85,
-        "distri_ci": [0.6, 0.9],
-    },
+    # {
+    #     "param_name": "time_variant_detection.end_value",
+    #     "distribution": "beta",
+    #     "distri_mean": 0.85,
+    #     "distri_ci": [0.6, 0.9],
+    # },
     {
         "param_name": "testing_to_detection.maximum_detection",
         "distribution": "uniform",
@@ -719,3 +719,5 @@ PAR_PRIORS += [
         "distri_params": [-6, -3]
     }
 ]
+
+PAR_PRIORS = add_dispersion_param_prior_for_gaussian(PAR_PRIORS, TARGET_OUTPUTS, {})

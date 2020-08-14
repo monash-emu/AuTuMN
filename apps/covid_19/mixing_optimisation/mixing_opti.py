4a2d3d40067b0c5bfac994b497f15f086a63e000
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-08-15 09:57:11 +1000

    Creat sensitivity analysis varying minimium mixing

--- apps/covid_19/mixing_optimisation/mixing_opti.py
+++ apps/covid_19/mixing_optimisation/mixing_opti.py
@@ -478,6 +478,33 @@ def run_sensitivity_perturbations(output_dir, country, config=2, mode="by_age", 
         yaml.dump(delta_contributions, f)
 
 
+def run_sensitivity_minimum_mixing(output_dir):
+    mode = 'by_age'
+    results = {}
+    for country in OPTI_REGIONS:
+        results[country] = {}
+        for config in [2, 3]:
+            results[country][config] = {}
+            for objective in ['deaths', 'yoll']:
+                results[country][config][objective] = {}
+                mle_params, decision_vars = get_mle_params_and_vars(output_dir, country, config, mode, objective)
+                if config == 2 and objective == 'deaths':  # to run the root_model only once
+                    print("running root model for " + country)
+                    root_model = run_root_model(country, mle_params)
+                for min_mixing in [0., .10, .20, .30, .40, .50]:
+                    modified_vars = copy.deepcopy(decision_vars)
+                    modified_vars = [max([v, min_mixing]) for v in modified_vars]
+                    print("evaluate objective for " + country + " " + str(config) + " " + objective)
+                    h, d, yoll, p_immune, _ = objective_function(modified_vars, root_model, mode, country,
+                                                                 config, mle_params)
+                    results[country][config][objective][min_mixing] = [h, d, yoll, p_immune]
+
+    param_file_path = "optimisation_outputs/sensitivity_min_mixing/results.yml"
+    with open(param_file_path, "w") as f:
+        yaml.dump(results, f)
+    return results
+
+
 def explore_optimal_plan(decision_vars, _root_model, mode, _country, config, param_set, delta=.1):
     n_vars = {'by_age': 16, "by_location": 3}
     h, d, yoll, p_immune, m = objective_function(decision_vars, _root_model, mode, _country, config,
@@ -531,7 +558,8 @@ if __name__ == "__main__":
     # looping through all countries and optimisation modes for testing purpose
     # optimisation will have to be performed separately for the different countries and modes.
     output_dir = "optimisation_outputs/6Aug2020/"
-
+    run_sensitivity_minimum_mixing(output_dir)
+    exit()
     # write_all_yml_files_from_outputs(output_dir, mode="by_age")
     write_all_yml_files_for_immunity_scenarios(output_dir)
     exit()

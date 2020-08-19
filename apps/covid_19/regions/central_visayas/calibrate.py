6fff1c8af1b91dd8ed6f1a2fb6b8121beaeb3cb1
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2020-08-19 14:00:20 +1000

    tweak central visayas calibration

--- apps/covid_19/regions/central_visayas/calibrate.py
+++ apps/covid_19/regions/central_visayas/calibrate.py
@@ -6,12 +6,13 @@ from apps.covid_19.calibration import (
     add_standard_philippines_params,
     assign_trailing_weights_to_halves,
 )
+from autumn.calibration.utils import add_dispersion_param_prior_for_gaussian
 from autumn.tool_kit.params import load_targets
 
 targets = load_targets("covid_19", Region.CENTRAL_VISAYAS)
 notifications = targets["notifications"]
-#icu_occupancy = targets["icu_occupancy"]
-
+icu_occupancy = targets["icu_occupancy"]
+total_infection_deaths = targets["total_infection_deaths"]
 
 def run_calibration_chain(max_seconds: int, run_id: int, num_chains: int):
     base.run_calibration_chain(
@@ -33,14 +34,20 @@ TARGET_OUTPUTS = [
         "loglikelihood_distri": "normal",
         "time_weights": assign_trailing_weights_to_halves(14, notifications["times"]),
     },
-#    {
-#        "output_key": "icu_occupancy",
-#        "years": icu_occupancy["times"],
-#        "values": icu_occupancy["values"],
-#        "loglikelihood_distri": "normal",
-#    },
+   {
+       "output_key": "icu_occupancy",
+       "years": icu_occupancy["times"],
+       "values": icu_occupancy["values"],
+       "loglikelihood_distri": "normal",
+   },
+   {
+       "output_key": total_infection_deaths["output_key"],
+       "years": total_infection_deaths["times"],
+       "values": total_infection_deaths["values"],
+       "loglikelihood_distri": "normal",
+   },
 ]
 
 PAR_PRIORS = provide_default_calibration_params()
-#PAR_PRIORS = add_dispersion_param_prior_for_gaussian(PAR_PRIORS, TARGET_OUTPUTS, "notifications")
+PAR_PRIORS = add_dispersion_param_prior_for_gaussian(PAR_PRIORS, TARGET_OUTPUTS)
 PAR_PRIORS = add_standard_philippines_params(PAR_PRIORS)

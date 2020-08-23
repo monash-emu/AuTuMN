e4086c131410d08c937b91e16cbb71fc0407a244
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-08-24 09:48:32 +1000

    Add test for tuberculosis app calibration

--- tests/test_apps/test_calibrations.py
+++ tests/test_apps/test_calibrations.py
@@ -1,6 +1,6 @@
 import pytest
 
-from apps import covid_19
+from apps import covid_19, tuberculosis
 
 
 @pytest.mark.calibrate_models
@@ -11,4 +11,15 @@ def test_covid_calibration(region):
     Calibration smoke test - make sure everything can run for 15 seconds without exploding.
     """
     region_app = covid_19.app.get_region(region)
+    region_app.calibrate_model(10, 1, 1)
+
+
+@pytest.mark.calibrate_models
+@pytest.mark.github_only
+@pytest.mark.parametrize("region", tuberculosis.app.region_names)
+def test_tuberculosis_calibration(region):
+    """
+    Calibration smoke test - make sure everything can run for 15 seconds without exploding.
+    """
+    region_app = tuberculosis.app.get_region(region)
     region_app.calibrate_model(10, 1, 1)

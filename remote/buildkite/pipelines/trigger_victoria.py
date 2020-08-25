2470314a75fb02a8f87cb49fe45ea66b4ff12c77
Matthew Segal <mattdsegal@gmail.com>
2020-08-25 14:24:48 +1000

    Fix bug in Buildkite code

--- remote/buildkite/pipelines/trigger_victoria.py
+++ remote/buildkite/pipelines/trigger_victoria.py
@@ -8,11 +8,12 @@ from remote.buildkite.buildkite import (
 
 from .calibrate import chains_field, branch_field, runtime_field, trigger_field
 
-
-input_step = InputStep(
-    key="calibration-settings",
-    run_condition=None,
-    fields=[chains_field, branch_field, runtime_field, trigger_field,],
-)
+fields = [
+    chains_field,
+    branch_field,
+    runtime_field,
+    trigger_field,
+]
+input_step = InputStep(key="calibration-settings", run_condition=None, fields=fields,)
 trigger_step = CommandStep(key="run-triggers", command="./scripts/buildkite.sh trigger victoria")
 pipeline = Pipeline(key="trigger-victoria", steps=[input_step, trigger_step])

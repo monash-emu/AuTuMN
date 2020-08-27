f130e0fa3d9579e022a5f8598dd0520526436b41
James Trauer <james.trauer@monash.edu>
2020-08-27 13:30:28 +1000

    Incorporate Our World in Data loading into main database loading

--- autumn/inputs/our_world_in_data/preprocess.py
+++ autumn/inputs/our_world_in_data/preprocess.py
@@ -4,10 +4,10 @@ import os
 from autumn import constants
 from autumn.db import Database
 
-OUR_WORLD_IN_DATA_DIRPATH = os.path.join(constants.INPUT_DATA_PATH, "our_world_in_data")
+OUR_WORLD_IN_DATA_DIRPATH = os.path.join(constants.INPUT_DATA_PATH, "our-world-in-data")
 OUR_WORLD_IN_DATA_CSV_PATH = os.path.join(OUR_WORLD_IN_DATA_DIRPATH, "owid-covid-data.csv")
 
 
 def preprocess_our_world_in_data(input_db: Database):
     df = pd.read_csv(OUR_WORLD_IN_DATA_CSV_PATH)
-    input_db.dump_df("our_world_in_data", df)
+    input_db.dump_df("our-world-in-data", df)

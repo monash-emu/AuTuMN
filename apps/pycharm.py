f475a78b8009c3a0c752df9043da8df98879921d
James Trauer <james.trauer@monash.edu>
2020-08-27 14:31:54 +1000

    Update database hash

--- apps/pycharm.py
+++ apps/pycharm.py
@@ -1,8 +1,12 @@
 """
 Entry point for PyCharm users to run an application
 """
+import os
+
 from autumn.constants import Region
 from apps import covid_19, sir_example, tuberculosis
+
+os.chdir('..')  # Make repo root the current directory
 
 # Run a COVID model manually.
 app_region = covid_19.app.get_region(Region.BARWON_SOUTH_WEST)
@@ -10,7 +14,7 @@ app_region.run_model(run_scenarios=True)
 
 # Simple SIR model for demonstration
 # app_region = sir_example.app.get_region(Region.AUSTRALIA)
-# app_region.run_model()
+# app_region.run_model()M
 
 
 # app_region = tuberculosis.app.get_region(Region.PHILIPPINES)

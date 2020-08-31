abcf8f2e44e436ec10d0ea060eb3fd71c31e8cf8
James Trauer <james.trauer@monash.edu>
2020-08-31 15:24:19 +1000

    Remove high number from Malaysia testing

--- autumn/inputs/owid/preprocess.py
+++ autumn/inputs/owid/preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import numpy as np
 
 from autumn import constants
 from autumn.db import Database
@@ -10,4 +11,8 @@ OUR_WORLD_IN_DATA_CSV_PATH = os.path.join(OUR_WORLD_IN_DATA_DIRPATH, "owid-covid
 
 def preprocess_our_world_in_data(input_db: Database):
     df = pd.read_csv(OUR_WORLD_IN_DATA_CSV_PATH)
+    df.loc[(df.iso_code == "MYS") & (df.new_tests > 1e5), "new_tests"] = np.nan
     input_db.dump_df("owid", df)
+
+
+preprocess_our_world_in_data(Database)

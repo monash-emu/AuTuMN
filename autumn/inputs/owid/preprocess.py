434f99e455a4cb54edc26241cc2294c8a94bccc9
James Trauer <james.trauer@monash.edu>
2020-08-31 15:30:05 +1000

    Still not getting it - comment out broken code

--- autumn/inputs/owid/preprocess.py
+++ autumn/inputs/owid/preprocess.py
@@ -11,8 +11,8 @@ OUR_WORLD_IN_DATA_CSV_PATH = os.path.join(OUR_WORLD_IN_DATA_DIRPATH, "owid-covid
 
 def preprocess_our_world_in_data(input_db: Database):
     df = pd.read_csv(OUR_WORLD_IN_DATA_CSV_PATH)
-    df.loc[(df.iso_code == "MYS") & (df.new_tests > 1e5), "new_tests"] = np.nan
+    # df.loc[(df.iso_code == "MYS") & (df.new_tests > 1e5), "new_tests"] = np.nan
     input_db.dump_df("owid", df)
 
 
-preprocess_our_world_in_data(Database)
+# preprocess_our_world_in_data(Database)

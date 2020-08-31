97677b0b461a7bd9c51d5f0327e5c207cce3f950
James Trauer <james.trauer@monash.edu>
2020-08-31 21:16:24 +1000

    ... and add comment

--- autumn/inputs/owid/preprocess.py
+++ autumn/inputs/owid/preprocess.py
@@ -11,5 +11,7 @@ OUR_WORLD_IN_DATA_CSV_PATH = os.path.join(OUR_WORLD_IN_DATA_DIRPATH, "owid-covid
 
 def preprocess_our_world_in_data(input_db: Database):
     df = pd.read_csv(OUR_WORLD_IN_DATA_CSV_PATH)
+
+    # Replace the one strange value for test numbers in Malaysia
     df.loc[(df.iso_code == "MYS") & (df.new_tests > 1e5), "new_tests"] = np.nan
     input_db.dump_df("owid", df)

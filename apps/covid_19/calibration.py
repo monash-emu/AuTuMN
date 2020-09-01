2425d71b6429ea2f9fc5cec5b723c0b36066b31c
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2020-09-01 17:12:05 +1000

    update calibration targets, microdistancing, and cdr

--- apps/covid_19/calibration.py
+++ apps/covid_19/calibration.py
@@ -202,16 +202,16 @@ def add_standard_philippines_targets(targets):
         },
         {
             "output_key": "icu_occupancy",
-            "years": [targets["icu_occupancy"]["times"][-1]],
-            "values": [targets["icu_occupancy"]["values"][-1]],
-            "loglikelihood_distri": "normal",
+            "years": [targets["icu_occupancy"]["times"]],
+            "values": [targets["icu_occupancy"]["values"]],
+            "loglikelihood_distri": "uniform",
 
         },
         {
             "output_key": "accum_deaths",
-            "years": [targets["total_infection_deaths"]["times"][-1]],
-            "values": [float(sum(targets["total_infection_deaths"]["values"]))],
-            "loglikelihood_distri": "normal",
+            "years": [targets["total_infection_deaths"]["times"]],
+            "values": [targets["total_infection_deaths"]["values"]],
+            "loglikelihood_distri": "uniform",
         },
     ]
 

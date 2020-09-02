551e89ad1587469abbdfeb4b82ea1c92561d8347
James Trauer <james.trauer@monash.edu>
2020-09-02 23:02:50 +1000

    Debug mortality over75 (#93)
    * Successful debug, I think
    
    * ... and apply to asymptomatics
    
    * Tidy up last part of code

--- autumn/tool_kit/utils.py
+++ autumn/tool_kit/utils.py
@@ -286,10 +286,12 @@ def convert_list_contents_to_int(input_list):
     return [int(i_element) for i_element in input_list]
 
 
-def element_wise_list_division(numerator, denominator):
+def element_wise_list_division(numerator, denominator, must_be_prop=False):
     """
     Simple function to find the quotients of two lists.
     """
+    if must_be_prop:
+        assert all([num <= den for num, den in zip(numerator, denominator)])
     return [num / den for num, den in zip(numerator, denominator)]
 
 

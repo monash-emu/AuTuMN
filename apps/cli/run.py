58c2646a5b511e4d8e93c53c27920880a8e95708
Matthew Segal <mattdsegal@gmail.com>
2020-09-21 10:27:02 +1000

    Add TB model to apps CLI

--- apps/cli/run.py
+++ apps/cli/run.py
@@ -8,7 +8,7 @@ You can access this script from your CLI by running:
 """
 import click
 
-from apps import covid_19, sir_example
+from apps import covid_19, sir_example, tuberculosis
 
 
 @click.group()
@@ -16,13 +16,22 @@ def run():
     """Run a model"""
 
 
+@run.command("tb")
+@click.argument("region", type=click.Choice(tuberculosis.app.region_names))
+@click.option("--no-scenarios", is_flag=True)
+def run_covid(region, no_scenarios):
+    """Run the tuberculosis model for some region"""
+    app_region = tuberculosis.app.get_region(region)
+    app_region.run_model(run_scenarios=not no_scenarios)
+
+
 @run.command("covid")
 @click.argument("region", type=click.Choice(covid_19.app.region_names))
 @click.option("--no-scenarios", is_flag=True)
 def run_covid(region, no_scenarios):
     """Run the COVID model for some region"""
-    covid_region = covid_19.app.get_region(region)
-    covid_region.run_model(run_scenarios=not no_scenarios)
+    app_region = covid_19.app.get_region(region)
+    app_region.run_model(run_scenarios=not no_scenarios)
 
 
 @run.command("example")
@@ -30,5 +39,5 @@ def run_covid(region, no_scenarios):
 @click.option("--no-scenarios", is_flag=True)
 def run_sir_example(region, no_scenarios):
     """Run the SIR example model for some region"""
-    sir_region = sir_example.app.get_region(region)
-    sir_region.run_model(run_scenarios=not no_scenarios)
+    app_region = sir_example.app.get_region(region)
+    app_region.run_model(run_scenarios=not no_scenarios)

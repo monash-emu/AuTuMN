a207093f0cc916abdc8771bcaed08e59c6fe545c
James Trauer <james.trauer@monash.edu>
2020-09-22 21:55:35 +1000

    Convert main function to factory function

--- apps/covid_19/model/susceptibility_heterogeneity.py
+++ apps/covid_19/model/susceptibility_heterogeneity.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def get_gamma(x: float, coeff_var: float):
+def get_gamma(coeff_var: float):
     """
     Use function described in caption to Extended Data Fig 1 of Aguas et al pre-print to produce gamma distribution
     from coefficient of variation and independent variable.
@@ -17,11 +17,13 @@ def get_gamma(x: float, coeff_var: float):
 
     recip_cv_2 = coeff_var ** -2.
 
-    return \
-        x ** (recip_cv_2 - 1.) * \
-        np.exp(-x * recip_cv_2) / \
-        special.gamma(recip_cv_2) / \
-        coeff_var ** (2. * recip_cv_2)
+    def gamma_func(x: float):
+        return x ** (recip_cv_2 - 1.) * \
+               np.exp(-x * recip_cv_2) / \
+               special.gamma(recip_cv_2) / \
+               coeff_var ** (2. * recip_cv_2)
+
+    return gamma_func
 
 
 def produce_gomes_exfig1():
@@ -33,7 +35,8 @@ def produce_gomes_exfig1():
     gamma_plot = plt.figure()
     axis = gamma_plot.add_subplot(1, 1, 1)
     for coeff in [0.5, 1., 2.]:
+        gamma_func = get_gamma(coeff)
         x_values = np.linspace(0.1, 3., 50)
-        y_values = [get_gamma(i, coeff) for i in x_values]
+        y_values = [gamma_func(i) for i in x_values]
         axis.plot(x_values, y_values)
     return gamma_plot

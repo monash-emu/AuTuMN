05107b060c54a8d1f46581a36faddbbd639c3806
James Trauer <james.trauer@monash.edu>
2020-09-29 20:53:29 +1000

    Working towards first basic plot

--- dash/dashboards/malaysia_paper.py
+++ dash/dashboards/malaysia_paper.py
@@ -16,18 +16,19 @@ BASE_DATE = pd.datetime(2019, 12, 31)
 
 PLOT_FUNCS = {}
 
-
-def plot_acceptance_ratio(
+def plot_posterior(
     plotter: StreamlitPlotter,
     calib_dir_path: str,
     mcmc_tables: List[pd.DataFrame],
     mcmc_params: List[pd.DataFrame],
     targets: dict,
 ):
-    plots.calibration.plots.plot_acceptance_ratio(plotter, mcmc_tables)
+    chosen_param = selectors.parameter(mcmc_params[0])
+    num_bins = st.sidebar.slider("Number of bins", 1, 50, 16)
+    plots.calibration.plots.plot_posterior(plotter, mcmc_params, chosen_param, num_bins)
 
 
-PLOT_FUNCS["Acceptance ratio"] = plot_acceptance_ratio
+PLOT_FUNCS["Posterior"] = plot_posterior
 
 
 def run_dashboard():
@@ -47,8 +48,9 @@ def run_dashboard():
 
     # Plot
     plotter = StreamlitPlotter(targets)
-    st.sidebar.selectbox("Select plot type", list(PLOT_FUNCS.keys()))
-    plot_func = plot_acceptance_ratio
+    # st.sidebar.selectbox("Select plot type", list(PLOT_FUNCS.keys()))
+    plot_type = st.sidebar.selectbox("Select plot type", list(PLOT_FUNCS.keys()))
+    plot_func = PLOT_FUNCS[plot_type]
     plot_func(plotter, calib_dirpath, mcmc_tables, mcmc_params, targets)
 
 

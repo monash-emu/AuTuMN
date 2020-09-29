68a0d8ddc610bf794ac7dedd1774a63349b1dece
James Trauer <james.trauer@monash.edu>
2020-09-29 16:13:01 +1000

    Simplify basic code

--- dash/dashboards/malaysia_paper.py
+++ dash/dashboards/malaysia_paper.py
@@ -27,17 +27,17 @@ def plot_acceptance_ratio(
     plots.calibration.plots.plot_acceptance_ratio(plotter, mcmc_tables)
 
 
+PLOT_FUNCS["Acceptance ratio"] = plot_acceptance_ratio
+
+
 def run_dashboard():
-    app_name, app_dirpath = "covid_19", "C:/Users/jtrauer/AuTuMN/data/outputs/calibrate/covid_19"
-
-    region_name, region_dirpath = selectors.output_region_name(app_dirpath)
-    if not region_name:
-        st.write("No region folder found")
-        return
-
-    calib_name, calib_dirpath = selectors.calibration_run(region_dirpath)
+    app_name, app_dirpath = \
+        "covid_19", "C:/Users/jtrauer/AuTuMN/data/outputs/calibrate/covid_19"
+    region_name = "malaysia"
+    calib_name, calib_dirpath = \
+        selectors.calibration_run("C:/Users/jtrauer/AuTuMN/data/outputs/calibrate/covid_19/malaysia")
     if not calib_name:
-        st.write("No model run folder found")
+        st.write("No model run folder found to contain the data to plot here")
         return
 
     # Load MCMC tables
@@ -45,8 +45,9 @@ def run_dashboard():
     mcmc_params = db.load.load_mcmc_params_tables(calib_dirpath)
     targets = load_targets(app_name, region_name)
 
+    # Plot
     plotter = StreamlitPlotter(targets)
-    plot_type = st.sidebar.selectbox("Select plot type", list(PLOT_FUNCS.keys()))
+    st.sidebar.selectbox("Select plot type", list(PLOT_FUNCS.keys()))
     plot_func = plot_acceptance_ratio
     plot_func(plotter, calib_dirpath, mcmc_tables, mcmc_params, targets)
 

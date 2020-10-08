f4025d8c54638365908dc70f1ed2bdb884874c82
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-10-08 22:56:39 +1100

    Dump WHO cases and deaths data to json files

--- scripts/read_who_covid_data.py
+++ scripts/read_who_covid_data.py
@@ -1,21 +1,23 @@
 import pandas as pd
 import os
 import datetime
+import json
+from apps.covid_19.mixing_optimisation.constants import OPTI_REGIONS
 
 WHO_DATA_FILE = os.path.join("../data", "who_covid", f"WHO-COVID-19-global-data.csv")
-country_mapping = {"united-kingdom": "The United Kingdom"}
+who_country_mapping = {"united-kingdom": "The United Kingdom"}
 
 
 def read_who_data_from_csv(
-    variable="confirmed", country="Australia", data_start_time=0, data_end_time=365
+    variable="confirmed", country="australia", data_start_time=0, data_end_time=365
 ):
     """
     Read WHO data from csv files
     :param variable: one of "confirmed", "deaths"
     :param country: country
     """
-    if country in country_mapping:
-        country_name = country_mapping[country]
+    if country in who_country_mapping:
+        country_name = who_country_mapping[country]
     else:
         country_name = country.title()
     path = WHO_DATA_FILE
@@ -44,3 +46,34 @@ def read_who_data_from_csv(
         values = list(data[data.iloc[:, 2] == country_name].iloc[:, 6])[start_index : end_index + 1]
 
     return times, values
+
+
+def drop_who_data_to_targets(
+    country="australia", data_start_time=0, data_end_time=365
+):
+    data = {}
+    output_name = {
+        "confirmed": "notifications",
+        "deaths": "infection_deaths"
+    }
+    for variable in ["confirmed", "deaths"]:
+        times, values = read_who_data_from_csv(
+            variable, country, data_start_time, data_end_time
+        )
+        data[variable] = {'times': times, 'values': values}
+
+    region = country if country != "united-kingdom" else "united_kingdom"
+    target_path = os.path.join("../apps", "covid_19", "regions", region, "targets.json")
+
+    with open(target_path, mode="r") as f:
+            targets = json.load(f)
+            for variable in ["confirmed", "deaths"]:
+                targets[output_name[variable]]['times'] = data[variable]['times']
+                targets[output_name[variable]]['values'] = data[variable]['values']
+    with open(target_path, "w") as f:
+        json.dump(targets, f, indent=2)
+
+
+if __name__ == "__main__":
+    for country in OPTI_REGIONS:
+        drop_who_data_to_targets(country)

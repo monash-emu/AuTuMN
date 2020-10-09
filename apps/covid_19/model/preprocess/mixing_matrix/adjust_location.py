e16d9c154b3d521017b7e9161744f0adf4e5f85f
James Trauer <james.trauer@monash.edu>
2020-10-09 14:04:35 +1100

    Allow for microdistancing waning

--- apps/covid_19/model/preprocess/mixing_matrix/adjust_location.py
+++ apps/covid_19/model/preprocess/mixing_matrix/adjust_location.py
@@ -180,28 +180,64 @@ def apply_microdistancing(params):
 
     # Collate the components to the microdistancing function
     microdist_component_funcs = []
-    for microdist_type in params:
-        if params[microdist_type].function_type == "tanh":
-            microdist_component_funcs.append(tanh_based_scaleup(**params[microdist_type].parameters.dict()))
-        elif params[microdist_type].function_type == "empiric":
-            micro_times = params[microdist_type].parameters.times
-            multiplier = params[microdist_type].parameters.max_effect
-            micro_vals = [
-                1.0 - multiplier * value for value in params[microdist_type].parameters.values
-            ]
-            microdist_component_funcs.append(scale_up_function(micro_times, micro_vals, method=4))
+    for microdist_type in [param for param in params if "_adjuster" not in params]:
+        adjustment_string = f"{microdist_type}_adjuster"
+        unadjusted_microdist_func = \
+            get_microdist_func_component(params, microdist_type)
+        microdist_component_funcs.append(
+            get_microdist_adjustment(params, adjustment_string, unadjusted_microdist_func)
+        )
 
-    # Generate the overall composite function representing the product of all the components
+    # Generate the overall composite contact adjustment function as the product of the reciprocal all the components
     def microdist_composite_func(time):
         return \
             np.product(
                 [
-                    microdist_component_funcs[i_func](time) for
+                    1. - microdist_component_funcs[i_func](time) for
                     i_func in range(len(microdist_component_funcs))
                 ]
             )
 
     return microdist_composite_func
+
+
+def get_empiric_microdist_func(params, microdist_type):
+    """
+    Construct a microdistancing function according to the "empiric" request format.
+    """
+
+    micro_times = params[microdist_type].parameters.times
+    multiplier = params[microdist_type].parameters.max_effect
+    micro_vals = [
+        multiplier * value for value in params[microdist_type].parameters.values
+    ]
+    return scale_up_function(micro_times, micro_vals, method=4)
+
+
+def get_microdist_adjustment(params, adjustment_string, unadjusted_microdist_func):
+    """
+    Adjust an existing microdistancing function according to another function that modifies this function (if one
+    exists / has been requested).
+    """
+
+    if adjustment_string in params:
+        waning_adjustment = \
+            get_microdist_func_component(params, adjustment_string)
+        return lambda time: unadjusted_microdist_func(time) * waning_adjustment(time)
+    else:
+        return unadjusted_microdist_func
+
+
+def get_microdist_func_component(params, adjustment_string):
+    """
+    Get one function of time using the standard parameter request structure for any microdistancing function or
+    adjustment to a microdistancing function.
+    """
+
+    if params[adjustment_string].function_type == "tanh":
+        return tanh_based_scaleup(**params[adjustment_string].parameters.dict())
+    elif params[adjustment_string].function_type == "empiric":
+        return get_empiric_microdist_func(params, adjustment_string)
 
 
 def parse_values(values):

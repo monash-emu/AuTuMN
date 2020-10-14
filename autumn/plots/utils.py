242c5660437ad57a5f875bc95c758e499dbec851
James Trauer <james.trauer@monash.edu>
2020-10-14 14:59:22 +1100

    Remove unnecessary argument from axis formatting function

--- autumn/plots/utils.py
+++ autumn/plots/utils.py
@@ -2,7 +2,7 @@ import matplotlib.ticker as ticker
 import datetime
 
 
-def change_xaxis_to_date(fig, axis, ref_date):
+def change_xaxis_to_date(axis, ref_date):
     """
     Change the format of a numerically formatted x-axis to date.
     """

710581e212a14a7aec5e5287ba32508af420bf5f
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-10-16 12:07:25 +1100

    [EUR] figure with posterior detection profile

--- apps/covid_19/mixing_optimisation/outputs/plots/calibration/figure_code.py
+++ apps/covid_19/mixing_optimisation/outputs/plots/calibration/figure_code.py
@@ -1,10 +1,13 @@
 from autumn.tool_kit.params import load_targets
 from autumn import db
 from autumn.plots.calibration.plots import get_posterior
+from autumn.curve import tanh_based_scaleup
 
 from apps.covid_19.mixing_optimisation.constants import OPTI_REGIONS
 import matplotlib.pyplot as plt
-from numpy import random, mean, quantile
+from numpy import mean, quantile
+import numpy as np
+import os
 
 
 # --------------  Load outputs form databases
@@ -12,16 +15,17 @@ def get_calibration_outputs(country):
     calib_dirpath = f"../../pbi_databases/calibration/{country}/"
     mcmc_tables = db.load.load_mcmc_tables(calib_dirpath)
     mcmc_params = db.load.load_mcmc_params_tables(calib_dirpath)
+    uncertainty_df = db.load.load_uncertainty_table(calib_dirpath)
 
-    return mcmc_tables, mcmc_params
+    return mcmc_tables, mcmc_params, uncertainty_df
 
 
 def get_all_countries_calibration_outputs():
     calibration_outputs = {}
     for country in OPTI_REGIONS:
-        mcmc_tables, mcmc_params = get_calibration_outputs(country)
+        mcmc_tables, mcmc_params, uncertainty_df = get_calibration_outputs(country)
         calibration_outputs[country] = {
-            "mcmc_tables": mcmc_tables, "mcmc_params": mcmc_params
+            "mcmc_tables": mcmc_tables, "mcmc_params": mcmc_params, "uncertainty_df": uncertainty_df
         }
 
     return calibration_outputs
@@ -30,6 +34,19 @@ def get_all_countries_calibration_outputs():
 def get_targets(region_name):
     return load_targets("covid_19", region_name)
 
+
+def get_parameter_values(calibration_outputs):
+    param_values = {}
+    for country, outputs in calibration_outputs.items():
+        if len(outputs["mcmc_params"]) == 0:
+            continue
+        param_values[country] = {}
+        all_param_names = [p for p in outputs["mcmc_params"][0]["name"].unique().tolist() if 'dispersion_param' not in p]
+        for param_name in all_param_names:
+            param_values[country][param_name] = get_posterior(
+                outputs["mcmc_params"], outputs["mcmc_tables"], param_name, 0
+            )[param_name].tolist()
+    return param_values
 
 # --------------  Make figure with posterior estimates
 
@@ -55,21 +72,7 @@ param_info = {
 }
 
 
-def make_posterior_ranges_figure(calibration_outputs):
-    param_values = {}
-    for country, outputs in calibration_outputs.items():
-        if len(outputs["mcmc_params"]) == 0:
-            continue
-        param_values[country] = {}
-        all_param_names = [p for p in outputs["mcmc_params"][0]["name"].unique().tolist() if 'dispersion_param' not in p]
-        for param_name in all_param_names:
-            param_values[country][param_name] = get_posterior(
-                outputs["mcmc_params"], outputs["mcmc_tables"], param_name, 0
-            )[param_name].tolist()
-    plot_param_posteriors(param_values)
-
-
-def plot_param_posteriors(param_values):
+def make_posterior_ranges_figure(param_values):
     n_panels = len(param_values['belgium'])
     country_list = OPTI_REGIONS[::-1]
     n_col = 4
@@ -128,4 +131,100 @@ def plot_param_posteriors(param_values):
     plt.savefig("figures/param_posteriors.pdf")
 
 
-# --------------  Make figure with posterior estimates
+# --------------  Make figure with posterior time-variant detection
+def get_all_posterior_detection_percentiles(param_values):
+    for country in OPTI_REGIONS:
+        print(country)
+        country_perc = get_country_posterior_detection_percentiles(param_values[country])
+        file_path_ = os.path.join('dumped_files', 'dumped_detection_percentiles_' + country + '.npy')
+        with open(file_path_, "wb") as f:
+            np.save(f, country_perc)
+
+
+def get_country_posterior_detection_percentiles(country_param_values):
+
+    calculated_times = list(range(300))[30:]
+    store_matrix = np.zeros((len(calculated_times), len(country_param_values['time.start'])))
+
+    for i in range(len(country_param_values['time.start'])):
+        if 'case_detection.start_value' in country_param_values:
+            sigma = country_param_values['case_detection.start_value'][i]
+        else:
+            sigma = 0.
+        my_func = tanh_based_scaleup(country_param_values['case_detection.maximum_gradient'][i],
+                                     country_param_values['case_detection.max_change_time'][i],
+                                     sigma,
+                                     country_param_values['case_detection.end_value'][i]
+                                     )
+        detect_vals = [my_func(float(t)) for t in calculated_times]
+        store_matrix[:, i] = detect_vals
+
+    perc = np.percentile(store_matrix, [2.5, 25, 50, 75, 97.5], axis=1)
+    calculated_times = np.array([calculated_times])
+    perc = np.concatenate((calculated_times, perc))
+
+    return perc
+
+
+def plot_posterior_detection():
+
+    # load percentiles previously dumped
+    percentiles = {}
+
+    x_ticks = [61, 122, 183, 245, 306]
+    x_ticks_labels = ["Mar 1", "May 1", "Jul 1", "Sep 1", "Nov 1"]
+
+    for country in OPTI_REGIONS:
+        file_path_ = os.path.join('dumped_files', 'dumped_detection_percentiles_' + country + '.npy')
+        with open(file_path_, "rb") as f:
+            percentiles[country] = np.load(f)
+
+    n_panels = len(OPTI_REGIONS)
+
+    n_col = 3
+    n_row = int(n_panels // n_col)
+    if n_col * n_row < n_panels:
+        n_row += 1
+
+    fig, axs = plt.subplots(n_row, n_col, figsize=(13, 7))
+    # plt.rcParams["font.family"] = "Times New Roman"
+    plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.15, hspace=.5)
+
+    i_col = -1
+    i_row = 0
+    for country in OPTI_REGIONS:
+        i_col += 1
+        if i_col >= n_col:
+            i_col = 0
+            i_row += 1
+
+        times = percentiles[country][0, :]
+        median = percentiles[country][3, :]
+        low_95 = percentiles[country][1, :]
+        up_95 = percentiles[country][5, :]
+        low_50 = percentiles[country][2, :]
+        up_50 = percentiles[country][4, :]
+
+        axs[i_row, i_col].fill_between(times, low_95, up_95, facecolor="lightsteelblue")
+        axs[i_row, i_col].fill_between(times, low_50, up_50, facecolor="cornflowerblue")
+        axs[i_row, i_col].plot(times, median, color="navy")
+
+        axs[i_row, i_col].set_ylim((0., 1.))
+
+        c_title = country.title() if country != "united-kingdom" else "United Kingdom"
+
+        axs[i_row, i_col].set_title(c_title)
+        axs[i_row, i_col].set_xticks(x_ticks)
+        axs[i_row, i_col].set_xticklabels(x_ticks_labels)
+        axs[i_row, i_col].set_xlim((45, 300))
+
+        if i_col == 0:
+            axs[i_row, i_col].set_ylabel("proportion of symptomatic\ndetected")
+
+    plt.tight_layout()
+    plt.savefig("figures/detection_posteriors.pdf")
+
+
+# --------------  Make figure with fits to data
+def make_calibration_fits_figure(calibration_outputs, targets):
+    pass
--- apps/covid_19/mixing_optimisation/outputs/plots/calibration/main_script.py
+++ apps/covid_19/mixing_optimisation/outputs/plots/calibration/main_script.py
@@ -1,6 +1,16 @@
 import apps.covid_19.mixing_optimisation.outputs.plots.calibration.figure_code as fc
 
+import matplotlib as mpl
+
+# Reset pyplot style
+mpl.rcParams.update(mpl.rcParamsDefault)
+mpl.pyplot.style.use('ggplot')
+
+# Get calibration data
 calibration_outputs = fc.get_all_countries_calibration_outputs()
+param_values = fc.get_parameter_values(calibration_outputs)
 
+fc.make_posterior_ranges_figure(param_values)
 
-fc.make_posterior_ranges_figure(calibration_outputs)
+# fc.get_all_posterior_detection_percentiles(param_values)   # Rerun this line to recalculate percentiles
+fc.plot_posterior_detection()

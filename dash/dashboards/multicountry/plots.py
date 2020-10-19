d1b86afe00ed55e227c981408230df62c9a8edc8
James Trauer <james.trauer@monash.edu>
2020-10-19 15:41:36 +1100

    Split out outputs gathering in case I can find a way to get burn-in working later

--- dash/dashboards/multicountry/plots.py
+++ dash/dashboards/multicountry/plots.py
@@ -15,24 +15,27 @@ def multi_country_fit(plotter, calib_dir_path, mcmc_tables, mcmc_params, targets
     # Set up interface
     available_outputs = [o["output_key"] for o in targets[0].values()]
     chosen_output = st.sidebar.selectbox("Select calibration target", available_outputs)
+    outputs = []
 
     fig, axes, _, n_rows, n_cols, indices = \
         plotter.get_figure(len(region_name), share_xaxis=True)
+
+    # Get data for plotting
+    for i_region in range(len(region_name)):
+        outputs.append(
+            get_uncertainty_data(calib_dir_path[i_region], mcmc_tables[i_region], chosen_output, 0)
+        )
 
     for i_region in range(n_rows * n_cols):
         axis = \
             axes[indices[i_region][0], indices[i_region][1]]
 
         if i_region < len(region_name):
-
-            # Get data for plotting
-            outputs = get_uncertainty_data(calib_dir_path[i_region], mcmc_tables[i_region], chosen_output, 0)
-
             # Call main plotting function
             plots.calibration.plots.plot_calibration(
                 axis,
                 chosen_output,
-                outputs,
+                outputs[i_region],
                 targets[i_region],
                 False
             )

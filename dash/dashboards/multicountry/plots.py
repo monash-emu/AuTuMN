ae0a8562e603c8864a6c6dfb9862017607acf331
James Trauer <james.trauer@monash.edu>
2020-10-19 13:23:34 +1100

    Get multi-panel plots working

--- dash/dashboards/multicountry/plots.py
+++ dash/dashboards/multicountry/plots.py
@@ -16,41 +16,36 @@ def test_only(plotter, calib_dir_path, mcmc_tables, mcmc_params, targets, app_na
 
     # Set up interface
     is_logscale = st.sidebar.checkbox("Log scale")
+    i_plotter = plotter[1]
 
-    max_n_col = 2
-    n_panels = len(region_name)
-    n_cols = min(max_n_col, n_panels)
-    n_rows = ceil(n_panels / max_n_col)
-    fig = pyplot.figure()
-    i_col = 0
-    i_row = 0
-    for i_region in range(1, len(region_name) + 1):
-        i_targets = targets[i_region]
-        i_mcmc_tables = mcmc_tables[i_region]
-        i_calib_dir_path = calib_dir_path[i_region]
+    fig, axes, _, n_rows, n_cols, indices = \
+        i_plotter.get_figure(len(region_name), share_xaxis=True)
 
-        # Get data for plotting
-        outputs = get_uncertainty_data(i_calib_dir_path, i_mcmc_tables, chosen_output, 0)
+    for i_region in range(n_rows * n_cols):
+        if i_region < len(region_name):
 
-        # Call main plotting function
-        axis = fig.add_subplot(1, 2, i_region)
+            i_targets = targets[i_region + 1]
+            i_mcmc_tables = mcmc_tables[i_region + 1]
+            i_calib_dir_path = calib_dir_path[i_region + 1]
 
-        plots.calibration.plots.plot_calibration(
-            axis,
-            chosen_output,
-            outputs,
-            i_targets,
-            is_logscale
-        )
+            # Get data for plotting
+            outputs = get_uncertainty_data(i_calib_dir_path, i_mcmc_tables, chosen_output, 0)
 
-        filename = f"calibration-fit-{chosen_output}"
-        title_text = f"Calibration fit for {chosen_output}"
-        plotter[1].save_figure(fig, filename=filename, title_text=title_text)
+            # Call main plotting function
+            plots.calibration.plots.plot_calibration(
+                axes[indices[i_region][0], indices[i_region][1]],
+                chosen_output,
+                outputs,
+                i_targets,
+                is_logscale
+            )
+        else:
+            axes[indices[i_region][0], indices[i_region][1]].axis("off")
 
-        i_col += 1
-        if i_col == max_n_col:
-            i_col = 0
-            i_row += 1
+    filename = f"calibration-fit-{chosen_output}"
+    title_text = f"Calibration fit for {chosen_output}"
+    i_plotter.save_figure(fig, filename=filename, title_text=title_text)
+
 
 
 PLOT_FUNCS["Test only"] = test_only

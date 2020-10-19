7b44a3de9fa052595733cf4daad80dac3ff7dcd5
James Trauer <james.trauer@monash.edu>
2020-10-19 13:10:49 +1100

    Get basic 2-panel plot running

--- dash/dashboards/multicountry/plots.py
+++ dash/dashboards/multicountry/plots.py
@@ -1,7 +1,8 @@
 import streamlit as st
-from autumn.plots.calibration.plots import find_shortest_chain_length
 from autumn import plots
 from dash.dashboards.calibration.plots import get_uncertainty_data
+from math import ceil
+import matplotlib.pyplot as pyplot
 
 PLOT_FUNCS = {}
 
@@ -11,30 +12,45 @@ def test_only(plotter, calib_dir_path, mcmc_tables, mcmc_params, targets, app_na
     Code taken directly from the fit calibration file at this stage.
     """
 
-    i_plotter = plotter[1]
-    i_targets = targets[1]
-    i_mcmc_tables = mcmc_tables[1]
-    i_calib_dir_path = calib_dir_path[1]
+    chosen_output = "incidence"
 
     # Set up interface
-    available_outputs = [o["output_key"] for o in i_targets.values()]
-    chain_length = find_shortest_chain_length(i_mcmc_tables)
-    burn_in = st.sidebar.slider("Burn in (select 0 for default behaviour of discarding first half)", 0,
-                                chain_length, 0)
-    chosen_output = st.sidebar.selectbox("Select calibration target", available_outputs)
     is_logscale = st.sidebar.checkbox("Log scale")
 
-    # Get data for plotting
-    outputs = get_uncertainty_data(i_calib_dir_path, i_mcmc_tables, chosen_output, burn_in)
+    max_n_col = 2
+    n_panels = len(region_name)
+    n_cols = min(max_n_col, n_panels)
+    n_rows = ceil(n_panels / max_n_col)
+    fig = pyplot.figure()
+    i_col = 0
+    i_row = 0
+    for i_region in range(1, len(region_name) + 1):
+        i_targets = targets[i_region]
+        i_mcmc_tables = mcmc_tables[i_region]
+        i_calib_dir_path = calib_dir_path[i_region]
 
-    # Call main plotting function
-    plots.calibration.plots.plot_calibration_fit(
-        i_plotter,
-        chosen_output,
-        outputs,
-        i_targets,
-        is_logscale
-    )
+        # Get data for plotting
+        outputs = get_uncertainty_data(i_calib_dir_path, i_mcmc_tables, chosen_output, 0)
+
+        # Call main plotting function
+        axis = fig.add_subplot(1, 2, i_region)
+
+        plots.calibration.plots.plot_calibration(
+            axis,
+            chosen_output,
+            outputs,
+            i_targets,
+            is_logscale
+        )
+
+        filename = f"calibration-fit-{chosen_output}"
+        title_text = f"Calibration fit for {chosen_output}"
+        plotter[1].save_figure(fig, filename=filename, title_text=title_text)
+
+        i_col += 1
+        if i_col == max_n_col:
+            i_col = 0
+            i_row += 1
 
 
 PLOT_FUNCS["Test only"] = test_only

c9dca557a4e1663593e8faeebab77711be62f057
milinda-abay <43531318+milinda-abay@users.noreply.github.com>
2020-10-22 14:34:56 +1100

    Malaysia and Sabah data importation update

--- scripts/mys_data_upload.py
+++ scripts/mys_data_upload.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+##!/usr/bin/env python
 """
 Script for loading MYS data into calibration targets and default.yml
 
@@ -18,40 +18,51 @@ import pandas as pd
 
 # From WHO google drive folder
 CASE_DATA_URL = "1mnZcmj2jfmrap1ytyg_ErD1DZ7zDptyJ"  # shareable link
+
 COVID_MYS_DIRPATH = os.path.join(constants.INPUT_DATA_PATH, "covid_mys")
 COVID_MYS_CASE_PATH = os.path.join(COVID_MYS_DIRPATH, "COVID_MYS_CASE.xlsx")
 COVID_MYS_DEATH_PATH = os.path.join(COVID_MYS_DIRPATH, "COVID_MYS_DEATH.csv")
+COVID_SABAH_CSV = os.path.join(COVID_MYS_DIRPATH, "COVID_REGIONAL.csv")
+
 
 gdd.download_file_from_google_drive(
     file_id=CASE_DATA_URL, dest_path=COVID_MYS_CASE_PATH, overwrite=True
 )
 
 COVID_BASE_DATE = pd.datetime(2019, 12, 31)
-REGION_DIR = os.path.join(constants.APPS_PATH, "covid_19", "regions", "malaysia")
+REGION_MYS = os.path.join(constants.APPS_PATH, "covid_19", "regions", "malaysia")
+REGION_SABAH = os.path.join(constants.APPS_PATH, "covid_19", "regions", "sabah")
 
-TARGETS_MAP = {
+
+TARGETS_MYS = {
     "notifications": "NC",
     "icu_occupancy": "ICU",
     "infection_deaths": "Death per day",
 }
 
 
+TARGETS_SABAH = {
+    "notifications": "local_cases",
+}
+
+
 def main():
 
-    update_calibration()
+    update_mys_calibration()
+    update_sabah_calibration()
 
 
-def update_calibration():
+def update_mys_calibration():
     """
     Update Malaysia's calibration targets
     """
 
     df = load_mys_data()
 
-    file_path = os.path.join(REGION_DIR, "targets.json")
+    file_path = os.path.join(REGION_MYS, "targets.json")
     with open(file_path, mode="r") as f:
         targets = json.load(f)
-    for key, val in TARGETS_MAP.items():
+    for key, val in TARGETS_MYS.items():
         # Drop the NaN value rows from df before writing data.
         temp_df = df[["date_index", val]].dropna(0, subset=[val])
 
@@ -59,12 +70,12 @@ def update_calibration():
         targets[key]["values"] = list(temp_df[val])
     with open(file_path, "w") as f:
         json.dump(targets, f, indent=2)
-    with open(REGION_DIR + "\\params\\default.yml", "r") as f:
+    with open(REGION_MYS + "\\params\\default.yml", "r") as f:
         default = f.readlines()
     end = default.index("  case_timeseries:\n")
     default = default[:end]
 
-    with open(REGION_DIR + "\\params\\default.yml", "r") as f:
+    with open(REGION_MYS + "\\params\\default.yml", "r") as f:
         tmp = yaml.load(f, Loader=yaml.FullLoader)
     tmp = {key: val for key, val in tmp.items() if key == "importation"}
 
@@ -73,15 +84,15 @@ def update_calibration():
     tmp["importation"]["case_timeseries"]["times"] = list(temp_df["date_index"])
     tmp["importation"]["case_timeseries"]["values"] = list(temp_df["IC"])
 
-    with open(REGION_DIR + "\\params\\default.yml", "w") as f:
+    with open(REGION_MYS + "\\params\\default.yml", "w") as f:
         yaml.dump(tmp, f)
-    with open(REGION_DIR + "\\params\\default.yml", "r") as f:
+    with open(REGION_MYS + "\\params\\default.yml", "r") as f:
         append = f.readlines()
 
     end = append.index("  movement_prop: null\n")
     default = default + append[1:end]
 
-    with open(REGION_DIR + "\\params\\default.yml", "w") as f:
+    with open(REGION_MYS + "\\params\\default.yml", "w") as f:
         f.writelines(default)
 
 
@@ -91,10 +102,7 @@ def load_mys_data():
     death_df = pd.read_csv(COVID_MYS_DEATH_PATH)
 
     case_df.Date = pd.to_datetime(
-        case_df["Date"],
-        errors="coerce",
-        format="%Y-%m-%d %H:%M:%S",
-        infer_datetime_format=True,
+        case_df["Date"], errors="coerce", format="%Y-%m-%d %H:%M:%S", infer_datetime_format=True,
     )
     case_df = case_df[(case_df.Date > "2020-03-02") & (case_df.Date < "2020-12-31")]
     case_df["date_index"] = (case_df.Date - COVID_BASE_DATE).dt.days
@@ -111,7 +119,7 @@ def load_mys_data():
             "Active cases (hospitalised) including ICU (C)",
             "Active cases exclude ICU (D = C - E)",
             "Ventilator Usage (F)",
-            "Unnamed: 7",
+            "percentage ICU usage",
         ],
         inplace=True,
     )
@@ -126,12 +134,59 @@ def load_mys_data():
     death_df["date_index"] = (death_df.Date - COVID_BASE_DATE).dt.days
     death_df = death_df[death_df.Date.notnull() & death_df["Death per day"].notnull()]
 
-    df = pd.merge(
-        case_df, death_df, how="left", left_on=["date_index"], right_on=["date_index"]
-    )
+    df = pd.merge(case_df, death_df, how="left", left_on=["date_index"], right_on=["date_index"])
     df.drop(columns=["Date_y"], inplace=True)
 
     return df
+
+
+def update_sabah_calibration():
+    """
+    Update Sabah's calibration targets
+    """
+
+    df = pd.read_csv(COVID_SABAH_CSV)
+    df = df[df.state == "Sabah"]
+    df.date = pd.to_datetime(
+        df["date"], errors="coerce", format="%d/%m/%Y", infer_datetime_format=False
+    )
+    df["date_index"] = (df.date - COVID_BASE_DATE).dt.days
+    df.sort_values(by=["date"], inplace=True)
+
+    file_path = os.path.join(REGION_SABAH, "targets.json")
+    with open(file_path, mode="r") as f:
+        targets = json.load(f)
+    for key, val in TARGETS_SABAH.items():
+        # Drop the NaN value rows from df before writing data.
+        temp_df = df[["date_index", val]].dropna(0, subset=[val])
+
+        targets[key]["times"] = list(temp_df["date_index"])
+        targets[key]["values"] = list(temp_df[val])
+    with open(file_path, "w") as f:
+        json.dump(targets, f, indent=2)
+
+    with open(REGION_SABAH + "\\params\\default.yml", "r") as f:
+        default = f.readlines()
+    end = default.index("  case_timeseries:\n")
+    default = default[:end]
+
+    with open(REGION_SABAH + "\\params\\default.yml", "r") as f:
+        tmp = yaml.load(f, Loader=yaml.FullLoader)
+    tmp = {key: val for key, val in tmp.items() if key == "importation"}
+    temp_df = df[["date_index", "import_cases"]].dropna(0, subset=["import_cases"])
+    tmp["importation"]["case_timeseries"]["times"] = list(temp_df["date_index"])
+    tmp["importation"]["case_timeseries"]["values"] = list(temp_df["import_cases"])
+
+    with open(REGION_SABAH + "\\params\\default.yml", "w") as f:
+        yaml.dump(tmp, f)
+    with open(REGION_SABAH + "\\params\\default.yml", "r") as f:
+        append = f.readlines()
+
+    end = append.index("  movement_prop: null\n")
+    default = default + append[1:end]
+
+    with open(REGION_SABAH + "\\params\\default.yml", "w") as f:
+        f.writelines(default)
 
 
 if __name__ == "__main__":

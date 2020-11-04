936f76348219bd07576dba2c73424f349c7eece5
Matthew Segal <mattdsegal@gmail.com>
2020-11-05 09:40:52 +1100

    Add db conversion CLI

--- autumn/cli/database.py
+++ autumn/cli/database.py
@@ -6,10 +6,12 @@ You can access this script from your CLI by running:
     python -m autumn db --help
 
 """
+from autumn.db.database import FeatherDatabase
 import os
 import click
 
 from autumn import db as autumn_db
+import autumn
 from autumn.inputs import build_input_database, fetch_input_data
 
 
@@ -35,38 +37,15 @@ def build_input_db(force):
     build_input_database(force)
 
 
-@db.command("collate")
-@click.argument("src_db_dir", type=str)
-@click.argument("dest_db_path", type=str)
-def collate(src_db_dir, dest_db_path):
-    """
-    Merge all databases from a folder into a single database.
-    """
-    assert os.path.isdir(src_db_dir), f"{src_db_dir} must be a folder"
-    src_db_paths = [
-        os.path.join(src_db_dir, fname) for fname in os.listdir(src_db_dir) if fname.endswith(".db")
-    ]
-    autumn_db.process.collate_databases(src_db_paths, dest_db_path)
-
-
-@db.command("prune")
+@db.command("feather2sql")
 @click.argument("src_db_path", type=str)
 @click.argument("dest_db_path", type=str)
-def prune(src_db_path, dest_db_path):
+def feather2sql(src_db_path, dest_db_path):
     """
-    Drop data for all outputs except for the MLE model run.
-    Requires MCMC run metadata.
+    Convert a Feather DB to a SQLite DB
     """
-    assert os.path.isfile(src_db_path), f"{src_db_path} must be a file"
-    autumn_db.process.prune(src_db_path, dest_db_path)
-
-
-@db.command("unpivot")
-@click.argument("src_db_path", type=str)
-@click.argument("dest_db_path", type=str)
-def unpivot(src_db_path, dest_db_path):
-    """
-    Convert model outputs into PowerBI-friendly unpivoted format.
-    """
-    assert os.path.isfile(src_db_path), f"{src_db_path} must be a file"
-    autumn_db.process.unpivot(src_db_path, dest_db_path)
+    assert autumn_db.FeatherDatabase.is_compatible(
+        src_db_path
+    ), "Source DB must be FeatherDatabase compatible"
+    src_db = FeatherDatabase(src_db_path)
+    autumn_db.database.convert_database(src_db, autumn_db.database.Database, dest_db_path)

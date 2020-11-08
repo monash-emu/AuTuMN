abf6d405fe368a42735f68810bc4aad2331d5eda
Romain Ragonnet <romain.ragonnet@monash.edu>
2020-11-09 09:54:49 +1100

    include PBI databases in find_db_paths function

--- autumn/db/load.py
+++ autumn/db/load.py
@@ -105,7 +105,7 @@ def load_derived_output_tables(calib_dirpath: str, column: str = None):
 def find_db_paths(dirpath: str):
     db_paths = []
     for fname in os.listdir(dirpath):
-        if fname.startswith("outputs") or fname.startswith("chain"):
+        if fname.startswith("outputs") or fname.startswith("chain") or fname.startswith("powerbi"):
             fpath = os.path.join(dirpath, fname)
             db_paths.append(fpath)
 

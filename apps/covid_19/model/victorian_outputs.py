43bb87acb3818242bbe4e2b2be6f8c90eb3b2dcb
Matthew Segal <mattdsegal@gmail.com>
2020-11-11 15:00:49 +1100

    Get notifications working

--- apps/covid_19/model/victorian_outputs.py
+++ apps/covid_19/model/victorian_outputs.py
@@ -1,3 +1,6 @@
+from typing import Dict
+import numpy as np
+
 from summer.model.strat_model import StratifiedModel
 from summer.model import StratifiedModel
 from summer.model.derived_outputs import (
@@ -6,12 +9,15 @@ from summer.model.derived_outputs import (
 )
 from apps.covid_19.constants import Compartment as CompartmentType, ClinicalStratum
 from autumn.constants import Region
+from apps.covid_19.model.outputs import NOTIFICATION_STRATUM
 
 
 CLUSTERS = [Region.to_filename(region) for region in Region.VICTORIA_SUBREGIONS]
 
 
-def add_victorian_derived_outputs(model: StratifiedModel):
+def add_victorian_derived_outputs(
+    model: StratifiedModel, icu_early_period: float, hospital_early_period: float
+):
 
     # Track infection deaths
     inf_death_conns = {}
@@ -27,7 +33,7 @@ def add_victorian_derived_outputs(model: StratifiedModel):
 
     model.add_flow_derived_outputs(inf_death_conns)
 
-    # Track incidence
+    # Track incidence of disease: transition from exposed to active
     incidence_conns = {}
     incidence_conns["incidence"] = TransitionFlowOutput(
         source=CompartmentType.LATE_EXPOSED,
@@ -46,7 +52,7 @@ def add_victorian_derived_outputs(model: StratifiedModel):
 
     model.add_flow_derived_outputs(incidence_conns)
 
-    # Track progress
+    # Track progress of disease: transition from early to late active
     progress_conns = {}
     progress_conns["progress"] = TransitionFlowOutput(
         source=CompartmentType.EARLY_ACTIVE,
@@ -65,8 +71,67 @@ def add_victorian_derived_outputs(model: StratifiedModel):
 
     model.add_flow_derived_outputs(progress_conns)
 
-    # notifications
+    # Track notifications: when we know someone has progressed from early to late active
+    # This is defined as the person who transitions ending up in isolation or hospital.
+    # We aggregate over each strata in a function later on.
+    notification_conns = {}
+    for clinical_stratum in NOTIFICATION_STRATUM:
+        notification_conns[f"notificationsX{clinical_stratum}"] = TransitionFlowOutput(
+            source=CompartmentType.EARLY_ACTIVE,
+            dest=CompartmentType.LATE_ACTIVE,
+            source_strata={},
+            dest_strata={"clinical": clinical_stratum},
+        )
+        for cluster in CLUSTERS:
+            output_key = f"notifications_for_cluster_{cluster}X{clinical_stratum}"
+            notification_conns[output_key] = TransitionFlowOutput(
+                source=CompartmentType.EARLY_ACTIVE,
+                dest=CompartmentType.LATE_ACTIVE,
+                source_strata={},
+                dest_strata={"cluster": cluster, "clinical": clinical_stratum},
+            )
+
+    model.add_flow_derived_outputs(notification_conns)
+
+    # Notification aggregation functions
+    model.add_function_derived_outputs(
+        {f"notifications_for_cluster_{c}": build_cluster_notification_func(c) for c in CLUSTERS}
+    )
+    model.add_function_derived_outputs({"notifications": total_notification_func})
+
+    # TODO?
     # hospital_occupancy
     # icu_occupancy
     # icu_admissions
     # hospital_admissions
+
+
+def total_notification_func(
+    time_idx: int,
+    model: StratifiedModel,
+    compartment_values: np.ndarray,
+    derived_outputs: Dict[str, np.ndarray],
+):
+    count = 0.0
+    for cluster in CLUSTERS:
+        output_key = f"notifications_for_cluster_{cluster}"
+        count += derived_outputs[output_key][time_idx]
+
+    return count
+
+
+def build_cluster_notification_func(cluster: str):
+    def notification_func(
+        time_idx: int,
+        model: StratifiedModel,
+        compartment_values: np.ndarray,
+        derived_outputs: Dict[str, np.ndarray],
+    ):
+        count = 0.0
+        for clinical_stratum in NOTIFICATION_STRATUM:
+            output_key = f"notifications_for_cluster_{cluster}X{clinical_stratum}"
+            count += derived_outputs[output_key][time_idx]
+
+        return count
+
+    return notification_func

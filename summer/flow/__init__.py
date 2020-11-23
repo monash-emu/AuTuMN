24dab5d35e560dd8d3eb1b9195d6bbb6b08c51f3
Matt Segal <mattdsegal@gmail.com>
2020-11-23 15:41:11 +1100

    Add flow graph to dashboard (#143)

--- summer/flow/__init__.py
+++ summer/flow/__init__.py
@@ -1,7 +1,8 @@
 from .crude_birth import CrudeBirthFlow
-from .infection import InfectionDensityFlow, InfectionFrequencyFlow
+from .infection import InfectionDensityFlow, InfectionFrequencyFlow, BaseInfectionFlow
 from .replace_birth import ReplacementBirthFlow
 from .standard import StandardFlow
-from .death import InfectionDeathFlow, UniversalDeathFlow
+from .death import InfectionDeathFlow, UniversalDeathFlow, BaseDeathFlow
 from .ageing import AgeingFlow
 from .importation import ImportFlow
+from .base import BaseTransitionFlow, BaseExitFlow, BaseEntryFlow

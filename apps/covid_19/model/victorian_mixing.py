86b3a1dc3bcb49ccd07b8f6bf004fa38d2e6bbad
James Trauer <james.trauer@monash.edu>
2020-11-26 14:56:21 +1100

    Move mixing parameter out of the mixing function

--- apps/covid_19/model/victorian_mixing.py
+++ apps/covid_19/model/victorian_mixing.py
@@ -14,10 +14,12 @@ MOB_REGIONS = [Region.to_filename(r) for r in Region.VICTORIA_SUBREGIONS]
 
 
 def build_victorian_mixing_matrix_func(
-    static_mixing_matrix,
-    mobility,
-    country,
+        static_mixing_matrix,
+        mobility,
+        country,
+        inter_cluster_mixing=0.01,
 ):
+
     cluster_age_mm_funcs = []
     for region in MOB_REGIONS:
         cluster_mobility = deepcopy(mobility)
@@ -31,44 +33,23 @@ def build_victorian_mixing_matrix_func(
 
     def get_mixing_matrix(self: StratifiedModel, time: float):
         """
-        Construct a 144 x 144 matrix that is the same shape as the Kronecker product
-        of our age-based mixing matrix and the identity matrix *except*, we swap in
-        region specific values. The matrix should have shape (for ages a and clutsers c)
-
-        a00c0         a01c0
-         a00c1         a01c1
-          a00c2         a01c2
-            a00c3        a01c3
-             a00c4        a01c4      ... 16 times
-              a00c5        a01c5
-               a00c6        a01c6
-                a00c7        a01c7
-                 a00c8        a01c8
-        a10c0
-         a10c1         .
-          a10c2         .
-            a10c3        .
-             a10c4
-              a10c5
-               a10c6
-                a10c7
-                 a10c8
-            .
-            .
-            .
-            16 times
-
         """
 
-        # Pre-allocate
-        combined_matrix_size = len(static_mixing_matrix) * len(MOB_REGIONS)
-        mm = np.zeros((combined_matrix_size, combined_matrix_size))
+        # Create the inter-cluster mixing matrix
+        assert 0. < inter_cluster_mixing <= 1. / len(MOB_REGIONS)
+        inter_cluster_mixing_matrix = \
+            np.ones([len(MOB_REGIONS), len(MOB_REGIONS)]) * inter_cluster_mixing
+        within_cluster_matrix = \
+            np.eye(len(MOB_REGIONS)) * \
+            (1. - len(MOB_REGIONS) * inter_cluster_mixing)
+        inter_cluster_mixing_matrix = inter_cluster_mixing_matrix + within_cluster_matrix
 
         # Get the individual cluster mixing matrix
         cluster_age_mms = [f(time) for f in cluster_age_mm_funcs]
 
-        # Define inter-cluster mixing matrix
-        inter_cluster_mixing_matrix = np.ones([len(MOB_REGIONS), len(MOB_REGIONS)])
+        # Pre-allocate
+        combined_matrix_size = len(static_mixing_matrix) * len(MOB_REGIONS)
+        mm = np.zeros((combined_matrix_size, combined_matrix_size))
 
         # Loop over clusters being infected
         for infectee_cluster, age_mm in enumerate(cluster_age_mms):
@@ -87,8 +68,6 @@ def build_victorian_mixing_matrix_func(
                            start_j + infector_cluster] = \
                             age_mm[age_i, age_j] * \
                             inter_cluster_mixing_matrix[infectee_cluster, infector_cluster]
-
-        print(mm)
 
         return mm
 

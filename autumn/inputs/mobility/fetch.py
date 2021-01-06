755bf623402f45f7d2712a7f278640b16c2b7d0d
milinda-abay <43531318+milinda-abay@users.noreply.github.com>
2021-01-07 10:47:34 +1100

    Google mobility and testing updated

--- autumn/inputs/mobility/fetch.py
+++ autumn/inputs/mobility/fetch.py
@@ -10,6 +10,27 @@ GOOGLE_MOBILITY_URL = "https://www.gstatic.com/covid19/mobility/Global_Mobility_
 MOBILITY_DIRPATH = os.path.join(constants.INPUT_DATA_PATH, "mobility")
 MOBILITY_CSV_PATH = os.path.join(MOBILITY_DIRPATH, "Google_Mobility_Report.csv")
 
+COUNTRY_FILTER = {
+    "BR",
+    "US",
+    "AR",
+    "IN",
+    "CO",
+    "CA",
+    "EC",
+    "PL",
+    "TR",
+    "RO",
+    "NG",
+    "PE",
+    "BG",
+    "SL",
+    "GT",
+    "CZ",
+    "SK",
+    "CL",
+}
+
 
 def fetch_mobility_data():
     df = pd.read_csv(GOOGLE_MOBILITY_URL)
@@ -25,4 +46,12 @@ def fetch_mobility_data():
         & (df.country_region_code != "TR")
         & (df.country_region_code != "RO")
         & (df.country_region_code != "NG")
+        & (df.country_region_code != "PE")
+        & (df.country_region_code != "BG")
+        & (df.country_region_code != "SL")
+        & (df.country_region_code != "GT")
+        & (df.country_region_code != "CZ")
+        & (df.country_region_code != "SK")
+        & (df.country_region_code != "CL")
+        & (df.country_region_code != "PT")
     ].to_csv(MOBILITY_CSV_PATH)

906de46b99c2734d1fcc5c0baa59a50f4eda68e2
Matthew Segal <mattdsegal@gmail.com>
2021-01-12 15:39:38 +1100

    more stuff

--- apps/covid_19/model/model.py
+++ apps/covid_19/model/model.py
@@ -705,14 +705,14 @@ def build_model(params: dict) -> CompartmentalModel:
             model.request_output_for_flow(
                 name=f"infection_deathsXagegroup_{agegroup}",
                 flow_name="infect_death",
-                dest_strata={"agegroup": agegroup},
+                source_strata={"agegroup": agegroup},
                 save_results=False,
             )
             for clinical in clinical_strata:
                 model.request_output_for_flow(
                     name=f"infection_deathsXagegroup_{agegroup}Xclinical_{clinical}",
                     flow_name="infect_death",
-                    dest_strata={"agegroup": agegroup, "clinical": clinical},
+                    source_strata={"agegroup": agegroup, "clinical": clinical},
                 )
 
         model.request_cumulative_output(name="accum_deaths", source="infection_deaths")
@@ -868,6 +868,10 @@ def build_model(params: dict) -> CompartmentalModel:
             model.request_aggregate_output(
                 name=f"notifications_for_cluster_{cluster}", sources=cluster_notification_sources
             )
+            model.request_cumulative_output(
+                name=f"accum_notifications_for_cluster_{cluster}",
+                source=f"notifications_for_cluster_{cluster}",
+            )
 
         # Track non-ICU hospital admissions (transition from early to late active in hospital, non-ICU stratum)
         model.request_output_for_flow(
@@ -898,6 +902,10 @@ def build_model(params: dict) -> CompartmentalModel:
                 source_strata={"clinical": Clinical.ICU, "cluster": cluster},
                 dest_strata={"clinical": Clinical.ICU, "cluster": cluster},
             )
+            model.request_cumulative_output(
+                name=f"accum_icu_admissions_for_cluster_{cluster}",
+                source=f"icu_admissions_for_cluster_{cluster}",
+            )
 
         # Create hospitalisation functions as sum of hospital non-ICU and ICU
         model.request_aggregate_output(
@@ -911,8 +919,94 @@ def build_model(params: dict) -> CompartmentalModel:
                     f"non_icu_admissions_for_cluster_{cluster}",
                 ],
             )
+            model.request_cumulative_output(
+                name=f"accum_hospital_admissions_for_cluster_{cluster}",
+                source=f"hospital_admissions_for_cluster_{cluster}",
+            )
 
         # Hospital occupancy
+        # We count all ICU and hospital late active compartments and a proportion of early active ICU cases.
+        compartment_periods = params.sojourn.compartment_periods
+        icu_early_period = compartment_periods["icu_early"]
+        hospital_early_period = compartment_periods["hospital_early"]
+        period_icu_patients_in_hospital = max(icu_early_period - hospital_early_period, 0.0)
+        proportion_icu_patients_in_hospital = period_icu_patients_in_hospital / icu_early_period
+        model.request_output_for_compartments(
+            "_late_active_hospital",
+            compartments=[Compartment.LATE_ACTIVE],
+            strata={"clinical": Clinical.HOSPITAL_NON_ICU},
+            save_results=False,
+        )
+        model.request_output_for_compartments(
+            "icu_occupancy",
+            compartments=[Compartment.LATE_ACTIVE],
+            strata={"clinical": Clinical.ICU},
+        )
+        model.request_output_for_compartments(
+            "_early_active_icu",
+            compartments=[Compartment.EARLY_ACTIVE],
+            strata={"clinical": Clinical.ICU},
+            save_results=False,
+        )
+        model.request_function_output(
+            name="_early_active_icu_proportion",
+            func=lambda patients: patients * proportion_icu_patients_in_hospital,
+            sources=["_early_active_icu"],
+            save_results=False,
+        )
+        model.request_aggregate_output(
+            name="hospital_occupancy",
+            sources=[
+                "_late_active_hospital",
+                "icu_occupancy",
+                "_early_active_icu_proportion",
+            ],
+        )
+        for cluster in clusters:
+            model.request_output_for_compartments(
+                f"_late_active_hospital_for_cluster_{cluster}",
+                compartments=[Compartment.LATE_ACTIVE],
+                strata={"clinical": Clinical.HOSPITAL_NON_ICU, "cluster": cluster},
+                save_results=False,
+            )
+            model.request_output_for_compartments(
+                f"icu_occupancy_for_cluster_{cluster}",
+                compartments=[Compartment.LATE_ACTIVE],
+                strata={"clinical": Clinical.ICU, "cluster": cluster},
+            )
+            model.request_output_for_compartments(
+                f"_early_active_icu_for_cluster_{cluster}",
+                compartments=[Compartment.EARLY_ACTIVE],
+                strata={"clinical": Clinical.ICU, "cluster": cluster},
+                save_results=False,
+            )
+            model.request_function_output(
+                name=f"_early_active_icu_proportion_for_cluster_{cluster}",
+                func=lambda patients: patients * proportion_icu_patients_in_hospital,
+                sources=["_early_active_icu"],
+                save_results=False,
+            )
+            model.request_aggregate_output(
+                name=f"hospital_occupancy_for_cluster_{cluster}",
+                sources=[
+                    f"_late_active_hospital_for_cluster_{cluster}",
+                    f"icu_occupancy_for_cluster_{cluster}",
+                    f"_early_active_icu_proportion_for_cluster_{cluster}",
+                ],
+            )
+
+        # Infection deaths.
+        model.request_output_for_flow(name="infection_deaths", flow_name="infect_death")
+        model.request_cumulative_output(name="accum_deaths", source="infection_deaths")
+        for cluster in clusters:
+            model.request_output_for_flow(
+                name=f"infection_deaths_for_cluster_{cluster}",
+                flow_name="infect_death",
+                source_strata={"cluster": cluster},
+            )
+            model.request_cumulative_output(
+                name=f"accum_infection_deaths_for_cluster_{cluster}", source="infection_deaths"
+            )
 
     return model
 

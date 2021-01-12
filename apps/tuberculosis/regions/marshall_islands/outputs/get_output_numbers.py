bc826c9cac3f3abd27128802bb1433978bf483b0
Romain Ragonnet <romain.ragonnet@monash.edu>
2021-01-12 15:03:58 +1100

    Create get_output_numbers.py

---
+++ apps/tuberculosis/regions/marshall_islands/outputs/get_output_numbers.py
@@ -0,0 +1,73 @@
+import os
+
+from autumn.constants import BASE_PATH
+from autumn.db.load import load_uncertainty_table
+
+from apps.tuberculosis.regions.marshall_islands.outputs.utils import OUTPUT_TITLES, REGION_TITLES, save_figure, get_format, make_output_directories
+
+FIGURE_PATH = os.path.join(
+    BASE_PATH, "apps", "tuberculosis", "regions", "marshall_islands", "outputs", "figures", "output_numbers"
+)
+
+DATA_PATH = os.path.join(
+    BASE_PATH, "apps", "tuberculosis", "regions", "marshall_islands", "outputs", "pbi_databases"
+)
+
+
+def main():
+    make_output_directories(FIGURE_PATH)
+    uncertainty_df = load_uncertainty_table(DATA_PATH)
+
+    # End TB Targets
+    print("End TB Targets")
+    for output in ["incidence", "mortality"]:
+        print_median_and_ci(uncertainty_df, output, 2015, 0)
+
+    # main outputs, national level
+    for output in ["incidence", "mortality"]:
+        for scenario in [1, 0]:
+            print_median_and_ci(uncertainty_df, output, 2020, scenario)
+
+    # regional level
+    for region in ["majuro", "ebeye"]:
+        for year in [2020, 2050]:
+            for scenario in [1, 0]:
+                print_median_and_ci(uncertainty_df, f"incidenceXlocation_{region}", year, scenario)
+
+    # diabetes scenarios
+    print()
+    print("Diabetes scenarios")
+    for scenario in [7, 8]:
+        print_median_and_ci(uncertainty_df, 'incidence', 2050, scenario)
+
+    # PT in all contacts
+    print()
+    print("PT in all contacts")
+    for output in ["incidence", "mortality"]:
+        print_median_and_ci(uncertainty_df, output, 2050, 9)
+
+
+def get_output_number(uncertainty_df, output, time, scenario_idx, quantile=0.5):
+    mask = (
+        (uncertainty_df["type"] == output)
+        & (uncertainty_df["scenario"] == scenario_idx)
+        & (uncertainty_df["time"] == time)
+        & (uncertainty_df["quantile"] == quantile)
+    )
+    value = uncertainty_df[mask]["value"].tolist()
+    return value[0]
+
+
+def print_median_and_ci(uncertainty_df, output, time, scenario_idx):
+    median_val = get_output_number(uncertainty_df, output, time, scenario_idx, 0.5)
+    lowest_val = get_output_number(uncertainty_df, output, time, scenario_idx, 0.025)
+    highest_val = get_output_number(uncertainty_df, output, time, scenario_idx, 0.975)
+
+    out_str = f"{median_val} ({lowest_val} - {highest_val})"
+    print("******************")
+    print(f"{output} in {time} for Scenario {scenario_idx}:")
+    print(out_str)
+
+
+if __name__ == "__main__":
+    main()

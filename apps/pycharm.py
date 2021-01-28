a502a77a0b65c3ea11e2f7744db54b6aac2afa5c
Romain Ragonnet <romain.ragonnet@monash.edu>
2021-01-29 09:42:31 +1100

    Fix tests

--- apps/pycharm.py
+++ apps/pycharm.py
@@ -24,7 +24,7 @@ os.chdir("..")  # Make repo root the current directory
 
 
 # # Run a calibration
-app_region = covid_19.app.get_region(Region.VICTORIA)
+app_region = covid_19.app.get_region(Region.PHILIPPINES)
 app_region.calibrate_model(max_seconds=60, run_id=1, num_chains=1)
 
 # # Run a calibration

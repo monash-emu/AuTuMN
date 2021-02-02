a983164b64c638d98350e3c6606c6e7c9751364a
github-action-benchmark <github@users.noreply.github.com>
2021-02-02 23:57:48 +0000

    add Benchmark (pytest) benchmark result for 29ddcc32ead6b1275e71a0c2cdf0e7df4d4163c4

---
+++ bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1612310268887,
+  "repoUrl": "https://github.com/monash-emu/AuTuMN",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "mattdsegal@gmail.com",
+            "name": "Matthew Segal",
+            "username": "MattSegal"
+          },
+          "committer": {
+            "email": "mattdsegal@gmail.com",
+            "name": "Matthew Segal",
+            "username": "MattSegal"
+          },
+          "distinct": true,
+          "id": "29ddcc32ead6b1275e71a0c2cdf0e7df4d4163c4",
+          "message": "Try fix benchmarking",
+          "timestamp": "2021-02-03T10:53:50+11:00",
+          "tree_id": "69d222935e067d5d3e015da8a49482d4348ea0cf",
+          "url": "https://github.com/monash-emu/AuTuMN/commit/29ddcc32ead6b1275e71a0c2cdf0e7df4d4163c4"
+        },
+        "date": 1612310268057,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_benchmark_covid_models[malaysia]",
+            "value": 0.3348190086670445,
+            "unit": "iter/sec",
+            "range": "stddev: 0.17038781503215703",
+            "extra": "mean: 2.9866882527999907 sec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}

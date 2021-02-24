9088b70a2b8791848e500a3e77de4fecacec55a0
milinda-abay <43531318+milinda-abay@users.noreply.github.com>
2021-02-24 14:33:11 +1100

    Update mobility and MYS targets.

--- autumn/inputs/covid_phl/fetch.py
+++ autumn/inputs/covid_phl/fetch.py
@@ -8,7 +8,7 @@ from google_drive_downloader import GoogleDriveDownloader as gdd
 from settings import INPUT_DATA_PATH
 
 # From DoH google drive folder, shareable link changes with every update
-DATA_URL = "1WNbezxkSnvxCiLJIbgGCLc_NCaMNFu1b"  # shareable link for sheet 07 testing data
+DATA_URL = "1G-bIptG1j9aQfLj54hTFSp5OCJcN8dZh"  # shareable link for sheet 07 testing data
 COVID_PHL_DIRPATH = os.path.join(INPUT_DATA_PATH, "covid_phl")
 COVID_PHL_CSV_PATH = os.path.join(COVID_PHL_DIRPATH, "COVID_Phl_testing.csv")
 

d2b037b72fd357423fc8c3b9cdddec48fd0ea96a
James Trauer <james.trauer@monash.edu>
2021-02-26 22:13:32 +1100

    Fix indentation issue, which wasn't the problem I was after

--- apps/covid_19/model/stratifications/immunity.py
+++ apps/covid_19/model/stratifications/immunity.py
@@ -45,68 +45,68 @@ def get_immunity_strat(params: Parameters) -> Stratification:
             }
         )
 
-    symptomatic_adjuster = \
-        params.clinical_stratification.props.symptomatic.multiplier * \
-        relative_severity_effect
-    hospital_adjuster = \
-        params.clinical_stratification.props.hospital.multiplier * \
-        relative_severity_effect
-    ifr_adjuster = \
-        params.infection_fatality.multiplier * \
-        relative_severity_effect
+        symptomatic_adjuster = \
+            params.clinical_stratification.props.symptomatic.multiplier * \
+            relative_severity_effect
+        hospital_adjuster = \
+            params.clinical_stratification.props.hospital.multiplier * \
+            relative_severity_effect
+        ifr_adjuster = \
+            params.infection_fatality.multiplier * \
+            relative_severity_effect
 
-    # Get all the adjustments in the same way as we did for the clinical stratification
-    entry_adjustments, death_adjs, progress_adjs, recovery_adjs, _, _ = \
-        get_all_adjs(
-            params.clinical_stratification,
-            params.country,
-            params.population,
-            params.infection_fatality.props,
-            params.sojourn,
-            params.testing_to_detection,
-            params.case_detection,
-            ifr_adjuster,
-            symptomatic_adjuster,
-            hospital_adjuster,
-        )
+        # Get all the adjustments in the same way as we did for the clinical stratification
+        entry_adjustments, death_adjs, progress_adjs, recovery_adjs, _, _ = \
+            get_all_adjs(
+                params.clinical_stratification,
+                params.country,
+                params.population,
+                params.infection_fatality.props,
+                params.sojourn,
+                params.testing_to_detection,
+                params.case_detection,
+                ifr_adjuster,
+                symptomatic_adjuster,
+                hospital_adjuster,
+            )
 
-    for i_age, agegroup in enumerate(AGEGROUP_STRATA):
-        for clinical_stratum in CLINICAL_STRATA:
-            relevant_strata = {
-                "agegroup": agegroup,
-                "clinical": clinical_stratum,
-            }
-            immunity_strat.add_flow_adjustments(
-                "infect_onset",
-                {
-                    "unvaccinated": None,
-                    "vaccinated": entry_adjustments[agegroup][clinical_stratum]
-                },
-                dest_strata=relevant_strata,  # Must be dest
-            )
-            immunity_strat.add_flow_adjustments(
-                "infect_death",
-                {
-                    "unvaccinated": None,
-                    "vaccinated": death_adjs[agegroup][clinical_stratum]
-                },
-                source_strata=relevant_strata,  # Must be source
-            )
-            immunity_strat.add_flow_adjustments(
-                "progress",
-                {
-                    "unvaccinated": None,
-                    "vaccinated": progress_adjs[clinical_stratum]
-                },
-                source_strata=relevant_strata,  # Either source or dest or both
-            )
-            immunity_strat.add_flow_adjustments(
-                "recovery",
-                {
-                    "unvaccinated": None,
-                    "vaccinated": recovery_adjs[agegroup][clinical_stratum]
-                },
-                source_strata=relevant_strata,  # Must be source
-            )
+        for i_age, agegroup in enumerate(AGEGROUP_STRATA):
+            for clinical_stratum in CLINICAL_STRATA:
+                relevant_strata = {
+                    "agegroup": agegroup,
+                    "clinical": clinical_stratum,
+                }
+                immunity_strat.add_flow_adjustments(
+                    "infect_onset",
+                    {
+                        "unvaccinated": None,
+                        "vaccinated": entry_adjustments[agegroup][clinical_stratum]
+                    },
+                    dest_strata=relevant_strata,  # Must be dest
+                )
+                immunity_strat.add_flow_adjustments(
+                    "infect_death",
+                    {
+                        "unvaccinated": None,
+                        "vaccinated": death_adjs[agegroup][clinical_stratum]
+                    },
+                    source_strata=relevant_strata,  # Must be source
+                )
+                immunity_strat.add_flow_adjustments(
+                    "progress",
+                    {
+                        "unvaccinated": None,
+                        "vaccinated": progress_adjs[clinical_stratum]
+                    },
+                    source_strata=relevant_strata,  # Either source or dest or both
+                )
+                immunity_strat.add_flow_adjustments(
+                    "recovery",
+                    {
+                        "unvaccinated": None,
+                        "vaccinated": recovery_adjs[agegroup][clinical_stratum]
+                    },
+                    source_strata=relevant_strata,  # Must be source
+                )
 
     return immunity_strat

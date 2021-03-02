3010c836955cc9bce55b45373aaa09ecd7f4fb0e
Matthew Segal <mattdsegal@gmail.com>
2021-03-02 15:17:48 +1100

    Try fix logs URL in buildkite output

--- remote/aws/remote.py
+++ remote/aws/remote.py
@@ -123,14 +123,14 @@ def run_task_pipeline(conn: Connection, pipeline_name: str, pipeline_args: dict)
     logger.info("Finished running task pipleine %s", pipeline_name)
 
 
-LOGS_URL = "https://monashemu.grafana.net/explore?orgId=1&left=%5B%22now-1h%22,%22now%22,%22grafanacloud-monashemu-logs%22,%7B%22expr%22:%22%7Bjob%3Dapp%7D%20%7C%3D%20${HOSTNAME}%22%7D%5D"
+LOGS_URL = "https://monashemu.grafana.net/explore?orgId=1&left=%5B%22now-1h%22,%22now%22,%22grafanacloud-monashemu-logs%22,%7B%22expr%22:%22%7Bjob%3D%5C%22app%5C%22%7D%20%7C%3D%20%5C%22${HOSTNAME}%22%22%7D%5D"
 METRICS_URL = "https://monashemu.grafana.net/d/SkUUUHyMk/nodes?orgId=1&refresh=30s&var-datasource=grafanacloud-monashemu-prom&var-instance=${HOSTNAME}:12345"
 
 
 def print_hostname(conn: Connection):
     conn.run('echo "Running on host $HOSTNAME"', echo=True)
-    conn.run(f'echo -e "\\nMETRICS: {METRICS_URL}"', echo=True)
-    conn.run(f'echo -e "LOGS: {LOGS_URL}\\n"', echo=True)
+    conn.run(f'echo "METRICS: {METRICS_URL}"', echo=True)
+    conn.run(f'echo "LOGS: {LOGS_URL}"', echo=True)
 
 
 def set_run_id(conn: Connection, run_id: str):

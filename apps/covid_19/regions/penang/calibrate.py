d08a78487fbfc0d163ccb0b1b841c86d96edcf8b
James Trauer <james.trauer@monash.edu>
2021-03-19 15:53:25 +1100

    Fix up targets issue with Penang

--- apps/covid_19/regions/penang/calibrate.py
+++ apps/covid_19/regions/penang/calibrate.py
@@ -7,7 +7,7 @@ from apps.covid_19.calibration import truncate_targets_from_time
 
 
 targets = load_targets("covid_19", Region.PENANG)
-notifications = truncate_targets_from_time(targets["notifications"], 280)
+notifications = truncate_targets_from_time(targets["notifications"], 291)
 
 TARGET_OUTPUTS = [
     {

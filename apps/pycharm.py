63d924dbb354f2b48052a288c0b7a494e5297c99
[Emma_McBryde] <jc343421@ad.jcu.edu.au>
2021-05-07 13:21:36 +1000

    changed the model to be run to TBstrains

--- apps/pycharm.py
+++ apps/pycharm.py
@@ -10,8 +10,8 @@ from apps.covid_19.mixing_optimisation.constants import OPTI_REGIONS
 os.chdir("..")  # Make repo root the current directory
 
 # Run a COVID model manually.
-app_region = covid_19.app.get_region(Region.FRANCE)
-app_region.run_model(run_scenarios=False)
+# app_region = covid_19.app.get_region(Region.FRANCE)
+# app_region.run_model(run_scenarios=False)
 
 # Simple SIR model for demonstration
 # app_region = sir_example.app.get_region(Region.AUSTRALIA)
@@ -27,9 +27,9 @@ app_region.run_model(run_scenarios=False)
 # app_region = covid_19.app.get_region(Region.VICTORIA)
 # app_region.calibrate_model(max_seconds=60, run_id=1, num_chains=1)
 
-# # Run a calibration
-# app_region = tuberculosis_strains.app.get_region(Region.PHILIPPINES)
-# app_region.calibrate_model(max_seconds=60, run_id=1, num_chains=1)
+ # Run a calibration
+app_region = tuberculosis_strains.app.get_region(Region.PHILIPPINES)
+app_region.calibrate_model(max_seconds=60, run_id=1, num_chains=1)
 
 # Used by Romain, please do not delete
 # for region in OPTI_REGIONS:

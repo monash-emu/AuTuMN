8262e82e49e98fb6ff1ed04fed351eb719aff16c
Romain Ragonnet <romain.ragonnet@monash.edu>
2021-05-25 14:07:24 +1000

    Update script to write EU hospital targets

--- scripts/write_hospital_data_to_targets.py
+++ scripts/write_hospital_data_to_targets.py
@@ -1,10 +1,8 @@
-import datetime
 import json
 import os
 
 import pandas as pd
 
-from apps.covid_19.mixing_optimisation.utils import get_weekly_summed_targets
 from autumn.region import Region
 
 base_dir = os.path.dirname(os.path.abspath(os.curdir))
@@ -12,14 +10,24 @@ HOSP_DATA_FILE = os.path.join(
     base_dir, "data", "inputs", "hospitalisation_data", "european_data.csv"
 )
 
-country_targets = {
-    Region.BELGIUM: ["bel_incid_hosp_in", "new_hospital_admissions"],
-    Region.FRANCE: ["fra_incid_hosp", "new_hospital_admissions"],
-    Region.ITALY: ["ita_prev_hosp", "hospital_occupancy"],
-    Region.SPAIN: ["esp_incid_hosp", "new_hospital_admissions"],
-    Region.SWEDEN: ["swe_incid_icu", "new_icu_admissions"],
-    Region.UNITED_KINGDOM: ["uk_incid_hosp", "new_hospital_admissions"],
+COUNTRY_TARGETS = {
+    Region.BELGIUM: ["bel_hosp_adm_per_100K", "new_hospital_admissions"],
+    Region.FRANCE: ["fra_hosp_adm_per_100K", "new_hospital_admissions"],
+    Region.ITALY: ["ita_hosp_adm_per_100K", "new_hospital_admissions"],
+    Region.SPAIN: ["spa_hosp_adm_per_100K", "new_hospital_admissions"],
+    Region.SWEDEN: ["swe_icu_adm_per_100K", "new_icu_admissions"],
+    Region.UNITED_KINGDOM: ["uk_hosp_adm", "new_hospital_admissions"],
 }
+
+POPULATION = {
+    Region.BELGIUM: 11589623,
+    Region.FRANCE: 65273511,
+    Region.ITALY: 60461826,
+    Region.SPAIN: 46754778,
+    Region.SWEDEN: 10099265,
+    Region.UNITED_KINGDOM: 67886011,  # UK not required
+}
+
 
 start_reporting = {
     Region.FRANCE: 79,
@@ -27,39 +35,60 @@ start_reporting = {
 }
 
 
+def convert_year_week_to_day(year_week):
+    """
+    return a day representing the 4th week defined by year_week
+    :param year_week: string such as "2020-W12"
+    :param reference_date: date corresponding to time t=0
+    :return: integer representing the 4th day of the week
+    """
+    year = year_week.split("-")[0]
+    week = year_week.split("W")[1]
+
+    # work out out date_int, which is the integer representing the first day of the selected week
+    ref_integer = -8 if year == "2020" else 363
+    date_int = ref_integer + 7 * float(week) + 3
+
+    return date_int
+
+
 def read_hospital_data(country, data_start_time=0, data_end_time=365):
     path = HOSP_DATA_FILE
     data = pd.read_csv(path)
-    date_ref = datetime.datetime.strptime("2019-12-31", "%Y-%m-%d")
 
-    mask = pd.notnull(data[country_targets[country][0]])
-    dates = list(data[mask]["date"])
-    values = [float(v) for v in data[mask][country_targets[country][0]]]
-    times = [(datetime.datetime.strptime(d, "%d/%m/%Y") - date_ref).days for d in dates]
+    mask = pd.notnull(data[COUNTRY_TARGETS[country][0]])
+    year_weeks = list(data[mask]["year_week"])
+    values = [float(v) for v in data[mask][COUNTRY_TARGETS[country][0]]]
+
+    times = [convert_year_week_to_day(y_w) for y_w in year_weeks]
 
     min_hosp_time = min(times)
     data_start_time = max(min_hosp_time, data_start_time)
-    start_index = times.index(data_start_time)
+    start_index = next(x[0] for x in enumerate(times) if x[1] >= data_start_time)
 
     max_hosp_time = max(times)
     data_end_time = min(max_hosp_time, data_end_time)
-    end_index = times.index(data_end_time)
+    end_index = next(x[0] for x in enumerate(times) if x[1] >= data_end_time) - 1
 
-    times = times[start_index : end_index + 1]
-    values = values[start_index : end_index + 1]
+    times = times[start_index: end_index + 1]
+    values = values[start_index: end_index + 1]
 
     return times, values
 
 
 def write_hospital_data_to_targets(
-    country, data_start_time=0, data_end_time=365, weekly_average=True
+    country, data_start_time=0, data_end_time=365
 ):
-    region = country if country != "united-kingdom" else "united_kingdom"
+    region = country if country != Region.UNITED_KINGDOM else "united_kingdom"
     target_path = os.path.join("../apps", "covid_19", "regions", region, "targets.json")
 
     times, values = read_hospital_data(country, data_start_time, data_end_time)
-    if weekly_average:
-        times, values = get_weekly_summed_targets(times, values)
+
+    # UK's data represents the average daily number of new hospitalisation for each week.
+    # For the other countries, this is total number of new hospitalisations for each week, expressed per 100k people
+    if country != Region.UNITED_KINGDOM:
+        population = POPULATION[country]
+        values = [v / 7 * population / 1.e5 for v in values]
 
     if country in list(start_reporting.keys()):
         min_time = start_reporting[country]
@@ -69,12 +98,13 @@ def write_hospital_data_to_targets(
 
     with open(target_path, mode="r") as f:
         targets = json.load(f)
-        targets[country_targets[country][1]]["times"] = times
-        targets[country_targets[country][1]]["values"] = values
+        targets[COUNTRY_TARGETS[country][1]]["times"] = times
+        targets[COUNTRY_TARGETS[country][1]]["values"] = values
     with open(target_path, "w") as f:
         json.dump(targets, f, indent=2)
 
 
 if __name__ == "__main__":
     for country in Region.MIXING_OPTI_REGIONS:
-        write_hospital_data_to_targets(country, data_end_time=275)  # t_max=275 is 1st October 2020
+        write_hospital_data_to_targets(country, data_end_time=365)
+

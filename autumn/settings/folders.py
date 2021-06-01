9ad3e472a337f3dc797492ffa1320c7704f4019b
milinda-abay <43531318+milinda-abay@users.noreply.github.com>
2021-06-01 15:50:01 +1000

    Update projects path and script files for targets

--- autumn/settings/folders.py
+++ autumn/settings/folders.py
@@ -4,7 +4,7 @@ from os.path import dirname
 # Filesystem paths
 BASE_PATH = os.path.abspath(dirname(dirname(dirname(__file__))))
 DATA_PATH = os.path.join(BASE_PATH, "data")
-PROJECTS_PATH = os.path.join(BASE_PATH, "projects")
+PROJECTS_PATH = os.path.join(BASE_PATH, "autumn", "projects")
 INPUT_DATA_PATH = os.path.join(DATA_PATH, "inputs")
 OUTPUT_DATA_PATH = os.path.join(DATA_PATH, "outputs")
 REMOTE_PATH = os.path.join(OUTPUT_DATA_PATH, "remote")

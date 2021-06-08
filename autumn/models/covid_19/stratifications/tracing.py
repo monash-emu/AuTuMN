104a0d3c2f3ba91c415ba12b60907092c31fe5f4
James Trauer <james.trauer@monash.edu>
2021-06-08 10:01:56 +1000

    Get as far as I can without working out how incidence will feed in here

--- autumn/models/covid_19/stratifications/tracing.py
+++ autumn/models/covid_19/stratifications/tracing.py
@@ -1,15 +1,48 @@
-from summer import Stratification
+from summer import Stratification, Multiply
 
 from autumn.models.covid_19.constants import INFECTIOUS_COMPARTMENTS
 from autumn.models.covid_19.parameters import Parameters
 
 
 def get_tracing_strat(params: Parameters) -> Stratification:
-    tracing_strat = Stratification("tracing", ["traced", "untraced"], INFECTIOUS_COMPARTMENTS)
+    tracing_strat = Stratification(
+        "tracing",
+        ["traced", "untraced"],
+        INFECTIOUS_COMPARTMENTS
+    )
+
+    # Current default for everyone to start out untraced
     tracing_strat.set_population_split(
         {
             "traced": 0.,
             "untraced": 1.,
         }
     )
+
+    # This thing needs to be a function that depends on incidence (as it emerges from the model) and CDR
+    prop_traced = lambda t: 0.
+    prop_not_traced = lambda t: 1. - prop_traced(t)
+
+    # Apply the contact tracing
+    tracing_strat.add_flow_adjustments(
+        "infect_onset",
+        {
+            "traced": Multiply(prop_traced),
+            "untraced": Multiply(prop_not_traced),
+        }
+    )
+
+    # This will just be a parameter
+    rel_infectiousness_traced = 0.2
+
+    # Adjust infectiousness of those traced
+    for comp in INFECTIOUS_COMPARTMENTS:
+        tracing_strat.add_infectiousness_adjustments(
+            comp,
+            {
+                "traced": Multiply(rel_infectiousness_traced),
+                "untraced": None,
+            },
+        )
+
     return tracing_strat

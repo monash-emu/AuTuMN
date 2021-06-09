0463f292a4fcda6f9f5443daf52e87528396c81c
James Trauer <james.trauer@monash.edu>
2021-06-09 22:48:11 +1000

    Little bit of tidying ahead of asking next question

--- autumn/models/covid_19/preprocess/tracing.py
+++ autumn/models/covid_19/preprocess/tracing.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from autumn.models.covid_19.constants import Compartment
+from autumn.models.covid_19.preprocess.case_detection import build_detected_proportion_func
 
 from summer.compute import DerivedValueProcessor, find_sum
 
@@ -16,19 +17,11 @@ class PrevalenceProc(DerivedValueProcessor):
         return find_sum(comp_vals[self.active_comps]) / find_sum(comp_vals)
 
 
-def trace_function(prop_traced, incidence_flow_rate, untraced_comp):
+def trace_function(prop_traced, incidence_flow_rate):
     def contact_tracing_func(
             flow, compartments, compartment_values, flows, flow_rates, derived_values, time
     ):
-
-        #Currently unused, but proof of concept - unfortunately adds ridiculously to run-time
-        #active_comps = \
-        #   [idx for idx, comp in enumerate(compartments) if
-        #    comp.has_name(Compartment.EARLY_ACTIVE) or comp.has_name(Compartment.LATE_ACTIVE)]
-        #prevalence = sum(compartment_values[active_comps]) / sum(compartment_values)
-
-        prevalence = derived_values['prevalence']
-
+        prevalence = derived_values["prevalence"]
         return incidence_flow_rate * prop_traced * compartment_values[flow.source.idx]
 
     return contact_tracing_func

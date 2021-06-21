947f122c91d63ce223a83bb163d07632bb4459ad
milinda-abay <43531318+milinda-abay@users.noreply.github.com>
2021-06-21 13:14:12 +1000

    Update targets for Davao region.

--- scripts/phl_data_upload.py
+++ scripts/phl_data_upload.py
@@ -20,12 +20,12 @@ PHL_doh_link = "1E6yke-Zi73d9EshfWE_qbzsYEBUJLvHg"  # sheet 05 daily report
 PHL_fassster_link = "1ItBw4DeDQZFPBWMxl9byHthgTLh0BUCE"
 
 # destination folders filepaths
-phl_inputs_dir = os.path.join(INPUT_DATA_PATH,"covid_phl")
-PHL_doh_dest = os.path.join(phl_inputs_dir,"PHL_icu.csv")
-PHL_fassster_dest = os.path.join(phl_inputs_dir,"PHL_ConfirmedCases.zip")
-icu_dest = os.path.join(phl_inputs_dir,"PHL_icu_processed.csv")
-deaths_dest = os.path.join(phl_inputs_dir,"PHL_deaths_processed.csv")
-notifications_dest = os.path.join(phl_inputs_dir,"PHL_notifications_processed.csv")
+phl_inputs_dir = os.path.join(INPUT_DATA_PATH, "covid_phl")
+PHL_doh_dest = os.path.join(phl_inputs_dir, "PHL_icu.csv")
+PHL_fassster_dest = os.path.join(phl_inputs_dir, "PHL_ConfirmedCases.zip")
+icu_dest = os.path.join(phl_inputs_dir, "PHL_icu_processed.csv")
+deaths_dest = os.path.join(phl_inputs_dir, "PHL_deaths_processed.csv")
+notifications_dest = os.path.join(phl_inputs_dir, "PHL_notifications_processed.csv")
 
 # start date to calculate time since Dec 31, 2019
 COVID_BASE_DATETIME = datetime(2019, 12, 31, 0, 0, 0)
@@ -43,6 +43,7 @@ def main():
         "NATIONAL CAPITAL REGION (NCR)",
         "REGION IV-A (CALABAR ZON)",
         "REGION VII (CENTRAL VISAYAS)",
+        "REGION XI (DAVAO REGION)",
     )
     working_df = duplicate_data(working_df, "region")
     working_df = filter_df_by_regions(working_df, "region")
@@ -50,7 +51,7 @@ def main():
 
     # Now fassster data
     working_df = copy_davao_city_to_region(fassster_filename)
-    working_df = rename_regions(working_df, "Region", "NCR", "4A", "07")
+    working_df = rename_regions(working_df, "Region", "NCR", "4A", "07", "11")
     working_df = duplicate_data(working_df, "Region")
     working_df = filter_df_by_regions(working_df, "Region")
     process_accumulated_death_data(working_df)
@@ -73,14 +74,19 @@ def fassster_data_filepath():
         for filename in os.listdir(phl_inputs_dir)
         if filename.startswith("ConfirmedCases_Final_")
     ]
-    fassster_filename = os.path.join(phl_inputs_dir ,fassster_filename[0])
+    fassster_filename = os.path.join(phl_inputs_dir, fassster_filename[0])
     return fassster_filename
 
 
-def rename_regions(df: pd.DataFrame, regionSpelling, ncrName, calName, cenVisName):
+def rename_regions(df: pd.DataFrame, regionSpelling, ncrName, calName, cenVisName, davName):
     # df = pd.read_csv(filePath)
     df[regionSpelling] = df[regionSpelling].replace(
-        {ncrName: "manila", calName: "calabarzon", cenVisName: "central_visayas"}
+        {
+            ncrName: "manila",
+            calName: "calabarzon",
+            cenVisName: "central_visayas",
+            davName: "davao_region",
+        }
     )
     return df
 
@@ -95,7 +101,14 @@ def duplicate_data(df: pd.DataFrame, regionSpelling):
 
 def filter_df_by_regions(df: pd.DataFrame, regionSpelling):
 
-    regions = ["calabarzon", "central_visayas", "manila", "davao_city", "philippines"]
+    regions = [
+        "calabarzon",
+        "central_visayas",
+        "manila",
+        "davao_city",
+        "davao_region",
+        "philippines",
+    ]
     df_regional = df[df[regionSpelling].isin(regions)]
     return df_regional
 
@@ -138,7 +151,7 @@ def process_notifications_data(df: pd.DataFrame):
     timeIndex = np.arange(
         min(fassster_data_agg["times"]), max(fassster_data_agg["times"]), 1.0
     ).tolist()
-    regions = ["calabarzon", "central_visayas", "manila", "davao_city", "philippines"]
+    regions = ["calabarzon", "central_visayas", "manila", "davao_city","davao_region", "philippines"]
     all_regions_x_times = pd.DataFrame(
         list(itertools.product(regions, timeIndex)), columns=["Region", "times"]
     )
@@ -169,7 +182,7 @@ def process_notifications_data(df: pd.DataFrame):
 
 
 def update_calibration_phl():
-    phl_regions = ["calabarzon", "central_visayas", "manila", "davao_city", "philippines"]
+    phl_regions = ["calabarzon", "central_visayas", "manila", "davao_city","davao_region", "philippines"]
     # read in csvs
     icu = pd.read_csv(icu_dest)
     deaths = pd.read_csv(deaths_dest)
@@ -177,8 +190,10 @@ def update_calibration_phl():
     for region in phl_regions:
         icu_tmp = icu.loc[icu["region"] == region]
         deaths_tmp = deaths.loc[deaths["Region"] == region]
-        notifications_tmp = notifications.loc[notifications["Region"] == region]
-        file_path = os.path.join(PROJECTS_PATH, "covid_19", "philippines", region, "timeseries.json")
+        notifications_tmp = notifications.loc[notifications["Region"] == region].dropna()
+        file_path = os.path.join(
+            PROJECTS_PATH, "covid_19", "philippines", region, "timeseries.json"
+        )
 
         with open(file_path, mode="r") as f:
             targets = json.load(f)

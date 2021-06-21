9e2e0b33c0c735542cfbe4720d307bd8ed015a3e
David Shipman <dshipman@gmail.com>
2021-06-21 14:04:38 +1000

    Add basic setup.py to allow using autumn as a module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='autumn',
+      version='0.1',
+      description='AuTuMN',
+      url='https://github.com/monash-emu/AuTuMN',
+      author='James Trauer',
+      author_email='james.trauer@monash.edu',
+      license='BSD-2-Clause',
+      packages=['autumn'],
+      zip_safe=False)

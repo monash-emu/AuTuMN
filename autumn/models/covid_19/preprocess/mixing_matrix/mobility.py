55f433cea0e084318f2af736f31e1bda5d7fd1d8
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2021-07-01 14:04:24 +1000

    adding SL scenarios and changing MLE

--- autumn/models/covid_19/preprocess/mixing_matrix/mobility.py
+++ autumn/models/covid_19/preprocess/mixing_matrix/mobility.py
@@ -202,6 +202,7 @@ def scale_prev_up_to_1(prev_vals: List[float], fraction: float):
 
 def close_gap_to_1(prev_vals: List[float], fraction: float):
     prev_val = prev_vals[-1]
+    print((1.0 - prev_val) * fraction + prev_val)
     return (1.0 - prev_val) * fraction + prev_val
 
 

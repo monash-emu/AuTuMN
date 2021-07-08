01f12277adc19f45fb2c4e99fc3a251daa265ac1
James Trauer <james.trauer@monash.edu>
2021-07-08 23:30:03 +1000

    Almost-final restructuring to the entry code

--- autumn/models/covid_19/preprocess/clinical.py
+++ autumn/models/covid_19/preprocess/clinical.py
@@ -12,156 +12,130 @@ from autumn.tools.utils.utils import (
 )
 NUM_AGE_STRATA = 16
 
-
-def get_entry_adjustments(abs_props, get_detected_proportion, early_rate):
-
-    adjustments = {}
-    for age_idx, agegroup in enumerate(AGEGROUP_STRATA):
-
-        # Function-based flow rates
-
-        # Get isolated rate for overwriting
-        get_abs_prop_isolated = get_abs_prop_isolated_factory(
-            age_idx, abs_props, get_detected_proportion
-        )
-
-        def isolate_flow_rate(
-            t, func=get_abs_prop_isolated
-        ):  # Function must be "bound" within loop
-            return func(t) * early_rate
-
-        # Get sympt non-hospital rate for overwriting
-        get_abs_prop_sympt_non_hospital = get_abs_prop_sympt_non_hospital_factory(
-            age_idx, abs_props, get_abs_prop_isolated
-        )
-
-        def sympt_non_hosp_rate(
-            t, func=get_abs_prop_sympt_non_hospital
-        ):  # Function must be "bound" within loop
-            return func(t) * early_rate
-
-        # Constant flow rates
-        clinical_non_sympt_rate = abs_props[Clinical.NON_SYMPT][age_idx] * early_rate
-        clinical_icu_rate = abs_props[Clinical.ICU][age_idx] * early_rate
-        hospital_non_icu_rate = abs_props[Clinical.HOSPITAL_NON_ICU][age_idx] * early_rate
-
-        # Age-specific adjustments object
-        adjustments[agegroup] = {
-            Clinical.NON_SYMPT: Overwrite(clinical_non_sympt_rate),
-            Clinical.ICU: Overwrite(clinical_icu_rate),
-            Clinical.HOSPITAL_NON_ICU: Overwrite(hospital_non_icu_rate),
-            Clinical.SYMPT_NON_HOSPITAL: Overwrite(sympt_non_hosp_rate),
-            Clinical.SYMPT_ISOLATE: Overwrite(isolate_flow_rate),
-        }
-
-    return adjustments
+"""
+General data processing functions
+"""
 
 
-def get_sympt_props(symptomatic_adjuster, hospital_adjuster, clinical_params):
+def apply_odds_ratio_to_multiple_proportions(props, adjuster):
+    return [apply_odds_ratio_to_proportion(i_prop, adjuster) for i_prop in props]
+
+
+def subdivide_props(base_props: np.ndarray, split_props: np.ndarray):
     """
-    Get the proportion of people in each clinical stratum, relative to total people in compartment.
+    Split an array (base_array) of proportions into two arrays (split_arr, complement_arr) according to the split
+    proportions provided (split_prop).
     """
-    symptomatic_props = get_proportion_symptomatic(clinical_params)
-    return get_absolute_strata_proportions(
-        symptomatic_props=symptomatic_props,
-        icu_props=clinical_params.icu_prop,
-        hospital_props=clinical_params.props.hospital.props,
-        symptomatic_props_multiplier=symptomatic_adjuster,
-        hospital_props_multiplier=hospital_adjuster,
-    )
+    split_arr = base_props * split_props
+    complement_arr = base_props * (1. - split_props)
+    return split_arr, complement_arr
 
 
-def get_proportion_symptomatic(clinical_params):
+"""
+Used in multiple sections
+"""
+
+
+def get_absolute_strata_proportions(symptomatic_props, icu_props, hospital_props):
     """
-    This is defined 8x10 year bands, 0-70+, which we transform into 16x5 year bands 0-75+.
+    Returns the proportion of people in each clinical stratum.
+    ie: Given all the people people who are infected, what proportion are in each strata?
+    Each of these are stratified into 16 age groups 0-75+
     """
-    return repeat_list_elements(2, clinical_params.props.symptomatic.props)
+
+    # Determine the absolute proportion of early exposed who become symptomatic as opposed to asymptomatic.
+    sympt, non_sympt = subdivide_props(1., np.array(symptomatic_props))
+
+    # Determine the absolute proportion of the symptomatics who become hospitalised vs not.
+    sympt_hospital, sympt_non_hospital = subdivide_props(sympt, np.array(hospital_props))  # sympt_non_hospital not used
+
+    # Determine the absolute proportion of those hospitalised who go to ICU versus those that don't.
+    sympt_hospital_icu, sympt_hospital_non_icu = subdivide_props(sympt_hospital, icu_props)
+
+    return {
+        Clinical.NON_SYMPT: non_sympt,
+        "sympt": sympt,  # Not a stratum, all those symptomatic
+        "hospital": sympt_hospital,  # Not a stratum, all those admitted to hospital
+        Clinical.ICU: sympt_hospital_icu,
+        Clinical.HOSPITAL_NON_ICU: sympt_hospital_non_icu,
+    }
+
+
+"""
+Entry-related functions
+"""
 
 
 def get_abs_prop_isolated_factory(age_idx, abs_props, prop_detect_among_sympt_func):
-    def get_abs_prop_isolated(t):
-        """
-        Returns the absolute proportion of infected becoming isolated at home.
-        Isolated people are those who are detected but not sent to hospital.
-        """
-        abs_prop_detected = abs_props["sympt"][age_idx] * prop_detect_among_sympt_func(t)
+    """
+    Returns the absolute proportion of infected becoming isolated at home.
+    Isolated people are those who are detected but not sent to hospital.
+    """
+
+    def get_abs_prop_isolated(time):
+        abs_prop_detected = abs_props["sympt"][age_idx] * prop_detect_among_sympt_func(time)
         abs_prop_isolated = abs_prop_detected - abs_props["hospital"][age_idx]
+
+        # If more people go to hospital than are detected, assume that no one is being detected/isolated
         if abs_prop_isolated < 0:
-            # If more people go to hospital than are detected, ignore detection
-            # proportion, and assume no one is being isolated.
-            abs_prop_isolated = 0
+            abs_prop_isolated = 0.
 
         return abs_prop_isolated
 
     return get_abs_prop_isolated
 
 
-def get_abs_prop_sympt_non_hospital_factory(age_idx, abs_props, get_abs_prop_isolated_func):
-    def get_abs_prop_sympt_non_hospital(t):
-        """
-        Returns the absolute proportion of infected not entering the hospital.
-        This also does not count people who are isolated.
-        This is only people who are not detected.
-        """
-        return (
-            abs_props["sympt"][age_idx]
-            - abs_props["hospital"][age_idx]
-            - get_abs_prop_isolated_func(t)
+def get_entry_adjustments(abs_props, get_detected_proportion, early_rate):
+    """
+    Gather together all the entry adjustments, two of which are functions of time and three are constant over time.
+    """
+    adjustments = {}
+    for age_idx, agegroup in enumerate(AGEGROUP_STRATA):
+
+        # Function-based flow rates
+
+        # Get isolated rate for overwriting
+        get_abs_prop_isolated = get_abs_prop_isolated_factory(age_idx, abs_props, get_detected_proportion)
+
+        def isolate_flow_rate(t, func=get_abs_prop_isolated):  # Function must be "bound" within loop
+            return func(t) * early_rate
+
+        # Get sympt non-hospital rate for overwriting
+        abs_prop_sympt_non_hospital_func = get_abs_prop_sympt_non_hospital_factory(
+            age_idx, abs_props, get_abs_prop_isolated
         )
 
-    return get_abs_prop_sympt_non_hospital
+        def sympt_non_hosp_rate(t, func=abs_prop_sympt_non_hospital_func):  # Function must be "bound" within loop
+            return func(t) * early_rate
+
+        # Constant flow rates
+        clinical_non_sympt_rate = abs_props[Clinical.NON_SYMPT][age_idx] * early_rate
+        hospital_non_icu_rate = abs_props[Clinical.HOSPITAL_NON_ICU][age_idx] * early_rate
+        clinical_icu_rate = abs_props[Clinical.ICU][age_idx] * early_rate
+
+        # Age-specific adjustments object
+        adjustments[agegroup] = {
+            Clinical.NON_SYMPT: Overwrite(clinical_non_sympt_rate),
+            Clinical.SYMPT_NON_HOSPITAL: Overwrite(sympt_non_hosp_rate),
+            Clinical.SYMPT_ISOLATE: Overwrite(isolate_flow_rate),
+            Clinical.HOSPITAL_NON_ICU: Overwrite(hospital_non_icu_rate),
+            Clinical.ICU: Overwrite(clinical_icu_rate),
+        }
+
+    return adjustments
 
 
-def get_absolute_strata_proportions(
-    symptomatic_props,
-    icu_props,
-    hospital_props,
-    symptomatic_props_multiplier,
-    hospital_props_multiplier,
-):
+def get_abs_prop_sympt_non_hospital_factory(age_idx, abs_props, get_abs_prop_isolated_func):
     """
-    Returns the proportion of people in each clinical stratum.
-    ie: Given all the people people who are infected, what proportion are in each strata?
-    Each of these are stratified into 16 age groups 0-75+
+    Returns the absolute proportion of infected not entering the hospital.
+    This also does not count people who are isolated/detected.
     """
-    # Apply multiplier to proportions
-    hospital_props = [
-        apply_odds_ratio_to_proportion(i_prop, hospital_props_multiplier)
-        for i_prop in hospital_props
-    ]
-    symptomatic_props = [
-        apply_odds_ratio_to_proportion(i_prop, symptomatic_props_multiplier)
-        for i_prop in symptomatic_props
-    ]
-
-    # Find the absolute progression proportions.
-    symptomatic_props_arr = np.array(symptomatic_props)
-    hospital_props_arr = np.array(hospital_props)
-    # Determine the absolute proportion of early exposed who become sympt vs non-sympt
-    sympt, non_sympt = subdivide_props(1, symptomatic_props_arr)
-    # Determine the absolute proportion of sympt who become hospitalized vs non-hospitalized.
-    sympt_hospital, sympt_non_hospital = subdivide_props(sympt, hospital_props_arr)
-    # Determine the absolute proportion of hospitalized who become icu vs non-icu.
-    sympt_hospital_icu, sympt_hospital_non_icu = subdivide_props(sympt_hospital, icu_props)
-
-    return {
-        "sympt": sympt,
-        "non_sympt": non_sympt,
-        "hospital": sympt_hospital,
-        "sympt_non_hospital": sympt_non_hospital,  # Over-ridden by a by time-varying proportion later
-        Clinical.ICU: sympt_hospital_icu,
-        Clinical.HOSPITAL_NON_ICU: sympt_hospital_non_icu,
-    }
+    return lambda t: abs_props["sympt"][age_idx] - abs_props["hospital"][age_idx] - get_abs_prop_isolated_func(t)
 
 
-def subdivide_props(base_props: np.ndarray, split_props: np.ndarray):
-    """
-    Split an array (base_array) of proportions into two arrays (split_arr, complement_arr)
-    according to the split proportions provided (split_prop).
-    """
-    split_arr = base_props * split_props
-    complement_arr = base_props * (1 - split_props)
-    return split_arr, complement_arr
+"""
+Death/exit-related functions
+"""
 
 
 def convert_ifr_agegroups(raw_ifr_props, iso3, pop_region, pop_year):
@@ -235,6 +209,11 @@ def get_rate_adjustments(rates):
     return rate_adjustments
 
 
+"""
+Master function
+"""
+
+
 def get_all_adjustments(
     clinical_params,
     country,
@@ -254,7 +233,14 @@ def get_all_adjustments(
     """
 
     # Work out the proportions of all infected people entering each clinical stratum.
-    abs_props = get_sympt_props(symptomatic_adjuster, hospital_adjuster, clinical_params)
+    symptomatic_props = repeat_list_elements(2, clinical_params.props.symptomatic.props)  # Convert to model age groups
+
+    # Apply odds ratio adjusters to proportions needing to be adjusted.
+    hospital_props = apply_odds_ratio_to_multiple_proportions(clinical_params.props.hospital.props, hospital_adjuster)
+    adjusted_symptomatic_props = apply_odds_ratio_to_multiple_proportions(symptomatic_props, symptomatic_adjuster)
+
+    # Get the proportions that don't vary over time, not all of these
+    abs_props = get_absolute_strata_proportions(adjusted_symptomatic_props, clinical_params.icu_prop, hospital_props)
 
     # Work out all the relevant sojourn times and the associated total rates at which they exit the compartments.
     # We assume everyone who dies does so at the end of their time in the "late active" compartment.
@@ -298,7 +284,7 @@ def get_all_adjustments(
     """
 
     # Scale raw IFR values according to adjuster parameter
-    adjusted_ifr_props = [apply_odds_ratio_to_proportion(i_prop, ifr_adjuster) for i_prop in raw_ifr_props]
+    adjusted_ifr_props = apply_odds_ratio_to_multiple_proportions(raw_ifr_props, ifr_adjuster)
 
     # Convert from provided age groups to model age groups
     final_ifr_props = convert_ifr_agegroups(adjusted_ifr_props, country.iso3, pop.region, pop.year)

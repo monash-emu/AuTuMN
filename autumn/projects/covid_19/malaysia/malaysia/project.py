536f0fc2955eb75222cd9016a50e144ffa321ccf
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2021-07-13 10:40:48 +1000

    setting to run  malaysia scenarios

--- autumn/projects/covid_19/malaysia/malaysia/project.py
+++ autumn/projects/covid_19/malaysia/malaysia/project.py
@@ -14,8 +14,7 @@ from autumn.projects.covid_19.calibration import COVID_GLOBAL_PRIORS
 # Load and configure model parameters.
 malaysia_path = build_rel_path("../malaysia/params/default.yml")
 default_path = build_rel_path("params/default.yml")
-scenario_dir_path = build_rel_path("params/")
-scenario_paths = get_all_available_scenario_paths(scenario_dir_path)
+scenario_paths = [build_rel_path(f"params/scenario-{i}.yml") for i in range(13, 16)]
 mle_path = build_rel_path("params/mle-params.yml")
 baseline_params = (
     base_params.update(malaysia_path).update(default_path).update(mle_path, calibration_format=True)

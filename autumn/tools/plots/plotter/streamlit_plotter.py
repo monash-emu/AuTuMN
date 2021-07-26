79b21f1972f5671fdcd368682835e0c95f244f58
Romain Ragonnet <romain.ragonnet@monash.edu>
2021-07-26 10:58:12 +1000

    Import Streamlit auto-save functionality into master

--- autumn/tools/plots/plotter/streamlit_plotter.py
+++ autumn/tools/plots/plotter/streamlit_plotter.py
@@ -21,12 +21,21 @@ class StreamlitPlotter(BasePlotter):
 
         st.pyplot(fig, dpi=dpi_request, bbox_inches="tight")
 
-        dir_name = "vic_figures"
-        if not os.path.isdir(dir_name):
-            os.makedirs(dir_name)
+
+class StreamlitSavingPlotter(StreamlitPlotter):
+    def save_figure(self, fig, filename: str, subdir=None, title_text=None, dpi_request=300, output_dirname="vic_figures"):
+        if title_text:
+            pretty_title = self.get_plot_title(title_text).replace("X", " ")
+            md = f"<p style='text-align: center;padding-left: 80px'>{pretty_title}</p>"
+            st.markdown(md, unsafe_allow_html=True)
+
+        st.pyplot(fig, dpi=dpi_request, bbox_inches="tight")
+
+        if not os.path.isdir(output_dirname):
+            os.makedirs(output_dirname)
 
         for extension in ["png", "pdf"]:
-            dir_extension_name = os.path.join(dir_name, extension)
+            dir_extension_name = os.path.join(output_dirname, extension)
             if not os.path.isdir(dir_extension_name):
                 os.makedirs(dir_extension_name)
             path = os.path.join(dir_extension_name, f"{filename}.{extension}")

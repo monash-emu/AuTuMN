2e614fa0c3716e587324c3d620940cd043960132
James Trauer <james.trauer@monash.edu>
2021-08-03 23:28:06 +1000

    Whoops, correct previous change

--- autumn/projects/covid_19/nepal/project.py
+++ autumn/projects/covid_19/nepal/project.py
@@ -35,8 +35,6 @@ priors = [
     UniformPrior("clinical_stratification.non_sympt_infect_multiplier", [0.15, 0.4]),
     BetaPrior("vaccination.vacc_prop_prevent_infection", mean=0.7, ci=[0.5, 0.9], sampling="lhs"),
     UniformPrior("vaccination.overall_efficacy", [0.0, 1.0], sampling="lhs"),
-    UniformPrior("voc_emergence.voc_strain(0).voc_components.contact_rate_multiplier", [1.2, 2.1]),
-    UniformPrior("voc_emergence.voc_strain(0).voc_components.start_time", [370, 400]),
 ]
 
 

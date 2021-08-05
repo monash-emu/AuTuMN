431e9fe2c6ad944654ea34cfda29f6444d104d76
Romain Ragonnet <romain.ragonnet@monash.edu>
2021-08-05 11:15:51 +1000

    VoC seeding in early exposed compartments

--- autumn/models/covid_19/model.py
+++ autumn/models/covid_19/model.py
@@ -149,7 +149,7 @@ def build_model(params: dict) -> CompartmentalModel:
             model.add_importation_flow(
                 f"seed_voc_{voc_name}",
                 make_voc_seed_func(characteristics.entry_rate, characteristics.start_time, characteristics.seed_duration),
-                dest=Compartment.EARLY_ACTIVE,
+                dest=Compartment.EARLY_EXPOSED,
                 dest_strata={"strain": voc_name},
             )
 

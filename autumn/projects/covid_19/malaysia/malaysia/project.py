ce721c9c21eb857222f4a552b64a01801448c949
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2021-08-10 16:18:40 +1000

    malaysia MLE update

--- autumn/projects/covid_19/malaysia/malaysia/project.py
+++ autumn/projects/covid_19/malaysia/malaysia/project.py
@@ -53,6 +53,7 @@ priors = [
     UniformPrior("clinical_stratification.icu_prop", [0.12, 0.25]),
     UniformPrior("clinical_stratification.non_sympt_infect_multiplier", [0.15, 0.4]),
     UniformPrior("clinical_stratification.props.symptomatic.multiplier", [0.01, 1.5]),
+    BetaPrior("vaccination.vacc_prop_prevent_infection", mean=0.7, ci=[0.5, 0.9], sampling="lhs"),
     UniformPrior("voc_emergence.alpha_beta.contact_rate_multiplier", [1.0, 2.75]),
     UniformPrior("voc_emergence.alpha_beta.start_time", [300, 400]),
 ]

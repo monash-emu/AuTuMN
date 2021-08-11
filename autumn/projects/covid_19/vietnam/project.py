c77744f07449919b5615dc640819ce051c683c77
Romain Ragonnet <romain.ragonnet@monash.edu>
2021-08-11 11:39:06 +1000

    Set up basic calibration for VNM

--- autumn/projects/covid_19/vietnam/project.py
+++ autumn/projects/covid_19/vietnam/project.py
@@ -1,9 +1,8 @@
 from autumn.tools.project import Project, ParameterSet, TimeSeriesSet, build_rel_path, get_all_available_scenario_paths
 from autumn.tools.calibration import Calibration
-from autumn.tools.calibration.priors import UniformPrior, BetaPrior
+from autumn.tools.calibration.priors import UniformPrior
 from autumn.tools.calibration.targets import (
     NormalTarget,
-    get_dispersion_priors_for_gaussian_targets,
 )
 from autumn.models.covid_19 import base_params, build_model
 from autumn.settings import Region, Models
@@ -21,8 +20,9 @@ scenario_params = [baseline_params.update(p) for p in scenario_paths]
 param_set = ParameterSet(baseline=baseline_params, scenarios=scenario_params)
 
 ts_set = TimeSeriesSet.from_file(build_rel_path("timeseries.json"))
-notifications_ts = ts_set.get("notifications").truncate_start_time(210)
-infection_deaths_ts = ts_set.get("infection_deaths").truncate_start_time(210).downsample(7)
+cutoff_time = 487  # 1 May 2021
+notifications_ts = ts_set.get("notifications").truncate_start_time(cutoff_time)
+infection_deaths_ts = ts_set.get("infection_deaths").truncate_start_time(cutoff_time)
 targets = [
     NormalTarget(notifications_ts),
     NormalTarget(infection_deaths_ts),
@@ -31,7 +31,9 @@ targets = [
 priors = [
     # Global COVID priors
     *COVID_GLOBAL_PRIORS,
-    UniformPrior("contact_rate", [0.015, 0.03]),
+    UniformPrior("infectious_seed", [10, 20]),
+    UniformPrior("contact_rate", [0.02, 0.03]),
+    UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.02, 0.07]),
 ]
 
 
@@ -46,5 +48,5 @@ with open(plot_spec_filepath) as f:
 
 
 project = Project(
-    Region.NEPAL, Models.COVID_19, build_model, param_set, calibration, plots=plot_spec
+    Region.VIETNAM, Models.COVID_19, build_model, param_set, calibration, plots=plot_spec
 )

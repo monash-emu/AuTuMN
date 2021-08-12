369cad7cd5a17c22483978cecceb4e14aef23d2d
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2021-08-12 18:20:15 +1000

    changing malaysia MLE

--- autumn/projects/covid_19/malaysia/malaysia/project.py
+++ autumn/projects/covid_19/malaysia/malaysia/project.py
@@ -42,20 +42,20 @@ priors = [
     # Dispersion parameters based on targets
     *get_dispersion_priors_for_gaussian_targets(targets),
     # Regional parameters
-    UniformPrior("contact_rate", [0.015, 0.06]),
-    UniformPrior("infectious_seed", [30.0, 200.0]),
+    UniformPrior("contact_rate", [0.015, 0.04]),
+    UniformPrior("infectious_seed", [30.0, 175.0]),
     # Detection
-    UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.02, 0.1]),
+    UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.02, 0.08]),
     # Microdistancing
-    UniformPrior("mobility.microdistancing.behaviour.parameters.upper_asymptote", [0.01, 0.4]),
+    UniformPrior("mobility.microdistancing.behaviour.parameters.upper_asymptote", [0.01, 0.3]),
     # Health system-related
     UniformPrior("clinical_stratification.props.hospital.multiplier", [0.7, 1.5]),
     UniformPrior("clinical_stratification.icu_prop", [0.12, 0.25]),
-    UniformPrior("clinical_stratification.non_sympt_infect_multiplier", [0.15, 0.4]),
+    UniformPrior("clinical_stratification.non_sympt_infect_multiplier", [0.35, 0.7]),
     UniformPrior("clinical_stratification.props.symptomatic.multiplier", [0.01, 1.5]),
     BetaPrior("vaccination.vacc_prop_prevent_infection", mean=0.7, ci=[0.5, 0.9], sampling="lhs"),
     UniformPrior("voc_emergence.alpha_beta.contact_rate_multiplier", [1.0, 2.75]),
-    UniformPrior("voc_emergence.alpha_beta.start_time", [300, 400]),
+    UniformPrior("voc_emergence.alpha_beta.start_time", [300, 380]),
 ]
 
 # Load proposal sds from yml file

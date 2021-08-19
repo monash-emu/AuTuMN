78d879acfe95a3591d2a9dbcfa86b0521164e263
Romain Ragonnet <romain.ragonnet@monash.edu>
2021-08-19 16:39:51 +1000

    Edit calibration config for HCMC

--- autumn/projects/covid_19/vietnam/ho_chi_minh_city/project.py
+++ autumn/projects/covid_19/vietnam/ho_chi_minh_city/project.py
@@ -20,15 +20,24 @@ scenario_params = [baseline_params.update(p) for p in scenario_paths]
 param_set = ParameterSet(baseline=baseline_params, scenarios=scenario_params)
 
 ts_set = TimeSeriesSet.from_file(build_rel_path("timeseries.json"))
-cutoff_time = 487  # 1 May 2021
-notifications_ts = ts_set.get("notifications").truncate_start_time(cutoff_time).moving_average(window=7).downsample(step=7)
-hosp_occupancy_ts = ts_set.get("hospital_occupancy").truncate_start_time(cutoff_time).moving_average(window=7).downsample(step=7)
-infection_deaths_ts = ts_set.get("infection_deaths").truncate_start_time(cutoff_time).moving_average(window=7).downsample(step=7)
-targets = [
-    NormalTarget(notifications_ts),
-    NormalTarget(hosp_occupancy_ts),
-    NormalTarget(infection_deaths_ts),
-]
+cutoff_time = 518  # 1 June 2021
+hosp_endtime = 582
+n_inflated_weight = 40
+
+targets = []
+for output_name in ["notifications", "hospital_occupancy", "infection_deaths"]:
+    if output_name == "hospital_occupancy":
+        series = ts_set.get(output_name).truncate_start_time(cutoff_time).truncate_end_time(hosp_endtime).moving_average(window=7)
+    else:
+        series = ts_set.get(output_name).truncate_start_time(cutoff_time).moving_average(window=7)
+
+    n = len(series.times)
+    max_weight = 10.
+    weights = [1.0 for _ in range(n - n_inflated_weight)] + [1.0 + (i + 1) * (max_weight - 1.) / n_inflated_weight for i in range(n_inflated_weight)]
+
+    targets.append(
+        NormalTarget(series, time_weights=weights)
+    )
 
 priors = [
     TruncNormalPrior(

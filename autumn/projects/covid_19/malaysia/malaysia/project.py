9eab38e096b6d7a5de78852730d2aea5901c080e
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2021-08-20 13:31:35 +1000

    fix icu and deaths timeseries

--- autumn/projects/covid_19/malaysia/malaysia/project.py
+++ autumn/projects/covid_19/malaysia/malaysia/project.py
@@ -28,9 +28,9 @@ param_set = ParameterSet(baseline=baseline_params, scenarios=scenario_params)
 
 ts_set = TimeSeriesSet.from_file(build_rel_path("timeseries.json"))
 notifications_ts = ts_set.get("notifications").truncate_start_time(210)
-icu_occupancy_ts = ts_set.get("icu_occupancy").moving_average(window=7).downsample(step=7)
-infection_deaths_ts = ts_set.get("infection_deaths").moving_average(window=7).downsample(step=7)
-hospital_occupancy_ts = ts_set.get("hospital_occupancy").moving_average(window=7).downsample(step=7)
+icu_occupancy_ts = ts_set.get("icu_occupancy").truncate_start_time(210).moving_average(window=7).downsample(step=7)
+infection_deaths_ts = ts_set.get("infection_deaths").truncate_start_time(210).moving_average(window=7).downsample(step=7)
+hospital_occupancy_ts = ts_set.get("hospital_occupancy").truncate_start_time(210).moving_average(window=7).downsample(step=7)
 
 targets = [
     NormalTarget(notifications_ts),

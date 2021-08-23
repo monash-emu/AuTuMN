184ab15335c278d14ff415e5074e3044baa5c2e9
milinda-abay <43531318+milinda-abay@users.noreply.github.com>
2021-08-23 10:43:48 +1000

    Update inputs.db and country application targets.

--- scripts/input_targets_philippines.py
+++ scripts/input_targets_philippines.py
@@ -16,8 +16,8 @@ from autumn.settings import PROJECTS_PATH
 from autumn.settings import INPUT_DATA_PATH
 
 # shareable google drive links
-PHL_doh_link = "18Nl5-BgMSzxFFuu70eQI8xgb-Zy4rEiw"  # sheet 05 daily report
-PHL_fassster_link = "1RCGP83B0Ap0vpWUuJjnESnp1IqUoJajc"
+PHL_doh_link = "1ZyW3V7ZOF_lRqL8g50vHVLu-_bcaxJIA"  # sheet 05 daily report
+PHL_fassster_link = "1HTceQXGhZHWW6LynAtP57nJQSpIip8Pt"
 
 # destination folders filepaths
 phl_inputs_dir = os.path.join(INPUT_DATA_PATH, "covid_phl")

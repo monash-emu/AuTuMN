a98c83810739781532ffabb29aa9b7dc290e8d0d
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2021-08-31 14:26:05 +1000

    SL update

--- autumn/projects/covid_19/sri_lanka/sri_lanka/project.py
+++ autumn/projects/covid_19/sri_lanka/sri_lanka/project.py
@@ -15,7 +15,7 @@ from autumn.projects.covid_19.calibration import COVID_GLOBAL_PRIORS
 
 # Load and configure model parameters.
 default_path = build_rel_path("params/default.yml")
-scenario_paths = [build_rel_path(f"params/scenario-{i}.yml") for i in range(1, 5)]
+scenario_paths = [build_rel_path(f"params/scenario-{i}.yml") for i in range(1, 6)]
 mle_path = build_rel_path("params/mle-params.yml")
 baseline_params = base_params.update(default_path).update(mle_path, calibration_format=True)
 scenario_params = [baseline_params.update(p) for p in scenario_paths]
@@ -39,13 +39,13 @@ priors = [
     UniformPrior("contact_rate", [0.018, 0.028]),
     UniformPrior("infectious_seed", [250.0, 320.0]),
     # Detection
-    UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.04, 0.07]),
-    UniformPrior("voc_emergence.alpha_beta.start_time", [395, 420]),
+    UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.045, 0.075]),
+    UniformPrior("voc_emergence.alpha_beta.start_time", [400, 420]),
     UniformPrior("voc_emergence.alpha_beta.contact_rate_multiplier", [2.5, 3]),
-    UniformPrior("voc_emergence.delta.start_time", [400, 465]),
+    UniformPrior("voc_emergence.delta.start_time", [430, 465]),
     UniformPrior("voc_emergence.delta.contact_rate_multiplier", [3.0, 4.2]),
     UniformPrior("contact_tracing.assumed_trace_prop", [0.4, 1.0]),
-    UniformPrior("infection_fatality.multiplier", [1.15, 2.8])
+    UniformPrior("infection_fatality.multiplier", [1.8, 3.0])
 ]
 
 # Load proposal sds from yml file

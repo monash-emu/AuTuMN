7acbfdcda86c2a6072243e03edbf4dbf653e2262
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2021-09-02 10:27:42 +1000

    changing regional MLE

--- autumn/projects/covid_19/malaysia/kuala_lumpur/project.py
+++ autumn/projects/covid_19/malaysia/kuala_lumpur/project.py
@@ -41,7 +41,7 @@ priors = [
     # Health system-related
     UniformPrior("clinical_stratification.icu_prop", [0.075, 0.175]),
     UniformPrior("clinical_stratification.non_sympt_infect_multiplier", [0.15, 0.6]),
-    UniformPrior("clinical_stratification.props.symptomatic.multiplier", [0.5, 1.5]),
+    UniformPrior("clinical_stratification.props.symptomatic.multiplier", [0.75, 1.75]),
     UniformPrior("infection_fatality.multiplier", [0.1, 1.0]),
     BetaPrior("vaccination.vacc_prop_prevent_infection", mean=0.7, ci=[0.5, 0.9], sampling="lhs"),
     UniformPrior("vaccination.coverage_override", [0.0, 1.0], sampling="lhs"),

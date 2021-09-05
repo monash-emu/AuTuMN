aefc1927861e0d5a0e6bc117892474fff9d952da
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2021-09-05 21:19:11 +1000

    downsampling SL notifications

--- autumn/projects/covid_19/sri_lanka/sri_lanka/project.py
+++ autumn/projects/covid_19/sri_lanka/sri_lanka/project.py
@@ -22,7 +22,7 @@ scenario_params = [baseline_params.update(p) for p in scenario_paths]
 param_set = ParameterSet(baseline=baseline_params, scenarios=scenario_params)
 
 ts_set = TimeSeriesSet.from_file(build_rel_path("timeseries.json"))
-notifications_ts = ts_set.get("notifications").truncate_start_time(350)
+notifications_ts = ts_set.get("notifications").truncate_start_time(350).moving_average(window=7).downsample(step=7)
 death_ts = ts_set.get("infection_deaths").truncate_start_time(350)
 targets = [
     NormalTarget(notifications_ts),

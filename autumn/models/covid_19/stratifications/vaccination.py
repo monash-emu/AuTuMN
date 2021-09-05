b4996ea71a4b3ef38ae8a3c30087c9cad6c94d50
James Trauer <james.trauer@monash.edu>
2021-09-05 22:59:07 +1000

    Update to most of what I had on my previous branch

--- autumn/models/covid_19/stratifications/vaccination.py
+++ autumn/models/covid_19/stratifications/vaccination.py
@@ -4,8 +4,9 @@ from autumn.models.covid_19.constants import (
     COMPARTMENTS, DISEASE_COMPARTMENTS, Vaccination, VACCINATION_STRATA, INFECTION, VACCINATED_CATEGORIES
 )
 from autumn.models.covid_19.parameters import Parameters
-from autumn.models.covid_19.preprocess.vaccination import \
+from autumn.models.covid_19.preprocess.vaccination import (
     add_clinical_adjustments_to_strat, add_vaccine_infection_and_severity
+)
 
 
 def get_vaccination_strat(params: Parameters) -> Stratification:
@@ -13,6 +14,7 @@ def get_vaccination_strat(params: Parameters) -> Stratification:
     Create the vaccination stratification as two strata applied to the whole model.
     """
 
+    # Apply requested vaccination strata to all of the model's compartments
     vacc_strat = Stratification("vaccination", VACCINATION_STRATA, COMPARTMENTS)
 
     # Everyone starts out unvaccinated
@@ -31,16 +33,10 @@ def get_vaccination_strat(params: Parameters) -> Stratification:
     symptomatic_adjuster *= params.clinical_stratification.props.symptomatic.multiplier
     ifr_adjuster *= params.infection_fatality.multiplier
 
-    # Add the clinical adjustments parameters as overwrites in the same way as for vaccination
+    # Add the clinical adjustments parameters as overwrites in the same way as for history
     vacc_strat = add_clinical_adjustments_to_strat(
-        vacc_strat,
-        Vaccination.UNVACCINATED,
-        Vaccination.FULLY_VACCINATED,
-        params,
-        symptomatic_adjuster,
-        hospital_adjuster,
-        ifr_adjuster,
-        params.infection_fatality.top_bracket_overwrite,
+        vacc_strat, Vaccination.UNVACCINATED, Vaccination.FULLY_VACCINATED, params, symptomatic_adjuster,
+        hospital_adjuster, ifr_adjuster, params.infection_fatality.top_bracket_overwrite,
         extra_stratum=Vaccination.ONE_DOSE,
     )
 

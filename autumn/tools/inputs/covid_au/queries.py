6cb8cb02703cdbce1c4f76987f4bbfb1879b3e74
milinda-abay <43531318+milinda-abay@users.noreply.github.com>
2021-09-08 21:36:29 +1000

    Victoria 2021 (#290)

--- autumn/tools/inputs/covid_au/queries.py
+++ autumn/tools/inputs/covid_au/queries.py
@@ -4,10 +4,10 @@ import numpy as np
 import pandas as pd
 
 from autumn.tools.inputs.database import get_input_db
-from autumn.tools.utils.utils import apply_moving_average
+from autumn.tools.utils.utils import apply_moving_average, COVID_BASE_DATETIME
 
 COVID_BASE_DATE = date(2019, 12, 31)
-COVID_BASE_DATETIME = datetime(2019, 12, 31, 0, 0, 0)
+
 
 
 def get_vic_testing_numbers():
@@ -44,3 +44,25 @@ def get_dhhs_testing_numbers(cluster: str = None):
     epsilon = 1e-6  # A really tiny number to avoid having any zeros
     avg_vals = np.array(apply_moving_average(test_values, 7)) + epsilon
     return test_dates, avg_vals
+
+
+def get_dhhs_vaccination_numbers(cluster: str = None):
+    """
+    Returns number of vaccinations administered in Victoria.
+    """
+    input_db = get_input_db()
+
+    if cluster is None:
+        df = input_db.query("covid_dhhs_victoria_2021", columns=["date_index", "dose_1"])
+        df = df.groupby("date", as_index=False).sum()
+    else:
+        df = input_db.query(
+            "covid_dhhs_victoria_2021", columns=["date_index", "dose_1"], conditions={"cluster_name": cluster}
+        )
+    date_str_to_int = lambda s: (datetime.strptime(s, "%Y-%m-%d") - COVID_BASE_DATETIME).days
+
+    vac_dates = df.date.to_numpy()
+    test_values = df.test.to_numpy()
+    epsilon = 1e-6  # A really tiny number to avoid having any zeros
+    avg_vals = np.array(apply_moving_average(test_values, 7)) + epsilon
+    return vac_dates, avg_vals

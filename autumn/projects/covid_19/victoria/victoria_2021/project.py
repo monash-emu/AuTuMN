99b5434978fbc6fae7ca674faf4a7fd20b9b4b9d
James Trauer <james.trauer@monash.edu>
2021-09-10 10:24:52 +1000

    Remove calibration parameter to get Vic tests passing

--- autumn/projects/covid_19/victoria/victoria_2021/project.py
+++ autumn/projects/covid_19/victoria/victoria_2021/project.py
@@ -80,7 +80,6 @@ priors = [
     UniformPrior("victorian_clusters.intercluster_mixing", [0.005, 0.05], jumping_stdev=0.01),
     UniformPrior("infectious_seed", [20., 70.], jumping_stdev=4.),
     UniformPrior("clinical_stratification.non_sympt_infect_multiplier", [0.2, 0.8], jumping_stdev=0.05),
-    UniformPrior("infection_fatality.top_bracket_overwrite", [0.05, 0.3], jumping_stdev=0.04),
     UniformPrior("clinical_stratification.props.hospital.multiplier", [0.5, 5.0], jumping_stdev=0.4),
     UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.05, 0.3], jumping_stdev=0.04),
     TruncNormalPrior(

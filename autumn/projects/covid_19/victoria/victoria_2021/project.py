03ed4ade7c221a992a76299c8e954359c99f2209
James Trauer <james.trauer@monash.edu>
2021-09-12 21:28:56 +1000

    Include marginal posteriors from 2020 as 2021 priors

--- autumn/projects/covid_19/victoria/victoria_2021/project.py
+++ autumn/projects/covid_19/victoria/victoria_2021/project.py
@@ -7,6 +7,7 @@ from autumn.tools.calibration.targets import NormalTarget, PoissonTarget
 from autumn.models.covid_19 import base_params, build_model
 from autumn.settings import Region, Models
 
+
 # TODO: Get calibration running
 # TODO: Apply David's marginal posteriors as priors code
 # TODO: Possibly move testing function out to a more obvious point in the code (less important)
@@ -69,6 +70,31 @@ for region in regions_for_multipliers:
     # Shouldn't be too peaked with these values
     prior = TruncNormalPrior(name, mean=1.0, stdev=0.5, trunc_range=[0.5, np.inf], jumping_stdev=0.15)
     cluster_priors.append(prior)
+
+# Marginal distributions of Vic 2020 to consider as priors for Vic 2021
+# "victorian_clusters.contact_rate_multiplier_regional", norm (0.7070792993624084, 0.11538988453463195)
+# "sojourn.compartment_periods_calculated.exposed.total_period", norm (6.095798813756773, 0.7810560402997285)
+# "sojourn.compartment_periods_calculated.active.total_period", norm (6.431724510638751, 0.6588899585941116)
+# "victorian_clusters.contact_rate_multiplier_regional", norm (0.7070792993624084, 0.11538988453463195)
+# "sojourn.compartment_periods.icu_early", norm (13.189283389438017, 3.267836334270357)
+# "victorian_clusters.metro.mobility.microdistancing.behaviour_adjuster.parameters.effect",
+#  norm (0.3336881545907932, 0.12974271665347392)
+#  or beta (2.233261027002466, 1.7150557025357558, 0.00300823791519224, 0.5850818483284497)
+# "victorian_clusters.metro.mobility.microdistancing.face_coverings_adjuster.parameters.effect",
+#  norm (0.4590192843551404, 0.054643498605008924)
+#  or beta (2.233261027002466, 1.7150557025357558, 0.00300823791519224, 0.5850818483284497)
+# "contact_rate", updated (0.005097283966437761, 0.04484184883556176)
+# "clinical_stratification.non_sympt_infect_multiplier",
+#  beta (5.070057160691058, 2.0783831204948724, -0.04627612686595504, 0.8467253773323684)
+# "clinical_stratification.props.hospital.multiplier", norm (3.072957401469314, 0.9230093569298286)
+# "testing_to_detection.assumed_cdr_parameter", norm (0.1875980041535647, 0.05487574154515127)
+# "clinical_stratification.icu_prop",
+#  norm (0.1875980041535647, 0.05487574154515127)
+#  or beta (2.1990413238757105, 1.8012738113610243, 0.05194745495028011, 0.24786655960440956)
+# "target_output_ratio",
+#  beta (2.3143351886463726, 1.0958870124857243, 0.19372944320390947, 0.5061375024454435)
+#  or norm (0.5376752874675825, 0.11298858887538074)
+# "contact_tracing.assumed_trace_prop", uniform (0.20052289494754472, 0.29896766288137805)
 
 incubation_period_name = "sojourn.compartment_periods_calculated.exposed.total_period"
 active_period_name = "sojourn.compartment_periods_calculated.active.total_period"

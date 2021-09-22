e24d48e5267f6739468f5cc13df16ce4f6091a3d
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2021-09-23 00:25:08 +1000

    SL priors update

--- autumn/projects/covid_19/sri_lanka/sri_lanka/project.py
+++ autumn/projects/covid_19/sri_lanka/sri_lanka/project.py
@@ -37,14 +37,14 @@ priors = [
     *get_dispersion_priors_for_gaussian_targets(targets),
     # Regional parameters
     UniformPrior("contact_rate", [0.02, 0.028]),
-    UniformPrior("infectious_seed", [50.0, 350.0]),
+    UniformPrior("infectious_seed", [150.0, 350.0]),
     # Detection
-    UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.005, 0.01]),
+    UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.0065, 0.01]),
     UniformPrior("infection_fatality.multiplier", [0.25, 0.75]),
-    UniformPrior("clinical_stratification.props.symptomatic.multiplier", [1.5, 3.0]),
+    UniformPrior("clinical_stratification.props.symptomatic.multiplier", [1.5, 2.3]),
     UniformPrior("contact_tracing.assumed_trace_prop", [0.075, 0.2]),
     #VoC
-    UniformPrior("voc_emergence.alpha_beta.start_time", [385, 425]),
+    UniformPrior("voc_emergence.alpha_beta.start_time", [395, 425]),
     UniformPrior("voc_emergence.alpha_beta.contact_rate_multiplier", [2.7, 3.5]),
     UniformPrior("voc_emergence.delta.start_time", [475, 550]),
     UniformPrior("voc_emergence.delta.contact_rate_multiplier", [5.2, 6.5]),

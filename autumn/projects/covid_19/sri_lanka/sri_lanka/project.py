0092d2f7fcdc0ff19f640c0ccb114ed588bbe448
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2021-09-23 00:26:33 +1000

    SL proposal SD

--- autumn/projects/covid_19/sri_lanka/sri_lanka/project.py
+++ autumn/projects/covid_19/sri_lanka/sri_lanka/project.py
@@ -51,7 +51,7 @@ priors = [
 ]
 
 # Load proposal sds from yml file
-# use_tuned_proposal_sds(priors, build_rel_path("proposal_sds.yml"))
+use_tuned_proposal_sds(priors, build_rel_path("proposal_sds.yml"))
 
 calibration = Calibration(priors, targets)
 

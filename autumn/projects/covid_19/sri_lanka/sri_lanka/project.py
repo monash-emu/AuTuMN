1da37829f17d055b045c3a7c5fd1a3b5d86d0018
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2021-09-23 21:27:49 +1000

    SL priors

--- autumn/projects/covid_19/sri_lanka/sri_lanka/project.py
+++ autumn/projects/covid_19/sri_lanka/sri_lanka/project.py
@@ -39,15 +39,15 @@ priors = [
     UniformPrior("contact_rate", [0.02, 0.024]),
     UniformPrior("infectious_seed", [150.0, 350.0]),
     # Detection
-    UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.007, 0.01]),
-    UniformPrior("infection_fatality.multiplier", [0.25, 0.75]),
-    UniformPrior("clinical_stratification.props.symptomatic.multiplier", [1.5, 2.3]),
+    UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.008, 0.01]),
+    UniformPrior("infection_fatality.multiplier", [0.25, 0.65]),
+    UniformPrior("clinical_stratification.props.symptomatic.multiplier", [1.7, 2.3]),
     UniformPrior("contact_tracing.assumed_trace_prop", [0.65, 0.95]),
     #VoC
     UniformPrior("voc_emergence.alpha_beta.start_time", [395, 425]),
     UniformPrior("voc_emergence.alpha_beta.contact_rate_multiplier", [2.9, 3.2]),
     UniformPrior("voc_emergence.delta.start_time", [475, 550]),
-    UniformPrior("voc_emergence.delta.contact_rate_multiplier", [5.2, 6.5]),
+    UniformPrior("voc_emergence.delta.contact_rate_multiplier", [5.0, 6.0]),
 ]
 
 # Load proposal sds from yml file

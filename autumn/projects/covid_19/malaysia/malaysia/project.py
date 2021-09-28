cfde9afc0cb4e9fa75d1afc0849696cf3e039f99
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2021-09-28 10:56:14 +1000

    MYS priors

--- autumn/projects/covid_19/malaysia/malaysia/project.py
+++ autumn/projects/covid_19/malaysia/malaysia/project.py
@@ -42,17 +42,17 @@ priors = [
     # Dispersion parameters based on targets
     *get_dispersion_priors_for_gaussian_targets(targets),
     # Regional parameters
-    UniformPrior("contact_rate", [0.02, 0.024]),
+    UniformPrior("contact_rate", [0.022, 0.0236]),
     UniformPrior("infectious_seed", [25.0, 150.0]),
     # Detection
     UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.005, 0.05]),
-    UniformPrior("infection_fatality.multiplier", [0.75, 2.5]),
+    UniformPrior("infection_fatality.multiplier", [1.75, 2.8]),
     UniformPrior("clinical_stratification.icu_prop", [0.1, 0.2]),
-    UniformPrior("mobility.microdistancing.behaviour.parameters.upper_asymptote", [0.09, 0.12]),
+    UniformPrior("mobility.microdistancing.behaviour.parameters.upper_asymptote", [0.09, 0.11]),
     UniformPrior("clinical_stratification.props.symptomatic.multiplier", [0.5, 0.7]),
-    UniformPrior("voc_emergence.alpha_beta.contact_rate_multiplier", [1.23, 1.7]),
+    UniformPrior("voc_emergence.alpha_beta.contact_rate_multiplier", [1.4, 1.7]),
     UniformPrior("voc_emergence.delta.contact_rate_multiplier", [2.65, 2.85]),
-    UniformPrior("voc_emergence.alpha_beta.start_time", [310, 360]),
+    UniformPrior("voc_emergence.alpha_beta.start_time", [320, 360]),
     UniformPrior("voc_emergence.delta.start_time", [468, 475]),
 ]
 

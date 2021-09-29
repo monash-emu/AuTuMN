ffdb6a068e531728c04c74891f6fc08f17d6523d
maba0001 <milinda.abayawardana@monash.edu>
2021-09-29 15:23:29 +1000

    Still not working

--- autumn/projects/covid_19/victoria/north_metro/project.py
+++ autumn/projects/covid_19/victoria/north_metro/project.py
@@ -1,7 +1,6 @@
 import numpy as np
-import numpy as np
 
-from autumn.tools.project import Project, ParameterSet, TimeSeriesSet, build_rel_path
+from autumn.tools.project import Project, ParameterSet, TimeSeriesSet, build_rel_path, use_tuned_proposal_sds
 from autumn.tools.calibration import Calibration
 from autumn.tools.calibration.priors import UniformPrior, TruncNormalPrior
 from autumn.tools.calibration.targets import NormalTarget, PoissonTarget
@@ -13,7 +12,7 @@ from autumn.settings import Region, Models
 default_path = build_rel_path("params/default.yml")
 mle_path = build_rel_path("params/mle-params.yml")
 scenario_paths = [build_rel_path("params/scenario-1.yml")]
-baseline_params = base_params.update(default_path)
+baseline_params = base_params.update(default_path).update(mle_path, calibration_format=True)
 scenario_params = [baseline_params.update(p) for p in scenario_paths]
 param_set = ParameterSet(baseline=baseline_params, scenarios=[scenario_params])
 
@@ -99,7 +98,7 @@ targets = [
     PoissonTarget(ts_set.get("icu_admissions").truncate_start_time(target_start_time)),
 ]
 # Load proposal sds from yml file
-# use_tuned_proposal_sds(priors, build_rel_path("proposal_sds.yml"))
+use_tuned_proposal_sds(priors, build_rel_path("proposal_sds.yml"))
 
 calibration = Calibration(
     priors,

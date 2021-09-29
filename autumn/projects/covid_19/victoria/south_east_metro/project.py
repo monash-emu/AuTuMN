4dd738fe5d34dc6ec5a4107011a822673e5fa3c7
James Trauer <james.trauer@monash.edu>
2021-09-29 22:30:38 +1000

    Actually get rid of the default parameter files entirely

--- autumn/projects/covid_19/victoria/south_east_metro/project.py
+++ autumn/projects/covid_19/victoria/south_east_metro/project.py
@@ -8,7 +8,7 @@ from autumn.projects.covid_19.victoria.region_calibration import priors
 
 # Load and configure model parameters
 vic_base_path = build_rel_path("../vic_submodel_params.yml")
-baseline_params = base_params.update(vic_base_path).update(build_rel_path("params/default.yml"))
+baseline_params = base_params.update(vic_base_path)
 param_set = ParameterSet(baseline=baseline_params, scenarios=[])
 
 # Load and configure calibration settings

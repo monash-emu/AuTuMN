f54e7561d6a01ed43b64467754db8ff0394a1f90
maba0001 <milinda.abayawardana@monash.edu>
2021-09-29 14:32:11 +1000

    Configure north_metro

--- autumn/projects/covid_19/victoria/north_metro/project.py
+++ autumn/projects/covid_19/victoria/north_metro/project.py
@@ -1,7 +1,7 @@
 from autumn.tools.project import Project, ParameterSet, TimeSeriesSet, build_rel_path
 from autumn.tools.calibration import Calibration, priors, targets
 from autumn.tools.calibration.priors import UniformPrior
-from autumn.tools.calibration.targets import NormalTarget
+from autumn.tools.calibration.targets import NormalTarget, PoissonTarget
 from autumn.models.example import base_params, build_model
 from autumn.settings import Region, Models
 
@@ -9,22 +9,25 @@ from autumn.settings import Region, Models
 # Load and configure model parameters.
 default_path = build_rel_path("params/default.yml")
 mle_path = build_rel_path("params/mle-params.yml")
-baseline_params = base_params.update(build_rel_path("params/baseline.yml"))
+scenario_paths = [build_rel_path("params/scenario-1.yml")]
+baseline_params = base_params.update(default_path)
 scenario_1_params = baseline_params.update(build_rel_path("params/scenario-1.yml"))
 param_set = ParameterSet(baseline=baseline_params, scenarios=[scenario_1_params])
 
+ts_set = TimeSeriesSet.from_file(build_rel_path("targets.secret.json"))
+target_start_time = 600
 
 # Load and configure calibration settings.
-ts_set = TimeSeriesSet.from_file(build_rel_path("timeseries.json"))
+ts_set = TimeSeriesSet.from_file(build_rel_path("targets.secret.json"))
 priors = [
     UniformPrior("contact_rate", [0.025, 0.05]),
     UniformPrior("recovery_rate", [0.9, 1.2]),
 ]
 targets = [
-    NormalTarget(
-        timeseries=ts_set["prevalence_infectious"],
-        time_weights=list(range(1, len(ts_set["prevalence_infectious"].times) + 1)),
-    )
+    PoissonTarget(ts_set.get("notifications").round_values().truncate_start_time(target_start_time)),
+    PoissonTarget(ts_set.get("hospital_admissions").truncate_start_time(target_start_time)),
+    PoissonTarget(ts_set.get("icu_admissions").truncate_start_time(target_start_time)),
+    
 ]
 calibration = Calibration(priors=priors, targets=targets)
 project = Project(Region.NORTH_METRO, Models.EXAMPLE, build_model, param_set, calibration)

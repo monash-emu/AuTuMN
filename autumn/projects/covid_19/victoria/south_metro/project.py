29017ecc81fb255c04e9e7d42632860ad714a84d
James Trauer <james.trauer@monash.edu>
2021-09-29 21:27:17 +1000

    Get the models to be actually Covid models

--- autumn/projects/covid_19/victoria/south_metro/project.py
+++ autumn/projects/covid_19/victoria/south_metro/project.py
@@ -2,14 +2,13 @@ from autumn.tools.project import Project, ParameterSet, TimeSeriesSet, build_rel
 from autumn.tools.calibration import Calibration, priors, targets
 from autumn.tools.calibration.priors import UniformPrior
 from autumn.tools.calibration.targets import NormalTarget
-from autumn.models.example import base_params, build_model
+from autumn.models.covid_19 import base_params, build_model
 from autumn.settings import Region, Models
 
 
 # Load and configure model parameters.
 baseline_params = base_params.update(build_rel_path("params/default.yml"))
-scenario_1_params = baseline_params.update(build_rel_path("params/scenario-1.yml"))
-param_set = ParameterSet(baseline=baseline_params, scenarios=[scenario_1_params])
+param_set = ParameterSet(baseline=baseline_params, scenarios=[])
 
 
 # Load and configure calibration settings.

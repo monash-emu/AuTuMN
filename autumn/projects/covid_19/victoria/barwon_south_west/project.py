00bb2c666a7c81f00ad81ce874c325b5981d65b7
James Trauer <james.trauer@monash.edu>
2021-10-01 06:15:03 +1000

    Get Vic hospital admission data in (#333)

--- autumn/projects/covid_19/victoria/barwon_south_west/project.py
+++ autumn/projects/covid_19/victoria/barwon_south_west/project.py
@@ -16,6 +16,10 @@ param_set = ParameterSet(baseline=baseline_params)
 # Load and configure calibration settings
 ts_set = TimeSeriesSet.from_file(build_rel_path("targets.secret.json"))
 notifications_ts = ts_set.get("notifications").truncate_start_time(454)
-targets = [NormalTarget(timeseries=notifications_ts)]
+hospital_admissions_ts = ts_set.get("hospital_admissions").truncate_start_time(454)
+targets = [
+    NormalTarget(timeseries=notifications_ts),
+    NormalTarget(timeseries=hospital_admissions_ts)
+]
 calibration = Calibration(priors=priors, targets=targets)
 project = Project(Region.BARWON_SOUTH_WEST, Models.COVID_19, build_model, param_set, calibration)

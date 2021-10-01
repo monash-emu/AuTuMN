a84118faee4dca017480a59483b3c01e5345fd52
James Trauer <james.trauer@monash.edu>
2021-10-01 21:08:48 +1000

    More todos

--- autumn/projects/covid_19/victoria/region_calibration.py
+++ autumn/projects/covid_19/victoria/region_calibration.py
@@ -2,6 +2,8 @@ from autumn.tools.calibration.priors import UniformPrior
 
 # TODO: Get as much calibration code as possible into this file
 # TODO: Check out vaccination and do visualisation for it in the inputs notebook
+# TODO: Un-bury CDR function creation
+# TODO: Look at contact tracing computed values
 
 priors = [
     UniformPrior("contact_rate", [0.025, 0.05]),

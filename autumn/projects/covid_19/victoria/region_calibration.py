04bcf6a76a249a65b5f6e000b063e964bd53e95e
James Trauer <james.trauer@monash.edu>
2021-10-01 21:06:17 +1000

    Get really basic manual calibration going

--- autumn/projects/covid_19/victoria/region_calibration.py
+++ autumn/projects/covid_19/victoria/region_calibration.py
@@ -1,5 +1,8 @@
 from autumn.tools.calibration.priors import UniformPrior
 
+# TODO: Get as much calibration code as possible into this file
+# TODO: Check out vaccination and do visualisation for it in the inputs notebook
+
 priors = [
     UniformPrior("contact_rate", [0.025, 0.05]),
 ]

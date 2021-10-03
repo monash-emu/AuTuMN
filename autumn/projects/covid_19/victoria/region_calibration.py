51e3cfd24ce2f8cae6d2249e18c2a9980ebd5414
James Trauer <james.trauer@monash.edu>
2021-10-03 15:26:42 +1100

    Generalise code using a loop

--- autumn/projects/covid_19/victoria/region_calibration.py
+++ autumn/projects/covid_19/victoria/region_calibration.py
@@ -3,6 +3,7 @@ from autumn.tools.calibration.targets import NormalTarget
 
 # TODO: Look at contact tracing computed values
 
+# Specify the general features of the calibration
 target_start_time = 454
 priors = [
     UniformPrior("contact_rate", [0.025, 0.05]),
@@ -10,18 +11,27 @@ priors = [
 
 
 def collate_regional_targets(ts_set):
-    notifications_ts = ts_set.get("notifications").truncate_start_time(target_start_time)
-    hospital_admissions_ts = ts_set.get("hospital_admissions").truncate_start_time(target_start_time)
-    return [
-        NormalTarget(timeseries=notifications_ts),
-        NormalTarget(timeseries=hospital_admissions_ts)
-    ]
+    """
+    Collate all the regional targets using the set of timeseries created in the project.py file from the application
+    folder.
+    """
+
+    targets = []
+    for target_name in ("notifications", "hospital_admissions"):
+        targets.append(
+            NormalTarget(timeseries=ts_set.get(target_name).truncate_start_time(target_start_time))
+        )
+    return targets
 
 
 def collate_metro_targets(ts_set):
-    notifications_ts = ts_set.get("notifications").truncate_start_time(target_start_time)
-    hospital_admissions_ts = ts_set.get("hospital_admissions").truncate_start_time(target_start_time)
-    return [
-        NormalTarget(timeseries=notifications_ts),
-        NormalTarget(timeseries=hospital_admissions_ts)
-    ]
+    """
+    As for function above, except for Metropolitan Melbourne clusters.
+    """
+
+    targets = []
+    for target_name in ("notifications", "hospital_admissions"):
+        targets.append(
+            NormalTarget(timeseries=ts_set.get(target_name).truncate_start_time(target_start_time))
+        )
+    return targets

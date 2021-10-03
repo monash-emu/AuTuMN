8c66056007478344ecce2e926f4db7b396ea22ba
James Trauer <james.trauer@monash.edu>
2021-10-03 15:53:34 +1100

    Simplify vic region calibration (#339)
    * Unbury CDR function
    
    * Update todo list
    
    * ... and update again
    
    * Move targets collation into common files
    
    * Make changes for all project files
    
    * Generalise code using a loop
    
    * ... and move requests to top of that file

--- autumn/projects/covid_19/victoria/region_calibration.py
+++ autumn/projects/covid_19/victoria/region_calibration.py
@@ -1,8 +1,39 @@
 from autumn.tools.calibration.priors import UniformPrior
+from autumn.tools.calibration.targets import NormalTarget
 
-# TODO: Get as much calibration code as possible into this file
 # TODO: Look at contact tracing computed values
 
+# Specify the general features of the calibration
+target_start_time = 454
 priors = [
     UniformPrior("contact_rate", [0.025, 0.05]),
 ]
+regional_target_names = ("notifications", "hospital_admissions")
+metro_target_names = ("notifications", "hospital_admissions")
+
+
+def collate_regional_targets(ts_set):
+    """
+    Collate all the regional targets using the set of timeseries created in the project.py file from the application
+    folder.
+    """
+
+    targets = []
+    for target_name in regional_target_names:
+        targets.append(
+            NormalTarget(timeseries=ts_set.get(target_name).truncate_start_time(target_start_time))
+        )
+    return targets
+
+
+def collate_metro_targets(ts_set):
+    """
+    As for function above, except for Metropolitan Melbourne clusters.
+    """
+
+    targets = []
+    for target_name in metro_target_names:
+        targets.append(
+            NormalTarget(timeseries=ts_set.get(target_name).truncate_start_time(target_start_time))
+        )
+    return targets

06bf1f109a0929e4f2790c5216096709dbf16cef
James Trauer <james.trauer@monash.edu>
2021-10-03 15:21:52 +1100

    Make changes for all project files

--- autumn/projects/covid_19/victoria/south_east_metro/project.py
+++ autumn/projects/covid_19/victoria/south_east_metro/project.py
@@ -1,10 +1,8 @@
 from autumn.tools.project import Project, ParameterSet, TimeSeriesSet, build_rel_path
 from autumn.tools.calibration import Calibration
-from autumn.tools.calibration.targets import NormalTarget
 from autumn.models.covid_19 import base_params, build_model
 from autumn.settings import Region, Models
-from autumn.projects.covid_19.victoria.region_calibration import priors
-
+from autumn.projects.covid_19.victoria.region_calibration import priors, collate_metro_targets
 
 # Load and configure model parameters
 vic_base_path = build_rel_path("../vic_submodel_params.yml")
@@ -14,11 +12,6 @@ param_set = ParameterSet(baseline=baseline_params)
 
 # Load and configure calibration settings
 ts_set = TimeSeriesSet.from_file(build_rel_path("targets.secret.json"))
-notifications_ts = ts_set.get("notifications").truncate_start_time(454)
-hospital_admissions_ts = ts_set.get("hospital_admissions").truncate_start_time(454)
-targets = [
-    NormalTarget(timeseries=notifications_ts),
-    NormalTarget(timeseries=hospital_admissions_ts)
-]
+targets = collate_metro_targets(ts_set)
 calibration = Calibration(priors=priors, targets=targets)
 project = Project(Region.SOUTH_EAST_METRO, Models.COVID_19, build_model, param_set, calibration)

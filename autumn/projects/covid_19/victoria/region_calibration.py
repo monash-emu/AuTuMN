f791548d8d2c20237892a372c2af87ba9de9df75
James Trauer <james.trauer@monash.edu>
2021-10-03 15:16:10 +1100

    Move targets collation into common files

--- autumn/projects/covid_19/victoria/region_calibration.py
+++ autumn/projects/covid_19/victoria/region_calibration.py
@@ -1,8 +1,27 @@
 from autumn.tools.calibration.priors import UniformPrior
+from autumn.tools.calibration.targets import NormalTarget
 
-# TODO: Get as much calibration code as possible into this file
 # TODO: Look at contact tracing computed values
 
+target_start_time = 454
 priors = [
     UniformPrior("contact_rate", [0.025, 0.05]),
 ]
+
+
+def collate_regional_targets(ts_set):
+    notifications_ts = ts_set.get("notifications").truncate_start_time(target_start_time)
+    hospital_admissions_ts = ts_set.get("hospital_admissions").truncate_start_time(target_start_time)
+    return [
+        NormalTarget(timeseries=notifications_ts),
+        NormalTarget(timeseries=hospital_admissions_ts)
+    ]
+
+
+def collate_metro_targets(ts_set):
+    notifications_ts = ts_set.get("notifications").truncate_start_time(target_start_time)
+    hospital_admissions_ts = ts_set.get("hospital_admissions").truncate_start_time(target_start_time)
+    return [
+        NormalTarget(timeseries=notifications_ts),
+        NormalTarget(timeseries=hospital_admissions_ts)
+    ]

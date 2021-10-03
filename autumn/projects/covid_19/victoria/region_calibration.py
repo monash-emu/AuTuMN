a3fe473ec45fea76b329c3cd07f7c2f111e810f3
James Trauer <james.trauer@monash.edu>
2021-10-03 15:03:17 +1100

    Update todo list

--- autumn/projects/covid_19/victoria/region_calibration.py
+++ autumn/projects/covid_19/victoria/region_calibration.py
@@ -2,7 +2,6 @@ from autumn.tools.calibration.priors import UniformPrior
 
 # TODO: Get as much calibration code as possible into this file
 # TODO: Check out vaccination and do visualisation for it in the inputs notebook
-# TODO: Un-bury CDR function creation
 # TODO: Look at contact tracing computed values
 
 priors = [

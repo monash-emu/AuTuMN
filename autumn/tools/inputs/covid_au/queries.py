dcbaa34a0a177c332e2995e71201809a2eaa6dac
James Trauer <james.trauer@monash.edu>
2021-10-04 21:18:44 +1100

    Add option for getting data from both vaccines

--- autumn/tools/inputs/covid_au/queries.py
+++ autumn/tools/inputs/covid_au/queries.py
@@ -75,6 +75,7 @@ def get_historical_vac_coverage(
     avg_vals = np.array(apply_moving_average(coverage_values, 7)) + TINY_NUMBER
     return vac_dates, avg_vals
 
+
 def update_cond_map(cluster, cond_map):
     if cluster is None:
         cluster = "Victoria"
@@ -99,6 +100,23 @@ def get_modelled_vac_num(input_db, cond_map, dose):
     )
 
     return df
+
+
+def get_both_vac_coverage(cluster: str=None, start_age: int=0, end_age: int=89, dose="dose_1"):
+    """
+    Use the following function (get_modelled_vac_coverage) to get the same data out for both vaccines from data provided
+    by Vida at the Department.
+    Returns data in the same format as for the individual vaccines.
+    """
+
+    # Extract the data
+    az_times, az_values = get_modelled_vac_coverage(cluster, start_age, end_age, vaccine="astra_zeneca", dose=dose)
+    pfizer_times, pfizer_values = get_modelled_vac_coverage(cluster, start_age, end_age, vaccine="pfizer", dose=dose)
+
+    assert all(az_times == pfizer_times), "Modelled coverage times are different for Pfizer and Astra-Zeneca"
+    times = az_times
+    both_values = az_values + pfizer_values
+    return times, both_values
 
 
 def get_modelled_vac_coverage(
@@ -132,7 +150,6 @@ def get_modelled_vac_coverage(
     coverage_values = df.cml_coverage.to_numpy()
     avg_vals = np.array(apply_moving_average(coverage_values, 7)) + TINY_NUMBER
     return vac_dates, avg_vals
-
 
 
 def get_pop(cluster, start_age, end_age, input_db):

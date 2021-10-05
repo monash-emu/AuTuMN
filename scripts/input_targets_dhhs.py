e953d99eada6af2c8c60f4221342162f8d0afddb
maba0001 <milinda.abayawardana@monash.edu>
2021-10-05 12:12:42 +1100

    Update DHHS targets and vaccination coverage

--- scripts/input_targets_dhhs.py
+++ scripts/input_targets_dhhs.py
@@ -20,7 +20,8 @@ COVID_DHHS_DEATH_CSV = os.path.join(COVID_AU_DIRPATH, "monashmodelextract_deaths
 COVID_DHHS_CASE_CSV = os.path.join(COVID_AU_DIRPATH, "monashmodelextract_cases.secret.csv")
 COVID_DHHS_ADMN_CSV = os.path.join(COVID_AU_DIRPATH, "monashmodelextract_admissions.secret.csv")
 COVID_DHHS_VAC_CSV = os.path.join(COVID_AU_DIRPATH, "monashmodelextract_vaccination.secret.csv")
-COVID_DHHS_MODEL_CSV = os.path.join(COVID_AU_DIRPATH, "vac_by_week_lga.secret.csv")
+COVID_VIDA_VAC_CSV = os.path.join(COVID_AU_DIRPATH, "vida_vac.secret.csv")
+COVID_VIDA_POP_CSV = os.path.join(COVID_AU_DIRPATH, "vida_pop.csv")
 
 
 COVID_VAC_CSV = os.path.join(COVID_AU_DIRPATH, "vac_cov.csv")
@@ -185,7 +186,7 @@ def main():
     df = fetch_vac_model()
     df = preprocess_vac_model(df)
 
-    df.to_csv(COVID_DHHS_MODEL_CSV)
+    df.to_csv(COVID_VIDA_VAC_CSV,index=False)
 
 
 def preprocess_admissions():
@@ -381,7 +382,7 @@ def load_deaths(df):
 def fetch_vac_model():
 
     
-    df = pd.read_csv(COVID_DHHS_MODEL_CSV, usecols=range(0, 6))
+    df = pd.read_csv(os.path.join(COVID_AU_DIRPATH, "vac_by_week_lga.secret.csv"), usecols=[0,1,2,3,4,5,8,])
     create_date_index(COVID_BASE_DATETIME, df, "week")
     df.lga.replace(fix_lga, inplace=True)
 
@@ -418,3 +419,18 @@ if __name__ == "__main__":
 
 
 
+# df = pd.read_csv(os.path.join(COVID_AU_DIRPATH, 'vac_by_week_lga.secret.csv'), usecols=[1,2,8,])
+# df = df.drop_duplicates()
+# df.lga.replace(fix_lga, inplace=True)
+# cluster_map_df = pd.read_csv(COVID_DHHS_CLUSTERS_CSV)
+# df = df.merge(cluster_map_df, left_on=["lga"], right_on=["lga_name"], how="left")
+# df.loc[df.cluster_id.isna(), ["cluster_id", "cluster_name", "proportion"]] = [0, "VIC", 1]
+# df.cluster_id.replace(CLUSTER_MAP, inplace=True)
+# df['popn'] = df.popn* df.proportion
+# df = df[["cluster_id", 'age_group', 'popn']].groupby(['cluster_id','age_group'], as_index=False).sum()
+# df.age_group.replace({"85+": "85-89"}, inplace=True)
+# df["start_age"] = df["age_group"].apply(lambda s: int(s.split("-")[0]))
+# df["end_age"] = df["age_group"].apply(lambda s: int(s.split("-")[1]))
+# df.to_csv(COVID_VIDA_POP_CSV,index=False)
+
+# df

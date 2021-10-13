16c677e53875ea1f8f74ec9f7ce4352a7c0a2a2b
maba0001 <milinda.abayawardana@monash.edu>
2021-10-14 09:42:48 +1100

    Fix Myanmar configuration

--- autumn/projects/covid_19/myanmar/project.py
+++ autumn/projects/covid_19/myanmar/project.py
@@ -1,9 +1,10 @@
 from autumn.tools.project import Project, ParameterSet, TimeSeriesSet, build_rel_path
-from autumn.tools.calibration import Calibration, priors, targets
+from autumn.tools.calibration import Calibration
 from autumn.tools.calibration.priors import UniformPrior
 from autumn.tools.calibration.targets import NormalTarget
-from autumn.models.example import base_params, build_model
+from autumn.models.covid_19 import base_params, build_model
 from autumn.settings import Region, Models
+from autumn.projects.covid_19.calibration import COVID_GLOBAL_PRIORS
 
 
 # Load and configure model parameters.
@@ -11,18 +12,20 @@ default_params = base_params.update(build_rel_path("params/default.yml"))
 scenario_1_params = default_params.update(build_rel_path("params/scenario-1.yml"))
 param_set = ParameterSet(baseline=default_params, scenarios=[scenario_1_params])
 
-
 # Load and configure calibration settings.
 ts_set = TimeSeriesSet.from_file(build_rel_path("timeseries.json"))
-priors = [
-    UniformPrior("contact_rate", [0.025, 0.05]),
-    UniformPrior("recovery_rate", [0.9, 1.2]),
-]
+notifications_ts = ts_set.get("notifications").truncate_start_time(500)
+infection_deaths_ts = ts_set.get("infection_deaths").truncate_start_time(500).downsample(7)
 targets = [
-    NormalTarget(
-        timeseries=ts_set["notifications"],
-        time_weights=list(range(1, len(ts_set["notifications"].times) + 1)),
-    )
+    NormalTarget(notifications_ts),
+    NormalTarget(infection_deaths_ts),
+]
+
+priors = [
+    # Global COVID priors
+    *COVID_GLOBAL_PRIORS,
+    UniformPrior("contact_rate", [0.025, 0.05]),
+        
 ]
 calibration = Calibration(priors=priors, targets=targets)
 project = Project(Region.MYANMAR, Models.COVID_19, build_model, param_set, calibration)

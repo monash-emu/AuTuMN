0d47e3a133e8b091e1306c59f777183dfb6cbd4c
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2021-10-14 23:54:55 +1100

    SL priors and MLE

--- autumn/projects/covid_19/sri_lanka/sri_lanka/project.py
+++ autumn/projects/covid_19/sri_lanka/sri_lanka/project.py
@@ -40,8 +40,8 @@ priors = [
     UniformPrior("contact_rate", [0.021, 0.025]),
     UniformPrior("infectious_seed", [250.0, 375.0]),
     # Detection
-    UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.001, 0.007]),
-    UniformPrior("infection_fatality.multiplier", [0.08, 0.4]),
+    UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.001, 0.005]),
+    UniformPrior("infection_fatality.multiplier", [0.075, 0.25]),
     TruncNormalPrior("clinical_stratification.props.symptomatic.multiplier", mean=1.0,\
                      stdev=0.5, trunc_range=[0.0, np.inf]),
     UniformPrior("contact_tracing.assumed_trace_prop", [0.825, 0.95]),

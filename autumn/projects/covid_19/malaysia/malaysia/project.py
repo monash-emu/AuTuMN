41095265a2e8f516923071f356545cb565dc6ce0
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2021-10-15 12:07:16 +1100

    MYS priors and MLE

--- autumn/projects/covid_19/malaysia/malaysia/project.py
+++ autumn/projects/covid_19/malaysia/malaysia/project.py
@@ -42,18 +42,22 @@ priors = [
     # Dispersion parameters based on targets
     *get_dispersion_priors_for_gaussian_targets(targets),
     # Regional parameters
-    UniformPrior("contact_rate", [0.022, 0.0236]),
-    UniformPrior("infectious_seed", [25.0, 150.0]),
+    UniformPrior("contact_rate", [0.015, 0.04]),
+    UniformPrior("infectious_seed", [250.0, 850.0]),
     # Detection
-    UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.005, 0.05]),
-    UniformPrior("infection_fatality.multiplier", [1.75, 2.8]),
-    UniformPrior("clinical_stratification.icu_prop", [0.1, 0.2]),
-    UniformPrior("mobility.microdistancing.behaviour.parameters.upper_asymptote", [0.09, 0.11]),
-    UniformPrior("clinical_stratification.props.symptomatic.multiplier", [0.5, 0.7]),
-    UniformPrior("voc_emergence.alpha_beta.contact_rate_multiplier", [1.4, 1.7]),
-    UniformPrior("voc_emergence.delta.contact_rate_multiplier", [2.65, 2.85]),
-    UniformPrior("voc_emergence.alpha_beta.start_time", [320, 360]),
-    UniformPrior("voc_emergence.delta.start_time", [468, 475]),
+    UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.005, 0.02]),
+    # Microdistancing
+    UniformPrior("mobility.microdistancing.behaviour.parameters.upper_asymptote", [0.05, 0.5]),
+    # Health system-related
+    UniformPrior("clinical_stratification.icu_prop", [0.05, 0.15]),
+    UniformPrior("clinical_stratification.non_sympt_infect_multiplier", [0.15, 0.6]),
+    UniformPrior("clinical_stratification.props.symptomatic.multiplier", [0.5, 1.5]),
+    UniformPrior("infection_fatality.multiplier", [0.1, 1.0]),
+    #VoC parameters
+    UniformPrior("voc_emergence.alpha_beta.contact_rate_multiplier", [1, 1.7]),
+    UniformPrior("voc_emergence.alpha_beta.start_time", [300, 400]),
+    UniformPrior("voc_emergence.delta.contact_rate_multiplier", [2.6, 2.85]),
+    UniformPrior("voc_emergence.delta.start_time", [468, 520]),
 ]
 
 # Load proposal sds from yml file

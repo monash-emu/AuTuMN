b7bf4a96d18d7080781608b8dcf3d5fbe585b169
James Trauer <james.trauer@monash.edu>
2021-11-03 09:21:09 +1100

    Finalise parameter checks

--- autumn/models/covid_19/parameters.py
+++ autumn/models/covid_19/parameters.py
@@ -525,10 +525,10 @@ class Vaccination(BaseModel):
     def check_vacc_range(cls, values):
 
         second_dose_delay = values["second_dose_delay"]
-        msg = f"Days to second dose is less than one: {second_dose_delay}"
-        if type(second_dose_delay) == float:
+        msg = f"Days to second dose is less than one"
+        if isinstance(second_dose_delay, (float, int)):
             assert second_dose_delay > 1., msg
-        else:
+        elif type(second_dose_delay) == TanhScaleup:
             assert second_dose_delay["start_asymptote"] > 1., msg
             assert second_dose_delay["end_asymptote"] > 1., msg
         return values

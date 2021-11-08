0f16a6eba31c10cb00aad34d483d54a8109e19dc
James Trauer <james.trauer@monash.edu>
2021-11-08 21:21:06 +1100

    Whoops, didn't mean that

--- autumn/projects/covid_19/myanmar/project.py
+++ autumn/projects/covid_19/myanmar/project.py
@@ -19,7 +19,7 @@ notifications_ts = ts_set.get("notifications").truncate_start_time(471)
 infection_deaths_ts = ts_set.get("infection_deaths").truncate_start_time(471)
 targets = [
     NormalTarget(notifications_ts),
-    # NormalTarget(infection_deaths_ts),
+    NormalTarget(infection_deaths_ts),
 ]
 
 priors = [

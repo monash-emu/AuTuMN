93e224d36d9194c990b3cd6656d116cc28392b17
milinda-abay <43531318+milinda-abay@users.noreply.github.com>
2021-11-09 16:11:06 +1100

    Input updates (#407) Refactor survey and include 'avoid_contact' table

--- autumn/tools/inputs/covid_survey/preprocess.py
+++ autumn/tools/inputs/covid_survey/preprocess.py
@@ -13,17 +13,30 @@ CSV_FILES = [
 ]
 MASKS = [file for file in CSV_FILES if "mask_" in file]
 
+AVOID_CONTACT = [file for file in CSV_FILES if "avoid_contact" in file]
+
+def preproc_csv(files):
+    df = pd.concat(map(pd.read_csv, files))
+    df.loc[:, "survey_date"] = pd.to_datetime(
+        df["survey_date"], format="%Y%m%d", infer_datetime_format=False
+    )
+
+    create_date_index(COVID_BASE_DATETIME, df, "survey_date")
+    return df
+
 
 def preprocess_covid_survey(input_db: Database):
     df = get_mask()
     input_db.dump_df("survey_mask", df)
 
+    df = get_avoid_contact()
+    input_db.dump_df("survey_avoid_contact", df)
+
+
 def get_mask():
-    df = pd.concat(map(pd.read_csv, MASKS))
-    df.loc[:, "survey_date"] = pd.to_datetime(
-        df["survey_date"], format="%Y%m%d", infer_datetime_format=False
-    )
-    create_date_index(COVID_BASE_DATETIME, df, "survey_date")
+    df = preproc_csv(MASKS)
+
+
     df = df[
         [
             "iso_code",
@@ -35,6 +48,24 @@ def get_mask():
             "mc_se",
             "percent_mc_unw",
             "mc_se_unw",
+        ]
+    ]
+
+    return df
+
+def get_avoid_contact():
+    df = preproc_csv(AVOID_CONTACT)
+    df = df[
+        [
+            "iso_code",
+            "region",
+            "date",
+            "date_index",
+            "sample_size",
+            "pct_avoid_contact",
+            "avoid_contact_se",
+            "pct_avoid_contact_unw",
+            "avoid_contact_se_unw",
         ]
     ]
 
--- autumn/tools/inputs/covid_survey/queries.py
+++ autumn/tools/inputs/covid_survey/queries.py
@@ -8,19 +8,30 @@ TODAY = (pd.to_datetime("today").date() - COVID_BASE_DATETIME.date()).days
 
 
 def get_percent_mc(iso3: str = None, col_name: str = "percent_mc"):
-    input_db = get_input_db()
-    df = input_db.query(
-        "survey_mask", columns=["date_index", col_name], conditions={"iso_code": iso3}
-    )
-    mask_dates = df["date_index"].to_numpy()
-    mask_values = df[col_name].to_numpy()
 
-    valid_dates = all(1 <= mask_dates) and all(mask_dates <= TODAY)
-    valid_values = all(0 <= mask_values) and all(mask_values <= 1)
+    return get_survey_results(iso3, col_name, "survey_mask")
+
+
+def get_percent_avoid_contact(iso3: str = None, col_name: str = "pct_avoid_contact"):
+    return get_survey_results(iso3, col_name, "survey_avoid_contact")
+
+
+def get_survey_results(iso3, col_name, table_name):
+    input_db = get_input_db()
+    df = input_db.query(table_name, columns=["date_index", col_name], conditions={"iso_code": iso3})
+    dates = df["date_index"].to_numpy()
+    values = df[col_name].to_numpy()
+
+    valid_dates = all(1 <= dates) and all(dates <= TODAY)
+    valid_values = all(0 <= values) and all(
+        values <= 1
+    )  # Assuming it's the percentage column for now.
+
 
     if not valid_dates:
         raise AssertionError("Date index out of range")
     if not valid_values:
         raise AssertionError("Percentage values out of logical bounds")
 
-    return mask_dates, mask_values
+
+    return dates, values

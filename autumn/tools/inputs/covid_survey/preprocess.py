2f3fc0db94d65e7c7b699987e0faf6284d161def
maba0001 <milinda.abayawardana@monash.edu>
2021-11-09 15:20:43 +1100

    Refactor covid survey

--- autumn/tools/inputs/covid_survey/preprocess.py
+++ autumn/tools/inputs/covid_survey/preprocess.py
@@ -12,18 +12,27 @@ CSV_FILES = [
     os.path.join(INPUT_DATA_PATH, "covid_survey", file) for file in os.listdir(COVID_SURVEY_PATH)
 ]
 MASKS = [file for file in CSV_FILES if "mask_" in file]
+AVOID_CONTACT = [file for file in CSV_FILES if "avoid_contact" in file]
 
+def preproc_csv(files):
+    df = pd.concat(map(pd.read_csv, files))
+    df.loc[:, "survey_date"] = pd.to_datetime(
+        df["survey_date"], format="%Y%m%d", infer_datetime_format=False
+    )
+
+    create_date_index(COVID_BASE_DATETIME, df, "survey_date")
+    return df
 
 def preprocess_covid_survey(input_db: Database):
     df = get_mask()
     input_db.dump_df("survey_mask", df)
+    df = get_avoid_contact()
+    input_db.dump_df("survey_avoid_contact", df)
+
 
 def get_mask():
-    df = pd.concat(map(pd.read_csv, MASKS))
-    df.loc[:, "survey_date"] = pd.to_datetime(
-        df["survey_date"], format="%Y%m%d", infer_datetime_format=False
-    )
-    create_date_index(COVID_BASE_DATETIME, df, "survey_date")
+    df = preproc_csv(MASKS)
+
     df = df[
         [
             "iso_code",
@@ -39,3 +48,25 @@ def get_mask():
     ]
 
     return df
+
+
+def get_avoid_contact():
+    df = preproc_csv(AVOID_CONTACT)
+    df = df[
+        [
+            "iso_code",
+            "region",
+            "date",
+            "date_index",
+            "sample_size",
+            "pct_avoid_contact",
+            "avoid_contact_se",
+            "pct_avoid_contact_unw",
+            "avoid_contact_se_unw",
+        ]
+    ]
+
+    return df
+
+
+

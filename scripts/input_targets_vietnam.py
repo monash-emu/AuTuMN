5a1295634816ec525d97f0786243b2d45c5891a3
milinda-abay <43531318+milinda-abay@users.noreply.github.com>
2021-11-12 10:01:56 +1100

    Update targets for Hanoi (#410)

--- scripts/input_targets_vietnam.py
+++ scripts/input_targets_vietnam.py
@@ -15,25 +15,34 @@ from autumn.tools.utils.utils import create_date_index
 
 # Use OWID csv for notification and death numbers.
 COVID_OWID = os.path.join(INPUT_DATA_PATH, "owid", "owid-covid-data.csv")
-COVID_VNM_TARGETS = os.path.join(PROJECTS_PATH, "covid_19", "vietnam", "vietnam", "timeseries.json")
-COVID_VNM_HCMC_TARGETS = os.path.join(
+VIETNAM_TIMESERIES = os.path.join(
+    PROJECTS_PATH, "covid_19", "vietnam", "vietnam", "timeseries.json"
+)
+HCMC_TIMESERIES = os.path.join(
     PROJECTS_PATH, "covid_19", "vietnam", "ho_chi_minh_city", "timeseries.json"
 )
-COVID_VMN_HCMC_CASES_CSV = os.path.join(INPUT_DATA_PATH, "covid_vnm", "cases.csv")
-COVID_VNM_HCMC_TEST_CSV = os.path.join(INPUT_DATA_PATH, "covid_vnm", "testing.csv")
-COVID_VNM_HCMC_URL = "https://docs.google.com/spreadsheets/d/11Z4HyBomVjI9VCbQrGFfti9CwAe_PZM6tbSzv7IA9LI/export?format=xlsx&id=11Z4HyBomVjI9VCbQrGFfti9CwAe_PZM6tbSzv7IA9LI"
+HANOI_TIMESERIES = os.path.join(PROJECTS_PATH, "covid_19", "vietnam", "hanoi", "timeseries.json")
+
+
+HCMC_DATA_CSV = os.path.join(INPUT_DATA_PATH, "covid_vnm", "cases.csv")
+HANOI_DATA_CSV = os.path.join(INPUT_DATA_PATH, "covid_vnm", "hanoi_data.csv")
+HCMC_DATA_URL = "https://docs.google.com/spreadsheets/d/11Z4HyBomVjI9VCbQrGFfti9CwAe_PZM6tbSzv7IA9LI/export?format=xlsx&id=11Z4HyBomVjI9VCbQrGFfti9CwAe_PZM6tbSzv7IA9LI"
+HANOI_DATA_URL = "https://docs.google.com/spreadsheets/d/10QEfHx9AeV6V4AoFaxHnNiRpAyavUYboKwwwqcKIsTo/export?format=xlsx&id=10QEfHx9AeV6V4AoFaxHnNiRpAyavUYboKwwwqcKIsTo"
+
 
 TARGET_MAP_VNM = {
     "notifications": "new_cases",
     "infection_deaths": "new_deaths",
 }
 
-TARGET_MAP_VNM_HCMC = {
+TARGET_MAP_HCMC = {
     "notifications": "notifications",
     "infection_deaths": "infection_deaths",
     "hospital_occupancy": "hospital_occupancy",
-    "icu_occupancy":"icu_occupancy",
+    "icu_occupancy": "icu_occupancy",
 }
+
+TARGET_MAP_HANOI = {"notifications": "notifications", "infection_deaths": "infection_deaths"}
 
 
 def preprocess_vnm_data():
@@ -44,12 +53,19 @@ def preprocess_vnm_data():
 
     return df
 
+
 # Update VNM targets per OWID
 df = preprocess_vnm_data()
-update_timeseries(TARGET_MAP_VNM, df, COVID_VNM_TARGETS)
+update_timeseries(TARGET_MAP_VNM, df, VIETNAM_TIMESERIES)
 
 # Update HCMC targets
-df_cases = pd.read_excel(COVID_VNM_HCMC_URL, usecols=[1,2,3,4,5])
+df_cases = pd.read_excel(HCMC_DATA_URL, usecols=[1, 2, 3, 4, 5])
 df_cases = create_date_index(COVID_BASE_DATETIME, df_cases, "Unnamed:_1")
-df_cases.to_csv(COVID_VMN_HCMC_CASES_CSV)
-update_timeseries(TARGET_MAP_VNM_HCMC, df_cases, COVID_VNM_HCMC_TARGETS)
+df_cases.to_csv(HCMC_DATA_CSV)
+update_timeseries(TARGET_MAP_HCMC, df_cases, HCMC_TIMESERIES)
+
+# Update HANOI targets
+df_cases = pd.read_excel(HANOI_DATA_URL)
+df_cases = create_date_index(COVID_BASE_DATETIME, df_cases, "date")
+df_cases.to_csv(HANOI_DATA_CSV)
+update_timeseries(TARGET_MAP_HANOI, df_cases, HANOI_TIMESERIES)

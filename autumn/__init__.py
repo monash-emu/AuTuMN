f06a84149c667cf0bfc661decd089e25457ffbff
James Trauer <james.trauer@monash.edu>
2021-11-15 22:13:57 +1100

    Remove Vic super-model projects

--- autumn/__init__.py
+++ autumn/__init__.py
@@ -23,20 +23,7 @@ register_project(Models.TB, Region.PHILIPPINES, "autumn.projects.tuberculosis.ph
 
 # Example projects
 register_project(Models.EXAMPLE, Region.PHILIPPINES, "autumn.projects.example.philippines.project")
-register_project(Models.EXAMPLE, Region.VICTORIA_2020, "autumn.projects.example.victoria.project")
 
-# COVID: Victoria state-wide super-model
-register_project(
-    Models.COVID_19,
-    Region.VICTORIA_2020,
-    "autumn.projects.covid_19.victoria.victoria_2020.project",
-)
-
-register_project(
-    Models.COVID_19,
-    Region.VICTORIA_2021,
-    "autumn.projects.covid_19.victoria.victoria_2021.project",
-)
 
 register_project(
     Models.COVID_19,

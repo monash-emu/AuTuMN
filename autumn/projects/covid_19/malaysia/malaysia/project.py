71a0217461e92b259aaf856fc8137625396beac7
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2021-11-18 00:34:48 +1100

    remove ICU targets

--- autumn/projects/covid_19/malaysia/malaysia/project.py
+++ autumn/projects/covid_19/malaysia/malaysia/project.py
@@ -36,7 +36,6 @@ infection_deaths_ts = ts_set.get("infection_deaths").truncate_start_time(210) #.
 targets = [
     NormalTarget(notifications_ts),
     NormalTarget(infection_deaths_ts),
-    NormalTarget(icu_occupancy_ts),
 ]
 
 priors = [

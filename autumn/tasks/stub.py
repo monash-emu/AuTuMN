a65493a7f15f5174ba75d9fb710cc3027b9eb7ad
David Shipman <dshipman@gmail.com>
2021-11-17 15:51:15 +1100

    Better stub task logging

--- autumn/tasks/stub.py
+++ autumn/tasks/stub.py
@@ -3,4 +3,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 def run_stub(**kwargs):
-    logger.info(kwargs)
+    for k, v in kwargs.items():
+        msg = f"Key: {k} Value: {v}"
+        logger.info(msg)

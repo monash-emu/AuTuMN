05c43398f36d125927ff15729536d81601d76b65
James Trauer <james.trauer@monash.edu>
2021-11-23 15:04:52 +1100

    Apply hospitalisation adjustments to stratifications

--- autumn/models/covid_19/model.py
+++ autumn/models/covid_19/model.py
@@ -147,8 +147,8 @@ def build_model(params: dict, build_options: dict = None) -> CompartmentalModel:
     Variants of concern stratification.
     """
 
-    voc_ifr_effect = {"wild": 1.}
-    voc_hosp_effect = {"wild": 1.}
+    voc_ifr_effects = {"wild": 1.}
+    voc_hosp_effects = {"wild": 1.}
     if params.voc_emergence:
         voc_params = params.voc_emergence
 
@@ -164,8 +164,8 @@ def build_model(params: dict, build_options: dict = None) -> CompartmentalModel:
             )
 
         # Get the adjustments to the IFR for each strain (if not requested, will have defaulted to a value of one)
-        voc_ifr_effect.update({s: params.voc_emergence[s].ifr_multiplier for s in strain_strat.strata[1:]})
-        voc_hosp_effect.update({s: params.voc_emergence[s].hosp_multiplier for s in strain_strat.strata[1:]})
+        voc_ifr_effects.update({s: params.voc_emergence[s].ifr_multiplier for s in strain_strat.strata[1:]})
+        voc_hosp_effects.update({s: params.voc_emergence[s].hosp_multiplier for s in strain_strat.strata[1:]})
 
     """
     Clinical stratification.
@@ -177,7 +177,7 @@ def build_model(params: dict, build_options: dict = None) -> CompartmentalModel:
     get_detected_proportion = find_cdr_function_from_test_data(
         params.testing_to_detection, country.iso3, override_test_region, pop.year
     )
-    clinical_strat = get_clinical_strat(params, voc_ifr_effect, voc_hosp_effect)
+    clinical_strat = get_clinical_strat(params, voc_ifr_effects, voc_hosp_effects)
     model.stratify_with(clinical_strat)
 
     """
@@ -278,7 +278,7 @@ def build_model(params: dict, build_options: dict = None) -> CompartmentalModel:
             vacc_strata = VACCINATION_STRATA
 
         # Get the vaccination stratification object
-        vaccination_strat = get_vaccination_strat(params, vacc_strata, voc_ifr_effect, voc_hosp_effect)
+        vaccination_strat = get_vaccination_strat(params, vacc_strata, voc_ifr_effects, voc_hosp_effects)
         model.stratify_with(vaccination_strat)
 
         # Victoria vaccination code is not generalisable
@@ -329,7 +329,7 @@ def build_model(params: dict, build_options: dict = None) -> CompartmentalModel:
 
     is_waning_immunity = bool(params.waning_immunity_duration)
     if is_waning_immunity:
-        history_strat = get_history_strat(params, voc_ifr_effect, voc_hosp_effect)
+        history_strat = get_history_strat(params, voc_ifr_effects, voc_hosp_effects)
         model.stratify_with(history_strat)
 
         # Waning immunity (if requested)
--- autumn/models/covid_19/strat_processing/vaccination.py
+++ autumn/models/covid_19/strat_processing/vaccination.py
@@ -349,7 +349,7 @@ def apply_standard_vacc_coverage(
             )
 
 
-def get_stratum_vacc_effect(params, stratum, voc_effect):
+def get_stratum_vacc_effect(params, stratum, voc_ifr_effect, voc_hosp_effect):
 
     # Parameters to directly pull out
     stratum_vacc_params = getattr(params.vaccination, stratum)
@@ -378,7 +378,8 @@ def get_stratum_vacc_effect(params, stratum, voc_effect):
     # Apply the calibration adjusters
 
     sympt_adjuster *= params.clinical_stratification.props.symptomatic.multiplier
-    ifr_adjuster *= params.infection_fatality.multiplier * voc_effect
+    ifr_adjuster *= params.infection_fatality.multiplier * voc_ifr_effect
+    hospital_adjuster *= params.infection_fatality.multiplier * voc_hosp_effect
 
     return vacc_effects, sympt_adjuster, hospital_adjuster, ifr_adjuster
 

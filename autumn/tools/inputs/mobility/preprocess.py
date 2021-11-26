d3c0405daba5202600d0ab1f0ae30913492eb218
milinda-abay <43531318+milinda-abay@users.noreply.github.com>
2021-11-26 16:52:09 +1100

    Input updates (#448) Fix for Vietnam mobility

--- autumn/tools/inputs/mobility/preprocess.py
+++ autumn/tools/inputs/mobility/preprocess.py
@@ -5,7 +5,7 @@ import pandas as pd
 from autumn.tools.db import Database
 from autumn.settings import INPUT_DATA_PATH
 
-from .fetch import MOBILITY_CSV_PATH
+from .fetch import MOBILITY_CSV_PATH, VNM_CSV_PATH
 
 NAN = float("nan")
 MOBILITY_SUFFIX = "_percent_change_from_baseline"
@@ -140,11 +140,10 @@ def preprocess_mobility(input_db: Database, country_df):
         (mob_df.sub_region_1 == "Federal Territory of Kuala Lumpur"), "sub_region_1"
     ] = "Kuala Lumpur"
 
-    # Create a copy of mobility data for Sri Lanka's western province
-    sri_lanka_wp = mob_df[mob_df.country_region == "Sri Lanka"].copy()
-    sri_lanka_wp.sub_region_1 = "western province"
+    # Read and append mobility predictions for Vietnam
+    vnm_mob = pd.read_csv(VNM_CSV_PATH)
+    mob_df = mob_df.append(vnm_mob)
 
-    mob_df = mob_df.append(sri_lanka_wp)
     mob_df = mob_df.append(dhhs_cluster_mobility)
 
     # Drop all rows that have NA values in 1 or more mobility columns.

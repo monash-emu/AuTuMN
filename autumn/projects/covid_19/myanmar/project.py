3c7bdd5597e6a6b033c4acf1cac0c55ba1ee7685
James Trauer <james.trauer@monash.edu>
2021-11-27 13:16:19 +1100

    Include VoC emergence time in calibration algorithm

--- autumn/projects/covid_19/myanmar/project.py
+++ autumn/projects/covid_19/myanmar/project.py
@@ -21,7 +21,7 @@ param_set = ParameterSet(baseline=baseline_params, scenarios=scenario_params)
 
 # Load and configure calibration settings
 ts_set = TimeSeriesSet.from_file(build_rel_path("timeseries.json"))
-notifications = ts_set.get("notifications").multiple_truncations([[199, 415], [570, 730]])
+notifications = ts_set.get("notifications").multiple_truncations([[199, 415], [561, 730]])
 infection_deaths = ts_set.get("infection_deaths").truncate_start_time(199)
 targets = [
     NormalTarget(notifications),
@@ -41,11 +41,15 @@ priors = [
     TruncNormalPrior(
         "vaccination.vacc_part_effect_duration",
         mean=365., stdev=90., trunc_range=(180., np.inf)),
+    TruncNormalPrior(
+        "voc_emergence.delta.contact_rate_multiplier",
+        mean=2.0, stdev=0.1, trunc_range=(1.0, np.inf),
+    ),
+    UniformPrior("voc_emergence.delta.start_time", (340., 375.), jumping_stdev=8.),  # Tighten up the lower limit to avoid wild runs
     UniformPrior("contact_rate", (0.05, 0.08), jumping_stdev=0.01),  # Tighten up the lower limit to avoid wild runs
     UniformPrior("infectious_seed", (50., 500.), jumping_stdev=40.),
     UniformPrior("testing_to_detection.assumed_cdr_parameter", (0.004, 0.012), jumping_stdev=0.002),
     UniformPrior("mobility.microdistancing.behaviour.parameters.end_asymptote", (0.1, 0.3), jumping_stdev=0.05),
-    UniformPrior("voc_emergence.delta.contact_rate_multiplier", (1.8, 2.2), jumping_stdev=0.1),
 ]
 calibration = Calibration(priors=priors, targets=targets)
 

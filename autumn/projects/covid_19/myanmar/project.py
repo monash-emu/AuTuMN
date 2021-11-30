9258a650e7225f2a575c46b69bb4207c47d301e2
James Trauer <james.trauer@monash.edu>
2021-12-01 06:43:59 +1100

    More calibration tweaks

--- autumn/projects/covid_19/myanmar/project.py
+++ autumn/projects/covid_19/myanmar/project.py
@@ -37,16 +37,16 @@ priors = [
         mean=6.5, stdev=0.77, trunc_range=(4.0, np.inf)),
     TruncNormalPrior(
         "history.natural_immunity_duration",
-        mean=365., stdev=90., trunc_range=(180., np.inf)),
+        mean=365., stdev=120., trunc_range=(180., np.inf)),
     TruncNormalPrior(
         "vaccination.vacc_part_effect_duration",
-        mean=365., stdev=90., trunc_range=(180., np.inf)),
+        mean=365., stdev=120., trunc_range=(180., np.inf)),
     UniformPrior("contact_rate", (0.05, 0.08), jumping_stdev=0.01),  # Tighten up the lower limit to avoid wild runs
     UniformPrior("infectious_seed", (50., 500.), jumping_stdev=40.),
     UniformPrior("testing_to_detection.assumed_cdr_parameter", (0.004, 0.012), jumping_stdev=0.002),
     UniformPrior("mobility.microdistancing.behaviour.parameters.end_asymptote", (0.1, 0.3), jumping_stdev=0.05),
-    UniformPrior("voc_emergence.delta.contact_rate_multiplier", (1.8, 2.2), jumping_stdev=0.1),
-    UniformPrior("voc_emergence.delta.start_time", (325., 390.), jumping_stdev=30.),
+    UniformPrior("voc_emergence.delta.contact_rate_multiplier", (1.8, 2.4), jumping_stdev=0.1),
+    UniformPrior("voc_emergence.delta.start_time", (330., 390.), jumping_stdev=30.),
 ]
 calibration = Calibration(priors=priors, targets=targets)
 

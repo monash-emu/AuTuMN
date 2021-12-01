c147c56df22ff66361680f1cbcfd82b9976a608b
James Trauer <james.trauer@monash.edu>
2021-12-01 14:26:15 +1100

    Comment the contact tracing file

--- autumn/models/covid_19/strat_processing/tracing.py
+++ autumn/models/covid_19/strat_processing/tracing.py
@@ -11,9 +11,23 @@ from autumn.models.covid_19.constants import (
 def get_tracing_param(assumed_trace_prop: float, assumed_prev: float, floor: float) -> float:
     """
     Calculate multiplier for the relationship between traced proportion and prevalence for use in the next function.
+    This is based on the assumption that there is a predictable relationship between per capita testing numbers and
+    case detection rate.
+    We further assume an exponential relationship between these two quantities.
+    For each model run, we also assume that we know the coordinates of one point on the curve, and given we have one
+    unknown parameter, we can solve to get the parameter for the exponent.
 
     Solving the following equation to get the returned value:
-    floor + (1 - floor) exp -(result * assumed_prev) = assumed_trace_prop
+        floor + (1 - floor) exp -(result * assumed_prev) = assumed_trace_prop
+
+    Args:
+        assumed_trace_prop: The proportion of contacts effectively traced at the prevalence of interest
+        assumed_prev: The prevalence value of interest
+        floor: The minimum proportion that would ever be traced (i.e. the asymptote level)
+
+    Returns:
+        The parameter value we need for the exponent (result in the equation above)
+
     """
 
     return -np.log((assumed_trace_prop - floor) / (1. - floor)) / assumed_prev
@@ -22,6 +36,10 @@ def get_tracing_param(assumed_trace_prop: float, assumed_prev: float, floor: flo
 def contact_tracing_func(time, computed_values):
     """
     Multiply the flow rate through by the source compartment to get the final absolute rate.
+
+    Args:
+        Standard summer functional flow rate arguments
+
     """
 
     return computed_values["traced_flow_rate"]
@@ -29,10 +47,7 @@ def contact_tracing_func(time, computed_values):
 
 @numba.jit(nopython=True)
 def get_proportion_detect_force_infection(
-        comp_values: np.ndarray,
-        notif_comps: np.ndarray,
-        notif_levels: np.ndarray,
-        non_notif_comps: np.ndarray,
+        comp_values: np.ndarray, notif_comps: np.ndarray, notif_levels: np.ndarray, non_notif_comps: np.ndarray,
         non_notif_levels: np.ndarray
 ) -> float:
     """
@@ -80,6 +95,10 @@ class PrevalenceProc(ComputedValueProcessor):
     """
     Track the current prevalence of all the active disease compartments for later use to determine the efficiency of
     contact tracing.
+
+    Attributes:
+        active_comps: The indices of the compartments representing active episodes (not including the incubation period)
+
     """
 
     def __init__(self):
@@ -90,13 +109,12 @@ class PrevalenceProc(ComputedValueProcessor):
         Identify the compartments with active disease for the prevalence calculation.
 
         Args:
-            compartments: All the compartments in the model
-            flows: All the flows in the model
+            Standard arguments for this method of a summer computed value processor
 
         """
 
         active_comps_list = [
-            idx for idx, comp in enumerate(compartments) if
+            i_comp for i_comp, comp in enumerate(compartments) if
             comp.has_name(Compartment.EARLY_ACTIVE) or comp.has_name(Compartment.LATE_ACTIVE)
         ]
         self.active_comps = np.array(active_comps_list, dtype=int)
@@ -106,9 +124,7 @@ class PrevalenceProc(ComputedValueProcessor):
         Now actually calculate the current prevalence during run-time.
 
         Args:
-            compartment_values: All the model compartment values
-            computed_values: All the computed values being tracked during run-time
-            time: Current time in integration
+            Standard arguments for this method of a summer computed value processor
 
         Returns:
             Prevalence of active disease at this point in time
@@ -121,16 +137,33 @@ class PrevalenceProc(ComputedValueProcessor):
 class PropIndexDetectedProc(ComputedValueProcessor):
     """
     Calculate the proportion of all contacts whose index case is ever detected.
+
+    Attributes:
+        non_sympt_infect_multiplier: Relative infectiousness of asymptomatic episodes
+        late_infect_multiplier: Relative infectiousness of persons undergoing quarantine
+        notif_comps: Indices of the compartments being notified
+        notif_levels: Infectiousness levels of the compartments being notified
+        non_notif_comps: Indices of the compartments not being notified
+        non_notif_levels: Infectiousness levels of the compartments not being notified
+
     """
 
     def __init__(self, non_sympt_infect_multiplier, late_infect_multiplier):
         self.non_sympt_infect_multiplier = non_sympt_infect_multiplier
         self.late_infect_multiplier = late_infect_multiplier
+        self.notif_comps = None
+        self.notif_levels = None
+        self.non_notif_comps = None
+        self.non_notif_levels = None
 
     def prepare_to_run(self, compartments, flows):
         """
         Identify the infectious compartments for the prevalence calculation by infection stage and clinical status.
         Also captures the infectiousness levels by infection stage and clinical status.
+
+        Args:
+            Standard arguments for this method of a summer computed value processor
+
         """
 
         notif_comps, non_notif_comps, notif_levels, non_notif_levels = [], [], [], []
@@ -168,6 +201,13 @@ class PropIndexDetectedProc(ComputedValueProcessor):
     def process(self, compartment_values, computed_values, time):
         """
         Calculate the proportion of the force of infection arising from ever-detected individuals
+
+        Args:
+            Standard arguments for this method of a summer computed value processor
+
+        Returns:
+            The proportion of interest
+
         """
 
         # Call the optimised numba JIT version of this function (we cannot JIT directly on the class member function)
@@ -179,6 +219,11 @@ class PropIndexDetectedProc(ComputedValueProcessor):
 class PropDetectedTracedProc(ComputedValueProcessor):
     """
     Calculate the proportion of contacts of successfully detected cases which are traced.
+
+    Attributes:
+        trace_param: The exponent parameter governing the relationship between testing rates and CDR
+        floor: The minimum CDR possible
+
     """
 
     def __init__(self, trace_param, floor):
@@ -187,14 +232,19 @@ class PropDetectedTracedProc(ComputedValueProcessor):
 
     def process(self, compartment_values, computed_values, time):
         """
-        Formula for calculating the proportion from the already-processed contact tracing parameter,
-        which has been worked out in the get_tracing_param function above.
+        Formula for calculating the proportion from the already-processed contact tracing parameter, which was
+        calculated in the get_tracing_param function above.
+
+        Args:
+            Standard arguments for this method of a summer computed value processor
+
         """
 
         # Decreasing exponential function of current prevalence descending from one to the floor value
         current_prevalence = computed_values["prevalence"]
         prop_of_detected_traced = self.floor + (1. - self.floor) * np.exp(-current_prevalence * self.trace_param)
 
+        # Should be impossible for this to fail
         msg = f"Proportion of detectable contacts detected not between floor value and 1: {prop_of_detected_traced}"
         assert self.floor <= prop_of_detected_traced <= 1., msg
 
@@ -204,6 +254,10 @@ class PropDetectedTracedProc(ComputedValueProcessor):
 class TracedFlowRateProc(ComputedValueProcessor):
     """
     Calculate the transition flow rate based on the only other outflow and the proportion of all new cases traced.
+
+    Attributes:
+        incidence_flow_rate: The total flow rate for all incident cases (i.e. reciprocal of late exposed sojourn time)
+
     """
 
     def __init__(self, incidence_flow_rate):
@@ -215,6 +269,9 @@ class TracedFlowRateProc(ComputedValueProcessor):
             traced_prop = traced_flow_rate / (traced_flow_rate + incidence_flow_rate)
         for traced_flow_rate gives the following:
 
+        Args:
+            Standard arguments for this method of a summer computed value processor
+
         """
 
         # Proportion of all infections that are contact traced
@@ -222,5 +279,8 @@ class TracedFlowRateProc(ComputedValueProcessor):
 
         # Applied to adjust the incidence flow rate
         traced_flow_rate = self.incidence_flow_rate * traced_prop / max((1. - traced_prop), 1e-6)
+
+        # Should be impossible for this to fail
         assert 0. <= traced_flow_rate
+
         return traced_flow_rate

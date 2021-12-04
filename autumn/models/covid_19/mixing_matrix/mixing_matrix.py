301ab9aedefcb5674ce9ba284adb52940e64295f
James Trauer <james.trauer@monash.edu>
2021-12-04 18:36:53 +1100

    Tidy mixing matrix file

--- autumn/models/covid_19/mixing_matrix/mixing_matrix.py
+++ autumn/models/covid_19/mixing_matrix/mixing_matrix.py
@@ -1,9 +1,7 @@
 from typing import Callable, Dict
-
 import numpy as np
 
 from autumn.models.covid_19.parameters import Country, Mobility
-
 from .microdistancing import get_microdistancing_funcs
 from .mixing_adjusters import AgeMixingAdjuster, LocationMixingAdjuster
 from .mobility import get_mobility_funcs
@@ -13,16 +11,23 @@ def build_dynamic_mixing_matrix(
     base_matrices: Dict[str, np.ndarray], mobility: Mobility, country: Country
 ) -> Callable[[float], np.ndarray]:
     """
-    Build a time-varying mixing matrix
-    Returns a function of time which returns a 16x16 mixing matrix.
+    Master function that builds the time-varying mixing matrix.
+
+    Args:
+        base_matrices: Empiric matrices of contact rates by location for the country of interest
+        mobility: Mobility parameters
+        country: The country being considered
+
+    Returns:
+        A function of time which returns a 16x16 mixing matrix
+
     """
 
     microdistancing_funcs = get_microdistancing_funcs(mobility.microdistancing, mobility.square_mobility_effect)
 
-    mobility_funcs = get_mobility_funcs(
-        country, mobility.region, mobility.mixing, mobility.google_mobility_locations, mobility.npi_effectiveness,
-        mobility.square_mobility_effect, mobility.smooth_google_data,
-    )
+    locs, npi_effects = mobility.google_mobility_locations, mobility.npi_effectiveness
+    square, smooth = mobility.square_mobility_effect, mobility.smooth_google_data
+    mobility_funcs = get_mobility_funcs(country, mobility.region, mobility.mixing, locs, npi_effects, square, smooth)
 
     # Get adjusters
     location_adjuster = LocationMixingAdjuster(base_matrices, mobility_funcs, microdistancing_funcs)
@@ -30,7 +35,7 @@ def build_dynamic_mixing_matrix(
 
     def mixing_matrix_function(time: float) -> np.ndarray:
         """
-        Returns a 16x16 mixing matrix for a given time.
+        The function to be called at model run-time to get the matrix.
         """
 
         # Base matrix
@@ -38,9 +43,6 @@ def build_dynamic_mixing_matrix(
 
         # Apply adjustments
         mixing_matrix = location_adjuster.get_adjustment(time, mixing_matrix)
-        if age_adjuster:
-            mixing_matrix = age_adjuster.get_adjustment(time, mixing_matrix)
-
-        return mixing_matrix
+        return age_adjuster.get_adjustment(time, mixing_matrix) if age_adjuster else mixing_matrix
 
     return mixing_matrix_function

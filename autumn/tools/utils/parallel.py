6d57f3a975d9aa376f68de2a1d1391b5d5206d23
David Shipman <dshipman@gmail.com>
2021-12-07 14:36:45 +1100

    Remove sentrySDK

--- autumn/tools/utils/parallel.py
+++ autumn/tools/utils/parallel.py
@@ -8,14 +8,10 @@ import multiprocessing as mp
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from typing import List, Callable, Any
 
-import sentry_sdk
-
 logger = logging.getLogger(__name__)
 
 
 MAX_WORKERS = mp.cpu_count() - 1
-SENTRY_ERROR_DELAY = 10  # seconds
-SENTRY_DSN = os.environ.get("SENTRY_DSN")
 
 def gather_exc_plus(filename='crash.log'):
     """
@@ -47,30 +43,6 @@ def gather_exc_plus(filename='crash.log'):
                 out_f.write(f"{value}\n")
             except:
                 out_f.write("Could not represent as string\n")
-
-def report_errors(func):
-    """
-    Decorator that ensures that errors found inside parallel tasks
-    are captured by Sentry, rather than being buried by the parent process.
-    """
-
-    @functools.wraps(func)
-    def error_handler_wrapper(*args, **kwargs):
-        try:
-            result = func(*args, **kwargs)
-            return result
-        except Exception as e:
-            if SENTRY_DSN:
-                # Sentry uses an async model to send exceptions to their server,
-                # so we need to wait for Sentry to send the message or else the parent process
-                # will kill the child before it can log the error
-                logger.info("Waiting %s seconds for Sentry to upload an error.", SENTRY_ERROR_DELAY)
-                sentry_sdk.capture_exception(e)
-                time.sleep(SENTRY_ERROR_DELAY)
-
-            raise e
-
-    return error_handler_wrapper
 
 
 def run_parallel_tasks(func: Callable, arg_list: List[Any], auto_exit=True):

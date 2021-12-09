4d4a823228ca6445771be08afa1489d07c64ad52
James Trauer <james.trauer@monash.edu>
2021-12-09 20:34:46 +1100

    Simple first calibration for Vic simple

--- autumn/projects/covid_19/victoria/victoria/project.py
+++ autumn/projects/covid_19/victoria/victoria/project.py
@@ -1,9 +1,10 @@
 import json
+import numpy as np
 
 from autumn.tools.project import Project, ParameterSet, TimeSeriesSet, build_rel_path, get_all_available_scenario_paths
 from autumn.tools.calibration import Calibration
-from autumn.tools.calibration.priors import UniformPrior
-from autumn.tools.calibration.targets import NormalTarget
+from autumn.tools.calibration.priors import UniformPrior, TruncNormalPrior
+from autumn.tools.calibration.targets import NormalTarget, PoissonTarget
 from autumn.models.covid_19 import base_params, build_model
 from autumn.settings import Region, Models
 
@@ -19,13 +20,20 @@ param_set = ParameterSet(baseline=baseline_params, scenarios=scenario_params)
 # Load and configure calibration settings
 ts_set = TimeSeriesSet.from_file(build_rel_path("targets.secret.json"))
 notifications = ts_set.get("notifications").truncate_start_time(551)
+infection_deaths = ts_set.get("infection_deaths").truncate_start_time(551)
+hosp_admissions = ts_set.get("hospital_admissions").truncate_start_time(551)
+icu_admissions = ts_set.get("icu_admissions").truncate_start_time(551)
 targets = [
-    NormalTarget(notifications),
-    # NormalTarget(infection_deaths)
+    PoissonTarget(notifications),
+    PoissonTarget(infection_deaths),
+    PoissonTarget(hosp_admissions),
+    PoissonTarget(icu_admissions)
 ]
 priors = [
-    UniformPrior("contact_rate", (0.05, 0.08), jumping_stdev=0.01),
-    
+    UniformPrior("contact_rate", (0.05, 0.1), jumping_stdev=0.01),
+    TruncNormalPrior(
+        "sojourn.compartment_periods_calculated.exposed.total_period", mean=3.5, stdev=1.0, trunc_range=(2.0, np.inf)
+    )
 ]
 
 calibration = Calibration(priors=priors, targets=targets)

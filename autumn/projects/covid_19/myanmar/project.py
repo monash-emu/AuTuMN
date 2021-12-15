94ec890d3a966b875bec5288186416117cbeb1b3
James Trauer <james.trauer@monash.edu>
2021-12-15 22:21:03 +1100

    Adjust some parameters and include hospital reporting

--- autumn/projects/covid_19/myanmar/project.py
+++ autumn/projects/covid_19/myanmar/project.py
@@ -21,7 +21,7 @@ param_set = ParameterSet(baseline=baseline_params, scenarios=scenario_params)
 
 # Load and configure calibration settings
 ts_set = TimeSeriesSet.from_file(build_rel_path("timeseries.json"))
-notifications = ts_set.get("notifications").multiple_truncations([[199, 415], [560, 730]])
+notifications = ts_set.get("notifications").multiple_truncations([[199, 400], [560, 730]])
 infection_deaths = ts_set.get("infection_deaths").truncate_start_time(199)
 targets = [
     NormalTarget(notifications),
@@ -44,9 +44,9 @@ priors = [
     UniformPrior("contact_rate", (0.05, 0.08), jumping_stdev=0.01),  # Tighten up the lower limit to avoid wild runs
     UniformPrior("infectious_seed", (50., 500.), jumping_stdev=40.),
     UniformPrior("testing_to_detection.assumed_cdr_parameter", (0.004, 0.012), jumping_stdev=0.002),
-    UniformPrior("mobility.microdistancing.behaviour.parameters.end_asymptote", (0.1, 0.3), jumping_stdev=0.05),
+    UniformPrior("mobility.microdistancing.behaviour.parameters.end_asymptote", (0.0, 0.2), jumping_stdev=0.05),
     UniformPrior("voc_emergence.delta.contact_rate_multiplier", (1.8, 2.4), jumping_stdev=0.1),
-    UniformPrior("voc_emergence.delta.start_time", (350., 390.), jumping_stdev=30.),
+    UniformPrior("voc_emergence.delta.start_time", (365., 390.), jumping_stdev=30.),
 ]
 calibration = Calibration(priors=priors, targets=targets)
 

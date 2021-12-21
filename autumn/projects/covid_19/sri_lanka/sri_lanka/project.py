f52f70f081b94a4c621fb187bd9a249af7723e21
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2021-12-21 23:49:49 +1100

    increase calibration parameters

--- autumn/projects/covid_19/sri_lanka/sri_lanka/project.py
+++ autumn/projects/covid_19/sri_lanka/sri_lanka/project.py
@@ -35,11 +35,15 @@ targets = [
 priors = [
     # Regional parameters
     UniformPrior("contact_rate", [0.045, 0.06]),
+    UniformPrior("infectious_seed", [350.0, 500.0]),
     # Detection
     UniformPrior("infection_fatality.multiplier", [0.17, 0.2]),
+    UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.009, 0.02]),
     #VoC
     UniformPrior("voc_emergence.alpha_beta.contact_rate_multiplier", [2.75, 3.3]),
+    UniformPrior("voc_emergence.alpha_beta.start_time", [400, 475]),
     UniformPrior("voc_emergence.delta.contact_rate_multiplier", [7.5, 11.5]),
+    UniformPrior("voc_emergence.delta.start_time", [475, 530]),
     TruncNormalPrior(
         "voc_emergence.delta.ifr_multiplier",
         mean=2., stdev=1.5, trunc_range=(0., 4)),

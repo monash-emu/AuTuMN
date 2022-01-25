2c1427532249baf1ec1962ea30ea1745ec86b0c0
Hoang Anh Ngo <h.a.ngo@sms.ed.ac.uk>
2022-01-25 14:49:16 +0100

    Update prior distribution for further calibration

--- autumn/projects/covid_19/vietnam/hanoi/project.py
+++ autumn/projects/covid_19/vietnam/hanoi/project.py
@@ -35,10 +35,10 @@ priors = [
     UniformPrior("infectious_seed", [1, 15]),
     UniformPrior("contact_rate", [0.05, 0.15]),
     # Health system-related
-    UniformPrior("clinical_stratification.icu_prop", [0.7, 0.95]),
+    UniformPrior("clinical_stratification.icu_prop", [0.15, 0.2]),
     # UniformPrior("clinical_stratification.non_sympt_infect_multiplier", [0.15, 1.0]),
     # UniformPrior("clinical_stratification.props.symptomatic.multiplier", [0.6, 1.0]),
-    UniformPrior("clinical_stratification.props.hospital.multiplier", [0.3, 0.5]),
+    UniformPrior("clinical_stratification.props.hospital.multiplier", [1., 2.]),
     UniformPrior("infection_fatality.multiplier", [0.6, 0.8]),
     # Detection
     UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.001, 0.01]),

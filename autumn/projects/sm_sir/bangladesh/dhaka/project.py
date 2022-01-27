512b5a2429fe4cee5baf9163881d1d46159be46f
milinda-abay <43531318+milinda-abay@users.noreply.github.com>
2022-01-27 16:53:46 +1100

    Input updates (#547) Deaths and notifications for BGD & DHK

--- autumn/projects/sm_sir/bangladesh/dhaka/project.py
+++ autumn/projects/sm_sir/bangladesh/dhaka/project.py
@@ -20,14 +20,11 @@ param_set = ParameterSet(baseline=baseline_params, scenarios=[])
 
 # Load and configure calibration settings.
 ts_set = load_timeseries(build_rel_path("timeseries.json"))
-priors = [
-    UniformPrior("contact_rate", [0.1, 0.2]),
-    UniformPrior("infectious_seed", [1, 400])
-]
+priors = [UniformPrior("contact_rate", [0.1, 0.2]), UniformPrior("infectious_seed", [1, 400])]
 
 targets = [
     NormalTarget(data=ts_set["notifications"]),
-    NormalTarget(data=ts_set["icu_occupancy"]),
+    # NormalTarget(data=ts_set["icu_occupancy"]),
     NormalTarget(data=ts_set["infection_deaths"]),
 ]
 
@@ -55,9 +52,7 @@ with open(plot_spec_filepath) as f:
 
 
 # Create and register the project.
-project = Project(
-    Region.DHAKA, Models.SM_SIR, build_model, param_set, calibration, plots=plot_spec
-)
+project = Project(Region.DHAKA, Models.SM_SIR, build_model, param_set, calibration, plots=plot_spec)
 
 
 # from autumn.tools.calibration.proposal_tuning import perform_all_params_proposal_tuning
---
+++ scripts/inputs/input_targets_bangladesh.py
@@ -0,0 +1,95 @@
+"""
+Script for loading Bangladesh, Dhaka and Cox's Bazar data into calibration targets and default.yml
+NOTE you will need to pip instal lxml to run this script
+
+"""
+from lib2to3.pytree import convert
+import os
+from typing import List
+import pandas as pd
+
+from autumn.settings import PROJECTS_PATH
+from autumn.settings import INPUT_DATA_PATH
+from autumn.tools.utils.utils import update_timeseries
+from autumn.tools.utils.utils import COVID_BASE_DATETIME
+from autumn.tools.utils.utils import create_date_index
+
+
+SM_SIR_BGD_TS = os.path.join(PROJECTS_PATH, "sm_sir", "bangladesh", "bangladesh", "timeseries.json")
+SM_SIR_DHK_TS = os.path.join(PROJECTS_PATH, "sm_sir", "bangladesh", "dhaka", "timeseries.json")
+
+DATA_PATH = os.path.join(INPUT_DATA_PATH, "covid_bgd")
+
+FILES = os.listdir(DATA_PATH)
+
+BGD_DATA = [os.path.join(DATA_PATH, file) for file in FILES if "BGD" in file]
+DHK_DATA = [os.path.join(DATA_PATH, file) for file in FILES if "DHK" in file]
+
+3
+TARGET_MAP_BGD = {
+    "notifications": "confirmed_case",
+    "infection_deaths": "death",
+}
+
+TARGET_MAP_DHK = {
+    "notifications": "confirmed_case",
+    "infection_deaths": "death",
+}
+
+
+def main():
+
+    save_to_excel(DHK_DATA + BGD_DATA)
+
+    for target in {"deaths", "cases"}:
+
+        update_region(SM_SIR_BGD_TS, BGD_DATA, TARGET_MAP_BGD, target)
+        update_region(SM_SIR_DHK_TS, DHK_DATA, TARGET_MAP_DHK, target)
+
+
+def save_to_excel(file_paths: List) -> None:
+    """Convert dashboard files to csv"""
+
+    for file in file_paths:
+        try:
+            pd.read_html(file)[0].to_csv(file, index=False)
+        except:
+            assert len(pd.read_csv(file)) > 0, f"Download {file} again"
+
+    return None
+
+
+def get_data_file(dtype: str, datafile: List[str]) -> list:
+    return [file for file in datafile if dtype.lower() in file.lower()]
+
+
+def create_dates(df: pd.DataFrame) -> pd.DataFrame:
+
+    # Find the length and end date to create a datetime column
+    periods = len(df)
+    end_date = pd.to_datetime(f'{df.tail(1)["Category"].values[0]}-2022')
+    df["date"] = pd.date_range(end=end_date, periods=periods).tolist()
+
+    # Compare and remove any non matching rows
+    df["compare"] = (
+        df["date"].dt.month_name().astype(str)
+        + "-"
+        + df["date"].dt.day.astype(str).str.rjust(2, "0")
+    )
+    filter_dates = df["Category"] == df["compare"]
+    df = df[filter_dates]
+
+    return df
+
+
+def update_region(timeseries, region_data, target_map, target) -> None:
+
+    df = pd.read_csv(get_data_file(target, region_data)[0])
+    df = create_dates(df)
+    df = create_date_index(COVID_BASE_DATETIME, df, "date")
+    update_timeseries(target_map, df, timeseries)
+    return None
+
+
+if __name__ == "__main__":
+    main()

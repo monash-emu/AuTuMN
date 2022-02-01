d7c0129e4f007965b1ca3e0add5a43697fdec4b3
Hoang Anh Ngo <h.a.ngo@sms.ed.ac.uk>
2022-02-01 01:42:46 +0100

    check for negativity instead of no check

--- autumn/models/covid_19/parameters.py
+++ autumn/models/covid_19/parameters.py
@@ -344,6 +344,15 @@ class VocComponent(BaseModel):
             assert 0. <= values["contact_rate_multiplier"], "Contact rate multiplier negative"
         if "entry_rate" in values:
             assert 0. <= values["entry_rate"], "Entry rate negative"
+        if "ifr_multiplier" in values:
+            assert 0. <= values["ifr_multiplier"], "VoC effect on mortality negative"
+        else:
+            values["ifr_multiplier"] = 1.
+        if "hosp_multiplier" in values:
+            hosp_multiplier = values["hosp_multiplier"]
+            assert 0. <= hosp_multiplier, f"VoC effect on hospitalisation negative"
+        else:
+            values["hosp_multiplier"] = 1.
         return values
 
 

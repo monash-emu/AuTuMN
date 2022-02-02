6abb5cc920489d9bcd5fd26b1dc67ec5a0160b5e
milinda <milinda@monash.edu>
2022-02-03 09:51:24 +1100

    Remove unwanted module

--- autumn/tools/inputs/covid_bgd/preprocess.py
+++ autumn/tools/inputs/covid_bgd/preprocess.py
@@ -1,6 +1,5 @@
-from venv import create
 import pandas as pd
-from pathlib2 import Path
+
 
 from autumn.tools.db import Database
 from autumn.tools.utils.utils import create_date_index, COVID_BASE_DATETIME
@@ -18,7 +17,7 @@ def preprocess_covid_bgd(input_db: Database):
     input_db.dump_df("bgd_vacc", df)
 
 
-def process_coxs_bazar_vaccination(COXS_VAC_DATA: Path) -> pd.DataFrame:
+def process_coxs_bazar_vaccination(COXS_VAC_DATA) -> pd.DataFrame:
     df = pd.read_excel(COXS_VAC_DATA, usecols=[4, 5, 7, 8], skipfooter=1)
     df.rename(
         columns={"# Total Vaccinated": "total_vaccinated", "First Dose": "dose"}, inplace=True
@@ -30,7 +29,7 @@ def process_coxs_bazar_vaccination(COXS_VAC_DATA: Path) -> pd.DataFrame:
     return df
 
 
-def process_coxs_bazar(COXS_DATA: Path) -> pd.DataFrame:
+def process_coxs_bazar(COXS_DATA) -> pd.DataFrame:
 
     df = pd.read_excel(COXS_DATA, skipfooter=1, usecols=[1, 2, 3, 4, 5, 6])
     df = create_date_index(COVID_BASE_DATETIME, df, "Unnamed: 1")

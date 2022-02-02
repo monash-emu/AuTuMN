781c2fba3486cb271aab6f2ddfb5a0791a60ac00
milinda <milinda@monash.edu>
2022-02-03 10:02:21 +1100

    Fix BGD preprocess module import

--- autumn/tools/inputs/covid_bgd/preprocess.py
+++ autumn/tools/inputs/covid_bgd/preprocess.py
@@ -1,7 +1,7 @@
 from operator import index
 from venv import create
 import pandas as pd
-from pathlib2 import Path
+
 
 from autumn.tools.db import Database
 from autumn.tools.utils.utils import create_date_index, COVID_BASE_DATETIME
@@ -17,7 +17,7 @@ def preprocess_covid_bgd(input_db: Database):
     input_db.dump_df("covid_coxs_bazar", df)
 
 
-def process_coxs_bazar_vaccination(COXS_VAC_DATA: Path) -> pd.DataFrame:
+def process_coxs_bazar_vaccination(COXS_VAC_DATA) -> pd.DataFrame:
     df = pd.read_excel(COXS_VAC_DATA, usecols=[4, 5, 7, 8], skipfooter=1)
     df.rename(
         columns={"# Total Vaccinated": "total_vaccinated", "First Dose": "dose"}, inplace=True
@@ -29,7 +29,7 @@ def process_coxs_bazar_vaccination(COXS_VAC_DATA: Path) -> pd.DataFrame:
     return df
 
 
-def process_coxs_bazar(COXS_DATA: Path) -> pd.DataFrame:
+def process_coxs_bazar(COXS_DATA) -> pd.DataFrame:
 
     df = pd.read_excel(COXS_DATA, skipfooter=1, usecols=[1, 2, 3, 4, 5, 6])
     df = create_date_index(COVID_BASE_DATETIME, df, "Unnamed: 1")

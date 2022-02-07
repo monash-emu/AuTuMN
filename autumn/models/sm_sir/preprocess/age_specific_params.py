9b6c5d1fad539a420a3845d43cb14b2e7568b7f6
James Trauer <james.trauer@monash.edu>
2022-02-07 14:23:24 +1100

    Clean up code into final format

--- autumn/models/sm_sir/preprocess/age_specific_params.py
+++ autumn/models/sm_sir/preprocess/age_specific_params.py
@@ -1,7 +1,38 @@
-import numpy as np
-from typing import List, Union
+from typing import List, Dict, Union
+
+import itertools
 
 from autumn.tools.inputs import get_population_by_agegroup
+
+
+def get_relevant_indices(
+        source_agebreaks: List[int],
+        modelled_age_groups: List[int],
+) -> Dict[int, List[int]]:
+    """
+    Find the standard source age brackets relevant to each modelled age bracket.
+
+    Args:
+        source_agebreaks: The standard source age brackets (5-year brackets to 75+)
+        modelled_age_groups: The age brackets being applied in the model
+
+    Returns:
+        The set of source age breaks applying to each modelled age group
+
+    """
+
+    relevant_source_indices = {}
+    for i_age, model_agegroup in enumerate(modelled_age_groups):
+        age_index_low = source_agebreaks.index(model_agegroup)
+        if model_agegroup == modelled_age_groups[-1]:
+            age_index_up = source_agebreaks[-1]
+        else:
+            age_index_up = source_agebreaks.index(modelled_age_groups[i_age + 1])
+        relevant_source_indices[model_agegroup] = source_agebreaks[age_index_low: age_index_up]
+
+    assert list(itertools.chain.from_iterable(relevant_source_indices.values())) == source_agebreaks
+
+    return relevant_source_indices
 
 
 def convert_param_agegroups(
@@ -15,38 +46,34 @@ def convert_param_agegroups(
 
     Args:
         source_parameters: A list of values provided by 5-year band, starting from 0-4
-        iso3:
-        region:
-        modelled_age_groups:
+        iso3, region, modelled_age_groups: The parameters needed for obtaining the population
 
     Returns:
         The list of the processed parameters in the format needed by the model
 
     """
 
-    # Get default age brackets and population structured in that way
-    source_agebreaks = [str(5 * i) for i in range(len(source_parameters))]
+    # Get default age brackets and population structured with these default categories
+    source_agebreaks = [5 * i for i in range(len(source_parameters))]
     total_pops_5year_bands = get_population_by_agegroup(source_agebreaks, iso3, region=region, year=2020)
 
-    print("________________________")
-    # Calculate the parameter values for the modelled age groups
+    # Check modelled age brackets are specified in the expected way
+    msg = "Modelled age brackets not multiples of 5, as expected"
+    assert all([i_age % 5 == 0 for i_age in modelled_age_groups]), msg
+
+    # Find out which of the standard source categories apply to each modelled age group
+    relevant_source_indices = get_relevant_indices(source_agebreaks, modelled_age_groups)
+
+    # Weight the parameter values according to the new structure
     param_values = []
     for i_age, model_agegroup in enumerate(modelled_age_groups):
-
-        # If the last modelled age group
-        if i_age == len(modelled_age_groups) - 1:
-            relevant_source_indices = list(range(i_age, len(source_agebreaks)))
-
-        else:
-            relevant_source_indices = [i_age]
-
         model_agegroup_pop = 0
         param_val = 0.
-        for relevant_source_index in relevant_source_indices:
+        for source_indices in relevant_source_indices[model_agegroup]:
+            relevant_source_index = source_agebreaks.index(source_indices)
             bin_pop = total_pops_5year_bands[relevant_source_index]
             model_agegroup_pop += bin_pop
             param_val += source_parameters[relevant_source_index] * bin_pop
-
         param_values.append(param_val / model_agegroup_pop)
 
     return param_values

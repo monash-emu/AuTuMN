d5d20f13c4ccc498c11f060cbb23a604b1377019
James Trauer <James.Trauer@monash.edu>
2022-02-08 12:38:33 +1100

    Add calibration algorithm

--- autumn/projects/sm_sir/bangladesh/bangladesh/project.py
+++ autumn/projects/sm_sir/bangladesh/bangladesh/project.py
@@ -18,25 +18,20 @@ notifications_ts = ts_set["notifications"].loc[calibration_start_time:]
 
 
 priors = [
-    UniformPrior("contact_rate", (0.08, 0.18)),
+    UniformPrior("contact_rate", (0.03, 0.12)),
     UniformPrior("testing_to_detection.assumed_cdr_parameter", (0.003, 0.012)),
-    UniformPrior("voc_emergence.omicron.new_voc_seed.start_time", (640., 680.)),
-    UniformPrior("voc_emergence.omicron.contact_rate_multiplier", (0.5, 1.))
+    UniformPrior("voc_emergence.omicron.new_voc_seed.start_time", (700., 730.)),
 ]
 
 targets = [
     NormalTarget(notifications_ts),
 ]
 
-calibration = Calibration(
-    priors=priors, targets=targets, random_process=None, metropolis_init="current_params"
-)
+calibration = Calibration(priors=priors, targets=targets, random_process=None, metropolis_init="current_params")
 
 plot_spec_filepath = build_rel_path("timeseries.json")
 with open(plot_spec_filepath) as f:
     plot_spec = json.load(f)
 
 # Create and register the project
-project = Project(
-    Region.BANGLADESH, Models.SM_SIR, build_model, param_set, calibration, plots=plot_spec
-)
+project = Project(Region.BANGLADESH, Models.SM_SIR, build_model, param_set, calibration, plots=plot_spec)

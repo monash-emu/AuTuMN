df94c9441fa4588f527f6d9e462c4abc380ab007
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2022-02-23 11:04:43 +1100

    MYS priors update

--- autumn/projects/sm_sir/malaysia/malaysia/project.py
+++ autumn/projects/sm_sir/malaysia/malaysia/project.py
@@ -28,8 +28,9 @@ targets = [
 priors = [
     UniformPrior("contact_rate", (0.1, 0.22)),
     UniformPrior("testing_to_detection.assumed_cdr_parameter", (0.08, 0.18)),
-    UniformPrior("sojourns.latent.total_time", (3., 5.)),
-    UniformPrior("voc_emergence.omicron.hosp_protection", (0.6, 0.9))
+    UniformPrior("sojourns.latent.total_time", (1., 5.)),
+    UniformPrior("voc_emergence.omicron.hosp_protection", (0.6, 0.9)),
+    UniformPrior("voc_emergence.omicron.death_protection", (0.5, 0.8))
 ]
 
 calibration = Calibration(

e56ef032333b08bc2497479f19ae9df802a63638
Hoang Anh NGO <50743576+hoanganhngo610@users.noreply.github.com>
2022-03-01 12:47:36 +0100

    Model update for HCMC SM-SIR (#596)
    * Remove all unrelated scenarios from traditional SEIS model.
    * Add latest results for calibration and proposal SDs.
    * Slightly modify the baseline parameters and fix certain parameters after various calibrations.

--- autumn/projects/sm_sir/vietnam/ho_chi_minh_city/project.py
+++ autumn/projects/sm_sir/vietnam/ho_chi_minh_city/project.py
@@ -11,18 +11,24 @@ from autumn.tools.project import (
 from autumn.tools.calibration import Calibration
 from autumn.tools.calibration.priors import UniformPrior
 from autumn.tools.calibration.targets import NormalTarget
-from autumn.models.sm_sir import base_params, build_model, set_up_random_process
+from autumn.models.sm_sir import (
+    base_params,
+    build_model,
+    set_up_random_process
+)
 from autumn.settings import Region, Models
 
 from autumn.projects.covid_19.calibration import COVID_GLOBAL_PRIORS
 
-scenario_dir_path = build_rel_path("params/")
-
-# Load and configure model parameters.
+# Load and configure model parameters
 mle_path = build_rel_path("params/mle-params.yml")
+scenario_dir_path = build_rel_path("params/")
+scenario_paths = get_all_available_scenario_paths(scenario_dir_path)
+
 baseline_params = base_params.update(build_rel_path("params/baseline.yml")).update(
     mle_path, calibration_format=True
 )
+scenario_params = [baseline_params.update(p) for p in scenario_paths]
 param_set = ParameterSet(baseline=baseline_params, scenarios=[])
 
 # Load and configure calibration settings.
@@ -31,43 +37,42 @@ ts_set = load_timeseries(build_rel_path("timeseries.json"))
 # notifications = ts_set["notifications"].multiple_truncations([[511, 575], [606, 700]])
 # truncated from 18th Jul to 28th Jul, then from 28th Aug onwards
 notifications = pd.concat(
-    [ts_set["notifications"].loc[511:575], ts_set["notifications"].loc[606:700]]
+    [
+     ts_set["notifications"].loc[606:639],  # form 28/02/2021 to 30/09/2021
+     ts_set["notifications"].loc[702:]  # from 02/12/2021 onwards
+    ]
 )
 
-icu_occupancy = ts_set["icu_occupancy"].loc[640:]  # truncated to 01 Oct 2021
-infection_deaths = ts_set["infection_deaths"].loc[556:]  # truncated to 9th Jul 2021
+icu_occupancy = ts_set["icu_occupancy"].loc[640:].rolling(7).mean()  # truncated to 01 Oct 2021
+infection_deaths = ts_set["infection_deaths"].loc[556:].rolling(7).mean()  # truncated to 9th Jul 2021
 
-targets = [NormalTarget(notifications), NormalTarget(icu_occupancy), NormalTarget(infection_deaths)]
+targets = [NormalTarget(notifications),
+           NormalTarget(icu_occupancy),
+           NormalTarget(infection_deaths)]
 
 
 priors = [
-    # Global COVID priors
-    *COVID_GLOBAL_PRIORS,
-    # Starting date
-    # UniformPrior("time.start", [455, 485], jumping_stdev=3.0),
-    # Regional parameters
-    UniformPrior("infectious_seed", [5, 15]),
-    UniformPrior("contact_rate", [0.2, 0.25]),
-    # Health system-related
-    UniformPrior("clinical_stratification.icu_prop", [0.14, 0.18]),
-    # UniformPrior("clinical_stratification.non_sympt_infect_multiplier", [0.15, 1.0]),
-    # UniformPrior("clinical_stratification.props.symptomatic.multiplier", [0.6, 1.0]),
-    UniformPrior("clinical_stratification.props.hospital.multiplier", [0.25, 0.4]),
-    UniformPrior("infection_fatality.multiplier", [0.7, 1.0]),
-    # Detection
-    UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.0005, 0.009]),
-    # Microdistancing
-    UniformPrior("mobility.microdistancing.behaviour.parameters.max_effect", [0.30, 0.40]),
-    # Waning immunity
-    # UniformPrior("waning_immunity_duration", (180, 360), jumping_stdev=30.),
-    # Vaccination parameters (independent sampling)
-    # TruncNormalPrior("vaccination.one_dose.ve_prop_prevent_infection", mean=0.9, stdev=0.02, truc_range=(0.8, 1)),
-    # TruncNormalPrior("vaccination.one_dose.ve_sympt_covid", mean=0.5, stdev=0.02, truc_range=(0.4, 0.6)),
-    # Partly-waned immunity of vaccine
-    # TruncNormalPrior("vaccination.part_waned.ve_sympt_covid", mean=0.5, stdev=0.02, truc_range=(0.4, 0.6)),
-    # TruncNormalPrior("vaccination.part_waned.ve_infectiousness", mean=0.5, stdev=0.02, truc_range=(0.2, 0.3)),
-    # TruncNormalPrior("vaccination.part_waned.ve_hospitalisation", mean=0.75, stdev=0.02, truc_range=(0.65, 0.85)),
-    # TruncNormalPrior("vaccination.part_waned.ve_death", mean=0.8, stdev=0.02, truc_range=(0.7, 0.9))
+    # infectious seed and contact rate
+    UniformPrior("infectious_seed", (50, 200)),
+    UniformPrior("contact_rate", (0.4, 0.7)),
+    # testing to detection params
+    UniformPrior("testing_to_detection.assumed_tests_parameter", (0.001, 0.025)),
+    # UniformPrior("testing_to_detection.assumed_cdr_parameter", (0.01, 0.02)),
+    # sojourns
+    # UniformPrior("sojourns.latent.total_time", (3, 5.0)),
+    # immunity stratification
+    UniformPrior("immunity_stratification.prop_immune", (0.5, 0.8)),
+    # UniformPrior("immunity_stratification.prop_high_among_immune", (0.0, 1.0)),
+    # age stratification
+    # UniformPrior("age_stratification.cfr.multiplier", (0.35, 0.55)),
+    # UniformPrior("age_stratification.prop_hospital.multiplier", (0.0, 1.0)),
+    # prop icu among hospitalization
+    UniformPrior("prop_icu_among_hospitalised", (0.05, 0.15)),
+    # start time of omicron
+    # UniformPrior("voc_emergence.omicron.new_voc_seed.start_time", (732.0, 763.0))
+    # sojourns
+    UniformPrior("sojourns.active.proportion_early", (0.2, 0.5)),
+    UniformPrior("sojourns.latent.proportion_early", (0.25, 0.75)),
 ]
 
 
@@ -101,4 +106,4 @@ project = Project(
 
 
 # from autumn.tools.calibration.proposal_tuning import perform_all_params_proposal_tuning
-# perform_all_params_proposal_tuning(project, calibration, priors, n_points=50, relative_likelihood_reduction=0.2)
+# perform_all_params_proposal_tuning(project, calibration, priors, n_points=20, relative_likelihood_reduction=0.2)

df1d32d27ca6121c158d92343d1a09c0956b769f
Hoang Anh NGO <50743576+hoanganhngo610@users.noreply.github.com>
2022-03-04 20:15:51 +0100

    Update MLE params for HCMC SM-SIR model (#598)
    Update related information for HCMC SM-SIR model, including MLE params, proposal SDs, targets and priors

--- autumn/projects/sm_sir/vietnam/ho_chi_minh_city/project.py
+++ autumn/projects/sm_sir/vietnam/ho_chi_minh_city/project.py
@@ -38,12 +38,12 @@ ts_set = load_timeseries(build_rel_path("timeseries.json"))
 # truncated from 18th Jul to 28th Jul, then from 28th Aug onwards
 notifications = pd.concat(
     [
-     ts_set["notifications"].loc[606:639],  # form 28/02/2021 to 30/09/2021
+     ts_set["notifications"].loc[606:639],  # form 28/08/2021 to 30/09/2021
      ts_set["notifications"].loc[702:]  # from 02/12/2021 onwards
     ]
 )
 
-icu_occupancy = ts_set["icu_occupancy"].loc[640:].rolling(7).mean()  # truncated to 01 Oct 2021
+icu_occupancy = ts_set["icu_occupancy"].loc[640:]  # truncated to 01 Oct 2021
 infection_deaths = ts_set["infection_deaths"].loc[556:].rolling(7).mean()  # truncated to 9th Jul 2021
 
 targets = [NormalTarget(notifications),
@@ -68,8 +68,9 @@ priors = [
     # UniformPrior("age_stratification.prop_hospital.multiplier", (0.0, 1.0)),
     # prop icu among hospitalization
     UniformPrior("prop_icu_among_hospitalised", (0.05, 0.15)),
-    # start time of omicron
-    # UniformPrior("voc_emergence.omicron.new_voc_seed.start_time", (732.0, 763.0))
+    # emergence of omicron
+    UniformPrior("voc_emergence.omicron.new_voc_seed.start_time", (746.0, 767.0)),  # 3 weeks interval
+    UniformPrior("voc_emergence.omicron.relative_latency", (0.45, 0.75)),
     # sojourns
     UniformPrior("sojourns.active.proportion_early", (0.2, 0.5)),
     UniformPrior("sojourns.latent.proportion_early", (0.25, 0.5)),

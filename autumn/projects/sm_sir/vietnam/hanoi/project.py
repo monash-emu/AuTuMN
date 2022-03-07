8fbbebe00b2ee9169530834e34b422a9c062cb5c
Hoang Anh Ngo <h.a.ngo@sms.ed.ac.uk>
2022-03-08 00:30:26 +0100

    Prior update for Hanoi SM-SIR model

--- autumn/projects/sm_sir/vietnam/hanoi/project.py
+++ autumn/projects/sm_sir/vietnam/hanoi/project.py
@@ -54,12 +54,12 @@ priors = [
     UniformPrior("infectious_seed", (5000., 10000.)),
     UniformPrior("contact_rate", (0.1, 0.5)),
     # testing to detection params
-    UniformPrior("testing_to_detection.assumed_tests_parameter", (0.0005, 0.003)),
-    UniformPrior("testing_to_detection.assumed_cdr_parameter", (0.03, 0.15)),
+    UniformPrior("testing_to_detection.assumed_tests_parameter", (0.0005, 0.002)),
+    UniformPrior("testing_to_detection.assumed_cdr_parameter", (0.03, 0.10)),
     # sojourns
     # UniformPrior("sojourns.latent.total_time", (3, 5.0)),
     # immunity stratification
-    UniformPrior("immunity_stratification.prop_immune", (0.75, 1.0)),
+    UniformPrior("immunity_stratification.prop_immune", (0.8, 1.0)),
     UniformPrior("immunity_stratification.prop_high_among_immune", (0.3, 0.6)),
     # age stratification
     UniformPrior("age_stratification.cfr.multiplier", (0., 0.1)),
@@ -68,7 +68,7 @@ priors = [
     UniformPrior("prop_icu_among_hospitalised", (0.01, 0.2)),
     # start time of omicron
     UniformPrior("voc_emergence.omicron.new_voc_seed.start_time", (715.0, 746.0)),  # 1 month interval
-    UniformPrior("voc_emergence.omicron.relative_latency", (0.25, 0.6)),
+    UniformPrior("voc_emergence.omicron.relative_latency", (0.2, 0.5)),
     # sojourns
     UniformPrior("sojourns.active.proportion_early", (0.25, 0.6)),
     UniformPrior("sojourns.latent.proportion_early", (0., 0.2)),

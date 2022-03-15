bfb5d9a42226b211c383ce0af9b4ef57d7527749
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2022-03-15 13:59:49 +1100

    update priors

--- autumn/projects/covid_19/sri_lanka/sri_lanka/project.py
+++ autumn/projects/covid_19/sri_lanka/sri_lanka/project.py
@@ -40,7 +40,7 @@ priors = [
     UniformPrior("infectious_seed", [450, 500]),
     # Detection
     UniformPrior("infection_fatality.multiplier", [0.1, 1.0]),
-    UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.003, 0.03]),
+    UniformPrior("testing_to_detection.assumed_cdr_parameter", [0.001, 0.02]),
     UniformPrior("contact_tracing.assumed_trace_prop", [0.55, 0.9]),
     UniformPrior("hospital_reporting", [0.25, 0.75]),
     TruncNormalPrior("clinical_stratification.props.symptomatic.multiplier",

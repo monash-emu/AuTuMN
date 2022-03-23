084f5ea91f6d5bea26ccac7f939442ae0b9cc0d4
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2022-03-23 17:05:12 +1100

    MYS priors update

--- autumn/projects/sm_sir/malaysia/malaysia/project.py
+++ autumn/projects/sm_sir/malaysia/malaysia/project.py
@@ -36,12 +36,12 @@ targets = [
 
 priors = [
     UniformPrior("contact_rate", (0.07, 0.19)),
-    UniformPrior("testing_to_detection.assumed_cdr_parameter", (0.05, 0.15)),
-    UniformPrior("sojourns.latent.total_time", (1.5, 4.0)),
-    UniformPrior("voc_emergence.omicron.contact_rate_multiplier", (0.8, 1.5)),
-    UniformPrior("voc_emergence.omicron.new_voc_seed.start_time", (520, 650)),
-    UniformPrior("voc_emergence.omicron.relative_latency", (0.45, 0.75)),
-    UniformPrior("voc_emergence.omicron.relative_active_period", (0.7, 1.3)),
+    UniformPrior("testing_to_detection.assumed_cdr_parameter", (0.01, 0.15)),
+    UniformPrior("sojourns.latent.total_time", (1.5, 7.0)),
+    UniformPrior("voc_emergence.omicron.contact_rate_multiplier", (0.3, 1.5)),
+    UniformPrior("voc_emergence.omicron.new_voc_seed.start_time", (520, 750)),
+    UniformPrior("voc_emergence.omicron.relative_latency", (0.15, 0.95)),
+    UniformPrior("voc_emergence.omicron.relative_active_period", (0.4, 1.3)),
 ]
 
 calibration = Calibration(

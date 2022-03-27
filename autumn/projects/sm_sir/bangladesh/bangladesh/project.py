87478bab4fa4185ce79d01cf5d570a7798b3d826
James Trauer <james.trauer@monash.edu>
2022-03-27 22:13:05 +1100

    Housekeeping to project file, Bangladesh

--- autumn/projects/sm_sir/bangladesh/bangladesh/project.py
+++ autumn/projects/sm_sir/bangladesh/bangladesh/project.py
@@ -13,15 +13,16 @@ from autumn.settings import Region, Models
 baseline_params = base_params.update(build_rel_path("params/baseline.yml"))
 param_set = ParameterSet(baseline=baseline_params, scenarios=[])
 
-
 # Load and configure calibration settings.
 ts_set = load_timeseries(build_rel_path("timeseries.json"))
 calibration_start_time = param_set.baseline.to_dict()["time"]["start"]
 
+# Work out date truncation points
 targets_start = (date(2021, 5, 15) - BASE_DATE).days
 notifications_trunc_point = (date(2021, 12, 1) - BASE_DATE).days
-notifications_ts = ts_set["notifications"].loc[targets_start: notifications_trunc_point]
 
+# Get the actual targets
+notifications_ts = ts_set["notifications"].loc[targets_start: notifications_trunc_point]
 hospital_admissions_ts = ts_set["hospital_admissions"].loc[targets_start:]
 deaths_ts = ts_set["infection_deaths"].loc[targets_start:]
 
@@ -29,7 +30,8 @@ priors = [
     UniformPrior("contact_rate", (0.07, 0.17)),
     UniformPrior("testing_to_detection.assumed_cdr_parameter", (0.005, 0.015)),
     UniformPrior("voc_emergence.omicron.new_voc_seed.start_time", (520., 570.)),
-    UniformPrior("voc_emergence.omicron.contact_rate_multiplier", (0.5, 1.))]
+    UniformPrior("voc_emergence.omicron.contact_rate_multiplier", (0.5, 1.)),
+]
 
 targets = [
     NormalTarget(notifications_ts),

17c5c8aa9c1aaa079b2f03f97df07b1782cb3b2a
James Trauer <james.trauer@monash.edu>
2022-04-02 22:38:44 +1100

    Remove Vic example model, which was similar to Philippines example anyway

--- autumn/projects/example/philippines/project.py
+++ autumn/projects/example/philippines/project.py
@@ -5,12 +5,12 @@ from autumn.tools.calibration.targets import NormalTarget
 from autumn.models.example import base_params, build_model
 from autumn.settings import Region, Models
 
-# Load and configure model parameters.
+# Load and configure model parameters
 baseline_params = base_params.update(build_rel_path("params/baseline.yml"))
 scenario_1_params = baseline_params.update(build_rel_path("params/scenario-1.yml"))
 param_set = ParameterSet(baseline=baseline_params, scenarios=[scenario_1_params])
 
-# Load and configure calibration settings.
+# Load and configure calibration settings
 ts_set = load_timeseries(build_rel_path("timeseries.json"))
 priors = [
     UniformPrior("contact_rate", [0.025, 0.05]),
@@ -24,5 +24,5 @@ targets = [
 ]
 calibration = Calibration(priors=priors, targets=targets)
 
-# Create and register the project.
+# Create and register the project
 project = Project(Region.PHILIPPINES, Models.EXAMPLE, build_model, param_set, calibration)

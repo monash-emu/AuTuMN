162aa5258f5834b46dea0bcc6f99fb4a7a232872
James Trauer <james.trauer@monash.edu>
2022-04-06 21:40:14 +1000

    Put floor on later hospitalisations

--- autumn/projects/sm_sir/bangladesh/bangladesh/project.py
+++ autumn/projects/sm_sir/bangladesh/bangladesh/project.py
@@ -1,4 +1,6 @@
 import json
+
+import numpy as np
 from pandas import Series
 from datetime import datetime
 
@@ -7,12 +9,16 @@ from autumn.settings.constants import COVID_BASE_DATETIME
 from autumn.tools.project import Project, ParameterSet, load_timeseries, build_rel_path
 from autumn.tools.calibration import Calibration
 from autumn.tools.calibration.priors import UniformPrior
-from autumn.tools.calibration.targets import NormalTarget
+from autumn.tools.calibration.targets import NormalTarget, TruncNormalTarget
 from autumn.models.sm_sir import base_params, build_model
 from autumn.settings import Region, Models
 
 # Load and configure model parameters
-baseline_params = base_params.update(build_rel_path("params/baseline.yml"))
+baseline_params = base_params.update(
+    build_rel_path("params/baseline.yml")
+).update(
+    build_rel_path("params/mle-params.yml"), calibration_format=True,
+)
 param_set = ParameterSet(baseline=baseline_params, scenarios=[])
 
 # Load and configure calibration settings.
@@ -23,17 +29,21 @@ calibration_start_time = param_set.baseline.to_dict()["time"]["start"]
 targets_start = (datetime(2021, 5, 15) - COVID_BASE_DATETIME).days
 notifications_trunc_point = (datetime(2021, 12, 1) - COVID_BASE_DATETIME).days
 notif_change_start_point = (datetime(2022, 1, 29) - COVID_BASE_DATETIME).days
+hospital_floor_start = (datetime(2022, 1, 1) - COVID_BASE_DATETIME).days
 
 # Get the actual targets
 notifications_ts = ts_set["notifications"].loc[targets_start: notifications_trunc_point]
-hospital_admissions_ts = ts_set["hospital_admissions"].loc[targets_start:]
+hospital_admit_early_ts = ts_set["hospital_admissions"].loc[targets_start: hospital_floor_start]
+hospital_late_early_ts = ts_set["hospital_admissions"].loc[hospital_floor_start:]
 late_hosp_admissions = ts_set["hospital_admissions"].loc[notifications_trunc_point:]
 deaths_ts = ts_set["infection_deaths"].loc[targets_start:]
 late_deaths = ts_set["infection_deaths"].loc[notifications_trunc_point:]
 
+
 # Build transformed target series
 def get_roc(series: Series) -> Series:
     return series.rolling(7).mean().pct_change(7)
+
 
 # Build targets for rate-of-change (roc) variables
 roc_vars = ["notifications"]
@@ -46,7 +56,8 @@ for roc_var in roc_vars:
 
 targets = [
     NormalTarget(notifications_ts),
-    NormalTarget(hospital_admissions_ts),
+    NormalTarget(hospital_admit_early_ts),
+    TruncNormalTarget(hospital_late_early_ts, trunc_range=(200., np.inf)),
     NormalTarget(deaths_ts),
     NormalTarget(late_hosp_admissions),
     NormalTarget(late_deaths)

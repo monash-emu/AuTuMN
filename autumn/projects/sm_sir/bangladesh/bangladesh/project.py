60f5fbfc023063d4d6c620182123f8dc3a26d2e0
James Trauer <james.trauer@monash.edu>
2022-04-19 11:37:00 +1000

    Get basic structure running with dummy parameters

--- autumn/projects/sm_sir/bangladesh/bangladesh/project.py
+++ autumn/projects/sm_sir/bangladesh/bangladesh/project.py
@@ -5,7 +5,9 @@ from datetime import datetime
 
 from autumn.tools.utils.utils import wrap_series_transform_for_ndarray
 from autumn.settings.constants import COVID_BASE_DATETIME
-from autumn.tools.project import Project, ParameterSet, load_timeseries, build_rel_path
+from autumn.tools.project import (
+    Project, ParameterSet, load_timeseries, build_rel_path, get_all_available_scenario_paths
+)
 from autumn.tools.calibration import Calibration
 from autumn.tools.calibration.priors import UniformPrior
 from autumn.tools.calibration.targets import NormalTarget, TruncNormalTarget
@@ -18,7 +20,11 @@ baseline_params = base_params.update(
 ).update(
     build_rel_path("params/mle-params.yml"), calibration_format=True,
 )
-param_set = ParameterSet(baseline=baseline_params, scenarios=[])
+scenario_dir_path = build_rel_path("params/")
+scenario_paths = get_all_available_scenario_paths(scenario_dir_path)
+scenario_params = [baseline_params.update(p) for p in scenario_paths]
+
+param_set = ParameterSet(baseline=baseline_params, scenarios=scenario_params)
 
 # Load and configure calibration settings.
 ts_set = load_timeseries(build_rel_path("timeseries.json"))

8fd3c9529653418547d02ff687e0de9d55b5c0ed
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2022-04-28 16:12:03 +1000

    butan VoC scenarios

--- autumn/projects/sm_sir/bhutan/bhutan/project.py
+++ autumn/projects/sm_sir/bhutan/bhutan/project.py
@@ -1,5 +1,5 @@
 import json
-from autumn.tools.project import Project, ParameterSet, load_timeseries, build_rel_path
+from autumn.tools.project import Project, ParameterSet, load_timeseries, build_rel_path, get_all_available_scenario_paths
 from autumn.tools.calibration import Calibration
 from autumn.tools.calibration.priors import UniformPrior
 from autumn.tools.calibration.targets import NormalTarget
@@ -11,8 +11,11 @@ mle_path = build_rel_path("params/mle-params.yml")
 baseline_params = base_params.update(build_rel_path("params/baseline.yml")).update(
     mle_path, calibration_format=True
 )
-scenario_1_params = baseline_params.update(build_rel_path("params/scenario-1.yml"))
-param_set = ParameterSet(baseline=baseline_params, scenarios=[scenario_1_params])
+scenario_dir_path = build_rel_path("params/")
+scenario_paths = get_all_available_scenario_paths(scenario_dir_path)
+
+scenario_params = [baseline_params.update(p) for p in scenario_paths]
+param_set = ParameterSet(baseline=baseline_params, scenarios=scenario_params)
 
 # Load and configure calibration settings
 calibration_start_time = param_set.baseline.to_dict()["time"]["start"]

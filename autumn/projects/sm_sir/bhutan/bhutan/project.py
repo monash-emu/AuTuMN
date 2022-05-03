f5383d845a7b045887f3b2e4255ed7a9e266ddb1
James Trauer <james.trauer@monash.edu>
2022-05-03 21:54:58 +1000

    Start from where Pavithra left off and aim for better calibration (#665)
    * calibrating bhutan cases
    
    * parameters with better fit
    
    * parameters with better fit
    
    * update priors
    
    * update priors
    
    * Revise base parameters for Bhutan calibration
    
    * Reduce serial interval
    
    * Fidget with some base parameters
    
    * More unnecessary fidgeting
    
    * Saving all my fidgets
    
    * Yet more fidgets
    
    * Turn off VoCs
    
    * Remove incorrect if statement for what is now a pandas object
    
    * Push up my calibration notebook
    
    * Fairly poor calibration with fixed home mobility
    
    * Update calibration algorithm to new parameters
    
    * Include infectious seed in calibration parameters
    
    * New MLEs and widen CDR prior
    
    Co-authored-by: Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>

--- autumn/projects/sm_sir/bhutan/bhutan/project.py
+++ autumn/projects/sm_sir/bhutan/bhutan/project.py
@@ -3,7 +3,7 @@ from autumn.tools.project import Project, ParameterSet, load_timeseries, build_r
 from autumn.tools.calibration import Calibration
 from autumn.tools.calibration.priors import UniformPrior
 from autumn.tools.calibration.targets import NormalTarget
-from autumn.models.sm_sir import base_params, build_model, set_up_random_process
+from autumn.models.sm_sir import base_params, build_model
 from autumn.settings import Region, Models
 
 # Load and configure model parameters.
@@ -11,11 +11,11 @@ mle_path = build_rel_path("params/mle-params.yml")
 baseline_params = base_params.update(build_rel_path("params/baseline.yml")).update(
     mle_path, calibration_format=True
 )
-scenario_dir_path = build_rel_path("params/")
-scenario_paths = get_all_available_scenario_paths(scenario_dir_path)
+# scenario_dir_path = build_rel_path("params/")
+# scenario_paths = get_all_available_scenario_paths(scenario_dir_path)
 
-scenario_params = [baseline_params.update(p) for p in scenario_paths]
-param_set = ParameterSet(baseline=baseline_params, scenarios=scenario_params)
+# scenario_params = [baseline_params.update(p) for p in scenario_paths]
+param_set = ParameterSet(baseline=baseline_params)
 
 # Load and configure calibration settings
 calibration_start_time = param_set.baseline.to_dict()["time"]["start"]
@@ -27,22 +27,14 @@ targets = [
 ]
 
 priors = [
-    UniformPrior("contact_rate", [0.08, 0.35]),
-    UniformPrior("sojourns.latent.total_time", [1, 5]),
-    UniformPrior("testing_to_detection.assumed_cdr_parameter", (0.001, 0.01)),
+    UniformPrior("contact_rate", (0.2, 0.4)),
+    UniformPrior("sojourns.latent.total_time", (1., 4.)),
+    UniformPrior("testing_to_detection.assumed_cdr_parameter", (0.0005, 0.005)),
+    UniformPrior("infectious_seed", (50., 250.))
 ]
 
-
-if baseline_params.to_dict()["activate_random_process"]:
-    time_params = baseline_params.to_dict()["time"]
-    rp = set_up_random_process(time_params["start"], time_params["end"])
-
-    # rp = None  # use this when tuning proposal jumping steps
-else:
-    rp = None
-
 calibration = Calibration(
-    priors=priors, targets=targets, random_process=rp, metropolis_init="current_params"
+    priors=priors, targets=targets, random_process=None, metropolis_init="current_params"
 )
 
 

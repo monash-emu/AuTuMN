e3529e94f383abcd8c9d1a069b82dc0a6c1abd95
David Shipman <dshipman@gmail.com>
2022-05-10 07:37:18 +1000

    Prototyping Jax dynamic transitions function

--- autumn/tools/dynamic_proportions/solve_transitions.py
+++ autumn/tools/dynamic_proportions/solve_transitions.py
@@ -7,6 +7,21 @@ from typing import List
 from autumn.tools.curve import scale_up_function
 from autumn.tools.utils.utils import flatten_list
 
+try:
+    from jax.config import config
+    config.update("jax_enable_x64", True)
+    from jaxlib.xla_extension import CompiledFunction
+    from jax import numpy as jnp
+    from jax import scipy as jsp
+    from jax import jit as jjit
+    from jax import grad
+
+    JAX_ENABLED = True
+except:
+    JAX_ENABLED = False
+
+JAX_MIN_FUNC = None
+JAX_MIN_FUNC_JAC = None
 
 def calculate_transition_rates_from_dynamic_props(props_df: pd.DataFrame, active_flows: dict) -> dict:
     """
@@ -32,6 +47,16 @@ def calculate_transition_rates_from_dynamic_props(props_df: pd.DataFrame, active
     strata = props_df.columns.to_list()
     times = props_df.index.to_list()
 
+    if JAX_ENABLED:
+        global JAX_MIN_FUNC, JAX_MIN_FUNC_JAC
+        if JAX_MIN_FUNC is None:
+            min_func, min_func_jac = get_jax_functions(strata, active_flows)
+            JAX_MIN_FUNC = min_func
+            JAX_MIN_FUNC_JAC = min_func_jac
+        else:
+            min_func, min_func_jac = JAX_MIN_FUNC, JAX_MIN_FUNC_JAC
+        
+
     # Work out the transition rates associated with each flow and each time interval
     tv_rates = {f_name: [] for f_name in active_flows}
     for i in range(len(times) - 1):
@@ -41,7 +66,10 @@ def calculate_transition_rates_from_dynamic_props(props_df: pd.DataFrame, active
         end_props = props_df.loc[times[i + 1]]
 
         # Main calculations requiring numerical solving
-        rates = calculate_rates_for_interval(start_props, end_props, delta_t, strata, active_flows)
+        if JAX_ENABLED:
+            rates = calculate_rates_for_interval_jax(start_props, end_props, delta_t, strata, active_flows, min_func, min_func_jac)
+        else:
+            rates = calculate_rates_for_interval(start_props, end_props, delta_t, strata, active_flows)
         for f_name in active_flows:
             tv_rates[f_name].append(rates[f_name])
 
@@ -127,6 +155,67 @@ def calculate_rates_for_interval(
     solution = minimize(function_to_zero, x0=np.zeros(n_params), bounds=bounds, method="TNC")
 
     return {ordered_flow_names[i]: solution.x[i] for i in range(len(ordered_flow_names))}
+
+def calculate_rates_for_interval_jax(
+        start_props: pd.Series, end_props: pd.Series, delta_t: float, strata: list[str],
+        active_flows: dict, min_func: CompiledFunction, min_func_jac: CompiledFunction
+) -> dict:
+    """Equivalent to calculate_rates_for_interval, but optimized for jax
+    """
+    # Determine some basic characteristics
+    n_params = len(active_flows)
+    ordered_flow_names = list(active_flows.keys())
+    
+    start_props = jnp.array(start_props)
+    end_props = jnp.array(end_props)
+
+    # Define bounds to force the parameters to be positive
+    bounds = [(0.0, None)] * n_params
+    
+    init_guess = np.repeat(0.0,n_params)
+    
+    #solution = min_func.run(start_props=start_props, end_props=end_props, init_params=jnp.zeros(n_params), bounds=bounds)
+    solution = scipy.optimize.minimize(min_func, jac=min_func_jac, args=(start_props, end_props, delta_t),x0=init_guess, bounds=bounds, method="TNC")
+
+    return {ordered_flow_names[i]: solution.x[i] for i in range(len(ordered_flow_names))}
+
+def get_jax_functions(strata, active_flows):
+
+    ordered_flow_names = list(active_flows.keys())
+    n_strata = len(strata)
+
+    @jjit
+    def function_to_zero(params, start_props, end_props, delta_t):
+        # params is a list ordered in the same order as ordered_flow_names
+
+        # Create the transition matrix associated with a given set of transition parameters
+        m = jnp.zeros((n_strata, n_strata))
+        for i_row, stratum_row in enumerate(strata):
+            for i_col, stratum_col in enumerate(strata):
+                if i_row == i_col:
+                    # Diagonal components capture flows starting from the associated stratum
+                    relevant_flow_names = [f_name for f_name, f_ends in active_flows.items() if f_ends[0] == stratum_row]
+                    for f_name in relevant_flow_names:
+                        #`x[idx] = y``, use ``x = x.at[idx].set(y
+                        m = m.at[i_row, i_col].set(m[i_row, i_col] - params[ordered_flow_names.index(f_name)])
+                        #m[i_row, i_col] -= params[ordered_flow_names.index(f_name)]
+                else:
+                    # Off-diagonal components capture flows from stratum_col to stratum_row
+                    for f_name, f_ends in active_flows.items():
+                        if f_ends == (stratum_col, stratum_row):
+                            #m[i_row, i_col] = params[ordered_flow_names.index(f_name)]
+                            m = m.at[i_row, i_col].set(params[ordered_flow_names.index(f_name)])
+
+        # Calculate the matrix exponential, accounting for the time interval width
+        exp_mt = jsp.linalg.expm(m * delta_t)
+        
+        # Calculate the difference between the left and right terms of the equation
+        diff = jnp.matmul(exp_mt, start_props) - end_props
+
+        # Return the norm of the vector to make the minimised function a scalar function
+        return jnp.linalg.norm(diff)
+
+    return function_to_zero, jjit(grad(function_to_zero))
 
 
 def check_requested_proportions(props_df: pd.DataFrame, active_flows: dict, rel_diff_tol: float = 0.01):

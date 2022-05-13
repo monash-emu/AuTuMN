833f451272367ba4f41736169727e7487507e8e7
James Trauer <james.trauer@monash.edu>
2022-05-13 22:58:04 +1000

    Some tiny changes and add FIXME statements

--- autumn/tools/utils/pretty.py
+++ autumn/tools/utils/pretty.py
@@ -2,6 +2,8 @@
 Utilities for pretty printing purposes.  May expand to a generic 'display' module at some point (Jupyter notebook extensions etc)
 """
 
+# FIXME: Move this to display.py (in this directory) and kill this file
+
 from pprint import PrettyPrinter
 
 

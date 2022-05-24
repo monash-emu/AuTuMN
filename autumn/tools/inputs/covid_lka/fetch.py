6b598d1805706b6d8adb55ff7971f5d0f1ad6529
maba0001 <milinda.abayawardana@monash.edu>
2022-05-24 14:28:09 +1000

    Refactor to remove os module from fetch queries.

--- autumn/tools/inputs/covid_lka/fetch.py
+++ autumn/tools/inputs/covid_lka/fetch.py
@@ -2,12 +2,13 @@
 This file is a placeholder. The data.csv must be manually downloaded 
 from [Link](https://covid-19.health.gov.lk/dhis-web-dashboard/)
 """
-import os
 from autumn.settings import INPUT_DATA_PATH
+from pathlib import Path
 
+INPUT_DATA_PATH = Path(INPUT_DATA_PATH)
 
-COVID_LKA_CSV = os.path.join(INPUT_DATA_PATH, "covid_lka", "data.csv")
-COVID_LKA_2021_CSV = os.path.join(INPUT_DATA_PATH, "covid_lka", "data_2021.csv")
+COVID_LKA_CSV = INPUT_DATA_PATH / "covid_lka" / "data.csv"
+COVID_LKA_2021_CSV = INPUT_DATA_PATH / "covid_lka" / "data_2021.csv"
 
 
 def fetch_covid_lka_data():
--- autumn/tools/inputs/owid/fetch.py
+++ autumn/tools/inputs/owid/fetch.py
@@ -6,13 +6,16 @@ import os
 import pandas as pd
 
 from autumn.settings import INPUT_DATA_PATH
+from pathlib import Path
+INPUT_DATA_PATH = Path(INPUT_DATA_PATH)
 
 OWID_URL = (
     "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv"
 )
-OWID_DIRPATH = os.path.join(INPUT_DATA_PATH, "owid")
-OWID_CSV_PATH = os.path.join(OWID_DIRPATH, "owid-covid-data.csv")
+OWID_DIRPATH = INPUT_DATA_PATH/ "owid"
+OWID_CSV_PATH = OWID_DIRPATH/ "owid-covid-data.csv"
 
 
 def fetch_owid_data():
-    df = pd.read_csv(OWID_URL).to_csv(OWID_CSV_PATH)
+    pd.read_csv(OWID_URL).to_csv(OWID_CSV_PATH)
+    return None

ce70fc77e5b9272264059496b16a6d5120e99f2c
Hoang Anh Ngo <h.a.ngo@sms.ed.ac.uk>
2022-06-02 14:58:40 +0700

    Add 2-dose and 3-dose coverage data and booster_effect_duration for HCMC SM-SIR model

--- autumn/models/sm_sir/stratifications/immunity.py
+++ autumn/models/sm_sir/stratifications/immunity.py
@@ -347,8 +347,10 @@ def apply_reported_vacc_coverage_with_booster(
         raw_data_booster = get_btn_vac_coverage(region="Bhutan", dose=3)
     elif iso3 == "VNM":
         if region == "Ho Chi Minh City":
-            raw_data_double = pd.Series({619: 0.1, 632: 0.2, 654: 0.55, 732: 0.7, 763: 0.75})
-            raw_data_booster = pd.Series({619: 0.001, 632: 0.001, 654: 0.001, 732: 0.001, 763: 0.001})
+            raw_data_double = pd.Series({619: 0.1, 632: 0.2, 654: 0.55, 710: 0.66,
+                                         732: 0.7, 746: 0.0725, 763: 0.7288, 791: 0.7338})
+            raw_data_booster = pd.Series({619: 0.001, 632: 0.001, 654: 0.001, 710: 0.001,
+                                          732: 0.142, 746: 0.383, 763: 0.456, 791: 0.474})
         elif region == "Hanoi":
             raw_data_double = pd.Series({822: 0.9, 884: 0.54})
             raw_data_booster = pd.Series({822: 0.045, 884: 0.045})

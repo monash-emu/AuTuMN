27e6878fdd547d1a9e803f22c0d966d7cfa36008
Hoang Anh Ngo <h.a.ngo@sms.ed.ac.uk>
2022-06-04 09:37:33 +0700

    Remove hosp_protection of Omicron variant for calibration

--- autumn/projects/sm_sir/vietnam/ho_chi_minh_city/project.py
+++ autumn/projects/sm_sir/vietnam/ho_chi_minh_city/project.py
@@ -73,7 +73,7 @@ priors = [
     # UniformPrior("voc_emergence.omicron.new_voc_seed.start_time", (746.0, 781.0)),  # 5 weeks interval
     # UniformPrior("voc_emergence.omicron.death_protection", (0.8, 1.0)),
     UniformPrior("voc_emergence.omicron.contact_rate_multiplier", (2, 5)),
-    UniformPrior("voc_emergence.omicron.hosp_protection", (0.8, 1.0)),
+    # UniformPrior("voc_emergence.omicron.hosp_protection", (0.8, 1.0)),
     UniformPrior("voc_emergence.omicron.icu_multiplier", (0, 0.4)),
     UniformPrior("voc_emergence.omicron.relative_active_period", (0.8, 1.5)),
     UniformPrior("voc_emergence.omicron.relative_latency", (0.01, 0.4)),

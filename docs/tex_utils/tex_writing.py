3f947a1f9b25c7fa9e6ad33025a150f44a0b9133
James Trauer <james.trauer@monash.edu>
2022-06-10 21:05:52 +1000

    Move folder handling to a function

---
+++ docs/tex_utils/tex_writing.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import os
+
+
+def get_params_folder(
+    model: str,
+    country: str,
+    region: str,
+):
+    """
+    Find the directory to where we want to keep the files for the parameters,
+    including add any paths that weren't already present.
+    
+    Args:
+        model: Name of the model type
+        country: The country from which the region comes
+        region: The region considered
+    
+    """
+    
+    base_dir = Path().absolute().parent.parent.parent  # Will need to change this
+    projects_dir = base_dir / "docs" / "tex_descriptions" / "projects"
+    
+    model_dir = projects_dir / model
+    model_dir.mkdir(exist_ok=True)
+    
+    country_dir = model_dir / country
+    country_dir.mkdir(exist_ok=True)
+    
+    app_dir = country_dir / region
+    app_dir.mkdir(exist_ok=True)
+    
+    return app_dir

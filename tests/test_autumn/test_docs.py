9cb09471461dc870442d9961a5bb9a9f5c2adce8
James Trauer <james.trauer@monash.edu>
2022-06-14 15:12:51 +1000

    Add some simplistic tests

---
+++ tests/test_autumn/test_docs.py
@@ -0,0 +1,39 @@
+from autumn.core.utils.tex_tools import (
+    get_params_folder, 
+    write_param_table_rows, 
+    write_prior_table_rows,
+)
+
+from autumn.settings import Region, Models
+from autumn.core.project import get_project
+
+
+def test_auto_params():
+    model = Models.SM_SIR
+    region = Region.BHUTAN
+    project = get_project(model, region)
+    selected_parameters = [
+        "contact_rate",
+        "ref_mixing_iso3",
+        "infectious_seed",
+        "sojourns.active.proportion_early",
+        "asympt_infectiousness_effect",
+        "isolate_infectiousness_effect",
+        "testing_to_detection.assumed_tests_parameter",
+        "testing_to_detection.assumed_cdr_parameter",
+        "testing_to_detection.floor_value",
+        "testing_to_detection.smoothing_period",    
+        "booster_effect_duration",
+        "immunity_stratification.infection_risk_reduction.high",
+        "immunity_stratification.infection_risk_reduction.low", 
+    ]
+    fixed_params_filename = get_params_folder(model, "bhutan", region, "auto_fixed_params")
+    write_param_table_rows(fixed_params_filename, project, selected_parameters)
+
+
+def test_auto_priors():
+    model = Models.SM_SIR
+    region = Region.BHUTAN
+    project = get_project(model, region)
+    prior_param_filename = get_params_folder(model, "bhutan", region, "auto_priors")
+    write_prior_table_rows(prior_param_filename, project)

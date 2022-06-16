448749b879694f8bdb68d626922342cc0dbbda4d
Hoang-Anh Ngo <50743576+hoanganhngo610@users.noreply.github.com>
2022-06-16 11:13:57 +0700

    HCMC SMSIR model update (#740)
    - Update baseline parameters (VoC related, microdistancing, etc.) and add one more scenario (partially immune escape, more severe VoC);
    - Update MLE params and priors;
    - Update notebooks after code refactoring.

--- autumn/projects/sm_sir/vietnam/ho_chi_minh_city/project.py
+++ autumn/projects/sm_sir/vietnam/ho_chi_minh_city/project.py
@@ -43,11 +43,11 @@ ts_set = load_timeseries(build_rel_path("timeseries.json"))
 hospital_occupancy = pd.concat(
     [
         ts_set["hospital_occupancy"].loc[592:615],  # from 14/08/2021 to 06/06/2021
-        ts_set["hospital_occupancy"].loc[632:],  # truncated to 23 Sep 2021
+        ts_set["hospital_occupancy"].loc[632:],  # truncated from 23 Sep 2021
     ]
 )
 
-icu_occupancy = ts_set["icu_occupancy"].loc[618:]  # truncated to 09 Sep 2021
+icu_occupancy = ts_set["icu_occupancy"].loc[618:]  # truncated from 09 Sep 2021
 # infection_deaths = ts_set["infection_deaths"].loc[556:].rolling(7).mean()  # truncated to 9th Jul 2021
 
 targets = [
@@ -63,35 +63,41 @@ priors = [
     # UniformPrior("age_stratification.cfr.multiplier", (0.4, 1.0)),
     # UniformPrior("age_stratification.prop_hospital.multiplier", (0.5, 1.0)),
     # infectious seed and contact rate
-    UniformPrior("infectious_seed", (200, 4000)),
-    UniformPrior("contact_rate", (0.05, 0.16)),
+    UniformPrior("infectious_seed", (100, 500)),
+    UniformPrior("contact_rate", (0.1, 0.2)),
     # testing to detection params
     # UniformPrior("testing_to_detection.assumed_tests_parameter", (0.001, 0.02)),
     # UniformPrior("testing_to_detection.assumed_cdr_parameter", (0.01, 0.1)),
     # sojourns
     # UniformPrior("sojourns.latent.total_time", (3, 5.0)),
+    # hospitalization multiplier
+    UniformPrior("age_stratification.prop_hospital.multiplier", (2.0, 3.0)),
     # immunity stratification
+    UniformPrior("immunity_stratification.infection_risk_reduction.low", (0.1, 0.4)),
+    UniformPrior("immunity_stratification.infection_risk_reduction.high", (0.4, 0.7)),
     # UniformPrior("immunity_stratification.prop_immune", (0.7, 0.9)),
     # UniformPrior("immunity_stratification.prop_high_among_immune", (0.0, 1.0)),
+    # Microdistancing
+    UniformPrior("mobility.microdistancing.behavior.parameters.max_effect", (0.01, 0.1)),
     # prop icu among hospitalization
-    # UniformPrior("prop_icu_among_hospitalised", (0.03, 0.1)),
+    UniformPrior("prop_icu_among_hospitalised", (0.02, 0.08)),
     # emergence of omicron
-    # UniformPrior("voc_emergence.omicron.new_voc_seed.start_time", (746.0, 781.0)),  # 5 weeks interval
+    UniformPrior("voc_emergence.omicron.new_voc_seed.start_time", (725.0, 770.0)),  # 5 weeks interval
     # UniformPrior("voc_emergence.omicron.death_protection", (0.8, 1.0)),
-    UniformPrior("voc_emergence.omicron.contact_rate_multiplier", (2, 6)),
+    UniformPrior("voc_emergence.omicron.contact_rate_multiplier", (1, 3)),
     # UniformPrior("voc_emergence.omicron.hosp_protection", (0.8, 1.0)),
-    UniformPrior("voc_emergence.omicron.icu_multiplier", (0.1, 0.75)),
-    UniformPrior("voc_emergence.omicron.relative_active_period", (1.0, 2.5)),
-    UniformPrior("voc_emergence.omicron.relative_latency", (0.01, 0.3)),
+    UniformPrior("voc_emergence.omicron.icu_multiplier", (0.3, 0.6)),
+    UniformPrior("voc_emergence.omicron.relative_active_period", (0.5, 1.0)),
+    UniformPrior("voc_emergence.omicron.relative_latency", (0.5, 1.0)),
     # emergence of delta
-    UniformPrior("voc_emergence.wild_type.icu_multiplier", (0.5, 1.5)),
-    UniformPrior("voc_emergence.wild_type.relative_active_period", (1.0, 3.5)),
-    UniformPrior("voc_emergence.wild_type.relative_latency", (0.5, 1.2)),
+    # UniformPrior("voc_emergence.wild_type.icu_multiplier", (0.5, 1.3)),
+    # UniformPrior("voc_emergence.wild_type.relative_active_period", (1.0, 3.5)),
+    # UniformPrior("voc_emergence.wild_type.relative_latency", (0.5, 1.2)),
     # sojourns
-    UniformPrior("sojourns.active.proportion_early", (0.5, 1.0)),
-    UniformPrior("sojourns.active.total_time", (5, 12)),
-    UniformPrior("sojourns.latent.proportion_early", (0.5, 1.0)),
-    UniformPrior("sojourns.latent.total_time", (2, 8)),
+    UniformPrior("sojourns.active.proportion_early", (0., 1.0)),
+    UniformPrior("sojourns.active.total_time", (4, 9)),
+    UniformPrior("sojourns.latent.proportion_early", (0., 1.0)),
+    UniformPrior("sojourns.latent.total_time", (1, 6)),
 ]
 
 
@@ -125,4 +131,4 @@ project = Project(
 
 
 # from autumn.calibration.proposal_tuning import perform_all_params_proposal_tuning
-# perform_all_params_proposal_tuning(project, calibration, priors, n_points=5, relative_likelihood_reduction=0.2)
+# perform_all_params_proposal_tuning(project, calibration, priors, n_points=10, relative_likelihood_reduction=0.2)

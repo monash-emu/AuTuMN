933731b94e51cb0dcf8deb0ffe0c1a5727e737a6
James Trauer <james.trauer@monash.edu>
2022-06-18 19:08:28 +1000

    Get owid testing function working again

--- autumn/core/inputs/owid/queries.py
+++ autumn/core/inputs/owid/queries.py
@@ -10,7 +10,7 @@ def get_international_owid_numbers(iso3, variable="new_tests"):
     df = input_db.query("owid", columns=["date", variable], conditions={"iso_code": iso3})
     df_with_data = df.dropna()  # dropna default behaviour is to drop entire row if any nas
     date_str_to_int = lambda s: (datetime.strptime(s, "%Y-%m-%d") - COVID_BASE_DATETIME).days
-    dates = list(df_with_data.date.apply(date_str_to_int).to_numpy())
+    dates = df_with_data.date.apply(date_str_to_int)
     numbers = list(df_with_data.loc[:, variable])
     return pd.Series(numbers, index=dates)
 

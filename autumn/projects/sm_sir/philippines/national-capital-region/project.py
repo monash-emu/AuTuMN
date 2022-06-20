1c3457c4ddc5011b9bcfb8d958e69f202d62315d
Romain Ragonnet <romain.ragonnet@monash.edu>
2022-06-20 14:10:47 +1000

    Dynamic generation of scenarios for NCR

--- autumn/projects/sm_sir/philippines/national-capital-region/project.py
+++ autumn/projects/sm_sir/philippines/national-capital-region/project.py
@@ -12,15 +12,65 @@ from autumn.calibration.targets import NormalTarget
 from autumn.models.sm_sir import base_params, build_model, set_up_random_process
 from autumn.settings import Region, Models
 
+
+def get_scenario_params(n_boosters, target, voc_emerge):
+    doses_txt = {
+        200000: "200K", 500000: "500K", 1000000: "1M"
+    }
+    target_txt = {True: "targeted", False: "uniform"}
+    voc_txt = {True: "with immune escape VoC", False: "no new VoC"}
+
+    description = f"{doses_txt[n_boosters]} boosters per mth / {target_txt[target]} allocation / {voc_txt[voc_emerge]}"
+    
+    sc_param_dict = {
+        "description": description,
+        "future_monthly_booster_rate": n_boosters,
+    }
+    
+    if target:
+        sc_param_dict["future_booster_age_allocation"] = [60, 50, 25, 15, 0]
+    
+    if voc_emerge:
+        sc_param_dict["voc_emergence"] = {
+            "omicron": {
+                "cross_protection": {
+                    "new_strain": {
+                        "early_reinfection": 0.,
+                        "late_reinfection": 0.
+                    }
+                }
+            },
+            "new_strain": {
+                "new_voc_seed": {
+                    "start_time": 897.,  # 15 June 2022
+                },
+                "contact_rate_multiplier": 1.,
+                "immune_escape": 1.
+            }
+        }
+
+    return sc_param_dict
 # Load and configure model parameters.
 mle_path = build_rel_path("params/mle-params.yml")
-scenario_dir_path = build_rel_path("params/")
-scenario_paths = get_all_available_scenario_paths(scenario_dir_path)
-
 baseline_params = base_params.update(build_rel_path("params/baseline.yml")).update(
     mle_path, calibration_format=True
 )
-scenario_params = [baseline_params.update(p) for p in scenario_paths]
+
+# scenario_dir_path = build_rel_path("params/")
+# scenario_paths = get_all_available_scenario_paths(scenario_dir_path)
+# scenario_params = [baseline_params.update(p) for p in scenario_paths]
+
+scenario_params = []
+for n_boosters in [200000, 500000, 1000000]:
+    for target in [False, True]:
+        for voc_emerge in [False, True]:
+            
+            if n_boosters == 200000 and not target and not voc_emerge:
+                continue  # as this is the baseline scenario
+            else:
+                update_params = get_scenario_params(n_boosters, target, voc_emerge)
+                scenario_params.append(baseline_params.update(update_params))
+
 param_set = ParameterSet(baseline=baseline_params, scenarios=scenario_params)
 
 # Load and configure calibration settings.

8c56163ca8cf71f4b987a6c8713bbfef78b328fd
longbui <longbui189@gmail.com>
2022-06-23 21:49:10 +1000

    NEW TB Model

--- autumn/models/tb_dynamics/parameters.py
+++ autumn/models/tb_dynamics/parameters.py
@@ -101,56 +101,5 @@ class Parameters:
     # Running time.
     time: Time
     # Output requests
-    calculated_outputs: list
-    outputs_stratification: dict
-    cumulative_output_start_time: float
-    # Model structure
-    stratify_by: list
-    age_breakpoints: list
-    user_defined_stratifications: dict
-    # Demographics
-    start_population_size: float
-    universal_death_rate: float
-    # Base disease model
-    contact_rate: float
-    age_specific_latency: dict
-    progression_multiplier: float
-    self_recovery_rate_dict: dict
-    infect_death_rate_dict: dict
-    rr_infection_latent: float
-    rr_infection_recovered: float
-    time_variant_bcg_perc: dict
-    bcg_effect: str
-    import_ltbi_cases: Optional[dict]
-    # Detection
-    time_variant_tb_screening_rate: dict
-    passive_screening_sensitivity: dict
-    # Treatment
-    treatment_duration: float
-    time_variant_tsr: dict
-    prop_death_among_negative_tx_outcome: float
-    on_treatment_infect_multiplier: float
-    # Characterising age stratification
-    age_infectiousness_switch: float
-    # Defining organ stratification
-    incidence_props_pulmonary: float
-    incidence_props_smear_positive_among_pulmonary: float
-    smear_negative_infect_multiplier: float
-    extrapulmonary_infect_multiplier: float
-    # Interventions
-    time_variant_acf: list
-    acf_screening_sensitivity: float
-    time_variant_ltbi_screening: list
-    ltbi_screening_sensitivity: float
-    pt_efficacy: float
-    pt_sae_prop: float
-    pt_destination_compartment: str
-    hh_contacts_pt: dict
-    awareness_raising: Optional[dict]
-    # Other
-    inflate_reactivation_for_diabetes: bool
-    extra_params: dict
-    haario_scaling_factor: float
-    metropolis_initialisation: str
 
  

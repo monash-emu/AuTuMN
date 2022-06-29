c53e61f933fe0cceba21ab06efb26acb8f0ada4c
Romain Ragonnet <romain.ragonnet@monash.edu>
2022-06-29 10:59:35 +1000

    Random process order and perioid are now params

--- pycharm.py
+++ pycharm.py
@@ -11,7 +11,7 @@ model = Models.SM_SIR
 project = get_project(model, region)
 
 # Run a model manually
-# run_project_locally(project, run_scenarios=False)
+run_project_locally(project, run_scenarios=False)
 
 # Run a calibration
-project.calibrate(max_seconds=20, chain_idx=1, num_chains=1)
+# project.calibrate(max_seconds=20, chain_idx=1, num_chains=1)

731c8eb319268bb6678914d109bdf565bcbe56a2
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2022-07-05 12:45:57 +1000

    further reduce priors

--- autumn/projects/sm_sir/common.py
+++ autumn/projects/sm_sir/common.py
@@ -8,7 +8,6 @@ def get_WPRO_priors():
         UniformPrior("contact_rate", (0.1, 0.3)),
         UniformPrior("voc_emergence.omicron.contact_rate_multiplier", (1.1, 1.3)),
         UniformPrior("voc_emergence.omicron.new_voc_seed.start_time", (520, 675)),
-        UniformPrior("voc_emergence.omicron.relative_latency", (0.45, 0.75)),
         UniformPrior("voc_emergence.omicron.relative_active_period", (0.4, 1.3))
     ]
     return priors

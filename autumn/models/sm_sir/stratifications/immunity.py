7af45d87ea9fdaeac9c1c1075116d13f9b6c5ab2
Hoang-Anh Ngo <50743576+hoanganhngo610@users.noreply.github.com>
2022-07-07 00:38:22 +0200

    HCMC SM-SIR model update (#764)
    - Update baseline scenario file to account for age-specific groups and add two new scenarios regarding additional booster dose administration;
    - Update historical two-dose and booster dose coverage for HCMC until 30th June 2022;
    - Update data for hospital and ICU occupancy of HCMC until 30th June 2022;
    - Misc. updates for notebooks.

--- autumn/models/sm_sir/stratifications/immunity.py
+++ autumn/models/sm_sir/stratifications/immunity.py
@@ -422,10 +422,10 @@ def get_historical_vacc_data(iso3, region, model_start_time, start_immune_prop, 
         raw_data_booster = get_btn_vac_coverage(region="Bhutan", dose=3)
     elif iso3 == "VNM":
         if region == "Ho Chi Minh City":
-            raw_data_double = pd.Series({619: 0.1, 632: 0.2, 654: 0.55, 710: 0.66,
-                                         732: 0.7, 746: 0.0725, 763: 0.7288, 791: 0.7338})
+            raw_data_double = pd.Series({619: 0.0909, 632: 0.1818, 654: 0.5, 710: 0.6,
+                                         732: 0.6364, 746: 0.6591, 763: 0.6652, 791: 0.6671, 912: 0.7})
             raw_data_booster = pd.Series({619: 0.001, 632: 0.001, 654: 0.001, 710: 0.001,
-                                          732: 0.142, 746: 0.383, 763: 0.456, 791: 0.474})
+                                          732: 0.1291, 746: 0.3482, 763: 0.4145, 791: 0.4309, 912: 0.5})
         elif region == "Hanoi":
             raw_data_double = pd.Series({822: 0.9, 884: 0.54})
             raw_data_booster = pd.Series({822: 0.045, 884: 0.045})

27ab6df65cb064f2eca669a193849d828529537c
milinda-abay <43531318+milinda-abay@users.noreply.github.com>
2022-07-18 12:49:20 +1000

    Fix university of Maryland Covid survey query

--- autumn/core/inputs/covid_survey/queries.py
+++ autumn/core/inputs/covid_survey/queries.py
@@ -1,16 +1,15 @@
+import pandas as pd
 from autumn.core.inputs.database import get_input_db
 from autumn.settings.constants import COVID_BASE_DATETIME
-
-import pandas as pd
 
 TODAY = (pd.to_datetime("today").date() - COVID_BASE_DATETIME.date()).days
 
 
-def get_percent_mc(iso3: str = None, col_name: str = "percent_mc"):
+def get_percent_mc(iso3: str = None, col_name: str = "pct"):
     return get_survey_results(iso3, col_name, "survey_mask")
 
 
-def get_percent_avoid_contact(iso3: str = None, col_name: str = "pct_avoid_contact"):
+def get_percent_avoid_contact(iso3: str = None, col_name: str = "pct"):
     return get_survey_results(iso3, col_name, "survey_avoid_contact")
 
 
@@ -20,15 +19,12 @@ def get_survey_results(iso3, col_name, table_name):
     dates = df["date_index"].to_numpy()
     values = df[col_name].to_numpy()
 
-    valid_dates = all(1 <= dates) and all(dates <= TODAY)
-    valid_values = all(0 <= values) and all(
-        values <= 1
-    )  # Assuming it's the percentage column for now.
+    valid_dates = all(dates >= 1) and all(dates <= TODAY)
+    valid_values = all(values >= 0) and all(values <= 1)
 
     if not valid_dates:
         raise AssertionError("Date index out of range")
     if not valid_values:
         raise AssertionError("Percentage values out of logical bounds")
-
 
     return dates, values

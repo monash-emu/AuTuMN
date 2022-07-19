1eaacd884bab08a4362ea5c0b591faf0e559ddbe
Romain Ragonnet <romain.ragonnet@monash.edu>
2022-07-19 12:20:26 +1000

    Make PHL data import code a module

--- notebooks/capacity_building/philippines/import_phl_data.py
+++ notebooks/capacity_building/philippines/import_phl_data.py
@@ -8,12 +8,8 @@ from zipfile import ZipFile # To manage zip files
 import pandas as pd  # pd is an alias for pandas. This is similar to dataframes in R
 import numpy as np
 
-# Shareable google drive links
-PHL_DOH_LINK = "1fFKoNVan7PS6BpBr01nwByNTLLu6z1AA"  # sheet 05 daily report.
-PHL_FASSSTER_LINK = "15eDyTjXng2Zh38DVhmeNy0nQSqOMlGj3" # Fassster google drive zip file.
-
 # By defining a region variable, we can easily change the analysis later.
-region = "NATIONAL CAPITAL REGION (NCR)"
+REGION = "NATIONAL CAPITAL REGION (NCR)"
 
 """
 Utility functions
@@ -40,31 +36,45 @@ def get_fassster_data(link:str)->pd.DataFrame:
     df = pd.read_csv(zipfile_ob.open(filename[0]))
     return df
 
-doh_df = fetch_doh_data(PHL_DOH_LINK)
-df_cases = get_fassster_data(PHL_FASSSTER_LINK)
+def get_population_and_epi_data(phl_doh_link: str, phl_fasster_link: str) -> (int, pd.DataFrame):
+    """
+    Master function that loads the Philippines population data and COVID-19 timeseries
 
-df_cases = df_cases.groupby(["Report_Date","Region"],as_index=False).size() # Because each row is a case we can use the group size.
-df_cases = df_cases[df_cases['Region']=="NCR"] # Filter for NCR cases
-df_cases = df_cases.rename(columns={"Report_Date":"reportdate", "size":"cases"}) # Rename columns to match DoH names.
-df_cases['reportdate'] = pd.to_datetime(df_cases['reportdate'])
+    Args:
+        phl_doh_link: Google Drive URL to DoH data
+        phl_fasster_link: Google Drive URL to FASSSTER data 
 
-doh_df = doh_df[['reportdate','region','cfname','nonicu_o','icu_o']]
-doh_df["reportdate"]  = pd.to_datetime(doh_df["reportdate"]).dt.tz_localize(None)
-doh_df = doh_df.groupby(['reportdate','region'], as_index=False).sum()
+    Returns:
+        initial_population: The size of the NCR population 
+        df: Epidemic data stored in a pandas dataframe
+    """
+    doh_df = fetch_doh_data(phl_doh_link)
+    df_cases = get_fassster_data(phl_fasster_link)
 
-mask = (doh_df['region'] == region)
-doh_df = doh_df[mask]
+    df_cases = df_cases.groupby(["Report_Date","Region"],as_index=False).size() # Because each row is a case we can use the group size.
+    df_cases = df_cases[df_cases['Region']=="NCR"] # Filter for NCR cases
+    df_cases = df_cases.rename(columns={"Report_Date":"reportdate", "size":"cases"}) # Rename columns to match DoH names.
+    df_cases['reportdate'] = pd.to_datetime(df_cases['reportdate'])
+
+    doh_df = doh_df[['reportdate','region','cfname','nonicu_o','icu_o']]
+    doh_df["reportdate"]  = pd.to_datetime(doh_df["reportdate"]).dt.tz_localize(None)
+    doh_df = doh_df.groupby(['reportdate','region'], as_index=False).sum()
+
+    mask = (doh_df['region'] == REGION)
+    doh_df = doh_df[mask]
 
 
-df = pd.merge(doh_df,df_cases, how='left', on='reportdate')
-# Set the index of this DataFrame to use calendar dates
-df = df.set_index('reportdate')
+    df = pd.merge(doh_df,df_cases, how='left', on='reportdate')
+    # Set the index of this DataFrame to use calendar dates
+    df = df.set_index('reportdate')
 
 
-population_url = 'https://github.com/monash-emu/AuTuMN/raw/master/data/inputs/world-population/subregions.csv'
-df_pop = pd.read_csv(population_url)
-df_pop = df_pop[df_pop['region']=='Metro Manila']
+    population_url = 'https://github.com/monash-emu/AuTuMN/raw/master/data/inputs/world-population/subregions.csv'
+    df_pop = pd.read_csv(population_url)
+    df_pop = df_pop[df_pop['region']=='Metro Manila']
 
-df_pop = df_pop.melt(id_vars=['country','iso3','region','year'], var_name='age_group', value_name='pop')
-df_pop = df_pop[['region','pop']].groupby('region',as_index=False).sum()
-initial_population = df_pop['pop'][0] * 1000 # We need to multiply by 1000 to covert it back to counts.
+    df_pop = df_pop.melt(id_vars=['country','iso3','region','year'], var_name='age_group', value_name='pop')
+    df_pop = df_pop[['region','pop']].groupby('region',as_index=False).sum()
+    initial_population = df_pop['pop'][0] * 1000 # We need to multiply by 1000 to covert it back to counts.
+
+    return initial_population, df

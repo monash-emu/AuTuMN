bce7baf0194ca259825b5d81cf630b1543407599
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2022-08-05 00:33:42 +1000

    change project file copying function

--- autumn/projects/sm_sir/WPRO/generate_directories.py
+++ autumn/projects/sm_sir/WPRO/generate_directories.py
@@ -1,6 +1,7 @@
 import os
 from WPR_constants import WPR_Countries
 from autumn.core.project import build_rel_path
+import shutil
 for country in WPR_Countries:
     # If a country folder does not exist generate one
     directory_path = os.getcwd()
@@ -15,14 +16,15 @@ for country in WPR_Countries:
 
     # generate a project file
     file_name = "project.py"
-    complete_name = os.path.join(folder_path, file_name)
-    project_file = open(complete_name, "w")
+    project_file_path = os.path.join(folder_path, file_name)
+    project_file_path = f"{project_file_path}"
 
     # generate a timeseries file
     timeseries_file_name = "timeseries.json"
     complete_timeseries_name = os.path.join(folder_path, timeseries_file_name)
     timeseries_file = open(complete_timeseries_name, "w")
 
-    # scanning each line in generate_project_WPR and writing into country specific project file
-    with open("generate_project_WPR.py", "r") as scan:
-        project_file.write(scan.read())
+    # writing generate_project_WPR into country specific project file
+    source_path = build_rel_path("generate_project_WPR.py")
+    source_path = f"{source_path}"
+    shutil.copy(source_path, project_file_path)

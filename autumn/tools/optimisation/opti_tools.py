dff476904ba27c51444c89dac6794f2ff2959399
Romain Ragonnet <romain.ragonnet@monash.edu>
2022-08-05 14:34:05 +1000

    Make opti code compatible with variable period

--- autumn/tools/optimisation/opti_tools.py
+++ autumn/tools/optimisation/opti_tools.py
@@ -7,6 +7,21 @@ def get_calibration_object(model, region):
 
     calib.end_time = 2 + max([max(t.data.index) for t in calib.targets])
     calib.model_parameters = project.param_set.baseline
+
+     # adjust random process values number if required
+    if calib.model_parameters['random_process']:
+        n_values = len(calib.model_parameters['random_process']['values'])
+        n_updates = len(calib.random_process.values)
+        if n_values >= n_updates:
+            calib.model_parameters['random_process']['values'] = calib.model_parameters['random_process']['values'][:n_updates]
+        else:
+            rp_values = calib.model_parameters['random_process']['values'] + [0.] * (n_updates - n_values)
+            calib.model_parameters = calib.model_parameters.update(
+                {
+                    "random_process.values": rp_values
+                }, calibration_format=True
+            )
+
     calib._is_first_run = False
     calib.project = project
     calib.run_mode = "autumn_mcmc"

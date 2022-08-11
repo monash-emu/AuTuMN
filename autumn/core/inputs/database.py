73a61fd3fac4e6cd552636d1e971c246ea24bc65
milinda <milinda@monash.edu>
2022-08-11 10:26:44 +1000

    NT cases and testing numbers

--- autumn/core/inputs/database.py
+++ autumn/core/inputs/database.py
@@ -59,6 +59,9 @@ def build_input_database(rebuild: bool = False):
         with Timer("Deleting all existing data."):
             input_db.delete_everything()
 
+        with Timer("Ingesting COVID NT data."):
+            preprocess_covid_au(input_db)
+
         with Timer("Ingesting COVID PHL data."):
             preprocess_covid_phl(input_db)
 

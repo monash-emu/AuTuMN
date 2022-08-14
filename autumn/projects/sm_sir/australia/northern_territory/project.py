f641afdd9d736c99be475c1b83455d4b35c6b1b0
jtrauer <james.trauer@monash.edu>
2022-08-14 21:05:59 +1000

    Create a basic project file

--- autumn/projects/sm_sir/australia/northern_territory/project.py
+++ autumn/projects/sm_sir/australia/northern_territory/project.py
@@ -9,7 +9,7 @@ from autumn.core.project import Project, ParameterSet, load_timeseries, build_re
 from autumn.calibration import Calibration
 from autumn.calibration.priors import UniformPrior
 from autumn.calibration.targets import NormalTarget
-from autumn.models.sm_sir import base_params, build_model, set_up_random_process
+from autumn.models.sm_sir import base_params, build_model
 from autumn.settings import Region, Models
 
 
@@ -39,39 +39,26 @@ def get_ts_date_indexes(
 
 # Load and configure model parameters
 baseline_params = base_params.update(build_rel_path("params/baseline.yml"))
-scenario_1_params = baseline_params.update(build_rel_path("params/scenario-1.yml"))
-param_set = ParameterSet(baseline=baseline_params, scenarios=[scenario_1_params])
+param_set = ParameterSet(baseline=baseline_params)
 
 # Load and configure calibration settings
 ts_set = load_timeseries(build_rel_path("timeseries.json"))
 priors = [
-    UniformPrior("contact_rate", [0.025, 0.05]),
+    UniformPrior("contact_rate", (0.03, 0.08)),
+    UniformPrior("voc_emergence.ba_1.cross_protection.ba_2.early_reinfection", (0.2, 0.6))
 ]
 targets = [
     NormalTarget(data=ts_set["notifications"]),
 ]
 
-if baseline_params.to_dict()["activate_random_process"]:
-    rp_params = baseline_params.to_dict()["random_process"]
-    rp = set_up_random_process(
-        rp_params["time"]["start"],
-        rp_params["time"]["end"],
-        rp_params["order"],
-        rp_params["time"]["step"],
-    )
-    # rp = None  # use this when tuning proposal jumping steps
-else:
-    rp = None
-
 calibration = Calibration(
-    priors=priors, targets=targets, random_process=rp, metropolis_init="current_params"
+    priors=priors, targets=targets, random_process=None, metropolis_init="current_params"
 )
 
 
 plot_spec_filepath = build_rel_path("timeseries.json")
 with open(plot_spec_filepath) as f:
     plot_spec = json.load(f)
-
 
 # Create and register the project
 project = Project(

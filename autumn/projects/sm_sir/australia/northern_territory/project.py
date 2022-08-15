dcd2cd2537a6ede1d41499373fa75b30378f7a12
David Shipman <dshipman@gmail.com>
2022-08-15 10:54:19 +1000

    Fix NT project to use timeseries.secret.json

--- autumn/projects/sm_sir/australia/northern_territory/project.py
+++ autumn/projects/sm_sir/australia/northern_territory/project.py
@@ -14,20 +14,20 @@ from autumn.settings import Region, Models
 
 
 def get_ts_date_indexes(
-    ts_set: Dict[str, pd.Series], 
+    ts_set: Dict[str, pd.Series],
     ref_time: datetime.datetime,
-)-> Dict[str, pd.Series]:
+) -> Dict[str, pd.Series]:
     """
     Get a version of a time series set, but with each
     constituent time series having date indexes,
     rather than integer.
-    
+
     Args:
         ts_set: The time series set we want to change
         ref_time: The model's reference time
     Returns:
         A modified version of the time series set
-    
+
     """
     new_ts_set = {}
     for indicator, ts in ts_set.items():
@@ -42,8 +42,10 @@ baseline_params = base_params.update(build_rel_path("params/baseline.yml"))
 scenario_1_params = baseline_params.update(build_rel_path("params/scenario-1.yml"))
 param_set = ParameterSet(baseline=baseline_params, scenarios=[scenario_1_params])
 
+ts_path = build_rel_path("timeseries.secret.json")
+
 # Load and configure calibration settings
-ts_set = load_timeseries(build_rel_path("timeseries.json"))
+ts_set = load_timeseries(ts_path)
 priors = [
     UniformPrior("contact_rate", [0.025, 0.05]),
 ]
@@ -68,18 +70,17 @@ calibration = Calibration(
 )
 
 
-plot_spec_filepath = build_rel_path("timeseries.json")
-with open(plot_spec_filepath) as f:
+with open(ts_path) as f:
     plot_spec = json.load(f)
 
 
 # Create and register the project
 project = Project(
-    Region.NORTHERN_TERRITORY, 
-    Models.SM_SIR, 
-    build_model, 
-    param_set, 
-    calibration, 
+    Region.NORTHERN_TERRITORY,
+    Models.SM_SIR,
+    build_model,
+    param_set,
+    calibration,
     plots=plot_spec,
     ts_set=ts_set,
 )

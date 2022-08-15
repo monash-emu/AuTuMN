2e426aab7aa839df020d9a0edf4bbab39dde68e9
milinda <milinda@monash.edu>
2022-08-15 11:32:47 +1000

    Input update

--- autumn/projects/sm_sir/australia/northern_territory/project.py
+++ autumn/projects/sm_sir/australia/northern_territory/project.py
@@ -1,33 +1,32 @@
-import json
-import pandas as pd
-from typing import Dict
 import datetime
+import json
+from typing import Dict
 
-from summer.utils import ref_times_to_dti
-
-from autumn.core.project import Project, ParameterSet, load_timeseries, build_rel_path
+import pandas as pd
 from autumn.calibration import Calibration
 from autumn.calibration.priors import UniformPrior
 from autumn.calibration.targets import NormalTarget
+from autumn.core.project import ParameterSet, Project, build_rel_path, load_timeseries
 from autumn.models.sm_sir import base_params, build_model, set_up_random_process
-from autumn.settings import Region, Models
+from autumn.settings import Models, Region
+from summer.utils import ref_times_to_dti
 
 
 def get_ts_date_indexes(
-    ts_set: Dict[str, pd.Series], 
+    ts_set: Dict[str, pd.Series],
     ref_time: datetime.datetime,
-)-> Dict[str, pd.Series]:
+) -> Dict[str, pd.Series]:
     """
     Get a version of a time series set, but with each
     constituent time series having date indexes,
     rather than integer.
-    
+
     Args:
         ts_set: The time series set we want to change
         ref_time: The model's reference time
     Returns:
         A modified version of the time series set
-    
+
     """
     new_ts_set = {}
     for indicator, ts in ts_set.items():
@@ -43,7 +42,7 @@ scenario_1_params = baseline_params.update(build_rel_path("params/scenario-1.yml
 param_set = ParameterSet(baseline=baseline_params, scenarios=[scenario_1_params])
 
 # Load and configure calibration settings
-ts_set = load_timeseries(build_rel_path("timeseries.json"))
+ts_set = load_timeseries(build_rel_path("timeseries.secret.json"))
 priors = [
     UniformPrior("contact_rate", [0.025, 0.05]),
 ]
@@ -68,18 +67,18 @@ calibration = Calibration(
 )
 
 
-plot_spec_filepath = build_rel_path("timeseries.json")
+plot_spec_filepath = build_rel_path("timeseries.secret.json")
 with open(plot_spec_filepath) as f:
     plot_spec = json.load(f)
 
 
 # Create and register the project
 project = Project(
-    Region.NORTHERN_TERRITORY, 
-    Models.SM_SIR, 
-    build_model, 
-    param_set, 
-    calibration, 
+    Region.NORTHERN_TERRITORY,
+    Models.SM_SIR,
+    build_model,
+    param_set,
+    calibration,
     plots=plot_spec,
     ts_set=ts_set,
 )

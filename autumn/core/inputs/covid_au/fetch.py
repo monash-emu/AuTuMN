a29ba3ecc601c6a594be45d07f96d7aa0b23fc4f
milinda <milinda@monash.edu>
2022-08-15 12:33:44 +1000

    Update NT data

--- autumn/core/inputs/covid_au/fetch.py
+++ autumn/core/inputs/covid_au/fetch.py
@@ -13,7 +13,7 @@ INPUT_DATA_PATH = Path(INPUT_DATA_PATH)
 DATA_URL = "https://raw.githubusercontent.com/M3IT/COVID-19_Data/master/Data/COVID_AU_state_daily_change.csv"
 COVID_AU_DIRPATH = INPUT_DATA_PATH / "covid_au"
 COVID_AU_CSV_PATH = COVID_AU_DIRPATH / "COVID_AU_state_daily_change.csv"
-NT_DATA = COVID_AU_DIRPATH / "NT data.secret.xlsx"
+NT_DATA = COVID_AU_DIRPATH / "NT-data.secret.xlsx"
 
 YOUGOV_URL = "https://github.com/YouGov-Data/covid-19-tracker/raw/master/data/australia.zip"
 COVID_AU_YOUGOV = COVID_AU_DIRPATH / "yougov_australia.csv"

a2b92e1c59e00ee80ab4b4fa172d3ee152529a86
longbui <longbui189@gmail.com>
2022-08-17 16:24:38 +1000

    Add tex documentation

--- autumn/models/tb_dynamics/outputs.py
+++ autumn/models/tb_dynamics/outputs.py
@@ -9,13 +9,6 @@ def request_outputs(model: CompartmentalModel):
     # Population
     out.request_compartment_output("population_size", BASE_COMPARTMENTS)
 
-    #latency
-    latent_comps = [BASE_COMPARTMENTS.LATE_LATENT, BASE_COMPARTMENTS.EARLY_LATENT]
-    out.request_compartment_output("latent_population_size", latent_comps, save_results=False)
-    sources = ["latent_population_size", "population_size"]
-    out.request_output_func("percentage_latent", calculate_percentage, sources)
-
-
 class TbOutputBuilder:
     """Helps build derived outputs for the TB model"""
 
@@ -28,18 +21,6 @@ class TbOutputBuilder:
             output_name, compartments, save_results=save_results
         )
 
-    def request_output_func(self, output_name, func, sources, save_results=True, stratify_by_loc=True):
-        self.model.request_function_output(output_name, func, sources, save_results=save_results)
-        if stratify_by_loc:
-            for location_stratum in self.locs:
-                loc_output_name = f"{output_name}Xlocation_{location_stratum}"
-                loc_sources = [f"{s}Xlocation_{location_stratum}" for s in sources]
-                self.model.request_function_output(
-                    loc_output_name, func, loc_sources, save_results=save_results
-                )
-
-def calculate_percentage(sub_pop_size, total_pop_size):
-    return 100 * sub_pop_size / total_pop_size
 
   
         

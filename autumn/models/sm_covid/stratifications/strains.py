a359ebd873f565b38001c4b440d8d2a8801ff89c
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2022-08-25 13:22:02 +1000

    voc emergence date from gisaid data

--- autumn/models/sm_covid/stratifications/strains.py
+++ autumn/models/sm_covid/stratifications/strains.py
@@ -52,11 +52,9 @@ def get_strain_strat(voc_params: Dict[str, VocComponent], compartments: List[str
 def get_first_variant_report_date(variant: str, country: str):
     """
     Determines the first report date of a given variant in a given country
-
     Args:
         variant: Name of the variant ('delta', 'omicron')
         country: Full name of the country
-
     Returns:
         Date of first report
     """
@@ -66,7 +64,7 @@ def get_first_variant_report_date(variant: str, country: str):
     }
 
     variants_global_emergence_date = {
-        "delta": date(2020, 10, 1),   # October 2020 according to WHO 
+        "delta": date(2020, 10, 1),   # October 2020 according to WHO
         "omicron": date(2021, 11, 1)  # November 2021 according to WHO
     }
 
@@ -74,7 +72,7 @@ def get_first_variant_report_date(variant: str, country: str):
 
     input_db = get_input_db()
     report_dates = input_db.query(
-        table_name='gisaid', 
+        table_name='gisaid',
         conditions={"Country": country, "Value": variants_map[variant]},
         columns=["Week prior to"]
     )["Week prior to"]
@@ -82,9 +80,9 @@ def get_first_variant_report_date(variant: str, country: str):
     if len(report_dates) == 0:
         return None
 
-    first_report_date = report_dates.min()    
+    first_report_date = report_dates.min()
     assert first_report_date >= variants_global_emergence_date[variant], "First report precedes global variant emergence"
-    
+
     return first_report_date
     
 

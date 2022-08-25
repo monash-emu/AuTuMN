95a8a9bc2ec99aed7c38eb8976658c3469b047ff
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2022-08-25 13:11:13 +1000

    generate project directories for each WPR country

---
+++ autumn/projects/sm_sir/WPRO/common.py
@@ -0,0 +1,28 @@
+from autumn.calibration.priors import UniformPrior
+from autumn.calibration.targets import NormalTarget
+from autumn.core.project import load_timeseries, build_rel_path
+
+
+def get_WPRO_priors():
+    priors = [
+        UniformPrior("contact_rate", (0.03, 0.3)),
+        UniformPrior("infectious_seed", (50, 500)),
+        UniformPrior("age_stratification.cfr.multiplier", (0.01, 0.7)),
+        UniformPrior("testing_to_detection.assumed_cdr_parameter", (0.01, 0.1)),
+    ]
+    return priors
+
+
+def get_tartgets(calibration_start_time, country_name, region_name):
+    seperator = "/"
+    timeseries_file = "timeseries.json"
+    time_series_path = build_rel_path(f"{country_name}{seperator}{region_name}{seperator}{timeseries_file}")
+    ts_set = load_timeseries(time_series_path)
+
+    infection_deaths_ts = ts_set["infection_deaths"].loc[calibration_start_time:]
+    notifications_ts = ts_set["notifications"].loc[calibration_start_time:]
+    targets = [
+        NormalTarget(infection_deaths_ts),
+        NormalTarget(notifications_ts)
+    ]
+    return targets
---
+++ autumn/projects/sm_sir/WPRO/generate_directories.py
@@ -0,0 +1,94 @@
+import os
+from autumn.core.project import build_rel_path
+from autumn.core.inputs.demography.queries import get_iso3_from_country_name
+import shutil
+import re
+import yaml
+import json
+from autumn.projects.sm_sir.WPRO.generate_timeseries import get_timeseries_data
+from pathlib import Path
+
+wpro_list = json.load(open(Path(__file__).parent.parent.parent.parent / "wpro_list.json"))
+WPR_Countries = wpro_list["region"]
+
+for country in WPR_Countries:
+    # If a country folder does not exist generate one
+    directory_path = os.getcwd()
+    folder_name = os.path.basename(directory_path)
+    folder_path = build_rel_path(f"{country}/{country}")
+    params_path = build_rel_path(f"{country}/{country}"+"/params")
+
+    # Check whether the specified path exists or not
+    isExist = os.path.exists(folder_path)
+    isExistbaseline = os.path.exists(params_path)
+
+    if not isExist:
+        os.makedirs(folder_path)  # Create a new directory because it does not exist
+    if not isExistbaseline:
+        os.makedirs(params_path)
+
+    # generate a project file
+    file_name = "project.py"
+    project_file_path = os.path.join(folder_path, file_name)
+    project_file_path = f"{project_file_path}"
+
+    # writing generate_project_WPR into country specific project file
+    source_path = build_rel_path("generate_project_WPR.py")
+    source_path = f"{source_path}"
+    shutil.copy(source_path, project_file_path)
+
+    # in each project file replacing the country name to corresponding country
+    with open(project_file_path, 'r+') as project_file:
+        read_file = project_file.read()
+        project_file.seek(0)
+
+        country_text = country
+        if '-' in country_text:
+            country_text = country_text.replace('-', '_')
+        region_name = country_text.upper()  # get the corresponding iso3 code
+
+        read_file = re.sub("country_name", f'"{country}"', read_file)
+        read_file = re.sub("COUNTRY_NAME", f"{region_name}", read_file)
+        project_file.write(read_file)
+        project_file.truncate()
+
+    country_text = country
+
+    if country == "south-korea":
+        iso3_name = "KOR"
+    else:
+        if '-' in country_text:
+            country_text = country_text.replace('-', " ")
+        iso3_name = get_iso3_from_country_name(f"{country_text.title()}")  # get the corresponding iso3 code
+
+    # generate timeseries.json file
+    timeseries_file_name = "timeseries.json"
+    complete_timeseries_name = os.path.join(folder_path, timeseries_file_name)
+    with open(complete_timeseries_name, "w") as timefile:
+        timeseries_data = get_timeseries_data(iso3_name)
+        json.dump(timeseries_data, timefile)
+
+    # create baseline.yml file
+    baseline_file_name = "baseline.yml"
+    baseline_file_path = os.path.join(params_path, baseline_file_name)
+    baseline_file_path = f"{baseline_file_path}"
+
+    # in the yml file update iso3 to corresponding country value
+    with open("generate_baseline_file.yml", 'r') as stream:
+        load_yml_params = yaml.safe_load(stream)
+
+    # Modify the fields from the dict
+    load_yml_params['country']['iso3'] = iso3_name
+    # Save the yml file again
+    with open("generate_baseline_file.yml", 'w') as stream:
+        yaml.dump(load_yml_params, stream, default_flow_style=False, sort_keys=False)
+
+    # writing basic baseline content into country specific baseline file
+    source_path_baseline = build_rel_path("generate_baseline_file.yml")
+    source_path_baseline = f"{source_path_baseline}"
+
+    # check if the baseline file exists and if so if it is empty
+    if os.path.exists(baseline_file_path) and os.stat(baseline_file_path).st_size == 0:
+        shutil.copyfile(source_path_baseline, baseline_file_path)
+    if not os.path.exists(baseline_file_path):  # if baseline file is created for the first time
+        shutil.copyfile(source_path_baseline, baseline_file_path)

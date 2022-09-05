d06d8b8d1af73677aa6a2c56cfc17d6a1d4b21f8
James Trauer <james.trauer@monash.edu>
2022-09-05 14:44:48 +1000

    Clean up the vaccination-related sections of the immunity stratification (sm_sir) (#827)
    * Get basic vaccination code running for NT
    
    * Implement dummy loop for age-specific vaccination
    
    * Allow for age-specific and non-age-specific vaccination coverage
    
    * Remove additional immunity points, not used
    
    * Save work to general vaccination function
    
    * New series summing function
    
    * Clean up series lag sums further
    
    * Whoops, and actually use it
    
    * Use the recent boosting thing now
    
    * Simplify some function calls and get rid of unused code
    
    * Implement structure for user requests
    
    * Delete all the old code
    
    * Theoretically running code, without parameterisation
    
    * Stop passing compartment names around vaccination functions
    
    * Restructure all the parameters
    
    * Move one section of code out to separate function
    
    * Clean up some bugs
    
    * Move second code section out to function
    
    * Hack away some NCR code to get tests passing
    
    * Remove parameter name from auto params test
    
    * Finish up with a few last comments and typing
    
    * Simpler way of calculating the recently vaccinated population
    
    * Cell to check outputs look sensible
    
    * Add some assertion checks
    
    * Remove switching off children vaccination
    
    * Reorganise some code to make sure everything really works for NCR
    
    * Only get vaccination coverage into model if available
    
    * Get NT working with new code approach
    
    * Remove junk import statement
    
    * Reorder requests before recency calculation

--- autumn/projects/sm_sir/philippines/national-capital-region/project.py
+++ autumn/projects/sm_sir/philippines/national-capital-region/project.py
@@ -13,6 +13,10 @@ from autumn.models.sm_sir import base_params, build_model, set_up_random_process
 from autumn.settings import Region, Models
 
 
+"""
+THIS FUNCTION HAS BEEN TOTALLY RUINED BY THE VACCINATION RESTRUCTURE
+"""
+
 def get_scenario_params(n_boosters, target, voc_emerge):
     doses_txt = {
         200000: "200K", 500000: "500K", 1000000: "1M"
@@ -24,11 +28,11 @@ def get_scenario_params(n_boosters, target, voc_emerge):
     
     sc_param_dict = {
         "description": description,
-        "future_monthly_booster_rate": n_boosters,
+        # "future_monthly_booster_rate": n_boosters,
     }
     
-    if target:
-        sc_param_dict["future_booster_age_allocation"] = [60, 50, 25, 15, 0]
+    # if target:
+        # sc_param_dict["future_booster_age_allocation"] = [60, 50, 25, 15, 0]
     
     if voc_emerge:
         sc_param_dict["voc_emergence"] = {
@@ -60,18 +64,18 @@ baseline_params = base_params.update(build_rel_path("params/baseline.yml")).upda
 # scenario_paths = get_all_available_scenario_paths(scenario_dir_path)
 # scenario_params = [baseline_params.update(p) for p in scenario_paths]
 
-scenario_params = []
-for n_boosters in [200000, 500000, 1000000]:
-    for target in [False, True]:
-        for voc_emerge in [False, True]:
+# scenario_params = []
+# for n_boosters in [200000, 500000, 1000000]:
+#     for target in [False, True]:
+#         for voc_emerge in [False, True]:
             
-            if n_boosters == 200000 and not target and not voc_emerge:
-                continue  # as this is the baseline scenario
-            else:
-                update_params = get_scenario_params(n_boosters, target, voc_emerge)
-                scenario_params.append(baseline_params.update(update_params))
+#             if n_boosters == 200000 and not target and not voc_emerge:
+#                 continue  # as this is the baseline scenario
+#             else:
+#                 update_params = get_scenario_params(n_boosters, target, voc_emerge)
+#                 scenario_params.append(baseline_params.update(update_params))
 
-param_set = ParameterSet(baseline=baseline_params, scenarios=scenario_params)
+param_set = ParameterSet(baseline=baseline_params)
 
 # Load and configure calibration settings.
 ts_set = load_timeseries(build_rel_path("timeseries.json"))

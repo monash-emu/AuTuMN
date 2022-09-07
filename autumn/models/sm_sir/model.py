cf1b7cab80ccd78672684a03b9d30ea68cb897d2
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2022-09-07 11:59:20 +1000

    manual calibration with coverage data

--- autumn/models/sm_sir/model.py
+++ autumn/models/sm_sir/model.py
@@ -514,7 +514,7 @@ def build_model(
     model.stratify_with(immunity_strat)
 
     # Apply vaccination coverage if available - Malaysia now supported by adding MYS to the following list
-    if iso3 in ["PHL", "AUS"]:
+    if iso3 in ["PHL", "AUS","MYS"]:
         apply_vacc_coverage(
             model,
             iso3,

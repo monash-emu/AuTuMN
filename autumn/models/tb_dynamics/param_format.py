b00a0014bc27c5b1f884833e1624e1ce609e0af7
longbui <longbui189@gmail.com>
2022-09-07 16:01:20 +1000

    Add params format

---
+++ autumn/models/tb_dynamics/param_format.py
@@ -0,0 +1,11 @@
+PARAMETER_DEFINITION = {
+    "start_population_size":"starting population size",
+    "infectious seed":"starting infectious seed",
+    "crude_birth_rate":"crude birth rate",
+    "crude_death_rate":"crude death rate",
+    "time": "running time",
+    "contact_rate":"contact rate",
+    "age_breakpoints":"age stratification",
+    "rr_infection_latent":"relative risk of reinfection while latently infected (ref. infection-naive)",
+    "rr_infection_recovered":"Relative risk of reinfection after recovery (ref. infection-naive)"
+}

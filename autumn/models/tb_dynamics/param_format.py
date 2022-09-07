b06e1f713a38c75ad824b034194cd5eaee89b099
longbui <longbui189@gmail.com>
2022-09-07 16:51:44 +1000

    Some corrections on PDF documentation

--- autumn/models/tb_dynamics/param_format.py
+++ autumn/models/tb_dynamics/param_format.py
@@ -1,5 +1,5 @@
 PARAMETER_DEFINITION = {
-    "start_population_size":"starting population size",
+    "start_population_size":"starting population size of Kiribati (1950)",
     "infectious seed":"starting infectious seed",
     "crude_birth_rate":"crude birth rate",
     "crude_death_rate":"crude death rate",
@@ -7,5 +7,21 @@ PARAMETER_DEFINITION = {
     "contact_rate":"contact rate",
     "age_breakpoints":"age stratification",
     "rr_infection_latent":"relative risk of reinfection while latently infected (ref. infection-naive)",
-    "rr_infection_recovered":"Relative risk of reinfection after recovery (ref. infection-naive)"
+    "rr_infection_recovered":"Relative risk of reinfection after recovery (ref. infection-naive)",
+    "self_recovery_rate:":"self recovery rate",
+    "infect_death_rate":"infect death rate"
+}
+
+PARAMETER_EVIDENCE = {
+    "infectious_seed":"abitrary",
+    "start_population_size":"UN Population Division",
+    "crude_birth_rate":"UN's Population Division",
+    "crude_death_rate":"UN's Population Division",
+    "contact_rate":"Assumed",
+    "rr_infection_latent":"\cite{ragonnet-2022}",
+    "rr_infection_recovered":"\cite{ragonnet-2022}",
+}
+
+PARAMETER_UNITS = {
+
 }

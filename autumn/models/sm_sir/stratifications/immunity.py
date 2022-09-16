9b1dc541d42beb4715622ceeaab87dbec7af0274
AngusEH <elford.hughes@gmail.com>
2022-09-16 17:12:29 +1000

    update scenario 4 params

--- autumn/models/sm_sir/stratifications/immunity.py
+++ autumn/models/sm_sir/stratifications/immunity.py
@@ -358,7 +358,7 @@ def apply_general_coverage(
         vaccine_data = vaccine_data.append(
             pd.DataFrame(
                 [
-                    [0.78, 0.43],  # Full, then boost values
+                    [0.68, 0.43],  # Full, then boost values
                 ], 
                 columns=["full", "boost"], 
                 index=[

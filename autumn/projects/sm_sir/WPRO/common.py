b1e4df7925bc3ff5d9868ee481688f561fe7e097
Pavithra Jayasundara <Pavithra.Jayasundara@monash.edu>
2022-09-30 15:44:40 +1000

    update priors

--- autumn/projects/sm_sir/WPRO/common.py
+++ autumn/projects/sm_sir/WPRO/common.py
@@ -6,9 +6,9 @@ from autumn.settings.constants import COVID_BASE_DATETIME
 
 def get_WPRO_priors(variant_times):
     priors = [
-        UniformPrior("contact_rate", (0.03, 0.3)),
+        UniformPrior("contact_rate", (0.01, 0.3)),
         UniformPrior("infectious_seed", (1000, 10000)),
-        UniformPrior("age_stratification.cfr.multiplier", (0.01, 0.7)),
+        UniformPrior("age_stratification.cfr.multiplier", (0.001, 0.7)),
         UniformPrior("testing_to_detection.assumed_cdr_parameter", (0.01, 0.1)),
         UniformPrior("sojourns.latent.total_time", (2, 12)),
         UniformPrior("voc_emergence.omicron.new_voc_seed.start_time", (variant_times[1]-150, variant_times[1]+100)),

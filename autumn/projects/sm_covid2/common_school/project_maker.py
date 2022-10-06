90b14e596ac81d889920daba39b7ea9ca44b968d
Romain Ragonnet <romain.ragonnet@monash.edu>
2022-10-06 21:17:05 +1100

    Reduce haario scaling factor

--- autumn/projects/sm_covid2/common_school/project_maker.py
+++ autumn/projects/sm_covid2/common_school/project_maker.py
@@ -102,7 +102,7 @@ def get_school_project(region):
         targets=targets,
         random_process=rp,
         metropolis_init="current_params",
-        haario_scaling_factor=2.4,
+        haario_scaling_factor=1.2, # 2.4,
         fixed_proposal_steps=500,
         metropolis_init_rel_step_size=0.02,
         using_summer2=True,

6f162ce235834cf98e9f91f2b3222d79212242a6
milinda-abay <43531318+milinda-abay@users.noreply.github.com>
2022-11-14 10:53:40 +1100

    Input updates

--- autumn/core/inputs/covid_phl/fetch.py
+++ autumn/core/inputs/covid_phl/fetch.py
@@ -11,7 +11,7 @@ from google_drive_downloader import GoogleDriveDownloader as gdd
 INPUT_DATA_PATH = Path(INPUT_DATA_PATH)
 
 # From DoH google drive folder, shareable link changes with every update
-DATA_URL = "1slIEIgpxsUwZ8d0BdSvTq1gAPntoBHGe"  # shareable link for sheet 07 testing data
+DATA_URL = "1BYEo6Qjdy2KBo4MTii0ZtopvF_JZn2y4"  # shareable link for sheet 07 testing data
 
 COVID_PHL_DIRPATH = INPUT_DATA_PATH / "covid_phl"
 COVID_PHL_CSV_PATH = COVID_PHL_DIRPATH / "COVID_Phl_testing.csv"

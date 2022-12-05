55b274bd2621dc2ae92c5920097aae1ac6c25ddd
Romain Ragonnet <romain.ragonnet@monash.edu>
2022-12-05 15:26:08 +1100

    Set up opti problem for Guillaume

---
+++ autumn/tools/optimisation/opti_tools.py
@@ -0,0 +1,43 @@
+from autumn.core.project import get_project
+
+
+def get_calibration_object(model, region):
+    project = get_project(model, region)
+    calib = project.calibration
+
+    calib.end_time = 2 + max([max(t.data.index) for t in calib.targets])
+    calib.model_parameters = project.param_set.baseline
+
+     # adjust random process values number if required
+    # if calib.model_parameters['random_process']:
+    #     n_values = len(calib.model_parameters['random_process']['values'])
+    #     n_updates = len(calib.random_process.values)
+    #     if n_values >= n_updates:
+    #         calib.model_parameters['random_process']['values'] = calib.model_parameters['random_process']['values'][:n_updates]
+    #     else:
+    #         rp_values = calib.model_parameters['random_process']['values'] + [0.] * (n_updates - n_values)
+    #         calib.model_parameters = calib.model_parameters.update(
+    #             {
+    #                 "random_process.values": rp_values
+    #             }, calibration_format=True
+    #         )
+
+    calib._is_first_run = False
+    calib.project = project
+    calib.run_mode = "autumn_mcmc"
+    calib.run_num = 0
+    target_names = [t.data.name for t in calib.targets]
+    calib.derived_outputs_whitelist = list(set(target_names))
+    calib.build_options = dict(enable_validation = False)
+
+    calib.workout_unspecified_target_sds()
+    calib.workout_unspecified_time_weights()
+    return calib
+
+
+def calculate_objective_to_minimize(calib, params_dict):
+    loglikelihood = calib.loglikelihood(params_dict)
+    logprior = calib.logprior(params_dict)
+    log_rp_likelihood = calib.random_process.evaluate_rp_loglikelihood()
+
+    return - (loglikelihood + logprior + log_rp_likelihood)
---
+++ autumn/tools/optimisation/running_code.py
@@ -0,0 +1,25 @@
+from autumn.settings import Region, Models
+from autumn.tools.optimisation.opti_tools import get_calibration_object, calculate_objective_to_minimize
+
+# Select the simulator
+model, region = Models.SM_COVID, Region.PHILIPPINES
+
+# Prepare the simulator
+calib = get_calibration_object(model, region)
+var_bounds = {
+    prior['param_name']: prior['distri_params'] for prior in calib.all_priors
+}
+print(len(var_bounds))
+# Now everything is ready to go.
+# The decision variables and their bounds can be found in var_bounds
+# The objective function is called using calculate_objective_to_minimize
+# See example below evaluating the objective at the variables' midpoints
+
+params_dict = {
+    param: (interval[0] + interval[1]) / 2. for param, interval in var_bounds.items()
+}
+
+y = calculate_objective_to_minimize(calib, params_dict)
+
+print(y)
+# I obtained y = 319.83403546126465 on my laptop 

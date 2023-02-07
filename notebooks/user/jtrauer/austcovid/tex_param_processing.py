cbe6718e584ab0e3ebb568ed3f3b9491da663f7d
James Trauer <james.trauer@monash.edu>
2023-02-08 10:21:12 +1100

    Move more TeX wrangling to a .py file

--- notebooks/user/jtrauer/austcovid/tex_param_processing.py
+++ notebooks/user/jtrauer/austcovid/tex_param_processing.py
@@ -1,3 +1,7 @@
+import pylatex as pl
+from pylatex.utils import NoEscape, bold
+
+from summer2 import CompartmentalModel
 
 
 def get_fixed_param_value_text(
@@ -66,3 +70,65 @@ def get_prior_dist_support(
         The bounds to the prior's distribution joined together
     """
     return " to ".join([str(i) for i in prior.bounds()])
+
+
+def add_param_table_to_doc(
+    model: CompartmentalModel,
+    doc: pl.document.Document, 
+    params: list, 
+    param_descriptions: dict, 
+    units: dict, 
+    evidence: dict, 
+    priors: list,
+):
+    """
+    Include a table for the non-calibrated parameters in a PyLaTeX document.
+    
+    Args:
+        doc: The document to modify
+        params: The parameters the model is expecting
+        descriptions: The longer parameter names
+        units: The Units of each parameter's value
+        evidence: Description of the evidence for each parameter
+        priors: The priors being used in calibration
+    """
+    doc.append("Parameter interpretation, with value (for parameters not included in calibration algorithm) and summary of evidence.\n")
+    param_headers = ["Name", "Value", "Evidence"]
+    with doc.create(pl.Tabular("p{2.7cm} " * 2 + "p{5.8cm}")) as parameters_table:
+        parameters_table.add_hline()
+        parameters_table.add_row([bold(i) for i in param_headers])
+        for param in model.get_input_parameters():
+            param_value_text = get_fixed_param_value_text(param, params, units, priors)
+            parameters_table.add_hline()
+            param_table_row = (param_descriptions[param], param_value_text, NoEscape(evidence[param]))
+            parameters_table.add_row(param_table_row)
+        parameters_table.add_hline()
+
+
+def add_calib_table_to_doc(
+    doc: pl.document.Document, 
+    priors: list, 
+    descriptions: dict,
+):
+    """
+    Include a table for the calibrated parameters in a PyLaTeX document.
+
+    Args:
+        doc: The document to modify
+        priors: The priors being used in calibration
+        descriptions: The longer parameter names
+    """
+    doc.append("Input parameters varied through calibration with uncertainty distribution parameters and support.\n")
+    calib_headers = ["Name", "Distribution", "Distribution parameters", "Support"]
+    with doc.create(pl.Tabular("p{2.7cm} " * 4)) as calibration_table:
+        calibration_table.add_hline()
+        calibration_table.add_row([bold(i) for i in calib_headers])
+        for prior in priors:
+            prior_desc = descriptions[prior.name]
+            dist_type = get_prior_dist_type(prior)
+            dist_params = get_prior_dist_param_str(prior)
+            dist_range = get_prior_dist_support(prior)
+            calibration_table.add_hline()
+            calib_table_row = (prior_desc, dist_type, dist_params, dist_range)
+            calibration_table.add_row(calib_table_row)
+        calibration_table.add_hline()

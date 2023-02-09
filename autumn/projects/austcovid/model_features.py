eae4e6ef6c13d943ab7c2ea2c8d3050c194ebe25
James Trauer <james.trauer@monash.edu>
2023-02-09 11:43:19 +1100

    Finish off documenting basic functionality

--- autumn/projects/austcovid/model_features.py
+++ autumn/projects/austcovid/model_features.py
@@ -1,7 +1,125 @@
 import pylatex as pl
+from datetime import datetime
 
 from summer2 import CompartmentalModel
 from summer2.parameters import Parameter, DerivedOutput
+
+REF_DATE = datetime(2019, 12, 31)
+
+
+def build_base_model(
+    start_date: datetime,
+    end_date: datetime,
+    doc: pl.document.Document,
+):
+    """
+    Generate the base compartmental model.
+
+    Args:
+        model: The model object
+        doc: The description document
+    """
+
+    model = CompartmentalModel(
+        times=(
+            (start_date - REF_DATE).days, 
+            (end_date - REF_DATE).days,
+        ),
+        compartments=(
+            "susceptible",
+            "infectious",
+            "recovered",
+        ),
+        infectious_compartments=("infectious",),
+        ref_date=REF_DATE,
+    )
+    
+    description = "The base model consists of just three states, " \
+        "representing fully susceptible, infected (and infectious) and recovered persons. "
+
+    if type(doc) == pl.document.Document:
+        doc.append(description)
+
+    return model
+
+
+def set_model_starting_conditions(
+    model: CompartmentalModel,
+    doc: pl.document.Document,
+):
+    """
+    Add the starting populations to the model as described below.
+
+    Args:
+        model: The model object
+        doc: The description document
+    """
+
+    model.set_initial_population(
+        {
+            "susceptible": 2.6e7,
+            "infectious": 1.0,
+        }
+    )
+    
+    description = "The simulation starts with 26 million susceptible persons " \
+        "and one infectious person to seed the epidemic. "
+
+    if type(doc) == pl.document.Document:
+        doc.append(description)
+
+
+def add_infection_to_model(
+    model: CompartmentalModel,
+    doc: pl.document.Document,
+):
+    """
+    Add infection as described below.
+
+    Args:
+        model: The model object
+        doc: The description document
+    """
+
+    model.add_infection_frequency_flow(
+        "infection",
+        Parameter("contact_rate"),
+        "susceptible",
+        "infectious",
+    )
+    
+    description = "Infection moves people from the fully susceptible " \
+        "compartment to the infectious compartment, " \
+        "under the frequency-dependent transmission assumption. "
+
+    if type(doc) == pl.document.Document:
+        doc.append(description)
+
+
+def add_recovery_to_model(
+    model: CompartmentalModel,
+    doc: pl.document.Document,
+):  
+    """
+    Add recovery as described below.
+
+    Args:
+        model: The model object
+        doc: The description document
+    """
+
+    model.add_transition_flow(
+        "recovery",
+        1.0 / Parameter("infectious_period"),
+        "infectious",
+        "recovered",
+    )
+
+    description = "The process recovery process moves " \
+        "people directly from the infectious state to a recovered compartment. "
+
+    if type(doc) == pl.document.Document:
+        doc.append(description)
 
 
 def add_notifications_output_to_model(
@@ -28,7 +146,7 @@ def add_notifications_output_to_model(
 
     description = "Notifications are calculated as " \
         "the absolute rate of infection in the community " \
-        "multiplied by the case detection rate."
+        "multiplied by the case detection rate. "
 
     if type(doc) == pl.document.Document:
         doc.append(description)

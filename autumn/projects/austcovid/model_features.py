cf142afc38cfdf9f004676eb8f1c1397c38deeac
James Trauer <james.trauer@monash.edu>
2023-02-17 13:46:20 +1100

    Add one more function back in

--- autumn/projects/austcovid/model_features.py
+++ autumn/projects/austcovid/model_features.py
@@ -79,7 +79,7 @@ class DocumentedModel:
                 "under the frequency-dependent transmission assumption. "
             self.doc_sections["General model construction"].append(description)
 
-    def add_recovery_to_model(self):  
+    def add_recovery_to_model(self):
         """
         Add recovery as described below.
         """
@@ -116,6 +116,63 @@ class DocumentedModel:
                 "the absolute rate of infection in the community " \
                 "multiplied by the case detection rate. "
             self.doc_sections["General model construction"].append(description)
+
+
+    def build_polymod_britain_matrix(
+        self,
+        strata,
+        doc: pl.document.Document,
+    ) -> np.array:
+        """
+        Get the raw data for Great Britain as described below.
+
+        Args:
+            strata: The age groups being applied in the model
+            doc: The description document
+        Returns:
+            15 by 15 matrix with daily contact rates for age groups
+        """
+
+        values = [
+            [1.92, 0.65, 0.41, 0.24, 0.46, 0.73, 0.67, 0.83, 0.24, 0.22, 0.36, 0.20, 0.20, 0.26, 0.13],
+            [0.95, 6.64, 1.09, 0.73, 0.61, 0.75, 0.95, 1.39, 0.90, 0.16, 0.30, 0.22, 0.50, 0.48, 0.20],
+            [0.48, 1.31, 6.85, 1.52, 0.27, 0.31, 0.48, 0.76, 1.00, 0.69, 0.32, 0.44, 0.27, 0.41, 0.33],
+            [0.33, 0.34, 1.03, 6.71, 1.58, 0.73, 0.42, 0.56, 0.85, 1.16, 0.70, 0.30, 0.20, 0.48, 0.63],
+            [0.45, 0.30, 0.22, 0.93, 2.59, 1.49, 0.75, 0.63, 0.77, 0.87, 0.88, 0.61, 0.53, 0.37, 0.33],
+            [0.79, 0.66, 0.44, 0.74, 1.29, 1.83, 0.97, 0.71, 0.74, 0.85, 0.88, 0.87, 0.67, 0.74, 0.33],
+            [0.97, 1.07, 0.62, 0.50, 0.88, 1.19, 1.67, 0.89, 1.02, 0.91, 0.92, 0.61, 0.76, 0.63, 0.27],
+            [1.02, 0.98, 1.26, 1.09, 0.76, 0.95, 1.53, 1.50, 1.32, 1.09, 0.83, 0.69, 1.02, 0.96, 0.20],
+            [0.55, 1.00, 1.14, 0.94, 0.73, 0.88, 0.82, 1.23, 1.35, 1.27, 0.89, 0.67, 0.94, 0.81, 0.80],
+            [0.29, 0.54, 0.57, 0.77, 0.97, 0.93, 0.57, 0.80, 1.32, 1.87, 0.61, 0.80, 0.61, 0.59, 0.57],
+            [0.33, 0.38, 0.40, 0.41, 0.44, 0.85, 0.60, 0.61, 0.71, 0.95, 0.74, 1.06, 0.59, 0.56, 0.57],
+            [0.31, 0.21, 0.25, 0.33, 0.39, 0.53, 0.68, 0.53, 0.55, 0.51, 0.82, 1.17, 0.85, 0.85, 0.33],
+            [0.26, 0.25, 0.19, 0.24, 0.19, 0.34, 0.40, 0.39, 0.47, 0.55, 0.41, 0.78, 0.65, 0.85, 0.57],
+            [0.09, 0.11, 0.12, 0.20, 0.19, 0.22, 0.13, 0.30, 0.23, 0.13, 0.21, 0.28, 0.36, 0.70, 0.60],
+            [0.14, 0.15, 0.21, 0.10, 0.24, 0.17, 0.15, 0.41, 0.50, 0.71, 0.53, 0.76, 0.47, 0.74, 1.47],
+        ]
+
+        matrix = np.array(values).T  # Transpose
+
+        if self.add_documentation:
+           description = "We took unadjusted estimates for interpersonal rates of contact by age " \
+            "from the United Kingdom data provided by Mossong et al.'s POLYMOD study \cite{mossong2008}. " \
+            "The data were obtained from https://doi.org/10.1371/journal.pmed.0050074.st005 " \
+            "on 12th February 2023 (downloaded in their native docx format). " \
+            "The matrix is transposed because summer assumes that rows represent infectees " \
+            "and columns represent infectors, whereas the POLYMOD data are labelled " \
+            "`age of contact' for the rows and `age group of participant' for the columns."
+           self.doc_sections["Age stratification"] = [NoEscape(description)]
+
+        # if isinstance(doc, pl.document.Document):
+        #     with doc.create(Section("Age stratification")):
+                # matrix_plotly_fig = px.imshow(matrix, x=strata, y=strata)
+                # matrix_plotly_fig.write_image("supplement/raw_matrix.jpg")
+                # with doc.create(pl.Figure()) as plot:
+                #     plot.add_image("raw_matrix.jpg", width="350px")
+                #     plot.add_caption("Raw matrices from Great Britain POLYMOD. Values are contacts per person per day.")
+
+        return matrix
+
 
     def compile_doc(self):
         
@@ -158,60 +215,6 @@ def add_age_stratification_to_model(
 
     if isinstance(doc, pl.document.Document):
         doc.append(description)
-
-
-def build_polymod_britain_matrix(
-    strata,
-    doc: pl.document.Document,
-) -> np.array:
-    """
-    Get the raw data for Great Britain as described below.
-
-    Args:
-        strata: The age groups being applied in the model
-        doc: The description document
-    Returns:
-        15 by 15 matrix with daily contact rates for age groups
-    """
-
-    values = [
-        [1.92, 0.65, 0.41, 0.24, 0.46, 0.73, 0.67, 0.83, 0.24, 0.22, 0.36, 0.20, 0.20, 0.26, 0.13],
-        [0.95, 6.64, 1.09, 0.73, 0.61, 0.75, 0.95, 1.39, 0.90, 0.16, 0.30, 0.22, 0.50, 0.48, 0.20],
-        [0.48, 1.31, 6.85, 1.52, 0.27, 0.31, 0.48, 0.76, 1.00, 0.69, 0.32, 0.44, 0.27, 0.41, 0.33],
-        [0.33, 0.34, 1.03, 6.71, 1.58, 0.73, 0.42, 0.56, 0.85, 1.16, 0.70, 0.30, 0.20, 0.48, 0.63],
-        [0.45, 0.30, 0.22, 0.93, 2.59, 1.49, 0.75, 0.63, 0.77, 0.87, 0.88, 0.61, 0.53, 0.37, 0.33],
-        [0.79, 0.66, 0.44, 0.74, 1.29, 1.83, 0.97, 0.71, 0.74, 0.85, 0.88, 0.87, 0.67, 0.74, 0.33],
-        [0.97, 1.07, 0.62, 0.50, 0.88, 1.19, 1.67, 0.89, 1.02, 0.91, 0.92, 0.61, 0.76, 0.63, 0.27],
-        [1.02, 0.98, 1.26, 1.09, 0.76, 0.95, 1.53, 1.50, 1.32, 1.09, 0.83, 0.69, 1.02, 0.96, 0.20],
-        [0.55, 1.00, 1.14, 0.94, 0.73, 0.88, 0.82, 1.23, 1.35, 1.27, 0.89, 0.67, 0.94, 0.81, 0.80],
-        [0.29, 0.54, 0.57, 0.77, 0.97, 0.93, 0.57, 0.80, 1.32, 1.87, 0.61, 0.80, 0.61, 0.59, 0.57],
-        [0.33, 0.38, 0.40, 0.41, 0.44, 0.85, 0.60, 0.61, 0.71, 0.95, 0.74, 1.06, 0.59, 0.56, 0.57],
-        [0.31, 0.21, 0.25, 0.33, 0.39, 0.53, 0.68, 0.53, 0.55, 0.51, 0.82, 1.17, 0.85, 0.85, 0.33],
-        [0.26, 0.25, 0.19, 0.24, 0.19, 0.34, 0.40, 0.39, 0.47, 0.55, 0.41, 0.78, 0.65, 0.85, 0.57],
-        [0.09, 0.11, 0.12, 0.20, 0.19, 0.22, 0.13, 0.30, 0.23, 0.13, 0.21, 0.28, 0.36, 0.70, 0.60],
-        [0.14, 0.15, 0.21, 0.10, 0.24, 0.17, 0.15, 0.41, 0.50, 0.71, 0.53, 0.76, 0.47, 0.74, 1.47],
-    ]
-
-    matrix = np.array(values).T  # Transpose
-
-    description = "We took unadjusted estimates for interpersonal rates of contact by age " \
-        "from the United Kingdom data provided by Mossong et al.'s POLYMOD study \cite{mossong2008}. " \
-        "The data were obtained from https://doi.org/10.1371/journal.pmed.0050074.st005 " \
-        "on 12th February 2023 (downloaded in their native docx format). " \
-        "The matrix is transposed because summer assumes that rows represent infectees " \
-        "and columns represent infectors, whereas the POLYMOD data are labelled " \
-        "`age of contact' for the rows and `age group of participant' for the columns."
-
-    if isinstance(doc, pl.document.Document):
-        with doc.create(Section("Age stratification")):
-            doc.append(NoEscape(description))
-            matrix_plotly_fig = px.imshow(matrix, x=strata, y=strata)
-            matrix_plotly_fig.write_image("supplement/raw_matrix.jpg")
-            with doc.create(pl.Figure()) as plot:
-                plot.add_image("raw_matrix.jpg", width="350px")
-                plot.add_caption("Raw matrices from Great Britain POLYMOD. Values are contacts per person per day.")
-
-    return matrix
 
 
 def adapt_gb_matrix_to_aust(

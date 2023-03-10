744b48e6c1b3605774634cbfc50b503ae688b7fa
longbui <longbui189@gmail.com>
2023-03-10 16:01:21 +1100

    Add calibration target

--- autumn/projects/tb_dynamics2/vietnam/camau/project.py
+++ autumn/projects/tb_dynamics2/vietnam/camau/project.py
@@ -23,17 +23,9 @@ targets = [
     NormalTarget(ts_set["total_population"], stdev=2500.0),
     NormalTarget(ts_set["notifications"], stdev=20.0),
     NormalTarget(ts_set["percentage_latent"], stdev=2.0),
+    NormalTarget(ts_set["prevalence_infectious"], stdev=20)
     #NormalTarget(ts_set["incidence"], stdev=5.0)
 ]
-
-CID_ESTIMATES = {
-    "infect_death_rate": {
-        "smear_positive": [0.335, 0.449],
-        "smear_negative": [0.017, 0.035],
-    },
-    "self_recovery_rate": {"smear_positive": [0.177, 0.288], "smear_negative": [0.073, 0.209]},
-}
-
 
 priors = [
     UniformPrior("start_population_size", [250000, 400000]),

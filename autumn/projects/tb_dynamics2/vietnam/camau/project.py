1de0302e79defbe7ea1e73bcb16eaee870c6171d
longbui <longbui189@gmail.com>
2023-03-11 18:07:10 +1100

    Add gender calib target

--- autumn/projects/tb_dynamics2/vietnam/camau/project.py
+++ autumn/projects/tb_dynamics2/vietnam/camau/project.py
@@ -23,23 +23,21 @@ targets = [
     NormalTarget(ts_set["total_population"], stdev=2500.0),
     NormalTarget(ts_set["notificationsXgender_male"], stdev=20.0),
     NormalTarget(ts_set["notificationsXgender_female"], stdev=20.0),
-    #NormalTarget(ts_set["percentage_latent"], stdev=2.0),
     NormalTarget(ts_set["prevalence_infectious"], stdev=20)
-    #NormalTarget(ts_set["incidence"], stdev=5.0)
 ]
 
 priors = [
-    UniformPrior("start_population_size", [250000, 400000]),
+    UniformPrior("start_population_size", [200000, 400000]),
     UniformPrior("contact_rate", [0.0001, 0.02]),
     UniformPrior("rr_infection_latent", [0.2, 0.5]),
     UniformPrior("rr_infection_recovered", [0.1, 0.5]),
     UniformPrior("progression_multiplier", [1.0, 2.0]),
-    UniformPrior("cdr_adjustment", [0.6, 1.4]),
+    UniformPrior("cdr_adjustment", [0.6, 1.0]),
     UniformPrior("infect_death_rate_dict.smear_positive", [0.335, 0.449]),
     UniformPrior("infect_death_rate_dict.smear_negative", [0.017, 0.035]),
     UniformPrior("self_recovery_rate_dict.smear_positive", [0.177, 0.288]),
     UniformPrior("self_recovery_rate_dict.smear_negative", [0.073, 0.209]),
-    UniformPrior("gender.adjustments.infection.male", [1.0, 2.0])
+    UniformPrior("gender.adjustments.infection.male", [1.0, 4.0])
 ]
 calibration = Calibration(
     priors=priors,

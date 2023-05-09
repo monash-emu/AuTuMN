6352e72aab94c2b3ffc5f9aa15b5886ac0e1a97d
Romain Ragonnet <romain.ragonnet@monash.edu>
2023-05-09 11:32:50 +1000

    Include more priors for school project

--- autumn/projects/sm_covid2/common_school/project_maker.py
+++ autumn/projects/sm_covid2/common_school/project_maker.py
@@ -355,11 +355,8 @@ def get_school_project_priors(first_date_with_death):
         UniformPrior("voc_emergence.omicron.new_voc_seed.time_from_gisaid_report", [-30, 30]),
 
         # Account for mixing matrix uncertainty
-        # UniformPrior("mobility.unesco_partial_opening_value", [0.1, 0.3]),
-        # UniformPrior("school_multiplier", [0.8, 1.2]),
-
-        # dispersion params for targets
-        UniformPrior("infection_deaths_dispersion_param", [50., 200.]),
+        UniformPrior("mobility.unesco_partial_opening_value", [0.1, 0.3]),
+        UniformPrior("school_multiplier", [0.8, 1.2]),
     ]
 
     return priors

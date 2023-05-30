49688e4a1e998d400c62b70c28ab58aae80fe25d
Romain Ragonnet <romain.ragonnet@monash.edu>
2023-05-30 12:05:26 +1000

    Update iso2 list for school countries

--- autumn/projects/sm_covid2/common_school/utils.py
+++ autumn/projects/sm_covid2/common_school/utils.py
@@ -12,10 +12,14 @@ def get_school_country_list(nationbal_only=False):
 
 
 SCHOOL_ISO2_LIST = [
-    'AL', 'AE', 'AR', 'AU', 'AT', 'BE', 'BJ', 'BF', 'BD', 'BA', 'BR', 'CA', 'CH', 'CL', 'CN', 
-    'CM', 'CD', 'CG', 'CO', 'CZ', 'DE', 'DK', 'DO', 'EC', 'EG', 'ES', 'EE', 'ET', 'FI', 'FR',
-    'GA', 'GB', 'GH', 'GR', 'HN', 'HR', 'HU', 'ID', 'IN', 'IE', 'IR', 'IQ', 'IS', 'IL', 'IT', 
-    'JO', 'JP', 'KZ', 'KE', 'KR', 'LB', 'LT', 'LU', 'MV', 'MX', 'MK', 'ML', 'MN', 'MZ', 'MW', 
-    'MY', 'NG', 'NL', 'NO', 'NP', 'OM', 'PK', 'PE', 'PH', 'PL', 'PT', 'PY', 'PS', 'QA', 'RO', 
-    'RU', 'SA', 'SN', 'SG', 'SL', 'SS', 'SI', 'SE', 'US', 'UZ', 'ZA', 'ZM', 'ZW'
+    'AW', 'AF', 'AO', 'AL', 'AE', 'AR', 'AM', 'AG', 'AU', 'AT', 'AZ', 'BI', 'BE', 'BJ', 'BF', 'BD', 'BG', 'BH', 'BS', 
+    'BA', 'BY', 'BZ', 'BO', 'BR', 'BB', 'BN', 'BW', 'CF', 'CA', 'CH', 'CL', 'CN', 'CI', 'CM', 'CD', 'CG', 'CO', 'KM', 
+    'CV', 'CR', 'CU', 'CW', 'CY', 'CZ', 'DE', 'DJ', 'DK', 'DO', 'DZ', 'EC', 'EG', 'ES', 'EE', 'ET', 'FI', 'FJ', 'FR', 
+    'GA', 'GB', 'GE', 'GH', 'GN', 'GM', 'GQ', 'GR', 'GD', 'GT', 'GY', 'HN', 'HR', 'HT', 'HU', 'ID', 'IN', 'IE', 'IR', 
+    'IQ', 'IS', 'IL', 'IT', 'JM', 'JO', 'JP', 'KZ', 'KE', 'KG', 'KH', 'KI', 'KR', 'KW', 'LA', 'LB', 'LR', 'LY', 'LC', 
+    'LK', 'LS', 'LT', 'LU', 'LV', 'MA', 'MD', 'MV', 'MX', 'MK', 'ML', 'MT', 'MM', 'ME', 'MN', 'MZ', 'MR', 'MU', 'MW', 
+    'MY', 'NA', 'NE', 'NG', 'NI', 'NL', 'NO', 'NP', 'NZ', 'OM', 'PK', 'PA', 'PE', 'PH', 'PG', 'PL', 'PT', 'PY', 'PS', 
+    'QA', 'RO', 'RU', 'RW', 'SA', 'SD', 'SN', 'SG', 'SB', 'SL', 'SV', 'RS', 'SS', 'ST', 'SR', 'SK', 'SI', 'SE', 'SZ', 
+    'SC', 'SY', 'TD', 'TG', 'TH', 'TL', 'TT', 'TN', 'TR', 'TZ', 'UG', 'UA', 'UY', 'US', 'UZ', 'VC', 'VE', 'VN', 'ZA', 
+    'ZM', 'ZW'
 ]

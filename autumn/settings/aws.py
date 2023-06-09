3407fa911eb1fc498e0490378dcb48c85ae531b1
David Shipman <dshipman@gmail.com>
2023-06-09 13:22:43 +1000

    Update AMI name in settings

--- autumn/settings/aws.py
+++ autumn/settings/aws.py
@@ -80,7 +80,7 @@ EC2_SPOT_MAX_PRICE = "1.2"
 EC2_AMI = {
     "36venv": "ami-0d27c531f813ff1cf",
     "310conda": "ami-0dbd9adffb9f07a62",
-    "springboard310": "ami-08481612b393e7935",
+    "springboard310": "ami-02ee8f059b67898ee",
 }
 EC2_SECURITY_GROUP = "sg-0b2fe230ac8853538"
 EC2_IAM_INSTANCE_PROFILE = "worker-profile"

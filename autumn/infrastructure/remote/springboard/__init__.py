250441307f1ce42cd4ab34539e4d25b4a1f3aaa0
David Shipman <dshipman@gmail.com>
2023-06-20 11:20:00 +1000

    Expose useful types in springboard namespace

--- autumn/infrastructure/remote/springboard/__init__.py
+++ autumn/infrastructure/remote/springboard/__init__.py
@@ -1,1 +1,7 @@
+# Import all submodules
 from . import aws, clients, scripting, task, launch
+
+# Any types needed to create tasks via the launch interface
+# should be in the root module namespace
+from .task import TaskSpec
+from .aws import EC2MachineSpec

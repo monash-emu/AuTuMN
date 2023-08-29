07e6f2a6521b4ba05f9262e5c5c6db1e602c792e
Romain Ragonnet <romain.ragonnet@monash.edu>
2023-08-29 11:40:54 +1000

    r_hat plots

--- autumn/projects/sm_covid2/common_school/calibration_plots/mc_plots.py
+++ autumn/projects/sm_covid2/common_school/calibration_plots/mc_plots.py
@@ -25,10 +25,22 @@ def make_post_mc_plots(idata, burn_in, output_folder=None):
         plt.savefig(output_folder_path / "mc_posteriors.png", facecolor="white", bbox_inches='tight')
         plt.close()
 
-    # ESS (exclding burn-in)
-    raw_ess_df = az.ess(idata).to_dataframe()
+    # ESS (excluding burn-in)
+    raw_ess_df = az.ess(burnt_idata).to_dataframe()
     ess_df = raw_ess_df.drop(columns="random_process.delta_values").loc[0]
     for i in range(len(raw_ess_df)):
         ess_df[f"random_process.delta_values[{i}]"] = raw_ess_df['random_process.delta_values'][i]
     if output_folder:
         ess_df.to_csv(output_folder_path / "mc_ess.csv", header=["ESS"])
+
+    # R_hat plot (excluding burn-in)
+    raw_rhat_df = az.rhat(burnt_idata).to_dataframe()
+    rhat_df = raw_rhat_df.drop(columns="random_process.delta_values").loc[0]
+    for i in range(len(raw_rhat_df)):
+        rhat_df[f"random_process.delta_values[{i}]"] = raw_rhat_df['random_process.delta_values'][i]
+    ax = rhat_df.plot.barh(xlim=(1.,1.105))
+    ax.vlines(x=1.05,ymin=-0.5, ymax=len(idata.posterior), linestyles="--", color='orange')
+    ax.vlines(x=1.1,ymin=-0.5, ymax=len(idata.posterior), linestyles="-",color='red')    
+    if output_folder:
+        plt.savefig(output_folder_path / "r_hats.jpg", facecolor="white", bbox_inches='tight')
+        plt.close()

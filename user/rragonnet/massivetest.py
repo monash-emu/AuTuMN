ccfff932dc573a11751c67bcb5d81ef1384fa188
Romain Ragonnet <romain.ragonnet@monash.edu>
2023-09-20 13:37:06 +1000

    Full run test config for massive

--- user/rragonnet/massivetest.py
+++ user/rragonnet/massivetest.py
@@ -1,11 +1,18 @@
-from autumn.projects.sm_covid2.common_school.calibration import get_bcm_object
+from autumn.projects.sm_covid2.common_school.runner_tools import run_full_analysis, DEFAULT_RUN_CONFIG, TEST_RUN_CONFIG
 import multiprocessing as mp
+from pathlib import Path
+
 
 if __name__ == "__main__":
     print("Start job")
     mp.set_start_method("forkserver")
-    bcm = get_bcm_object("FRA", 'main')
-    for p in bcm.priors:
-        print(p)
+
+
+    run_name = "short_full_test"
+    this_dir_path = Path(__file__).resolve().parent
+    out_path = this_dir_path / "outputs" / run_name
+    out_path.mkdir(exist_ok=True)
+
+    _, _, _ = run_full_analysis("FRA", "main", TEST_RUN_CONFIG, out_path)
     
     print("success!!!")

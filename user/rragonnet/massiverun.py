00b47051f78b9fcaec5b6013b764de34a60126c9
Romain Ragonnet <romain.ragonnet@monash.edu>
2023-09-21 13:48:56 +1000

    Use default config but no mcmc

--- user/rragonnet/massiverun.py
+++ user/rragonnet/massiverun.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from time import time
 
 ANALYSIS = 'main'
-RUN_CONFIG = TEST_RUN_CONFIG | {'metropolis_draws': 0}
+RUN_CONFIG = DEFAULT_RUN_CONFIG | {'metropolis_draws': 0}
 
 if __name__ == "__main__":
     start_time = time()

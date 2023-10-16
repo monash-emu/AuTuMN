7d9ace27621a8d9ffafa8a712f716d3222273b26
Romain Ragonnet <romain.ragonnet@monash.edu>
2023-10-16 14:15:25 +1100

    Plot analyses comparison

--- autumn/projects/sm_covid2/common_school/output_plots/multicountry.py
+++ autumn/projects/sm_covid2/common_school/output_plots/multicountry.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 from matplotlib.patches import Rectangle
 import pandas as pd
+from math import ceil
 
 import plotly.graph_objects as go
 
@@ -16,6 +17,12 @@ YLAB_LOOKUP_SPLIT = {
    "delta_hospital_peak_relative": "Relative reduction in<br>peak hospital occupancy (%)"
 }
 
+BOX_COLORS= {
+   "cases_averted_relative": "black",
+   "deaths_averted_relative": "purple",
+   "delta_hospital_peak_relative": "firebrick"
+}
+
 def plot_multic_relative_outputs(output_dfs_dict: dict[str, pd.DataFrame], req_outputs=["cases_averted_relative", "deaths_averted_relative", "delta_hospital_peak_relative"]):
     n_subplots = len(req_outputs)
     fig, axes = plt.subplots(n_subplots, 1, figsize=(25, n_subplots*6))
@@ -25,10 +32,9 @@ def plot_multic_relative_outputs(output_dfs_dict: dict[str, pd.DataFrame], req_o
 
     box_width = .4
     med_color = 'white'
-    box_colors= ['black', 'purple', 'firebrick']
     
     for i_output, output in enumerate(req_outputs):
-        box_color = box_colors[i_output]
+        box_color = BOX_COLORS[output]
         axis = axes[i_output]
 
         mean_values = [output_dfs_dict[iso3][output].loc[0.5] for iso3 in this_iso3_list]
@@ -79,6 +85,79 @@ def plot_multic_relative_outputs(output_dfs_dict: dict[str, pd.DataFrame], req_o
     plt.tight_layout()
 
     return fig
+
+
+def plot_analyses_comparison(output_dfs_dict: dict[str, dict], output="cases_averted_relative"):
+
+   n_countries = len(output_dfs_dict)
+   n_subplots = 3
+   n_countries_per_subplot = ceil(n_countries / n_subplots)
+   this_iso3_list = list(output_dfs_dict.keys())
+
+   fig, axes = plt.subplots(n_subplots, 1, figsize=(25, n_subplots*6))
+
+   box_width = .4
+   med_color = 'white'
+
+   for i_subplot in range(n_subplots):
+      iso3_sublist = this_iso3_list[i_subplot * n_countries_per_subplot: (i_subplot + 1) * n_countries_per_subplot]
+
+      box_color = BOX_COLORS[output]
+      axis = axes[i_subplot]
+      
+      y_max_abs = 0.
+      x_ticks = []
+      for i_iso3, iso3 in enumerate(iso3_sublist):
+         x_ticks.append(1. + 3 * i_iso3 + 1)
+         for i_analysis, analysis in enumerate(['main', 'increased_hh_contacts', 'no_google_mobility']):
+            x = 1. + 3 * i_iso3 + i_analysis
+
+            data = - 100. * output_dfs_dict[iso3][analysis][output] # use %. And use "-" so positive nbs indicate positive effect of closures
+
+            # median
+            axis.hlines(y=data.loc[0.5], xmin=x - box_width / 2. , xmax= x + box_width / 2., lw=2., color=med_color, zorder=3)    
+            
+            # IQR
+            q_75 = data.loc[0.75]
+            q_25 = data.loc[0.25]
+            rect = Rectangle(xy=(x - box_width / 2., q_25), width=box_width, height=q_75 - q_25, zorder=2, facecolor=box_color)
+            axis.add_patch(rect)
+
+            # 95% CI
+            q_025 = data.loc[0.025]
+            q_975 = data.loc[0.975]
+            axis.vlines(x=x, ymin=q_025 , ymax=q_975, lw=1.5, color=box_color, zorder=1)
+
+            y_max_abs = max(abs(q_975), y_max_abs)
+            y_max_abs = max(abs(q_025), y_max_abs)
+         
+         if i_iso3 < len(iso3_sublist) - 1: 
+            axis.vlines(x=x+.5,ymin=-1.e6,ymax=1.e6, lw=1., color="black", zorder=5)
+
+      axis.set_xlim((0, 3 * n_countries_per_subplot + 1))
+      axis.set_ylim(-1.2*y_max_abs, 1.2*y_max_abs)
+
+      axis.set_xticks(ticks=x_ticks, labels=iso3_sublist, rotation=90, fontsize=13)
+
+      y_label = YLAB_LOOKUP[output]
+      axis.set_ylabel(y_label, fontsize=15)
+
+      # add coloured backgorund patches
+      xmin, xmax = axis.get_xlim()
+      ymin, ymax = axis.get_ylim() 
+      rect_up = Rectangle(xy=(xmin, 0.), width=xmax - xmin, height=(ymax - ymin)/2., zorder=-1, facecolor="white")  #"honeydew")
+      axis.add_patch(rect_up)
+      rect_low = Rectangle(xy=(xmin, ymin), width=xmax - xmin, height=(ymax - ymin)/2., zorder=-1, facecolor="gainsboro", alpha=.5)  #"mistyrose")
+      axis.add_patch(rect_low)
+
+      # axis.text(n_countries * .75, ymax / 2., s="Positive effect of\nschool closures", fontsize=13)
+      # axis.text(n_countries * .25, ymin / 2., s="Negative effect of\nschool closures", fontsize=13)   
+
+   plt.tight_layout()
+
+   return fig
+
+
 
 
 def plot_relative_map(output_dfs_dict: dict[str, pd.DataFrame], req_output="delta_hospital_peak_relative"):

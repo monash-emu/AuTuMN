179073e1f8bb500c0ffbb8c52ba55c1c2e29e279
David Shipman <dshipman@gmail.com>
2024-01-16 09:57:32 +1100

    Remove old autumn tests

--- tests/test_nightly/test_stub.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-@pytest.mark.nightly
-@pytest.mark.github_only
-def test_run_stub_test():
-    """
-    Empty test just to make sure our nightly job has something to do
-    """
-    pass

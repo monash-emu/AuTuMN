c3210e79264c4ad38b71c4519d4e51587ac33b67
Romain Ragonnet <romain.ragonnet@monash.edu>
2024-08-23 12:41:48 +1000

    Prepare mistry runs

--- user/rragonnet/massiverun.py
+++ user/rragonnet/massiverun.py
@@ -9,21 +9,21 @@ import sys
 from pathlib import Path
 from time import time
 
-ANALYSIS = 'increased_hh_contacts'  # ["main", "no_google_mobility", "increased_hh_contacts"]
-RUN_CONFIG = DEFAULT_RUN_CONFIG
+ANALYSIS = 'main'  # ["main", "no_google_mobility", "increased_hh_contacts"]
+RUN_CONFIG = TEST_RUN_CONFIG # DEFAULT_RUN_CONFIG
 
 if __name__ == "__main__":
     start_time = time()
 
     # Retrieve country iso3 to run
     array_task_id = int(sys.argv[2])  # specific to this particular run/country
-    iso3 = list(INCLUDED_COUNTRIES['all'].keys())[array_task_id - 1]
+    iso3 = list(INCLUDED_COUNTRIES['mistry'].keys())[array_task_id - 1]
     print(f"Start job #{array_task_id}, iso3={iso3}, analysis={ANALYSIS}", flush=True)
 
     mp.set_start_method("spawn")  # previously "forkserver"
 
     # create parent output directory for multi-country analysis if required
-    analysis_name = "test_full_analysis_24Jan2024"
+    analysis_name = "mistry_23Aug2024"
     output_root_dir = Path.home() / "sh30/users/rragonnet/outputs/"
     array_job_id = sys.argv[1]  # common to all the tasks from this array job
     analysis_output_dir = output_root_dir / f"{array_job_id}_{analysis_name}_{ANALYSIS}"
